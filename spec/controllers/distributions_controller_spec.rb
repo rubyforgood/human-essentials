d956a46419f63116e7501d464f008659a5471885
Adam Bachman <adam.bachman@gmail.com>
2017-05-19 16:01:14 -0400

    distributions controller spec is passing

--- spec/controllers/distributions_controller_spec.rb
+++ spec/controllers/distributions_controller_spec.rb
@@ -1,22 +1,24 @@
 RSpec.describe DistributionsController, type: :controller do
-  let!(:current_organization) { create(:organization) }
+  before do
+    sign_in(@user)
+  end
 
   describe "GET #print" do
-    subject { get :print, params: { organization_id: current_organization, id: create(:distribution).id } }
+    subject { get :print, params: { organization_id: @organization, id: create(:distribution).id } }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "GET #reclaim" do
-    subject { get :reclaim, params: { organization_id: current_organization, id: create(:distribution).id } }
+    subject { get :reclaim, params: { organization_id: @organization, id: create(:distribution).id } }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "GET #index" do
-    subject { get :index, params: { organization_id: current_organization } }
+    subject { get :index, params: { organization_id: @organization } }
     it "returns http success" do
       expect(subject).to be_successful
     end
@@ -30,16 +32,16 @@ RSpec.describe DistributionsController, type: :controller do
       expect(i).to be_valid
       expect(p).to be_valid
 
-      post :create, params: { organization_id: current_organization.short_name,
+      post :create, params: { organization_id: @organization.short_name,
                               distribution: { storage_location_id: i.id, partner_id: p.id } }
       expect(response).to have_http_status(:redirect)
 
       d = Distribution.last
-      expect(response).to redirect_to(distribution_path(d.id, organization_id: current_organization.short_name))
+      expect(response).to redirect_to(distribution_path(d.id, organization_id: @organization.short_name))
     end
 
     it "renders #new again on failure, with notice" do
-      post :create, params: { organization_id: current_organization.short_name,
+      post :create, params: { organization_id: @organization.short_name,
                               distribution: { comment: nil, partner_id: nil, storage_location_id: nil } }
       expect(response).to be_successful
       expect(flash[:notice]).to match(/error/i)
@@ -47,14 +49,14 @@ RSpec.describe DistributionsController, type: :controller do
   end
 
   describe "GET #new" do
-    subject { get :new, params:{ organization_id: current_organization.short_name } }
+    subject { get :new, params:{ organization_id: @organization.short_name } }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "GET #show" do
-    subject { get :show, params: { organization_id: current_organization.short_name, id: create(:distribution).id } }
+    subject { get :show, params: { organization_id: @organization.short_name, id: create(:distribution).id } }
     it "returns http success" do
       expect(subject).to be_successful
     end
