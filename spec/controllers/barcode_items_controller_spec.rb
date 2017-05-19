582919633774a6bc58b79325c741dc4a0900483b
Adam Bachman <adam.bachman@gmail.com>
2017-05-19 17:02:25 -0400

    all controller specs passing

--- spec/controllers/barcode_items_controller_spec.rb
+++ spec/controllers/barcode_items_controller_spec.rb
@@ -1,38 +1,42 @@
 RSpec.describe BarcodeItemsController, type: :controller do
+  let(:default_params) {
+    { organization_id: @organization.to_param }
+  }
+
   before do
     sign_in(@user)
   end
 
   describe "GET #index" do
-    subject { get :index, params: { organization_id: @organization.short_name } }
+    subject { get :index, params: default_params }
     it "returns http success" do
       expect(subject).to have_http_status(:success)
     end
   end
 
   describe "GET #new" do
-    subject { get :new, params: { organization_id: @organization.short_name } }
+    subject { get :new, params: default_params }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "GET #edit" do
-    subject { get :edit, params: { id: create(:barcode_item), organization_id: @organization.short_name } }
+    subject { get :edit, params: default_params.merge({ id: create(:barcode_item) }) }
     it "returns http success" do
       expect(subject).to have_http_status(:success)
     end
   end
 
   describe "GET #show" do
-    subject { get :show, params: { id: create(:barcode_item), organization_id: @organization.short_name } }
+    subject { get :show, params: default_params.merge({ id: create(:barcode_item) }) }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "DELETE #destroy" do
-    subject { delete :destroy, params: { id: create(:barcode_item), organization_id: @organization.short_name } }
+    subject { delete :destroy, params: default_params.merge({ id: create(:barcode_item) }) }
     it "redirecst to the index" do
       expect(subject).to redirect_to(barcode_items_path)
     end
--- spec/controllers/distributions_controller_spec.rb
+++ spec/controllers/distributions_controller_spec.rb
@@ -1,24 +1,28 @@
 RSpec.describe DistributionsController, type: :controller do
+  let(:default_params) {
+    { organization_id: @organization.to_param }
+  }
+
   before do
     sign_in(@user)
   end
 
   describe "GET #print" do
-    subject { get :print, params: { organization_id: @organization, id: create(:distribution).id } }
+    subject { get :print, params: default_params.merge({ id: create(:distribution).id }) }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "GET #reclaim" do
-    subject { get :reclaim, params: { organization_id: @organization, id: create(:distribution).id } }
+    subject { get :reclaim, params: default_params.merge({ organization_id: @organization, id: create(:distribution).id }) }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "GET #index" do
-    subject { get :index, params: { organization_id: @organization } }
+    subject { get :index, params: default_params }
     it "returns http success" do
       expect(subject).to be_successful
     end
@@ -32,8 +36,7 @@ RSpec.describe DistributionsController, type: :controller do
       expect(i).to be_valid
       expect(p).to be_valid
 
-      post :create, params: { organization_id: @organization.short_name,
-                              distribution: { storage_location_id: i.id, partner_id: p.id } }
+      post :create, params: default_params.merge(distribution: { storage_location_id: i.id, partner_id: p.id })
       expect(response).to have_http_status(:redirect)
 
       d = Distribution.last
@@ -41,22 +44,21 @@ RSpec.describe DistributionsController, type: :controller do
     end
 
     it "renders #new again on failure, with notice" do
-      post :create, params: { organization_id: @organization.short_name,
-                              distribution: { comment: nil, partner_id: nil, storage_location_id: nil } }
+      post :create, params:default_params.merge(distribution: { comment: nil, partner_id: nil, storage_location_id: nil })
       expect(response).to be_successful
       expect(flash[:notice]).to match(/error/i)
     end
   end
 
   describe "GET #new" do
-    subject { get :new, params:{ organization_id: @organization.short_name } }
+    subject { get :new, params: default_params }
     it "returns http success" do
       expect(subject).to be_successful
     end
   end
 
   describe "GET #show" do
-    subject { get :show, params: { organization_id: @organization.short_name, id: create(:distribution).id } }
+    subject { get :show, params: default_params.merge(id: create(:distribution).id) }
     it "returns http success" do
       expect(subject).to be_successful
     end
