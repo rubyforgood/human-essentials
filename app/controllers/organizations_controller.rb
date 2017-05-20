fec9a334f95849263ca8c4dd38c0ad98f28af338
Adam Bachman <adam.bachman@gmail.com>
2017-05-20 11:22:27 -0400

    add basic user + organization managment

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,14 @@
+class OrganizationsController < ApplicationController
+  def edit
+    @organization = current_organization
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
