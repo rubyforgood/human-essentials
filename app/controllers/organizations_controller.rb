f93b0f918c3364032a4d9febeda184999b02d646
Adam Bachman <adam.bachman@gmail.com>
2017-05-20 12:06:51 -0400

    add organization edit form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,11 +4,25 @@ class OrganizationsController < ApplicationController
   end
 
   def update
+    @organization = current_organization
+
+    if current_organization.update_attributes(organization_params)
+      redirect_to edit_organization_path, notice: 'Updated organization!'
+    else
+      flash[:alert] = 'Failed to update organization'
+      render :edit
+    end
   end
 
   def new
   end
 
   def create
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).permit(:name, :short_name, :address, :email, :url)
   end
 end
