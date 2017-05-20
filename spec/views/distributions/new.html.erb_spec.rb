5a02ebfb4e2e2cac01f36a062fd7cdf6a5473b96
Daniel <quillendaniel@gmail.com>
2017-05-20 12:09:35 -0400

    Adds todo for testing line item adding

--- spec/views/distributions/new.html.erb_spec.rb
+++ spec/views/distributions/new.html.erb_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe "distributions/new.html.erb", type: :view do
     expect(rendered).to have_xpath("//form/div/select[@name='distribution[storage_location_id]']")
     expect(rendered).to have_xpath("//form/div/select[@name='distribution[partner_id]']")
   end
+
+  xit "shows fields for the user to add items to this distribution" do
+    # TODO: How should this work?
+  end
 end
