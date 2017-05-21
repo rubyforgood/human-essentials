bd0d645c2aaca3448e3d667c63eb37687ce94c3c
Adam Bachman <adam.bachman@gmail.com>
2017-05-20 20:40:27 -0400

    scope {barcode_,}item controllers to org

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,37 +1,37 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.alphabetized.filter(filter_params)
+    @items = current_organization.items.alphabetized.filter(filter_params)
     @categories = Item.categories
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = current_organization.items.create(item_params)
     redirect_to @item, notice: "#{@item.name} added!"
   end
 
   def new
-    @item = Item.new
+    @item = current_organization.items.new
   end
 
   def edit
-    @item = Item.find(params[:id])
+    @item = current_organization.items.find(params[:id])
   end
 
   def show
-    @item = Item.find(params[:id])
+    @item = current_organization.items.find(params[:id])
     @items_in_category = Item.in_same_category_as(@item)
     @storage_locations_containing = Item.storage_locations_containing(@item)
     @barcodes_for = Item.barcodes_for(@item)
   end
 
   def update
-    @item = Item.find(params[:id])
+    @item = current_organization.items.find(params[:id])
     @item.update_attributes(item_params)
     redirect_to @item, notice: "#{@item.name} updated!"
   end
 
   def destroy
-    Item.find(params[:id]).destroy
+    current_organization.items.find(params[:id]).destroy
     redirect_to items_path
   end
 
