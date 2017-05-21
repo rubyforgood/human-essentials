b6bccd95637d9bc41aa7860500246dfe57e9b716
Adam Bachman <adam.bachman@gmail.com>
2017-05-21 10:44:53 -0400

    include organization logo in Distributions printed pdf

--- app/helpers/distributions_helper.rb
+++ app/helpers/distributions_helper.rb
@@ -1,2 +1,11 @@
 module DistributionsHelper
+
+  def logo_file_path(organization)
+    if organization.logo.present?
+      organization.logo.path
+    else
+      Rails.root.join('app', 'assets', 'images', 'DiaperBase-Logo.png')
+    end
+  end
+
 end
