a4d737c5651f9d508227b76b184d0200d7c6bebc
Keith Walters <keith.walters@cattywamp.us>
2017-05-21 12:08:23 -0400

    Cap task to create database.yml file

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml .env}
+set :linked_files, %w{config/database.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
