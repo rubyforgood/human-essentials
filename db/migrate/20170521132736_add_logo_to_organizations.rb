d7169e1358e0543aedd634d78cbad97a3f2f9a66
Adam Bachman <adam.bachman@gmail.com>
2017-05-21 09:55:46 -0400

    add paperclip logo field to organization

---
+++ db/migrate/20170521132736_add_logo_to_organizations.rb
@@ -0,0 +1,9 @@
+class AddLogoToOrganizations < ActiveRecord::Migration[5.0]
+  def up
+    add_attachment :organizations, :logo
+  end
+
+  def down
+    remove_attachment :organizations, :logo
+  end
+end
