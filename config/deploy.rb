a39705c1ddfd84c3a7855908a6fb903baf50df8c
Sean Marcia <smarcia@gmail.com>
2017-07-16 23:13:54 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,6 +59,13 @@ namespace :deploy do
     end
   end
 
+  desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
