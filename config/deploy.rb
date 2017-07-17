68434c6928c64128a69c2411c7c38dfc8938ae3f
Sean Marcia <smarcia@gmail.com>
2017-07-16 23:19:49 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,18 +66,9 @@ namespace :deploy do
     end
   end
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing,    :restart  
 end
 
 # ps aux | grep puma    # Get puma pid
