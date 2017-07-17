d37522bf4113c5131059f228f41ee04ee73ba22a
Sean Marcia <smarcia@gmail.com>
2017-07-16 23:01:52 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,6 +70,7 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
+  after  :finishing,    :restart  
 end
 
 # ps aux | grep puma    # Get puma pid
