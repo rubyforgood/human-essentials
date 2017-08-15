151e5ffe2371df6a00d68f068fbdae8500e4e172
Sean Marcia <sean@cs.uoregon.edu>
2017-08-15 12:15:36 -0400

    Update the static controller to not use the default layout.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,8 +2,11 @@ class StaticController < ApplicationController
   skip_before_action :authorize_user
   skip_before_action :authenticate_user!
 
+  layout false
+
   def index
     redirect_to dashboard_url(current_user.organization) if current_user
+
   end
 
   def page
