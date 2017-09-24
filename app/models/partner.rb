1e41d79f692272e3a5db4633d8a746c3f91bf062
Sean Marcia <sean@cs.uoregon.edu>
2017-09-24 17:38:44 -0400

    Partners

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -11,10 +11,20 @@
 #
 
 class Partner < ApplicationRecord
+  require 'csv'
+
   belongs_to :organization
   has_many :distributions
 
   validates :organization, presence: true
   validates :name, :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
+  def self.import_csv(filename,organization)
+    CSV.parse(filename, :headers => true) do |row|
+      loc = Partner.new(row.to_hash)
+      loc.organization_id = organization
+      loc.save!
+    end
+  end
 end
