05d99626b59a704123e08af48d5d98ad288757e4
Sean Marcia <sean@cs.uoregon.edu>
2018-05-25 10:10:30 -0400

    Admin spec fix for factorybot as it uses symbols not strings.

--- spec/features/admins_spec.rb
+++ spec/features/admins_spec.rb
@@ -7,27 +7,28 @@ RSpec.feature "Site Administration", type: :feature do
   scenario "Admin can create a new organization" do
     click_link "Add New Organization"
     org_params = attributes_for(:organization)
-    fill_in "Name", with: org_params["name"]
-    fill_in "Short name", with: org_params["short_name"]
-    fill_in "Url", with: org_params["url"]
-    fill_in "Email", with: org_params["email"]
-    fill_in "Street", with: org_params["street"]
-    fill_in "City", with: org_params["city"]
-    select(org_params["state"], :from => 'State')
-    fill_in "Zipcode", with: org_params["zipcode"]
+    fill_in "Name", with: org_params[:name]
+    fill_in "Short name", with: org_params[:short_name]
+    fill_in "Url", with: org_params[:url]
+    fill_in "Email", with: org_params[:email]
+    fill_in "Street", with: "1234 Banana Drive"
+    fill_in "City", with: "Boston"
+    select("MA", :from => 'State')
+    fill_in "Zipcode", with: "12345"
 
     click_button "Create"
 
-    expect(page).to have_content(org_params["name"])
-    expect(page).to have_current_path(admins_path)
+    expect(page).to have_content("All Diaperbase Organizations")
 
-    click_button "View"
-
-    expect(page).to have_content(org_params["name"])
-    expect(page).to have_content(org_params["street"])
-    expect(page).to have_content(org_params["city"])
-    expect(page).to have_content(org_params["state"])
-    expect(page).to have_content(org_params["zipcode"])
+    within('tr', text: org_params[:name]) do
+      first(:link, 'View').click
+    end
+  
+    expect(page).to have_content(org_params[:name])
+    expect(page).to have_content("Banana")
+    expect(page).to have_content("Boston")
+    expect(page).to have_content("MA")
+    expect(page).to have_content("12345")
 
   end
 
