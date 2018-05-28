c30fffec0ad5c614bc5a6a4fc87d04876bd8acd8
Aaron Hill <armahillo@gmail.com>
2018-05-28 13:19:18 -0400

    Barcode Item specs mostly fixed -- still need to resolve some issues with the filters

--- spec/models/diaper_drive_participant_spec.rb
+++ spec/models/diaper_drive_participant_spec.rb
@@ -2,7 +2,7 @@
 #
 # Table name: diaper_drive_participants
 #
-#  id              :integer          not null, primary key
+#  id              :bigint(8)        not null, primary key
 #  name            :string
 #  contact_name    :string
 #  email           :string
