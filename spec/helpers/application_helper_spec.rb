e2eea9c46f22a5aa4b3f61e022321aef195ecd2b
John Newcomb <jxnewcomb@godaddy.com>
2018-06-08 10:40:16 -0400

    Using double quotes

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe 'confirm_delete_msg' do
-    let(:item) { 'Adult Briefs (Medium/Large)' }
+  describe "confirm_delete_msg" do
+    let(:item) { "Adult Briefs (Medium/Large)" }
 
     it "subs in string" do
       expect(helper.confirm_delete_msg(item)).to include(item)
