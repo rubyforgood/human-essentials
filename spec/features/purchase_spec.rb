cc8c448fc2313fc54824dbe7ac8be398f5728812
joshcano <joshcano@gmail.com>
2018-06-08 15:29:03 -0400

    Create failing test

--- spec/features/purchase_spec.rb
+++ spec/features/purchase_spec.rb
@@ -92,13 +92,12 @@ RSpec.feature "Purchases", type: :feature, js: true do
        end
     end
 
+    # Bug fix -- Issue #378
+    # A user can view another organizations purchase
     context "Editing purchase" do
-      # Bug fix -- Issue #378
-      # A user can view another organizations purchase
-
       before(:each) do
         purchase = create(:purchase, organization: create(:organization))
-        visit edit_purchase_path(@user.organization.name, purchase)
+        visit edit_purchase_path(@user.organization.short_name, purchase)
       end
 
       scenario "A user can view another organizations puchanse" do
