6d9230a64de259e9876740bc1eab8ba557c46b35
Marius Pop <marius@mlpinit.com>
2018-06-10 11:33:43 -0400

    default inventory item quantity to 0

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -11,8 +11,6 @@
 #
 
 class InventoryItem < ApplicationRecord
-  after_initialize :set_quantity
-
   belongs_to :storage_location
   belongs_to :item
 
@@ -25,9 +23,5 @@ class InventoryItem < ApplicationRecord
 
   def self.quantity_by_category
     self.includes(:item).select("items.category").group("items.category").sum(:quantity).sort_by { |_, v| -v }
-  end
-
-  def set_quantity
-    self.quantity ||= 0
   end
 end
---
+++ db/migrate/20180610152822_default_inventory_quantity_to_zero.rb
@@ -0,0 +1,5 @@
+class DefaultInventoryQuantityToZero < ActiveRecord::Migration[5.2]
+  def change
+    change_column :inventory_items, :quantity, :integer, default: 0
+  end
+end
