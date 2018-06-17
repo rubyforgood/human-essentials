8ba9cfad13b34d2735d1eff6865ddc29c237a177
John Newcomb <jxnewcomb@godaddy.com>
2018-06-16 18:36:41 -0700

    Making routes more clear

--- app/controllers/data_exports_controller.rb
+++ app/controllers/data_exports_controller.rb
@@ -1,11 +1,10 @@
 class DataExportsController < ApplicationController
-  respond_to :html, :csv
+  respond_to :csv
 
   def csv
     type = params[:type]
 
     respond_to do |format|
-      format.html
       format.csv { send_data data(type), filename: "#{type}-#{Time.zone.today}.csv" }
     end
   end
