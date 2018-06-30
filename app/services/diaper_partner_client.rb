e404d09b1c99e768e2b9985bc48aa8307915d6d8
Brock Wilcox <awwaiid@thelackthereof.org>
2018-06-30 10:44:39 -0400

    Clean README and disable partnerbase by default [#430]

--- app/services/diaper_partner_client.rb
+++ app/services/diaper_partner_client.rb
@@ -1,7 +1,5 @@
 module DiaperPartnerClient
   def self.post(path, attributes)
-    return if Rails.env.test? # FIXME: Work out how to do integration testing
-
     diaper_partner_url = ENV["DIAPER_PARTNER_URL"]
     return if diaper_partner_url.blank?
 
