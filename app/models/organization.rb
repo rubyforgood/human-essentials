ca359d6af7c02f9f49cec9c061eb21dde36a28cb
Sean Marcia <smarcia@gmail.com>
2018-08-18 19:35:34 -0400

    Update organization.rb
    Temp fix so users can continue to print receipts.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -98,11 +98,11 @@ class Organization < ApplicationRecord
   end
 
   def logo_path
-    if logo.attached?
-      ActiveStorage::Blob.service.send(:path_for, logo.key).to_s
-    else
+    #if logo.attached?
+    #  ActiveStorage::Blob.service.send(:path_for, logo.key).to_s
+    #else
       Organization::DIAPER_APP_LOGO.to_s
-    end
+    #end
   end
 
   private
