ce7092c2f060543288cb3d22871752cc4edc4c38
Aaron (Home) <armahillo@gmail.com>
2018-08-18 14:15:10 -0400

    Remove 'authorization' group from shared spec; redundant

--- spec/support/authorization_specs.rb
+++ spec/support/authorization_specs.rb
@@ -15,21 +15,3 @@ RSpec.shared_examples "requiring authentication" do
     expect(response).to be_redirect
   end
 end
-
-RSpec.shared_examples "requiring authorization" do
-  it "Disallows all access for CRUD actions" do
-    single_params = { organization_id: object.organization.to_param, id: object.id }
-
-    get :index, params: { organization_id: object.organization.to_param }
-    expect(response).to be_redirect
-
-    get :new, params: { organization_id: object.organization.to_param }
-    expect(response).to be_redirect
-
-    get :show, params: single_params
-    expect(response).to be_redirect
-
-    post :create, params: { organization_id: object.organization.to_param }
-    expect(response).to be_redirect
-  end
-end
