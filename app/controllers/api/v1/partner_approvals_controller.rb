9d22b28c1f4c5da54f0c7fc69dbd621fefd6b6e1
Sean Marcia <smarcia@gmail.com>
2018-10-02 18:50:02 -0400

    wip

---
+++ app/controllers/api/v1/partner_approvals_controller.rb
@@ -0,0 +1,26 @@
+class API::V1::PartnerAoorovalsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
+  skip_before_action :authorize_user
+  respond_to :json
+
+  def create
+    return head :forbidden unless api_key_valid?
+    request = Request.new(request_params)
+    if request.save
+      render json: request, status: :created
+    else
+      render json: request.errors, status: :bad_request
+    end
+  end
+
+  private
+
+  def api_key_valid?
+    request.headers["X-Api-Key"] == ENV["PARTNER_KEY"]
+  end
+
+  def request_params
+    params.require(:request).permit(:organization_id, :partner_id, :comments)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
       resources :partner_requests, only: :create
+      resources :partner_approvals, only: :create
     end
   end
 
