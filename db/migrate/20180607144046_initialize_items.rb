60e3b01bc6165722123168bf8807a7352ebd7416
Aaron Hill <armahillo@gmail.com>
2018-10-16 00:53:48 -0400

    Travis is choking on migrations because of model validations. Modifying a migration to work better with it

--- db/migrate/20180607144046_initialize_items.rb
+++ db/migrate/20180607144046_initialize_items.rb
@@ -1,8 +1,10 @@
 class InitializeItems < ActiveRecord::Migration[5.2]
   def up
-    ci = CanonicalItem.find_or_create_by(name: "Other") do |ci|
+    ci = CanonicalItem.find_or_initialize_by(name: "Other") do |ci|
       ci.category = "Miscellaneous"
     end
+    # Skipping validations because requirements about `partner_key` cause problems if we don't
+    ci.save(validate: false)
     Item.where(canonical_item_id: nil).update_all(canonical_item_id: ci.id)
     CanonicalItem.reset_column_information
     Item.reset_column_information
