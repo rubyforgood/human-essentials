f866396616a6a3fdd657abf55575204c48030e4b
Aaron (Home) <armahillo@gmail.com>
2018-10-21 02:51:59 -0400

    Standardizes buttons via the UiHelper module

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  layout "application"
+  
+  def edit
 
+    super
+  end
   # GET /resource/sign_up
   # def new
   #   super
--- spec/factories/request.rb
+++ spec/factories/request.rb
@@ -1,8 +1,12 @@
+def random_keys sample_size
+  CanonicalItem.all.pluck(:partner_key).sample(sample_size).uniq.map(&:to_sym)
+end
+
 FactoryBot.define do
   factory :request do
     partner { Partner.try(:first) || create(:partner) }
     organization { Organization.try(:first) || create(:organization) }
-    request_items { { k_size5: 3, k_size6: 2 } }
+    request_items { random_keys(3).collect { |k| [k,rand(3..10)] }.to_h }
     status "Active"
     comments "Urgent"
   end
