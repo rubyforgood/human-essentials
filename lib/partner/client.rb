5657d02459b3d52b60acc5b114e0dc405aaae5b2
jamacmang <julianmacmang@gmail.com>
2018-10-25 00:17:06 +0800

    initial rubocop fixes

--- lib/partner/client.rb
+++ lib/partner/client.rb
@@ -1,15 +1,14 @@
 module Partner
-    class Client
-        
-        API_KEY = 'secretpartnerkey'
+  class Client
+    API_KEY = "secretpartnerkey".freeze
 
-        def call
-            response = RestClient::Request.execute(method: :get, 
-                                                   url: 'https://partner.diaper.app/api/v1/partners/1',
-                                                   headers: {'X-Api-Key' => API_KEY})
-            if response.code = 200 
-                response = JSON.parse response.body
-            end
-        end
+    def call
+      response = RestClient::Request.execute(method: :get,
+                                             url: "https://partner.diaper.app/api/v1/partners/1",
+                                             headers: { "X-Api-Key" => API_KEY })
+      if response.code = 200
+        _response = JSON.parse response.body
+      end
     end
+  end
 end
