8d0cc22eee3c15147f8bbe3b26204dc500956712
Thomas Hart <myrridin@gmail.com>
2018-10-25 18:44:09 -0400

    Make RSpec more robust for single tests and set random test order

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,40 @@ Capybara.asset_host = "http://localhost:3000"
 # Only keep the most recent run
 Capybara::Screenshot.prune_strategy = :keep_last_run
 
+def stub_addresses
+  Geocoder.configure(lookup: :test)
+
+  ["1500 Remount Road, Front Royal, VA",
+   "1111 Panda ave. Front Royal, VA 12345",
+   "Smithsonian Institute new",
+   "Smithsonian Conservation Center new",
+   "3700 O St NW, Washington, DC 20057",
+   "1500 Remount Road Front Royal, VA",
+   "1500 Remount Road, Front Royal, VA 22630",
+   "1234 Banana Drive Boston, MA 12345",
+   "1234 Banana Drive, Boston, MA 12345",
+   "123 Donation Site Way",
+   "456 Donation Site Way",
+   "789 Donation Site Way",
+   "123 Location Way",
+   "456 Location Way",
+   "789 Location Way"].each do |address|
+    Geocoder::Lookup::Test.add_stub(
+        address, [
+        {
+            "latitude" => 40.7143528,
+            "longitude" => -74.0059731,
+            "address" => "1500 Remount Road, Front Royal, VA",
+            "state" => "Virginia",
+            "state_code" => "VA",
+            "country" => "United States",
+            "country_code" => "US"
+        }
+    ]
+    )
+  end
+end
+
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
@@ -126,35 +160,7 @@ RSpec.configure do |config|
 
   # Stub out Geocoder or else...
   config.before(:all) do
-    Geocoder.configure(lookup: :test)
-
-    ["1500 Remount Road, Front Royal, VA",
-     "1111 Panda ave. Front Royal, VA 12345",
-     "Smithsonian Institute new",
-     "Smithsonian Conservation Center new",
-     "3700 O St NW, Washington, DC 20057",
-     "1500 Remount Road Front Royal, VA",
-     "1234 Banana Drive Boston, MA 12345",
-     "123 Donation Site Way",
-     "456 Donation Site Way",
-     "789 Donation Site Way",
-     "123 Location Way",
-     "456 Location Way",
-     "789 Location Way"].each do |address|
-      Geocoder::Lookup::Test.add_stub(
-        address, [
-          {
-            "latitude"     => 40.7143528,
-            "longitude"    => -74.0059731,
-            "address"      => "1500 Remount Road, Front Royal, VA",
-            "state"        => "Virginia",
-            "state_code"   => "VA",
-            "country"      => "United States",
-            "country_code" => "US"
-          }
-        ]
-      )
-    end
+    stub_addresses
   end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
