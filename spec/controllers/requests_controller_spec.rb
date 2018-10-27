9287bd95b56c59a252272f7e3e2cbfaf826f05b9
Kate Donaldson <kate@katelovescode.com>
2018-10-27 15:03:41 -0500

    Grabs all the inventory_items for the current org for use in the view, adds request_controller spec and updates request feature spec

---
+++ spec/controllers/requests_controller_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe RequestsController, type: :controller do
+  let(:default_params) do
+    { organization_id: @organization.to_param }
+  end
+
+  context "While signed in" do
+    before do
+      sign_in(@user)
+    end
+
+    describe "GET #index" do
+      subject { get :index, params: default_params }
+      it "returns http success" do
+        expect(subject).to be_successful
+      end
+    end
+
+    describe "GET #show" do
+      subject { get :show, params: default_params.merge(id: create(:request, organization: @organization)) }
+      it "returns http success" do
+        expect(subject).to be_successful
+      end
+    end
+  end
+
+  context "While not signed in" do
+    let(:object) { create(:request) }
+
+    include_examples "requiring authorization"
+  end
+end
--- spec/features/request_spec.rb
+++ spec/features/request_spec.rb
@@ -3,6 +3,15 @@ RSpec.feature "Requests", type: :feature do
     sign_in(@user)
     @request = create(:request, organization: @organization)
     @storage_location = create(:storage_location, organization: @organization)
+    # creates an inventory item for the request's first item and gives it a quantity of 234
+    @storage_location.inventory_items.create!(
+      quantity: 234,
+      item: Item.find_by(
+        canonical_item: CanonicalItem.find_by(
+          partner_key: @request.request_items.keys.first
+        )
+      )
+    )
   end
   let!(:url_prefix) { "/#{@organization.to_param}" }
 
@@ -20,6 +29,8 @@ RSpec.feature "Requests", type: :feature do
     scenario "the request is shown", js: true do
       visit url_prefix + "/requests/#{@request.id}"
       expect(page).to have_content("Request from #{@request.partner.name}")
+      expect(page).to have_content("Estimated on-hand")
+      expect(page).to have_content("234")
     end
 
     scenario "the request is fullfillable", js: true do
