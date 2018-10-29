c90d6462cc517a7664dbd17b8129cde19fc9bb94
JC Avena <jcavena@gmail.com>
2018-10-29 07:38:33 -0500

    590 - Adds feature tests for super admin with no organization id.

--- spec/features/admin/organizations_spec.rb
+++ spec/features/admin/organizations_spec.rb
@@ -1,34 +1,70 @@
-RSpec.feature "Organizations Admin" do
-  before :each do
-    sign_in(@super_admin)
-  end
-
-  scenario "creating a new organization" do
-    visit new_admin_organization_path
-    screenshot_and_open_image
-    click_link "Add New Organization"
-    org_params = attributes_for(:organization)
-    fill_in "Name", with: org_params[:name]
-    fill_in "Short name", with: org_params[:short_name]
-    fill_in "Url", with: org_params[:url]
-    fill_in "Email", with: org_params[:email]
-    fill_in "Street", with: "1234 Banana Drive"
-    fill_in "City", with: "Boston"
-    select("MA", from: "State")
-    fill_in "Zipcode", with: "12345"
-
-    click_on "Save"
-
-    expect(page).to have_content("All Diaperbase Organizations")
-
-    within("tr.#{org_params[:short_name]}") do
-      first(:link, "View").click
+RSpec.feature "Admin Organization Management" do
+  context "While signed in as an Administrative User (super admin)" do
+    before :each do
+      sign_in(@super_admin)
     end
 
-    expect(page).to have_content(org_params[:name])
-    expect(page).to have_content("Banana")
-    expect(page).to have_content("Boston")
-    expect(page).to have_content("MA")
-    expect(page).to have_content("12345")
+    scenario "creating a new organization" do
+      visit new_admin_organization_path
+      screenshot_and_open_image
+      click_link "Add New Organization"
+      org_params = attributes_for(:organization)
+      fill_in "Name", with: org_params[:name]
+      fill_in "Short name", with: org_params[:short_name]
+      fill_in "Url", with: org_params[:url]
+      fill_in "Email", with: org_params[:email]
+      fill_in "Street", with: "1234 Banana Drive"
+      fill_in "City", with: "Boston"
+      select("MA", from: "State")
+      fill_in "Zipcode", with: "12345"
+
+      click_on "Save"
+
+      expect(page).to have_content("All Diaperbase Organizations")
+
+      within("tr.#{org_params[:short_name]}") do
+        first(:link, "View").click
+      end
+
+      expect(page).to have_content(org_params[:name])
+      expect(page).to have_content("Banana")
+      expect(page).to have_content("Boston")
+      expect(page).to have_content("MA")
+      expect(page).to have_content("12345")
+    end
+  end
+  context "While signd in as an Administrative User with no organization (super admin no org)" do
+    before :each do
+      sign_in(@super_admin_no_org)
+    end
+
+    scenario "creating a new organization" do
+      visit new_admin_organization_path
+      screenshot_and_open_image
+      click_link "Add New Organization"
+      org_params = attributes_for(:organization)
+      fill_in "Name", with: org_params[:name]
+      fill_in "Short name", with: org_params[:short_name]
+      fill_in "Url", with: org_params[:url]
+      fill_in "Email", with: org_params[:email]
+      fill_in "Street", with: "1234 Potato Drive"
+      fill_in "City", with: "New York"
+      select("NY", from: "State")
+      fill_in "Zipcode", with: "54321"
+
+      click_on "Save"
+
+      expect(page).to have_content("All Diaperbase Organizations")
+
+      within("tr.#{org_params[:short_name]}") do
+        first(:link, "View").click
+      end
+
+      expect(page).to have_content(org_params[:name])
+      expect(page).to have_content("Potato")
+      expect(page).to have_content("New York")
+      expect(page).to have_content("NY")
+      expect(page).to have_content("54321")
+    end
   end
 end
