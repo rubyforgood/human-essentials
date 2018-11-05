dab6cce6956346da7decada97163516dfc2e8c69
JC Avena <jcavena@gmail.com>
2018-11-04 18:00:05 -0600

    326 - Sort organizations by name.

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -14,7 +14,7 @@ class Admin::OrganizationsController < AdminController
   end
 
   def index
-    @organizations = Organization.all
+    @organizations = Organization.order(:name).all
   end
 
   def new
