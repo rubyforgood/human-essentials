253883ddded3cc1947e6ea331ece2623290c06d9
Sean Marcia <smarcia@gmail.com>
2018-11-05 20:50:58 -0500

    Setting up staging server

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_i
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-#set :stage,           :production
+# set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -32,10 +32,10 @@ set :rails_env, :staging
 role :app, "deploy@45.79.146.211"
 
 server "45.79.146.211", roles: %w{web app db}, primary: true,
-       ssh_options: {
-           forward_agent: true,
-           keys: ["~/.ssh/id_rsa"]
-       }
+                        ssh_options: {
+                          forward_agent: true,
+                          keys: ["~/.ssh/id_rsa"]
+                        }
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
