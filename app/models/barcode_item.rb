0de3d1045c1e0c694c032a6d74e3afa007b71cc6
Aaron Hill <armahillo@gmail.com>
2018-11-12 13:39:40 -0500

    Adds new scope to get organizational barcodes. Adds new method to find items with barcode items. Specs to cover it. Modifies JS to handle new method for barcodes.

--- app/models/barcode_item.rb
+++ app/models/barcode_item.rb
@@ -31,6 +31,7 @@ class BarcodeItem < ApplicationRecord
   scope :by_item_partner_key, ->(partner_key) { joins("INNER JOIN items ON items.id = barcode_items.barcodeable_id").where(barcodeable_type: "Item", items: { partner_key: partner_key }) }
   scope :by_canonical_item_partner_key, ->(partner_key) { joins("INNER JOIN canonical_items ON canonical_items.id = barcode_items.barcodeable_id").where(barcodeable_type: "CanonicalItem", canonical_items: { partner_key: partner_key }) }
   scope :by_value,       ->(value) { where(value: value) }
+  scope :organization_barcodes_with_globals, ->(organization) { where(global: true).or(where(organization_id: organization, global: false)) }
   scope :include_global, ->(global) { where(global: [false, global]) }
   scope :for_csv_export, ->(organization) {
     where(organization: organization)
