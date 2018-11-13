4b775bd9d448a389d4dba87f959b6f893a04cc44
Aaron Hill <armahillo@gmail.com>
2018-11-12 22:48:26 -0500

    Rubocop and rspec fixes

--- app/controllers/barcode_items_controller.rb
+++ app/controllers/barcode_items_controller.rb
@@ -37,7 +37,7 @@ class BarcodeItemsController < ApplicationController
   def find
     # First, we do a naive lookup
     @barcode_item = BarcodeItem.includes(:barcodeable).organization_barcodes_with_globals(@organization).find_by!(value: barcode_item_params[:value])
-    # Depending on whether or not the result is solely a global barcode, we may need additional queries 
+    # Depending on whether or not the result is solely a global barcode, we may need additional queries
     # Global barcodes don't explicitly map to organization items, so we can do a lookup to clarify that
     if @barcode_item.global?
       # So in this case, we need to do an item lookup. #593 clarifies that we should fall through to the
