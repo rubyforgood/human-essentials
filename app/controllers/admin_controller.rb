90bc77cf96c7f0c43732d6eba616a84e730782af
Aaron Hill <armahillo@gmail.com>
2018-11-12 22:28:20 -0500

    Adds dashboard viz, re-adds dashboard to nav

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,5 +5,13 @@ class AdminController < ApplicationController
     verboten! unless current_user.super_admin?
   end
 
-  def dashboard; end
+  def dashboard
+    @recent_organizations = Organization.where('created_at > ?', 1.week.ago)
+    @recent_users = User.where('created_at > ?', 1.week.ago)
+    @top_10_other = Item.by_partner_key('other').where.not(name: "Other").group(:name).limit(10).order('count_name DESC').count(:name)
+    @donation_count = Donation.where('created_at > ?', 1.week.ago).count
+    @distribution_count = Distribution.where('created_at > ?', 1.week.ago).count
+    @request_count = Request.where('created_at > ?', 1.week.ago).count
+    @organization_count = Organization.all.count
+  end
 end
