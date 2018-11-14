92cb0ad9f17d96097331312d4f9e2b4b503b8d08
JC Avena <jcavena@gmail.com>
2018-11-14 07:52:45 -0600

    326 - Adds users active within past week using `last_request_at`.
    `last_request_at` is updated at a minimum 10 minute interval so it doesn't update the database on each request.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_023326) do
+ActiveRecord::Schema.define(version: 2018_11_14_131213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -275,6 +275,7 @@ ActiveRecord::Schema.define(version: 2018_10_30_023326) do
     t.boolean "organization_admin"
     t.string "name", default: "CHANGEME", null: false
     t.boolean "super_admin", default: false
+    t.datetime "last_request_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
