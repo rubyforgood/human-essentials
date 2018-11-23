71e1adec0a513eba953eb72a08f0e346c1988fa6
Chase Southard <chase.southard@gmail.com>
2018-11-23 18:59:44 -0500

    adds links to partner proof files

--- app/helpers/partners_helper.rb
+++ app/helpers/partners_helper.rb
@@ -1,2 +1,8 @@
 module PartnersHelper
+  def documentation_url(path)
+    # NOTE(chaserx): add to .env with http://localhost:3001 or https://partnerbase.org
+    uri = URI(ENV['PARTNER_BASE_URL'])
+    uri.path = path
+    uri.to_s
+  end
 end
