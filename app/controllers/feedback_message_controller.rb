168fd25974cf6aadb70eeb2692757d949fdde423
Michael Dworken <michael.dworken@framebridge.com>
2019-01-07 04:47:35 -0500

    Fix Rubocop Violations

--- app/controllers/feedback_message_controller.rb
+++ app/controllers/feedback_message_controller.rb
@@ -1,18 +1,18 @@
 class FeedbackMessageController < ApplicationController
-
   def create
     @feedback_message = FeedbackMessage.new(feedback_message_params)
     @feedback_message.user_name = current_user.name
     @feedback_message.user_id = current_user.id
     @feedback_message.user_email = current_user.email
     @feedback_message.path = request.original_url
-    @feedback_message.timestamp = DateTime.now
+    @feedback_message.timestamp = DateTime.current
     FeedbackMessageMailer.feedback_email(@feedback_message).deliver_now
     flash[:notice] = "Your feedback has been logged!"
     redirect_to request.referer
   end
 
   private
+
   def feedback_message_params
     params.permit(:message)
   end
