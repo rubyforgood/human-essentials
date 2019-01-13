97b39893867c29d8b3d0713d25d5b175c22edec4
Bhanu Teja <pbteja1998@gmail.com>
2019-01-13 16:39:13 +0530

    model specs

--- spec/factories/audit.rb
+++ spec/factories/audit.rb
@@ -14,8 +14,8 @@
 
 FactoryBot.define do
   factory :audit do
-    user { User.try(:first) || create(:user) }
     organization { Organization.try(:first) || create(:organization) }
+    user { nil }
     storage_location { nil }
     adjustment { nil }
     status { :in_progress }
@@ -24,21 +24,25 @@ FactoryBot.define do
       instance.storage_location = evaluator.storage_location || create(:storage_location, organization: instance.organization)
     end
 
+    after(:build) do |instance, evaluator|
+      instance.user = evaluator.user || create(:organization_admin, organization: instance.organization)
+    end
+
     trait :with_items do
       transient do
         item_quantity { 100 }
         item { nil }
       end
-    end
 
-    after(:build) do |instance, evaluator|
-      instance.storage_location ||= create(:storage_location, :with_items, item: evaluator.item)
-      item = if evaluator.item.nil?
-               instance.storage_location.inventory_items.first.items
-             else
-               evaluator.item
-             end
-      instance.line_items << build(:line_item, quantity: evaluator.item_quantity, item: item)
+      after(:build) do |instance, evaluator|
+        instance.storage_location ||= create(:storage_location, :with_items, item: evaluator.item, organization: instance.organization)
+        item = if evaluator.item.nil?
+                 instance.storage_location.inventory_items.first.item
+               else
+                 evaluator.item
+               end
+        instance.line_items << build(:line_item, quantity: evaluator.item_quantity, item: item)
+      end
     end
   end
 end
--- spec/models/audit_spec.rb
+++ spec/models/audit_spec.rb
@@ -20,26 +20,44 @@ RSpec.describe Audit, type: :model do
   context "Validations >" do
     it "must belong to an organization" do
       expect(build(:audit, storage_location: create(:storage_location), organization_id: nil)).not_to be_valid
+      expect(build(:audit, organization: @organization)).to be_valid
+    end
+
+    it "must belong to a storage location of its organization" do
+      new_organization = create(:organization)
+      storage_location1 = create(:storage_location, organization: @organization)
+      storage_location2 = create(:storage_location, organization: new_organization)
+      expect(build(:audit, storage_location: storage_location2, organization: @organization, user: @organization_admin)).not_to be_valid
+      expect(build(:audit, storage_location: storage_location1, organization: @organization, user: @organization_admin)).to be_valid
+    end
+
+    it "must belong to an organization admin of its organization" do
+      expect(build(:audit, storage_location: create(:storage_location, organization: @organization), user: @user, organization: @organization)).not_to be_valid
+      expect(build(:audit, storage_location: create(:storage_location, organization: @organization), user: @organization_admin,  organization: @organization)).to be_valid
+    end
+
+    it "is valid with valid attributes" do
+      expect(build(:audit)).to be_valid
     end
   end
 
   context "Scopes >" do
     it "`at_location` can filter out audits to a specific location" do
-      aud1 = create(:audit)
+      audit1 = create(:audit)
       create(:audit)
-      expect(Audit.at_location(aud1.storage_location_id).size).to eq(1)
+      expect(Audit.at_location(audit1.storage_location_id).size).to eq(1)
     end
   end
 
   context "Methods >" do
-    it "`self.storage_locations_audited_for` returns only storage_locations that are used in audits for one org" do
+    it "`self.storage_locations_audited_for` returns only storage_locations that are used for one org" do
       storage_location1 = create(:storage_location, organization: @organization)
       storage_location2 = create(:storage_location, organization: @organization)
       create(:storage_location, organization: @organization)
       storage_location4 = create(:storage_location, organization: create(:organization))
-      create(:audit, storage_location: storage_location1, organization: @organization, user: @organization_admin)
-      create(:audit, storage_location: storage_location2, organization: @organization, user: @organization_admin)
-      create(:audit, storage_location: storage_location4, organization: storage_location4.organization, user: @organization_admin)
+      create(:audit, storage_location: storage_location1, organization: @organization)
+      create(:audit, storage_location: storage_location2, organization: @organization)
+      create(:audit, storage_location: storage_location4, organization: storage_location4.organization)
       expect(Audit.storage_locations_audited_for(@organization).to_a).to match_array([storage_location1, storage_location2])
     end
   end
@@ -48,9 +66,9 @@ RSpec.describe Audit, type: :model do
     it "accepts them" do
       item = create(:item)
       storage_location = create(:storage_location, :with_items, item: item, item_quantity: 10)
+
       new_audit = build(:audit,
                         storage_location: storage_location,
-                        user: @organization_admin,
                         line_items_attributes: [
                           { item_id: storage_location.items.first.id, quantity: 10 }
                         ])
