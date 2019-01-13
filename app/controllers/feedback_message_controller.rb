61a46c7eebb7315f6d7736239c472cfdfec1bc8b
Michael Dworken <michael.dworken@framebridge.com>
2019-01-13 16:50:32 -0500

    Save and reloadfeedback message before passing to mailer

--- app/controllers/feedback_message_controller.rb
+++ app/controllers/feedback_message_controller.rb
@@ -3,6 +3,8 @@ class FeedbackMessageController < ApplicationController
     @feedback_message = FeedbackMessage.new(feedback_message_params)
     @feedback_message.user = current_user
     @feedback_message.path = request.referer
+    @feedback_message.save
+    @feedback_message.reload
     FeedbackMessageMailer.feedback_email(@feedback_message).deliver_now
     flash[:notice] = "Your feedback has been logged!"
     redirect_to request.referer
