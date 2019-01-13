96042fc7f2bceb69e39e5b3e5c15c9a85efb6631
Michael Dworken <michael.dworken@framebridge.com>
2019-01-13 16:45:02 -0500

    Change FeedbackMessage model to use ActiveRecord instead of just ActiveModel - improves ability to track feedback overtime #580

--- app/models/feedback_message.rb
+++ app/models/feedback_message.rb
@@ -1,4 +1,3 @@
-class FeedbackMessage
-  include ActiveModel::Model
-  attr_accessor :user_name, :user_id, :user_email, :path, :timestamp, :message
+class FeedbackMessage < ApplicationRecord
+  belongs_to :user
 end
