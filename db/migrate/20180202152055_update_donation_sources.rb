b2bf526029a9e7de94ea20d9186afff874c2fa4a
Sean Marcia <smarcia@gmail.com>
2019-01-19 23:47:29 -0500

    Revert "Fixes #661 Fixes migrations"

--- db/migrate/20180202152055_update_donation_sources.rb
+++ db/migrate/20180202152055_update_donation_sources.rb
@@ -1,13 +1,5 @@
 class UpdateDonationSources < ActiveRecord::Migration[5.1]
-  class MigrationDonation < ActiveRecord::Base
-    self.table_name = :donations
-  end
-
-  def up
-    MigrationDonation.where(source: 'Donation Pickup Location').update_all(source: 'Donation Site')
-  end
-
-  def down
-    MigrationDonation.where(source: 'Donation Site').update_all(source: 'Donation Pickup Location')
+  def change
+    execute("update donations set source = 'Donation Site' where source = 'Donation Pickup Location'")
   end
 end
