899e65e728590c6fc21b60430ba2e107b792c2b0
IlinDmitry <339966289@mail.ru>
2019-02-08 14:05:56 +0300

    Default Organization Email Text

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -42,6 +42,6 @@ class OrganizationsController < ApplicationController
   end
 
   def organization_params
-    params.require(:organization).permit(:name, :short_name, :street, :city, :state, :zipcode, :email, :url, :logo, :intake_location)
+    params.require(:organization).permit(:name, :short_name, :street, :city, :state, :zipcode, :email, :url, :logo, :intake_location, :default_email_text)
   end
 end
