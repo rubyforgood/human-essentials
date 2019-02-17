fc67e16c4d60c28479a6476b6578e4f066875740
Michael Dworken <michael.dworken@framebridge.com>
2019-02-17 13:50:07 -0500

    review current models and refactor confusing sections

--- app/models/storage_location.rb
+++ app/models/storage_location.rb
@@ -77,25 +77,23 @@ class StorageLocation < ApplicationRecord
     log = {}
     adjustment.line_items.each do |line_item|
       inventory_item = InventoryItem.find_or_create_by(storage_location_id: id,
-                                                       item_id: line_item.item_id) do |inv_item|
-        inv_item.quantity = 0
-      end
-      inventory_item.quantity += begin
-                                   line_item.quantity
-                                 rescue StandardError
-                                   0
-                                 end
+                                                       item_id: line_item.item_id)
+      inventory_item.quantity += line_item&.quantity || 0
       inventory_item.save
       log[line_item.item_id] = "+#{line_item.quantity}"
     end
     log
   end
 
-  def remove!(donation_or_purchase)
+  # NOTE: This knows too much about donations/purchases
+  # NOTE: Can we do logging better? It seems like a side effect
+  def remove!(itemizable)
     log = {}
-    donation_or_purchase.line_items.each do |line_item|
+    itemizable.line_items.each do |line_item|
       inventory_item = InventoryItem.find_by(storage_location: id, item_id: line_item.item_id)
+      # NOTE: Code smell - are we sure we want to allow this to be destroyed if < 0?
       if (inventory_item.quantity - line_item.quantity) <= 0
+        # NOTE: Instead of deleting, maybe we could leave it at 0, and hide it in the UI instead
         inventory_item.destroy
       else
         inventory_item.update(quantity: inventory_item.quantity - line_item.quantity)
@@ -105,8 +103,10 @@ class StorageLocation < ApplicationRecord
     log
   end
 
-  def adjust_from_past!(donation_or_purchase, previous_line_item_values)
-    donation_or_purchase.line_items.each do |line_item|
+  # NOTE: Make this code clearer in its intent -- needs more context
+  def adjust_from_past!(itemizable, previous_line_item_values)
+    itemizable.line_items.each do |line_item|
+      # NOTE: Can't we do an association lookup, instead of going all the way up to the model?
       inventory_item = InventoryItem.find_or_create_by(storage_location_id: id, item_id: line_item.item_id)
       # If the item wasn't deleted by the user, then it will be present to be deleted
       # here, and delete returns the item as a return value.
@@ -118,7 +118,7 @@ class StorageLocation < ApplicationRecord
       inventory_item.destroy! if inventory_item.quantity.zero?
     end
     # Update storage for line items that are no longer persisted because they
-    # were removed durring the updated/delete process.
+    # were removed during the update/delete process.
     previous_line_item_values.values.each do |value|
       inventory_item = InventoryItem.find_or_create_by(storage_location_id: id, item_id: value.item_id)
       inventory_item.decrement!(:quantity, value.quantity)
@@ -126,11 +126,16 @@ class StorageLocation < ApplicationRecord
     end
   end
 
-  def distribute!(distribution)
+  # This is the "subtract inventory method"
+  # NOTE: This is not returning a log object
+  def distribute!(itemizable)
+    # This is passed to update_inventory_inventory_items
     updated_quantities = {}
+    # Used in the exception return value
     insufficient_items = []
-    distribution.line_items.each do |line_item|
+    itemizable.line_items.each do |line_item|
       inventory_item = inventory_items.find_by(item: line_item.item)
+      # NOTE: If the distribution isn't able to find the inventory item, it continues
       next if inventory_item.nil?
 
       if inventory_item.quantity >= line_item.quantity
@@ -146,16 +151,19 @@ class StorageLocation < ApplicationRecord
       end
     end
 
+    # NOTE: Could this be handled by a validation instead?
     unless insufficient_items.empty?
       raise Errors::InsufficientAllotment.new(
-        "Distribution line_items exceed the available inventory",
+        "#{itemizable.class.name} line_items exceed the available inventory",
         insufficient_items
       )
     end
 
+    # NOTE: This executes the transaction to actually change the data
     update_inventory_inventory_items(updated_quantities)
   end
 
+  # NOTE: We should generalize this elsewhere -- Importable concern?
   def self.import_csv(csv, organization)
     csv.each do |row|
       loc = StorageLocation.new(row.to_hash)
@@ -164,6 +172,7 @@ class StorageLocation < ApplicationRecord
     end
   end
 
+  # NOTE: We should generalize this elsewhere -- Importable concern?
   def self.import_inventory(filename, org, loc)
     current_org = Organization.find(org)
     adjustment = current_org.adjustments.create(storage_location_id: loc.to_i, comment: "Starting Inventory")
@@ -176,34 +185,42 @@ class StorageLocation < ApplicationRecord
 
   # Used to move inventory between StorageLocations; reflects items being physically moved
   # Ex: move 500 size "2" diapers from main warehouse to overflow warehouse because insufficient space in main warehouse
+  # This could all be moved over to the `Transfer` model
   def move_inventory!(transfer)
+    # Contains the total deltas from/to for all the InventoryItem records that are being changed
     updated_quantities = {}
     item_validator = Errors::InsufficientAllotment.new("Transfer items exceeds \
                                                         the available inventory")
     transfer.line_items.each do |line_item|
-      inventory_item = inventory_items.find_by(item: line_item.item)
-      new_inventory_item = transfer.to.inventory_items.find_or_create_by(item: line_item.item)
-      next if inventory_item.nil? || inventory_item.quantity.zero?
+      # NOTE: We should do it this way more
+      from_inventory_item = inventory_items.find_by(item: line_item.item)
+      # NOTE: Initialize the inventory item on the destination storage location; "to" = "a storage location"
+      to_inventory_item = transfer.to.inventory_items.find_or_create_by(item: line_item.item)
+      # NOTE: this is for if the transfer includes inventory items that are nonexistent / zero, we can't transfer them
+      # maybe we could do this as a validation, or at the model level instead?
+      next if from_inventory_item.nil? || from_inventory_item.quantity.zero?
 
-      if inventory_item.quantity >= line_item.quantity
-        updated_quantities[inventory_item.id] = (updated_quantities[inventory_item.id] ||
-                                                 inventory_item.quantity) - line_item.quantity
-        updated_quantities[new_inventory_item.id] = (updated_quantities[new_inventory_item.id] ||
-          new_inventory_item.quantity) + line_item.quantity
+      if from_inventory_item.quantity >= line_item.quantity
+        # NOTE: this is subtracting the inventory found on each line item, from the running total of inventory available at the source location
+        updated_quantities[from_inventory_item.id] = (updated_quantities[from_inventory_item.id] || from_inventory_item.quantity) - line_item.quantity
+        # NOTE: this is adding the inventory found to the new destination at the new storage location
+        updated_quantities[to_inventory_item.id] = (updated_quantities[to_inventory_item.id] || to_inventory_item.quantity) + line_item.quantity
       else
         item_validator.add_insufficiency(line_item.item,
-                                         inventory_item.quantity,
+                                         from_inventory_item.quantity,
                                          line_item.quantity)
       end
     end
 
     raise item_validator unless item_validator.satisfied?
 
+    # NOTE: Run the transaction
     update_inventory_inventory_items(updated_quantities)
   end
 
   # Used to adjust inventory at a StorageLocation to reflect reality
   # Ex: we thought we had 200 size "5" diapers, but we actually have 180 size "5" diapers
+  # NOTE: Has too much knowledge about adjustments -- should be moved to `Adjustment`
   def adjust!(adjustment)
     updated_quantities = {}
     item_validator = Errors::InsufficientAllotment.new("Adjustment exceeds the available inventory")
@@ -227,10 +244,13 @@ class StorageLocation < ApplicationRecord
     update_inventory_inventory_items(updated_quantities)
   end
 
+  # NOTE: This reverses a distribution
+  # NOTE: Too much knowledge about distribution
   def reclaim!(distribution)
     ActiveRecord::Base.transaction do
       distribution.line_items.each do |line_item|
         if line_item.item.nil? || !line_item.item.active?
+          # If the item was previously hidden (inactive), make it active
           Item.unscoped.find(line_item.item_id).update(active: true)
           line_item.reload
         end
@@ -240,6 +260,7 @@ class StorageLocation < ApplicationRecord
     end
   end
 
+  # NOTE: This has WAY too much knowledge of distribution
   def update_distribution!(distribution, new_distribution_params)
     ActiveRecord::Base.transaction do
       distribution.line_items.each do |line_item|
@@ -263,6 +284,23 @@ class StorageLocation < ApplicationRecord
     end
   rescue ActiveRecord::RecordInvalid
     false
+  end
+
+  def increase_inventory(itemizable)
+    itemizable.line_items.each do |line_item|
+      inventory_item = inventory_items.find_or_create_by!(item: line_item.item)
+      inventory_item.increment(:quantity, line_item.quantity)
+    end
+  end
+
+  end
+
+  def decrease_inventory(itemizable)
+    itemizble.line_items.each do |line_item|
+      # NOTE: Handle the ActiveRecord::RecordNotFound exception
+      inventory_item = inventory_items.find_by!(item: line_item.item)
+      inventory_item.decrement(:quantity, line_item.quantity)
+    end
   end
 
   def self.csv_export_headers
