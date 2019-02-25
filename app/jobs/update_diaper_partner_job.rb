daffa0ef6b00c00cde1c1d89cfbcf4e226161652
Joey Ruzevich <jruzevich@gmail.com>
2019-02-24 21:36:22 -0600

    Draft Pull Request. Changes removed before push

--- app/jobs/update_diaper_partner_job.rb
+++ app/jobs/update_diaper_partner_job.rb
@@ -5,7 +5,19 @@ class UpdateDiaperPartnerJob
 
   def perform(partner_id)
     @partner = Partner.find(partner_id)
+
     DiaperPartnerClient.post(@partner.attributes) if Flipper.enabled?(:email_active)
-    @partner.update(status: "Pending")
-  end
+
+    #one line here that checks the post request to see if successful and if so pass to boolean and use that in the if/else below
+    #run partner app
+    #make post request and see what attributes are required for a successful response
+    #figure out what response looks like (in terms of how to read the body of text returned
+    #Validate post method throws error when unsuccessful post occurs
+    #or add something that does and then see where "post" method is being called and adjust accordingly
+#    if
+#    {@partner.update(status: "Pending")}
+#else
+#    @partner.updates(status: )
+#
+# end
 end
