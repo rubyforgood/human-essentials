b881165bb20d9da1709e88f63a4c037a70d553bf
Kevin Kuchta <kevin@kevinkuchta.com>
2019-03-30 11:41:41 -0700

    Add families

---
+++ app/controllers/families_controller.rb
@@ -0,0 +1,74 @@
+class FamiliesController < ApplicationController
+  before_action :set_family, only: [:show, :edit, :update, :destroy]
+
+  # GET /families
+  # GET /families.json
+  def index
+    @families = Family.all
+  end
+
+  # GET /families/1
+  # GET /families/1.json
+  def show
+  end
+
+  # GET /families/new
+  def new
+    @family = Family.new
+  end
+
+  # GET /families/1/edit
+  def edit
+  end
+
+  # POST /families
+  # POST /families.json
+  def create
+    @family = Family.new(family_params)
+
+    respond_to do |format|
+      if @family.save
+        format.html { redirect_to @family, notice: 'Family was successfully created.' }
+        format.json { render :show, status: :created, location: @family }
+      else
+        format.html { render :new }
+        format.json { render json: @family.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /families/1
+  # PATCH/PUT /families/1.json
+  def update
+    respond_to do |format|
+      if @family.update(family_params)
+        format.html { redirect_to @family, notice: 'Family was successfully updated.' }
+        format.json { render :show, status: :ok, location: @family }
+      else
+        format.html { render :edit }
+        format.json { render json: @family.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /families/1
+  # DELETE /families/1.json
+  def destroy
+    @family.destroy
+    respond_to do |format|
+      format.html { redirect_to families_url, notice: 'Family was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_family
+      @family = Family.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def family_params
+      params.require(:family).permit(:guardian_first_name, :guardian_last_name, :guardian_zip_code, :guardian_country, :guardian_phone, :agency_guardian_id, :home_adult_count, :home_child_count, :home_young_child_count, :sources_of_income, :guardian_employed, :guardian_employment_type, :guardian_monthly_pay, :guardian_health_insurance, :comments)
+    end
+end
