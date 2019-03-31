4c67b6392205177b948025a996d5d128fc420c00
Oleksiy Babich <oleksiy@oleksiy.od.ua>
2019-03-31 16:32:18 -0700

    #762 Rename `CanonicalItem` to `BaseItem`   - all changes done   - migrations included   - redis warning on deployment (!!)

--- spec/models/base_item_spec.rb
+++ spec/models/base_item_spec.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: canonical_items
+# Table name: base_items
 #
 #  id            :bigint(8)        not null, primary key
 #  name          :string
@@ -15,31 +15,31 @@
 
 require "rails_helper"
 
-RSpec.describe CanonicalItem, type: :model do
+RSpec.describe BaseItem, type: :model do
   describe "Validations >" do
     it "is invalid without a name" do
-      expect(build(:canonical_item, name: nil)).not_to be_valid
+      expect(build(:base_item, name: nil)).not_to be_valid
     end
 
     it "is invalid without a unique name" do
-      f = create(:canonical_item)
-      expect(build(:canonical_item, name: f.name)).not_to be_valid
+      f = create(:base_item)
+      expect(build(:base_item, name: f.name)).not_to be_valid
     end
 
     it "is invalid without a partner key" do
-      expect(build(:canonical_item, partner_key: nil)).not_to be_valid
+      expect(build(:base_item, partner_key: nil)).not_to be_valid
     end
 
     it "is invalid without a uniqueness key" do
-      f = create(:canonical_item)
-      expect(build(:canonical_item, partner_key: f.partner_key)).not_to be_valid
+      f = create(:base_item)
+      expect(build(:base_item, partner_key: f.partner_key)).not_to be_valid
     end
   end
 
   describe "Associations >" do
     it "keeps count of its associated items" do
-      c = CanonicalItem.first
-      expect { create_list(:item, 2, canonical_item: c) }.to change { c.item_count }.by(2)
+      c = BaseItem.first
+      expect { create_list(:item, 2, base_item: c) }.to change { c.item_count }.by(2)
     end
   end
 
@@ -48,9 +48,9 @@ RSpec.describe CanonicalItem, type: :model do
 
   describe "Filtering >" do
     describe "->by_partner_key" do
-      it "shows the Canonical Items by partner_key" do
-        expect(CanonicalItem.by_partner_key(CanonicalItem.first.partner_key).size).to eq(1)
-        expect(CanonicalItem.by_partner_key("random_string").size).to eq(0)
+      it "shows the Base Items by partner_key" do
+        expect(BaseItem.by_partner_key(BaseItem.first.partner_key).size).to eq(1)
+        expect(BaseItem.by_partner_key("random_string").size).to eq(0)
       end
     end
   end
