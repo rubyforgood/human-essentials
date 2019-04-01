7655479f91a1c7f9b8de5e0ffc0c12bd9925f59c
Aaron Hill <armahillo@gmail.com>
2019-04-01 11:00:04 -0700

    Implements hack to accommodate old model name in migrations where they explicitly referenced the model

--- db/migrate/20181016002337_add_partner_key_to_canonical_item.rb
+++ db/migrate/20181016002337_add_partner_key_to_canonical_item.rb
@@ -1,9 +1,12 @@
+# `CanonicalItem` is later renamed to `BaseItem`. It was a bad choice to
+# do it this way in a migration, but here we are. The database doesn't
+# yet know about the BaseItem name change, but the codebase does.
+class CanonicalItem < ApplicationRecord; end
+
 class AddPartnerKeyToCanonicalItem < ActiveRecord::Migration[5.2]
   def up
     add_column :canonical_items, :partner_key, :string
-    # `CanonicalItem` is later renamed to `BaseItem`. It was a bad choice to
-    # do it this way in a migration, but here we are. The database doesn't
-    # yet know about the BaseItem name change, but the codebase does.
+
     canonical_items = File.read(Rails.root.join("db", "base_items.json"))
     items_by_category = JSON.parse(canonical_items)
     # Creates the Canonical Items
@@ -12,7 +15,7 @@ class AddPartnerKeyToCanonicalItem < ActiveRecord::Migration[5.2]
         # `CanonicalItem` is later renamed to `BaseItem`. It was a bad choice to
         # do it this way in a migration, but here we are. The database doesn't
         # yet know about the BaseItem name change, but the codebase does.
-        c = BaseItem.find_or_initialize_by(name: entry["name"])
+        c = CanonicalItem.find_or_initialize_by(name: entry["name"])
         c.category ||= category
         c.partner_key = entry["key"]
         c.save
