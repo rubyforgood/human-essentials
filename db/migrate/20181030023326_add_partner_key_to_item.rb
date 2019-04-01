39a7ed03566acc1ebdccb0afc1d0d4d1a6421934
Oleksiy Babich <oleksiy@oleksiy.od.ua>
2019-04-01 00:53:44 -0700

    Revert "#762 Rename `CanonicalItem` to `BaseItem`"
    This reverts commit 465aff795e083d6882a8c7dc80c198a2909ee2f5.

--- db/migrate/20181030023326_add_partner_key_to_item.rb
+++ db/migrate/20181030023326_add_partner_key_to_item.rb
@@ -1,12 +1,10 @@
 class AddPartnerKeyToItem < ActiveRecord::Migration[5.2]
-    # #762 - I have to change migration here because this is not pure migration, but seed
-
-    def up
+  def up
     add_column :items, :partner_key, :string
     add_index :items, :partner_key
 
     # Migrate existing references to partner keys
-    canonical_items = BaseItem.pluck(:id, :partner_key).to_h
+    canonical_items = CanonicalItem.pluck(:id, :partner_key).to_h
     Item.all.each do |i|
       i.update_attribute(:partner_key, canonical_items[i.canonical_item_id])
     end
@@ -18,7 +16,7 @@ class AddPartnerKeyToItem < ActiveRecord::Migration[5.2]
     add_column :items, :canonical_item_id, :integer
 
     # Migrate existing references back to numerical id
-    canonical_items = BaseItem.pluck(:partner_key, :id).to_h
+    canonical_items = CanonicalItem.pluck(:partner_key, :id).to_h
     Item.all.each do |i|
       i.update_attribute(:canonical_item_id, canonical_items[i.partner_key])
     end
