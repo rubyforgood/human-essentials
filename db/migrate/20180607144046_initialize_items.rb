fbf7bfb36e979e6c222f0036dd680d1bf1c7c6a6
Oleksiy Babich <oleksiy@oleksiy.od.ua>
2019-04-01 00:53:44 -0700

    Revert "#762 Rename `CanonicalItem` to `BaseItem`"
    This reverts commit 39f8068de6395e125849b3f5e0ff505ced1cbce5.

--- db/migrate/20180607144046_initialize_items.rb
+++ db/migrate/20180607144046_initialize_items.rb
@@ -1,17 +1,15 @@
 class InitializeItems < ActiveRecord::Migration[5.2]
   def up
-    # #762 - I have to change migration here because this is not pure migration, but seed
-
-    ci = BaseItem.find_or_initialize_by(name: "Other") do |ci|
+    ci = CanonicalItem.find_or_initialize_by(name: "Other") do |ci|
       ci.category = "Miscellaneous"
     end
     # Skipping validations because requirements about `partner_key` cause problems if we don't
     ci.save(validate: false)
     Item.where(canonical_item_id: nil).update_all(canonical_item_id: ci.id)
-    BaseItem.reset_column_information
+    CanonicalItem.reset_column_information
     Item.reset_column_information
   end
-
+  
   def down
   end
 end
