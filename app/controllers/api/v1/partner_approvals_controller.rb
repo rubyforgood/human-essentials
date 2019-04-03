e33d2571dc11002e76a774bde1200fb5696b64dc
Sean Marcia <smarcia@gmail.com>
2019-04-03 00:38:38 -0400

    rubocop

--- app/controllers/api/v1/partner_approvals_controller.rb
+++ app/controllers/api/v1/partner_approvals_controller.rb
@@ -20,6 +20,7 @@ class API::V1::PartnerApprovalsController < ApplicationController
 
   def api_key_valid?
     return true if Rails.env.development?
+
     request.headers["X-Api-Key"] == ENV["PARTNER_KEY"]
   end
 end
