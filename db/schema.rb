d99070862b41c56a7fd180d8f2b07622d16d60b8
Aaron (Home) <armahillo@gmail.com>
2019-04-07 17:01:12 -0400

    Adds migration to fix polymorphic association in Barcodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_231244) do
+ActiveRecord::Schema.define(version: 2019_04_07_203351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
