5d22ad1321d56f32bffdc2a5b0fd5fb85e5f4a31
Chase Southard <chase.southard@gmail.com>
2019-04-14 10:13:38 -0400

    adding some debugging for failing tests

--- db/schema.rb
+++ db/schema.rb
@@ -234,7 +234,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_220545) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "organization_id"
-    t.string "status"
+    t.integer "status", default: 0
     t.boolean "send_reminders", default: false, null: false
     t.index ["organization_id"], name: "index_partners_on_organization_id"
   end
