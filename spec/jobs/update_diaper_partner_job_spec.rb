381bceceec931b79522d98891a6142f575f00c8f
Chase Southard <chase.southard@gmail.com>
2019-04-14 18:14:13 -0400

    remove debugging

--- spec/jobs/update_diaper_partner_job_spec.rb
+++ spec/jobs/update_diaper_partner_job_spec.rb
@@ -3,7 +3,6 @@ RSpec.describe UpdateDiaperPartnerJob, job: true do
     context "with a successful POST response" do
       before do
         @partner = create(:partner)
-        puts "XXXXX - #{@partner.inspect}"
         response = double("Response", value: Net::HTTPSuccess)
         allow(DiaperPartnerClient).to receive(:post).and_return(response)
       end
@@ -12,11 +11,8 @@ RSpec.describe UpdateDiaperPartnerJob, job: true do
         with_features email_active: true do
           Sidekiq::Testing.inline! do
             expect do
-              puts "YYYYY before perform async - #{@partner.inspect}"
               UpdateDiaperPartnerJob.perform_async(@partner.id)
-              puts "YYYYY after perform async - #{@partner.inspect}"
               @partner.reload
-              puts "YYYYY after reload - #{@partner.inspect}"
             end
             expect(@partner.status).to eq("pending")
           end
@@ -27,7 +23,6 @@ RSpec.describe UpdateDiaperPartnerJob, job: true do
     context "with a unsuccessful POST response" do
       before do
         @partner = create(:partner)
-        puts "ZZZZ partner - #{@partner.inspect}"
         response = double("Response", value: nil)
         allow(DiaperPartnerClient).to receive(:post).and_return(response)
       end
@@ -36,15 +31,13 @@ RSpec.describe UpdateDiaperPartnerJob, job: true do
         with_features email_active: true do
           Sidekiq::Testing.inline! do
             expect do
-              puts "ZZZZ before perform_async - #{@partner.inspect}"
               UpdateDiaperPartnerJob.perform_async(@partner.id)
-              puts "ZZZZ after perform_async - #{@partner.inspect}"
               @partner.reload
-              puts "ZZZZ after reload - #{@partner.inspect}"
             end.to change { @partner.status }.to("error")
           end
         end
       end
+
       it "posts via DiaperPartnerClient" do
         with_features email_active: true do
           Sidekiq::Testing.inline! do
