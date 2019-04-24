df625e75eca4eccb2f012ee27bf08bc2db241dd3
Juarez Lustosa <juarez.lustosa@gmail.com>
2019-04-24 19:19:36 -0300

    Add active scope on line_items

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -17,6 +17,7 @@ class LineItem < ApplicationRecord
 
   validates :item_id, presence: true
   validates :quantity, numericality: { other_than: 0, only_integer: true }
+  scope :active, -> { joins(:item).where(items: { active: true }) }
 
   def value_per_line_item
     item.value * quantity
