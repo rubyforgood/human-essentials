d7f07e0a91a28e71d301e8804d24eb9d40c5c982
Sean Marcia <smarcia@gmail.com>
2019-04-26 14:22:35 -0400

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_203351) do
+ActiveRecord::Schema.define(version: 2019_04_22_130439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -245,8 +245,8 @@ ActiveRecord::Schema.define(version: 2019_04_07_203351) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "organization_id"
-    t.integer "status", default: 0
     t.boolean "send_reminders", default: false, null: false
+    t.integer "status", default: 0
     t.index ["organization_id"], name: "index_partners_on_organization_id"
   end
 
@@ -267,14 +267,15 @@ ActiveRecord::Schema.define(version: 2019_04_07_203351) do
   create_table "requests", force: :cascade do |t|
     t.bigint "partner_id"
     t.bigint "organization_id"
-    t.string "status", default: "Active"
     t.jsonb "request_items", default: {}
     t.text "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "distribution_id"
+    t.integer "status", default: 0
     t.index ["organization_id"], name: "index_requests_on_organization_id"
     t.index ["partner_id"], name: "index_requests_on_partner_id"
+    t.index ["status"], name: "index_requests_on_status"
   end
 
   create_table "storage_locations", id: :serial, force: :cascade do |t|
