e996f4c323f2912206649fd34bf54f13a2bf4512
Svetlana Vileshina <svileshina@gmail.com>
2019-05-02 13:28:18 -0400

    removed the old log in feature spec

--- spec/features/log_in_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.feature "Authentication", type: :feature do
-  describe "Success" do
-    scenario "should show dashboard upon signin" do
-      sign_in(@user)
-      visit "/"
-      expect(page.find("h1")).to have_content "Dashboard"
-    end
-  end
-end
