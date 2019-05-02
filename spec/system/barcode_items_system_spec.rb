9de0d774f12bbd20891893b51aae14aebf873298
ejuten <elayne.juten@gmail.com>
2019-05-02 16:31:38 -0500

    Run bundle exec rubocop -a to make rubocop happy

--- spec/system/barcode_items_system_spec.rb
+++ spec/system/barcode_items_system_spec.rb
@@ -1,24 +1,24 @@
 RSpec.describe "Barcode Items Admin" do
+  before do
+    sign_in(@super_admin)
+  end
+  context "while signed in as a super admin" do
     before do
-        sign_in(@super_admin)
+      visit admin_barcode_items_path
     end
-    context "while signed in as a super admin" do
-        before do
-        visit admin_barcode_items_path
-        end
-        let(:item) { create(:item) }
-        it "should create a new global barcode", js: true do
-        click_on "Add New Barcode"
-        fill_in "Quantity", with: 100
-        select item.base_item.name, from: "barcode_item_barcodeable_id"
-        fill_in "Barcode", with: 6666
-        click_on "Save"
-        expect(page).to have_content("6666")
-        expect(page).to have_content("Tampons")
-        expect(page).to have_content("100")
-        end
-        it "should edit an existing global barcode"
-        it "should delete a global barcode"
-        it "should view a barcode shows details about it"
+    let(:item) { create(:item) }
+    it "should create a new global barcode", js: true do
+      click_on "Add New Barcode"
+      fill_in "Quantity", with: 100
+      select item.base_item.name, from: "barcode_item_barcodeable_id"
+      fill_in "Barcode", with: 6666
+      click_on "Save"
+      expect(page).to have_content("6666")
+      expect(page).to have_content("Tampons")
+      expect(page).to have_content("100")
     end
+    it "should edit an existing global barcode"
+    it "should delete a global barcode"
+    it "should view a barcode shows details about it"
+  end
 end
