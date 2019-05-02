166a77d48a04b682c26517d3c139fcc8c7b73dbf
Sebastian Armano <sebarmano@gmail.com>
2019-05-02 12:17:44 -0500

    Convert authorization feature test to system test
    This fixes Issue #826
    
    Also I changed the language of the test from "It should.." to "It
    does..". I think it makes it more readable. However, now is not
    consistent with the rest of the tests, so I'm happy to change it back if
    you think it's better in the old way.

--- spec/system/authorization_spec.rb
+++ spec/system/authorization_spec.rb
@@ -1,14 +1,16 @@
-RSpec.feature "Authorization", type: :feature do
-  scenario "should redirect to dashboard when unauthorized user attempts access" do
+RSpec.describe "Authorization", type: :system do
+  it "redirects to the dashboard when unauthorized user attempts access" do
     sign_in(@user)
     visit "/admin/dashboard"
+
     expect(page.find("h1")).to have_content "Dashboard"
     expect(page.find(".alert")).to have_content "Access Denied"
   end
 
-  scenario "redirect to organization dashboard when authorized" do
+  it "redirects to the organization dashboard when authorized" do
     sign_in(@user)
     visit "/#{@organization.name}/dashboard"
+
     expect(current_path).to eql "/DEFAULT/dashboard"
   end
 end
