c24e3b02cfc2b0f6080d8e6a1f70c5e23356044a
Michael Dworken <michael.dworken@framebridge.com>
2019-05-27 10:40:14 -0400

    Further buffs up query specs

--- spec/queries/items_in_total_query_spec.rb
+++ spec/queries/items_in_total_query_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe ItemsInTotalQuery do
   let!(:storage_location) { create(:storage_location, organization: @organization) }
   let!(:other_storage_location) { create(:storage_location, :with_items, item: create(:item), item_quantity: 10, organization: @organization) }
-
+  let!(:shared_item) { create(:item) }
   subject { ItemsInTotalQuery.new(storage_location: storage_location, organization: @organization).call }
 
   describe "items_in" do
@@ -18,18 +18,33 @@ RSpec.describe ItemsInTotalQuery do
       expect(subject).to eq(40)
     end
 
+    it "counts shared items together" do
+      create(:donation, :with_items, item: shared_item, item_quantity: 50, storage_location: storage_location)
+      create(:purchase, :with_items, item: shared_item, item_quantity: 100, storage_location: storage_location)
+      create(:adjustment, :with_items, item: shared_item, item_quantity: 50, storage_location: storage_location)
+      create(:donation, :with_items, item: shared_item, item_quantity: 100, storage_location: storage_location)
+      create(:purchase, :with_items, item: shared_item, item_quantity: 50, storage_location: storage_location)
+      create(:adjustment, :with_items, item: shared_item, item_quantity: 100, storage_location: storage_location)
+      expect(subject).to eq(490)
+    end
+
     it "does not count negative adjustments towards in-flow" do
-      shared_item = create(:item)
       create(:donation, :with_items, item: shared_item, item_quantity: 10, storage_location: storage_location)
       create(:adjustment, :with_items, item: shared_item, item_quantity: -10, storage_location: storage_location)
       expect(subject).to eq(50)
     end
 
     it "does not count transfers going in the negative direction" do
-      shared_item = create(:item)
       create(:donation, :with_items, item: shared_item, item_quantity: 10, storage_location: storage_location)
       create(:transfer, :with_items, item_quantity: 10, item: shared_item, from: storage_location, to: other_storage_location)
       expect(subject).to eq(50)
+    end
+
+    it "does not count donations, purchases, adjustments to other storage locations" do
+      create(:donation, :with_items, item: create(:item), item_quantity: 100, storage_location: other_storage_location)
+      create(:purchase, :with_items, item: create(:item), item_quantity: 10, storage_location: other_storage_location)
+      create(:adjustment, :with_items, item: create(:item), item_quantity: 10, storage_location: other_storage_location)
+      expect(subject).to eq(40)
     end
   end
 end
