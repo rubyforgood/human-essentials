64e50b6f4b668b8e5ce24276f2e46c34c5582b55
Marius Pop <marius@mlpinit.com>
2019-06-01 21:30:11 -0500

    Scope child actions to current partner or family.

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,11 +1,13 @@
 class ChildrenController < ApplicationController
-  before_action :set_child, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_partner!
+
+  helper_method :child, :children, :family
+  attr_reader :children, :child
 
   # GET /children
   # GET /children.json
   def index
-    @children = Child.all
+    @children ||= current_partner.children.all
   end
 
   # GET /children/1
@@ -14,11 +16,11 @@ class ChildrenController < ApplicationController
 
   # GET /children/new
   def new
-    @child = Child.new(family_id: params[:family_id])
+    @child = family.children.new
   end
 
   def active
-    child = Child.find(params[:child_id])
+    child = family.find(params[:child_id])
     child.active = !child.active
     child.save
   end
@@ -29,15 +31,15 @@ class ChildrenController < ApplicationController
   # POST /children
   # POST /children.json
   def create
-    @child = Child.new(child_params)
+    child = family.children.new(child_params)
 
     respond_to do |format|
-      if @child.save
-        format.html { redirect_to @child, notice: "Child was successfully created." }
-        format.json { render :show, status: :created, location: @child }
+      if child.save
+        format.html { redirect_to child, notice: "Child was successfully created." }
+        format.json { render :show, status: :created, location: child }
       else
         format.html { render :new }
-        format.json { render json: @child.errors, status: :unprocessable_entity }
+        format.json { render json: child.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -46,12 +48,12 @@ class ChildrenController < ApplicationController
   # PATCH/PUT /children/1.json
   def update
     respond_to do |format|
-      if @child.update(child_params)
-        format.html { redirect_to @child, notice: "Child was successfully updated." }
-        format.json { render :show, status: :ok, location: @child }
+      if child.update(child_params)
+        format.html { redirect_to child, notice: "Child was successfully updated." }
+        format.json { render :show, status: :ok, location: child }
       else
         format.html { render :edit }
-        format.json { render json: @child.errors, status: :unprocessable_entity }
+        format.json { render json: child.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -59,7 +61,7 @@ class ChildrenController < ApplicationController
   # DELETE /children/1
   # DELETE /children/1.json
   def destroy
-    @child.destroy
+    child.destroy
     respond_to do |format|
       format.html { redirect_to children_url, notice: "Child was successfully destroyed." }
       format.json { head :no_content }
@@ -69,12 +71,28 @@ class ChildrenController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_child
-    @child = Child.find(params[:id])
+  def child
+    @child ||= current_partner.children.find(params[:id])
+  end
+
+  def family
+    @family ||= current_partner.families.find_by(id: params[:family_id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def child_params
-    params.require(:child).permit(:first_name, :last_name, :date_of_birth, :gender, :child_lives_with, :race, :agency_child_id, :health_insurance, :item_needed_diaperid, :comments, :family_id, :active)
+    params.require(:child).permit(
+      :active,
+      :agency_child_id,
+      :child_lives_with,
+      :comments,
+      :date_of_birth,
+      :first_name,
+      :gender,
+      :health_insurance,
+      :item_needed_diaperid,
+      :last_name,
+      :race
+    )
   end
 end
