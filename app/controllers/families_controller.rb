acae34bacf4d1a269ea5a1f1f25a23f673c1954d
Marius Pop <marius@mlpinit.com>
2019-06-01 21:38:56 -0500

    Scope Family actions to current partner.

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,11 +1,13 @@
 class FamiliesController < ApplicationController
   before_action :authenticate_partner!
-  before_action :set_family, only: [:show, :edit, :update, :destroy]
+
+  helper_method :family, :families
+  attr_reader :families
 
   # GET /families
   # GET /families.json
   def index
-    @families = Family.all
+    @families = current_partner.families
   end
 
   # GET /families/1
@@ -14,7 +16,7 @@ class FamiliesController < ApplicationController
 
   # GET /families/new
   def new
-    @family = Family.new
+    @family = current_partner.families.new
   end
 
   # GET /families/1/edit
@@ -40,12 +42,12 @@ class FamiliesController < ApplicationController
   # PATCH/PUT /families/1.json
   def update
     respond_to do |format|
-      if @family.update(family_params)
-        format.html { redirect_to @family, notice: "Family was successfully updated." }
-        format.json { render :show, status: :ok, location: @family }
+      if family.update(family_params)
+        format.html { redirect_to family, notice: "Family was successfully updated." }
+        format.json { render :show, status: :ok, location: family }
       else
         format.html { render :edit }
-        format.json { render json: @family.errors, status: :unprocessable_entity }
+        format.json { render json: family.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +55,7 @@ class FamiliesController < ApplicationController
   # DELETE /families/1
   # DELETE /families/1.json
   def destroy
-    @family.destroy
+    family.destroy
     respond_to do |format|
       format.html { redirect_to families_url, notice: "Family was successfully destroyed." }
       format.json { head :no_content }
@@ -63,12 +65,28 @@ class FamiliesController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_family
-    @family = Family.find(params[:id])
+  def family
+    @family ||= current_partner.families.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def family_params
-    params.require(:family).permit(:guardian_first_name, :guardian_last_name, :guardian_zip_code, :guardian_country, :guardian_phone, :agency_guardian_id, :home_adult_count, :home_child_count, :home_young_child_count, :sources_of_income, :guardian_employed, :guardian_employment_type, :guardian_monthly_pay, :guardian_health_insurance, :comments)
+    params.require(:family).permit(
+      :agency_guardian_id,
+      :comments,
+      :guardian_country,
+      :guardian_employed,
+      :guardian_employment_type,
+      :guardian_first_name,
+      :guardian_health_insurance,
+      :guardian_last_name,
+      :guardian_monthly_pay,
+      :guardian_phone,
+      :guardian_zip_code,
+      :home_adult_count,
+      :home_child_count,
+      :home_young_child_count,
+      :sources_of_income
+    )
   end
 end
