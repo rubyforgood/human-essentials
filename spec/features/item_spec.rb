da93bd0d6b44e26e4b446d13af70c1b57d1b15e6
Valerie Woolard <valerie.woolard@gmail.com>
2019-06-04 16:44:58 -0400

    WIP spec not working

--- spec/features/item_spec.rb
+++ spec/features/item_spec.rb
@@ -28,6 +28,25 @@ RSpec.feature "Item management", type: :feature do
     expect(page.find(".alert")).to have_content "updated"
   end
 
+  scenario "User leaves the distribution quantity field empty" do
+    item = create(:item)
+    visit url_prefix + "/items/#{item.id}/edit"
+    click_button "Save"
+    expect(item.reload.distribution_quantity).to be_nil
+  end
+
+  scenario "User sets a distribution quantity and package size" do
+    item = create(:item)
+    visit url_prefix + "/items/#{item.id}/edit"
+    fill_in "Distribution quantity", with: 75
+    fill_in "Package size", with: "50"
+    click_button "Save"
+
+    find("[data-item-id=#{item.id}] [href$=edit]").click
+    expect(page).to have_content("75")
+    expect(page).to have_content("50")
+  end
+
   scenario "User updates an existing item with empty attributes" do
     item = create(:item)
     visit url_prefix + "/items/#{item.id}/edit"
