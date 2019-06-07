080e9b0c45c4090a95c16fcbbf27e2a874c66e1a
GabrielRMuller <gabrielmuller97@gmail.com>
2019-06-07 15:20:21 -0300

    add tests for account system

--- spec/system/account_system_spec.rb
+++ spec/system/account_system_spec.rb
@@ -6,12 +6,30 @@ RSpec.describe "User account management", type: :system, js: true do
     visit subject
   end
 
-  it "should change a user name" do
+  it "should change an user name" do
     name = @user.name + "aaa"
     fill_in "Name", with: name
     fill_in "user_current_password", with: @user.password
     click_button "Save"
 
     expect(page).to have_content(name)
+  end
+
+  it "should change the email" do
+    email = "example@example.com"
+    fill_in "Email", with: email
+    fill_in "user_current_password", with: @user.password
+    click_button "Save"
+
+    expect(page).to have_content('Your account has been updated successfully.')
+  end
+
+  it "should fail when the email is invalid" do
+    invalid_email = "invalid email"
+    fill_in "Email", with: invalid_email
+    fill_in "user_current_password", with: @user.password
+    click_button "Save"
+
+    expect(page).to have_content('is invalid')
   end
 end
