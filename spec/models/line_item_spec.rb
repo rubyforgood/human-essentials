9eafeadea9713e2550da6bde8cf5a3dd98503320
Sean Marcia <smarcia@gmail.com>
2019-06-13 12:51:38 -0400

    Remove decimal from package count when it is .0

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe LineItem, type: :model do
     it "is equal to the quanity divided by the package_size" do
       item = create(:item, package_size: 10)
       line_item = create(:line_item, :purchase, quantity: 100, item_id: item.id)
-      expect(line_item.package_count).to eq(10)
+      expect(line_item.package_count).to eq("10")
     end
 
     it "is nil if there is no package_size" do
