159b6c8f78918a0e8a84aed25e96fd88f4001eb7
Eduardo <jeduardo824@gmail.com>
2019-06-14 16:14:55 -0300

    add specs to check if a user can update his email

--- spec/system/account_system_spec.rb
+++ spec/system/account_system_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe "User account management", type: :system, js: true do
 
     expect(page).to have_content(name)
   end
+
+  it "should change a user email" do
+    email = "test@diaper.com"
+    fill_in "Email", with: email
+    fill_in "user_current_password", with: @user.password
+    click_button "Save"
+    visit "/users/edit"
+
+    expect(page).to have_field("user[email]", :with => email)
+  end
 end
