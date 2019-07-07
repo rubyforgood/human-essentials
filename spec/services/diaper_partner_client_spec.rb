c9be49e5ef8707af0ddc9f31e29251bd3e1d089a
maxbeizer <max.beizer@gmail.com>
2019-07-07 08:27:15 -0500

    Refactor out request stub helper method

--- spec/services/diaper_partner_client_spec.rb
+++ spec/services/diaper_partner_client_spec.rb
@@ -18,19 +18,7 @@ RSpec.describe DiaperPartnerClient, type: :service do
           email: attributes["email"]
         }
       }.to_json
-      stub_request(:post, 'https://partner-register.com/')
-        .with(
-          body: expected_body,
-          headers: {
-            'Accept' => '*/*',
-            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'Content-Type' => 'application/json',
-            'Host' => 'partner-register.com',
-            'User-Agent' => 'Ruby',
-            'X-Api-Key' => 'partner-key'
-          }
-        )
-        .to_return(status: 200, body: 'success', headers: {})
+      stub_partner_request(:post, 'https://partner-register.com/', body: expected_body)
       result = DiaperPartnerClient.post(attributes)
       expect(result).to eq 'success'
     end
@@ -38,18 +26,7 @@ RSpec.describe DiaperPartnerClient, type: :service do
 
   describe '::get' do
     it 'performs a GET request' do
-      stub_request(:get, "https://partner-register.com/123")
-        .with(
-          headers: {
-            'Accept' => '*/*',
-            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'Content-Type' => 'application/json',
-            'Host' => 'partner-register.com',
-            'User-Agent' => 'Ruby',
-            'X-Api-Key' => 'partner-key'
-          }
-        )
-        .to_return(status: 200, body: 'success', headers: {})
+      stub_partner_request(:get, 'https://partner-register.com/123')
       result = DiaperPartnerClient.get(id: 123)
       expect(result).to eq 'success'
     end
@@ -64,21 +41,26 @@ RSpec.describe DiaperPartnerClient, type: :service do
           status: attributes[:status]
         }
       }.to_json
-      stub_request(:put, 'https://partner-register.com/123')
-        .with(
-          body: expected_body,
-          headers: {
-            'Accept' => '*/*',
-            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'Content-Type' => 'application/json',
-            'Host' => 'partner-register.com',
-            'User-Agent' => 'Ruby',
-            'X-Api-Key' => 'partner-key'
-          }
-        )
-        .to_return(status: 200, body: 'success', headers: {})
+      stub_partner_request(:put, 'https://partner-register.com/123', body: expected_body)
       result = DiaperPartnerClient.put(attributes)
       expect(result.body).to eq 'success'
     end
+  end
+
+  private
+
+  def stub_partner_request(method, url, request = {}, response = {})
+    stub_request(method, url)
+      .with({
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type' => 'application/json',
+          'Host' => 'partner-register.com',
+          'User-Agent' => 'Ruby',
+          'X-Api-Key' => 'partner-key'
+        }
+      }.merge(request))
+      .to_return({ status: 200, body: 'success', headers: {} }.merge(response))
   end
 end
