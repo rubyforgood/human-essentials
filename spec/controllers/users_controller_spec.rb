7ada359b7b59a8f32a1b3b9ca74355d2ac117105
maxbeizer <max.beizer@gmail.com>
2019-07-07 13:33:06 -0500

    Add spec for UsersController#index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe UsersController, type: :controller do
     sign_in(@user)
   end
 
+  describe "GET #index" do
+    it "returns http success" do
+      get :index, params: default_params
+      expect(response).to be_successful
+    end
+  end
+
   describe "GET #new" do
     it "returns http success" do
       get :new, params: default_params
