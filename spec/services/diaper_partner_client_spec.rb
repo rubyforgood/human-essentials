0cd0b3a8f00618a272fb205f2567031b814b4a28
maxbeizer <max.beizer@gmail.com>
2019-07-07 08:12:55 -0500

    Add spec for ::post

--- spec/services/diaper_partner_client_spec.rb
+++ spec/services/diaper_partner_client_spec.rb
@@ -2,10 +2,42 @@ require 'spec_helper'
 require_relative '../support/env_helper'
 
 RSpec.describe DiaperPartnerClient, type: :service do
+  before do
+    stub_env('PARTNER_REGISTER_URL', 'https://partner-register.com')
+    stub_env('PARTNER_KEY', 'partner-key')
+  end
+
+  describe '::post' do
+    it 'performs a POST request' do
+      attributes = { 'id' => 123, 'organization_id' => 456, 'email' => 'foo@bar.com' }
+      expected_body = {
+        partner:
+        {
+          diaper_bank_id: attributes["organization_id"],
+          diaper_partner_id: attributes["id"],
+          email: attributes["email"]
+        }
+      }.to_json
+      stub_request(:post, "https://partner-register.com/")
+        .with(
+          body: expected_body,
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type' => 'application/json',
+            'Host' => 'partner-register.com',
+            'User-Agent' => 'Ruby',
+            'X-Api-Key' => 'partner-key'
+          }
+        )
+        .to_return(status: 200, body: 'success', headers: {})
+      result = DiaperPartnerClient.post(attributes)
+      expect(result).to eq 'success'
+    end
+  end
+
   describe '::get' do
     it 'performs a GET request' do
-      stub_env('PARTNER_REGISTER_URL', 'https://partner-register.com')
-      stub_env('PARTNER_KEY', 'partner-key')
       stub_request(:get, "https://partner-register.com/123")
         .with(
           headers: {
@@ -17,9 +49,9 @@ RSpec.describe DiaperPartnerClient, type: :service do
             'X-Api-Key' => 'partner-key'
           }
         )
-        .to_return(status: 200, body: "success", headers: {})
+        .to_return(status: 200, body: 'success', headers: {})
       result = DiaperPartnerClient.get(id: 123)
-      expect(result).to eq "success"
+      expect(result).to eq 'success'
     end
   end
 end
