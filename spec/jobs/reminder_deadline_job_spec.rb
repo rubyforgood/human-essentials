0070bd89912747e9da6b528ccb35e55f03a739e0
Max Beizer <mbeizer@heroku.com>
2019-07-08 22:01:52 -0500

    Add specs for ReminderDeadlineJob (#1084)

--- spec/jobs/reminder_deadline_job_spec.rb
+++ spec/jobs/reminder_deadline_job_spec.rb
@@ -3,16 +3,29 @@ require 'spec_helper'
 RSpec.describe ReminderDeadlineJob, type: :job do
   describe '#perform' do
     let(:organization) { create :organization, reminder_day: Date.current.day, deadline_day: Date.current.day + 10 }
-    let(:message_delivery) { instance_double(ActionMailer::MessageDelivery) }
 
     before do
       travel_to Date.new(2019, 6, 10)
-      allow(ReminderDeadlineMailer).to receive(:notify_deadline).and_return(message_delivery)
-      allow(message_delivery).to receive(:deliver_now)
     end
 
     after do
       travel_back
+    end
+
+    it 'adds job to the queue' do
+      Sidekiq::Testing.fake! do
+        expect do
+          ReminderDeadlineJob.perform_async
+        end.to change(ReminderDeadlineJob.jobs, :size).by(1)
+      end
+    end
+
+    it 'sends an email' do
+      Sidekiq::Testing.inline! do
+        expect do
+          ReminderDeadlineJob.perform_async
+        end .to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
     end
 
     context 'partner with send reminders active' do
