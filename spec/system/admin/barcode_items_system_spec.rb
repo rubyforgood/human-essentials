fac978ae1da85275f44447fd0800fe16275aa966
duaimei <adetwiler@optoro.com>
2019-07-26 14:03:47 -0400

    comme

--- spec/system/admin/barcode_items_system_spec.rb
+++ spec/system/admin/barcode_items_system_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe "Barcode Items Admin", type: :system, js: true do
     let(:item) { create(:item) }
 
     it "should create a new global barcode" do
+      barcode_value = BarcodeItem.count > 0 ? Barcode.last.value + 1 : 66666
       click_on "Add New Barcode"
       fill_in "Quantity", with: 100
       select item.base_item.name, from: "barcode_item_barcodeable_id"
-      fill_in "Barcode", with: BarcodeItem.count > 0 ? Barcode.last.value + 1 : 66666
-      binding.pry
+      fill_in "Barcode", with: barcode_value
       click_on "Save"
-      expect(page).to have_content("6667")
+      expect(page).to have_content(barcode_value)
       expect(page).to have_content("Tampons")
       expect(page).to have_content("100")
     end
