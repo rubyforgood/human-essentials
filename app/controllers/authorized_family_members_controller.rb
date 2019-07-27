b5863cfda48ceadeafeb70a310a9a8608ff70d90
Gregory Dombchik <gbdjobs1@gmail.com>
2019-07-27 15:02:47 -0400

    Remove all json endpoints that are not in the api

--- app/controllers/authorized_family_members_controller.rb
+++ app/controllers/authorized_family_members_controller.rb
@@ -9,27 +9,18 @@ class AuthorizedFamilyMembersController < ApplicationController
 
   def create
     member = family.authorized_family_members.new(authorized_family_member_params)
-
-    respond_to do |format|
-      if member.save
-        format.html { redirect_to member, notice: "Authorized member was successfully created." }
-        format.json { render :root }
-      else
-        format.html { render :new }
-        format.json { render json: member.errors, status: :unprocessable_entity }
-      end
+    if member.save
+      redirect_to member, notice: "Authorized member was successfully created."
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if authorized_family_member.update(authorized_family_member_params)
-        format.html { redirect_to authorized_family_member, notice: "Authorized family member was successfully updated." }
-        format.json { render :show, status: :ok, location: authorized_family_member }
-      else
-        format.html { render :edit }
-        format.json { render json: authorized_family_member.errors, status: :unprocessable_entity }
-      end
+    if authorized_family_member.update(authorized_family_member_params)
+      redirect_to authorized_family_member, notice: "Authorized family member was successfully updated."
+    else
+      render :edit
     end
   end
 
