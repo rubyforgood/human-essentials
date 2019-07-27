80cffc8e01ccd306dc021ff656cdb30b424d5c65
Lee Sharma <lee@leesharma.com>
2019-07-27 10:00:57 -0400

    Validate and confirm item quantity for large donations (#1118)
    * Add model validations for int range
    
    Towards #1090
    
    Major changes:
        - Add tests for invalid values for 4 byte integers (e.g. > 2**31
          or < -2**31-1)
        - Add validations to associated models
    
    I changed the `other_than: 0` numericality validation in line_item to an
    `excludes` validation so that I could include the int range requirements
    without a custom method.
    
    I used boundaries near the actual limits (+/- 2**31) as named constants;
    hopefully this balances readability and accuracy.
    
    * Remove empty asset files
    
    * Add a popup alert to unusually large item donations
    
    * Replace feature spec with system spec
    
    As a bonus, verify that the action actually saves the donation.
    
    On line 301, the extra `expect(page).not_to have_xpath(...)` forces
    Capybara to wait for the action to complete. Without that line, the
    donation change expectation runs (and fails) before the "create" action
    completes.

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -11,13 +11,15 @@
 #
 
 class InventoryItem < ApplicationRecord
+  MAX_INT = 2**31
+
   belongs_to :storage_location
   belongs_to :item
 
   validates :quantity, presence: true
   validates :storage_location_id, presence: true
   validates :item_id, presence: true
-  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: MAX_INT }
 
   scope :by_partner_key, ->(partner_key) { joins(:item).merge(Item.by_partner_key(partner_key)) }
 
