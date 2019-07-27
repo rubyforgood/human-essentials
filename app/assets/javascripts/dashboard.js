8040adc10ca702a55b3272a73a4dccef7b4e15fc
Russell Osborne <russell.osborne@framebridge.com>
2019-07-27 16:39:58 -0400

    Removes implict returns from dashboard.js

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,14 +1,9 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(() =>
   $("#filters.submit-on-change select#filters_interval").on("change", function(e) {
-    return this.form.submit();
+    this.form.submit();
   })
 );
