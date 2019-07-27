3ddf0cc7f0d37795bdf22223729d5c1bf21154e5
Russell Osborne <russell.osborne@framebridge.com>
2019-07-27 16:39:31 -0400

    Removes implicit returns from the adjustments files

--- app/assets/javascripts/adjustments.js
+++ app/assets/javascripts/adjustments.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
@@ -19,26 +14,26 @@ $(function() {
 
   $(document).on("change", control_id, function(evt) {
     const control = $(evt.target);
-    return $.ajax({
+    $.ajax({
       url: control.data("storage-location-inventory-path").replace(":id", control.val()),
       dataType: "json",
       success(data) {
         const options = $.map(data, item_option);
-        return $("#adjustment_storage_location_line_items select").html(options);
+        $("#adjustment_storage_location_line_items select").html(options);
       }
     });
   });
 
-  return $(document).on("cocoon:after-insert", "form#new_adjustment", function(e, insertedItem) {
+  $(document).on("cocoon:after-insert", "form#new_adjustment", function(e, insertedItem) {
     const control = $(control_id);
     insertedItem.find('#_barcode-lookup-new_line_items').attr('id', `_barcode-lookup-${$('.nested-fields').size() - 1}`);
-    return $.ajax({
+    $.ajax({
       url: control.data("storage-location-inventory-path").replace(":id", control.val()),
       dataType: "json",
       success(data) {
         const options = $.map(data, item_option);
         console.log("inserted item", insertedItem);
-        return $("select", insertedItem).html(options);
+        $("select", insertedItem).html(options);
       }
     });
   });
