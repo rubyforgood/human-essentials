6a8ef89ff7dc6a432b83e53ff0f77c93b24da960
Sean Marcia <smarcia@gmail.com>
2019-08-17 19:41:12 -0400

    update the sidekiq initializer to grab the scheldule.yml based off the rails root and put the reminder job behind a feature flipper.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,5 +2,5 @@ require 'sidekiq'
 require 'sidekiq-scheduler'
 
 Sidekiq::Scheduler.enabled = true
-Sidekiq.schedule = YAML.load_file(File.expand_path('../../scheduler.yml', __FILE__))
+Sidekiq.schedule = YAML.load_file(File.expand_path(Rails.root+'config/scheduler.yml', __FILE__))
 SidekiqScheduler::Scheduler.load_schedule!
