92b5f6214938f221af2c7fdefaa49154c6f5a416
Michael Dworken <michael.dworken@framebridge.com>
2019-08-19 16:10:47 -0400

    Adds reasonable default for when PARTNER_BASE_URL is not set in ENV

--- app/helpers/partners_helper.rb
+++ app/helpers/partners_helper.rb
@@ -2,7 +2,7 @@
 module PartnersHelper
   def documentation_url(path)
     # NOTE(chaserx): add to .env with http://localhost:3001 or https://partnerbase.org
-    uri = URI(ENV['PARTNER_BASE_URL'])
+    uri = URI(ENV['PARTNER_BASE_URL'] || partners.test)
     uri.path = path
     uri.to_s
   end
