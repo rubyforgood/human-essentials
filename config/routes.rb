432f93ac804f7e5f95c8569fc8a213a20ed458e5
Keith Bennett <keithrbennett@gmail.com>
2019-09-28 20:08:58 +0700

    Add 4 spaces for line continuation, see if Rubocop cooperates.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ def set_up_sidekiq
   if Rails.env.production?
     Sidekiq::Web.use Rack::Auth::Basic do |username, password|
       ActiveSupport::SecurityUtils.secure_compare(username, ENV["SIDEKIQ_USERNAME"]) \
-      && \
-      ActiveSupport::SecurityUtils.secure_compare(password, ENV["SIDEKIQ_PASSWORD"])
+          && \
+          ActiveSupport::SecurityUtils.secure_compare(password, ENV["SIDEKIQ_PASSWORD"])
     end
   end
 
