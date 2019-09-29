da19db9c95fce512a6abcf91f3b03805c399a73b
GabrielRMuller <gabrielmuller97@gmail.com>
2019-09-29 12:25:03 -0300

    add state to distributions using enum (#1213)
    * add state to distributions using enum
    
    * adjustments to state on distributions

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -12,7 +12,7 @@ require 'time_util'
 #  organization_id     :integer
 #  issued_at           :datetime
 #  agency_rep          :string
-#
+#  state               :integer
 
 class Distribution < ApplicationRecord
   # Distributions are issued from a single storage location, so we associate
@@ -35,6 +35,8 @@ class Distribution < ApplicationRecord
   include IssuedAt
 
   before_save :combine_distribution
+
+  enum state: { started: 0, scheduled: 1, complete: 10 }
 
   include Filterable
   # add item_id scope to allow filtering distributions by item
