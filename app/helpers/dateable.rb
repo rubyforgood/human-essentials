2a99938767ece5ddd4c839b546abac6f34a56bf0
edumoreira1506 <contato@eduardoem.com.br>
2019-10-02 16:13:09 -0300

    Changed function

--- app/helpers/dateable.rb
+++ app/helpers/dateable.rb
@@ -6,6 +6,18 @@ module Dateable
   end
 
   def date_range
+    start_date = date_params[:date_from]&.to_date || "Jan, 2, 1970".to_date
+    end_date = date_params[:date_to]&.to_date || "Jan, 1, 2037".to_date
+
+    # Rails does a time-sensitive comparison, and the date is treated as 12:00 am that day
+    # this means that timestamps for that day itself would be counted out
+    # calling end_of_day present this from happening
+    end_date.end_of_day
+
+    start_date..end_date
+  end
+  
+  def date_range_picker
     start_date = date_params[:date_from]&.split('/')
     end_date = date_params[:date_to]&.split('/')
 
