b46aefa670f90dac29e2e3e0b8c219f54a10d740
edumoreira1506 <contato@eduardoem.com.br>
2019-10-03 09:50:32 -0300

    Fix bug on helper

--- app/helpers/dateable.rb
+++ app/helpers/dateable.rb
@@ -21,8 +21,8 @@ module Dateable
     start_date = date_params[:date_from]&.split('/')
     end_date = date_params[:date_to]&.split('/')
 
-    start_date = start_date ? "#{start_date[1]}/#{start_date[0]}/#{start_date[2]}"&.to_date : "Jan, 2, 1970".to_date
-    end_date = end_date ? "#{end_date[1]}/#{end_date[0]}/#{end_date[2]}"&.to_date : "Jan, 1, 2037".to_date
+    start_date = !!start_date ? "#{start_date[1]}/#{start_date[0]}/#{start_date[2]}"&.to_date : "Jan, 2, 1970".to_date
+    end_date = !!end_date ? "#{end_date[1]}/#{end_date[0]}/#{end_date[2]}"&.to_date : "Jan, 1, 2037".to_date
 
     # Rails does a time-sensitive comparison, and the date is treated as 12:00 am that day
     # this means that timestamps for that day itself would be counted out
