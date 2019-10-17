457accc3677d13706401feaf3eb1f5570bb61f31
Juarez Lustosa <juarez.lustosa@gmail.com>
2019-10-17 16:04:03 -0300

    Add visit to avoid intermittent fail2

--- spec/system/admin/barcode_items_system_spec.rb
+++ spec/system/admin/barcode_items_system_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe "Barcode Items Admin", type: :system, js: true do
 
     it "should edit an existing global barcode"
     it "should delete a global barcode" do
+      visit admin_barcode_items_path
       page.refresh
       expect(page).to have_content barcode_item.base_item.name
       expect(
