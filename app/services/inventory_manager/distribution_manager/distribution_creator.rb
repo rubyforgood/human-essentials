6eaa6c12bd6e91ee39f1ba92e2192d0b42438e35
michael <mdworken@gmail.com>
2019-10-20 13:57:06 -0400

    Implements initial service object for inventory methods

---
+++ app/services/inventory_manager/distribution_manager/distribution_creator.rb
@@ -0,0 +1,20 @@
+module InventoryManager
+  module DistributionManager
+    class DistributionCreator
+      def initialize(distribution)
+        @distribution = distribution
+      end
+
+      def call
+        @distribution.transaction do
+          @distribution.save
+          @distribution.storage_location.decrease_inventory @distribution
+          OpenStruct.new(success?: true)
+        end
+      rescue StandardError => e
+        Rails.logger.error "[!] DistributionsController#create failed to save distribution for #{@distribution.organization.short_name}: #{@distribution.errors.full_messages} [#{e.inspect}]"
+        OpenStruct.new(success: false, error: e)
+      end
+    end
+  end
+end
