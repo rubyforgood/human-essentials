f3d6261ef0ab3ebb74adee18167074865acaa67e
duaimei <adetwiler@optoro.com>
2019-10-20 23:00:38 -0400

    1311 rubocop

--- app/models/diaper_drive.rb
+++ app/models/diaper_drive.rb
@@ -11,9 +11,9 @@
 #
 
 class DiaperDrive < ApplicationRecord
-  has_many :donations
+  has_many :donations, dependent: :nullify
   validates :name, presence:
     { message: "A name must be chosen." }
-  validates :start_date, presence: 
-    { message: "Please enter a start date."}
+  validates :start_date, presence:
+    { message: "Please enter a start date." }
 end
