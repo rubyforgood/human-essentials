ebdea76a8dc9b5716b4c840b111470d8abdbe03c
mdworken <31595784+mdworken@users.noreply.github.com>
2019-10-24 17:05:10 -0400

    Fixes translation issue (#1336)

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -95,7 +95,7 @@ class DistributionsController < ApplicationController
       @storage_locations = current_organization.storage_locations.alphabetized
     else
       flash[:error] = 'To edit a distribution,
-      you must be an organization admin or the current date must be major then today.'
+      you must be an organization admin or the current date must be later than today.'
       redirect_to distributions_path
     end
   end
