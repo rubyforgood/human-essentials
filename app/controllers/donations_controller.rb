556b9840d3304ef04c237aca03a0dd05eaa31805
Joseph Hunt <josephb.hunt@gmail.com>
2019-10-25 15:22:15 -0400

    Added money raised column and total to donations page.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -19,6 +19,7 @@ class DonationsController < ApplicationController
     @donations_quantity = @donations.collect(&:total_quantity).sum
     @paginated_donations_quantity = @paginated_donations.collect(&:total_quantity).sum
     @total_value_all_donations = total_value(@donations)
+    @total_money_raised = @donations.inject(0){|sum, d| sum + d.money_raised.to_i}
     @storage_locations = @donations.collect(&:storage_location).compact.uniq.sort
     @selected_storage_location = filter_params[:at_storage_location]
     @sources = @donations.collect(&:source).uniq.sort
