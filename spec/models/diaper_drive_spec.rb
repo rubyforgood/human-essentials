c7f56e1fe1a8022963a2ff23366977c0cfb55a03
duaimei <detwiler.amy@gmail.com>
2019-10-27 19:56:51 -0400

    1320 got rid of some commented code

--- spec/models/diaper_drive_spec.rb
+++ spec/models/diaper_drive_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe DiaperDrive, type: :model do
   let!(:diaper_drive) { create(:diaper_drive) }
   let!(:donation) { create(:donation, :with_items, item_quantity: 7, diaper_drive: diaper_drive) }
   let!(:donation_2) { create(:donation, :with_items, item_quantity: 9, diaper_drive: diaper_drive) }
-  let!(:extra_line_item) { create(:line_item, itemizable: donation, quantity: 4)}
-  
+  let!(:extra_line_item) { create(:line_item, itemizable: donation, quantity: 4) }
+
   it "calculates donation quantity" do
     expect(diaper_drive.donation_quantity).to eq 20
   end
 
   it "calculates in-kind value" do
-  	expect(diaper_drive.in_kind_value).to be_a Integer
+    expect(diaper_drive.in_kind_value).to be_a Integer
   end
 end
