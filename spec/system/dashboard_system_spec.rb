27f3d19d7b2124b62c362215506cefbaa2191f0a
Ben Klang <bklang@wirehack.net>
2019-11-02 17:25:53 -0400

    Re-activate pended specs now that time is synced

--- spec/system/dashboard_system_spec.rb
+++ spec/system/dashboard_system_spec.rb
@@ -149,7 +149,6 @@ RSpec.describe "Dashboard", type: :system, js: true do
 
         context "when constrained to date range" do
           before do
-            skip "FIXME: These are currently failing but they work when done manually. Marking pending so we can get this feature out at NBDN"
             @organization.donations.destroy_all
 
             @this_years_donations = {
@@ -323,7 +322,6 @@ RSpec.describe "Dashboard", type: :system, js: true do
 
         context "when constrained to date range" do
           before do
-            skip "FIXME: These are currently failing but they work when done manually. Marking pending so we can get this feature out at NBDN"
             @organization.purchases.destroy_all
             storage_location = create(:storage_location, :with_items, item_quantity: 0, organization: @organization)
             @this_years_purchases = {
@@ -478,7 +476,6 @@ RSpec.describe "Dashboard", type: :system, js: true do
 
         context "when constrained to date range" do
           before do
-            skip "FIXME: These are currently failing but they work when done manually. Marking pending so we can get this feature out at NBDN"
             @organization.donations.destroy_all
             storage_location = create(:storage_location, :with_items, item_quantity: 0, organization: @organization)
             diaper_drive1 = create(:diaper_drive_participant, business_name: "First Diaper Drive", organization: @organization)
@@ -683,7 +680,6 @@ RSpec.describe "Dashboard", type: :system, js: true do
 
         context "when constrained to date range" do
           before do
-            skip "FIXME: These are currently failing but they work when done manually. Marking pending so we can get this feature out at NBDN"
             @organization.donations.destroy_all
             storage_location = create(:storage_location, :with_items, item_quantity: 0, organization: @organization)
             manufacturer1 = create(:manufacturer, name: "ABC Corp", organization: @organization)
@@ -901,9 +897,6 @@ RSpec.describe "Dashboard", type: :system, js: true do
         end
 
         context "When Date Filtering >" do
-          before do
-            skip "FIXME: These are currently failing but they work when done manually. Marking pending so we can get this feature out at NBDN"
-          end
 
           context "with year-to-date selected" do
             before do
