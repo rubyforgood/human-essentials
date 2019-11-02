cb77e5ca1869aa47a098fcb7963a5bc054c7f0d7
Ben Klang <bklang@wirehack.net>
2019-11-02 17:25:14 -0400

    Use Sinon to have Capybara browser sync to Timecop

---
+++ app/javascript/packs/sinon.js
@@ -0,0 +1,3 @@
+  // Sinon is used to fake the browser's time when used with Timecop
+  import sinon from "sinon/pkg/sinon"
+  window.sinon = sinon
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -2,4 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+// Workaround Sinon+Webpack issues as documented at https://gist.github.com/rrgayhart/cf5dcefdf3975598f491
+const sinonLoader = {
+  test: /sinon\.js$/, loader: "imports-loader?define=>false,require=>false"
+}
+
+environment.loaders.append('sinon', sinonLoader)
+
 module.exports = environment.toWebpackConfig()
