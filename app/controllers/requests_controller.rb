f831a97bbb485c110fb9a22d44f395ce39406991
Jill Tankersley <16630021+that-jill@users.noreply.github.com>
2019-11-14 21:01:11 -0500

    Requests controller refactor (#1372)
    * Clarify what should be public-facing
    
    These methods are never designed to be used outside the controller, and their implementation should be a private concern.
    
    * Refactor into poro to make intent clearer
    
    Moving this from a struct to a poro creates a very well-defined name, and we could use this in the future to add validations around the business logic for request items. Also this addresses a warning that would appear every time the method is called (in tests, as well as every time the controller action was hit).
    
    * Define expected behavior

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -14,7 +14,7 @@ class RequestsController < ApplicationController
 
   def show
     @request = Request.find(params[:id])
-    @request_items = get_items(@request.request_items)
+    @request_items = load_items
   end
 
   # Clicking the "New Distribution" button will set the the request to started
@@ -27,22 +27,6 @@ class RequestsController < ApplicationController
     redirect_to new_distribution_path(request_id: request.id)
   end
 
-  # TODO: This should probably be made private
-  def get_items(request_items)
-    # using Struct vs Hash so we can use dot notation in the view
-    return unless request_items
-
-    Struct.new('Item', :name, :quantity, :on_hand)
-    request_items.map do |key|
-      Struct::Item.new(Item.find(key["item_id"]).name, key["quantity"], sum_inventory(Item.find(key["item_id"])))
-    end
-  end
-
-  # TODO: This should probably be made private
-  def sum_inventory(key)
-    current_organization.inventory_items.where(item_id: key).sum(:quantity)
-  end
-
   # TODO: kick off job to send email
   def destroy
     ActiveRecord::Base.transaction do
@@ -50,5 +34,13 @@ class RequestsController < ApplicationController
     end
     flash[:notice] = "Request #{params[:id]} has been removed!"
     redirect_to requests_path
+  end
+
+  private
+
+  def load_items
+    return unless @request.request_items
+
+    @request.request_items.map { |json| RequestItem.from_json(json, current_organization) }
   end
 end
