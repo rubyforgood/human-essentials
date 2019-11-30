3e29704872de7322071e2912b31583a707f2e4ec
Joe Hunt <josephb.hunt@gmail.com>
2019-11-30 13:32:59 -0500

    1356 diaper drive filters (#1364)
    * Added diaper drive seed data.
    
    * Added diaper drive name select and data range picker.
    
    * Added funcationality of name filter and date range filter.
    
    * Added default date range using existing pattern. Added an old seeded diaper drive record.
    
    * Fixed rubocop.
    
    * Added results styles for diaper drive index page.
    
    * Added capybara tests for diaper drive index page.
    
    * Fixed rubocop.
    
    * Remove old link to new diaper drive page.
    
    * Collaps multiple assignments into one line.

--- app/models/diaper_drive.rb
+++ app/models/diaper_drive.rb
@@ -11,9 +11,23 @@
 #
 
 class DiaperDrive < ApplicationRecord
+  include Filterable
+
+  scope :by_name, ->(name_filter) { where(name: name_filter) }
+
+  scope :within_date_rage, ->(search_range) {
+    search_dates = search_date_range(search_range)
+    where('end_date >= ? AND start_date <= ?', search_dates[:start_date], search_dates[:end_date])
+  }
+
   has_many :donations, dependent: :nullify
   validates :name, presence:
     { message: "A name must be chosen." }
   validates :start_date, presence:
     { message: "Please enter a start date." }
+
+  def self.search_date_range(dates)
+    dates = dates.split(" - ")
+    @search_date_range = { start_date: dates[0], end_date: dates[1] }
+  end
 end
