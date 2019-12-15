9adbd315e82566d177ae39fb2bcc06e351cd8a5e
Sirius Dely <siriusdely@icloud.com>
2019-12-15 16:24:07 +0700

    improvements based on previous PR #1382 reviews

--- spec/system/purchase_system_spec.rb
+++ spec/system/purchase_system_spec.rb
@@ -34,19 +34,8 @@ RSpec.describe "Purchases", type: :system, js: true do
       purchases = [purchase1, purchase2]
       page.refresh
       expect(page).to have_text("Total")
-      expect(page).to have_text(purchases.collect(&:total_quantity).sum)
-      expect(page).to have_text(item_value(purchases.collect(&:amount_spent_in_cents).sum))
-      expect(page).to have_text(item_value(3579))
-    end
-
-    it "User sees total purchases value" do
-      purchase1 = create(:purchase, amount_spent_in_cents: 1234)
-      purchase2 = create(:purchase, amount_spent_in_cents: 2345)
-      purchases = [purchase1, purchase2]
-      page.refresh
-      expect(page).to have_text("Total")
-      expect(page).to have_text(purchases.collect(&:total_quantity).sum)
-      expect(page).to have_text(item_value(purchases.collect(&:amount_spent_in_cents).sum))
+      expect(page).to have_text(purchases.sum(&:total_quantity))
+      expect(page).to have_text(item_value(purchases.sum(&:amount_spent_in_cents)))
       expect(page).to have_text(item_value(3579))
     end
 
