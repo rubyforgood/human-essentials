c3b1ced07b3fc6b2ce940a8733e26520a7b5829d
Mauricio Pereira <mauriciopdvg@gmail.com>
2019-12-26 18:15:07 -0300

    Add DiaperDriver tests (#1394)

--- Gemfile
+++ Gemfile
@@ -89,10 +89,10 @@ group :test do
   gem "database_cleaner"
   gem "factory_bot_rails"
   gem "launchy"
-  gem 'webdrivers', '~> 3.0'
   gem "rails-controller-testing"
-  gem 'simplecov'
   gem "webmock", "~> 3.5"
+  gem 'simplecov'
+  gem 'webdrivers', '~> 3.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/models/diaper_drive_spec.rb
+++ spec/models/diaper_drive_spec.rb
@@ -13,5 +13,59 @@
 require 'rails_helper'
 
 RSpec.describe DiaperDrive, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { expect(build(:diaper_drive, name: nil)).not_to be_valid }
+    it { expect(build(:diaper_drive, start_date: nil)).not_to be_valid }
+  end
+
+  describe 'associations' do
+    let!(:donation) { create(:donation) }
+    subject { create(:diaper_drive) }
+
+    it 'has_many donations' do
+      subject.donations << donation
+
+      expect(subject.donations).to include(donation)
+    end
+  end
+
+  describe 'scopes' do
+    describe '.by_name' do
+      let!(:diaper_drive1) { create(:diaper_drive, name: 'some_name') }
+      let!(:diaper_drive2) { create(:diaper_drive, name: 'other_name') }
+
+      it 'returns the diaper_drive with name some_name' do
+        expect(described_class.by_name('some_name')).to include(diaper_drive1)
+      end
+
+      it 'does not return other_name' do
+        expect(described_class.by_name('some_name')).not_to include(diaper_drive2)
+      end
+    end
+
+    describe '.within_date_range' do
+      let(:start_date) { '2019-12-17' }
+      let(:end_date) { '2019-12-19' }
+      let(:other_start_date) { '2016-11-1' }
+      let(:other_end_date) { '2017-9-12' }
+      let!(:diaper_drive1) { create(:diaper_drive, start_date: start_date, end_date: end_date) }
+      let!(:diaper_drive2) { create(:diaper_drive, start_date: other_start_date, end_date: other_end_date) }
+
+      it 'retuns the diaper drive 1' do
+        expect(described_class.within_date_range("#{start_date} - #{end_date}")).to include(diaper_drive1)
+      end
+
+      it 'does not include diaper drive 2' do
+        expect(described_class.within_date_range("#{start_date} - #{end_date}")).not_to include(diaper_drive2)
+      end
+    end
+  end
+
+  describe '.search_date_range' do
+    let(:range_date) { '2019-12-17 - 2019-12-19' }
+
+    subject { described_class.search_date_range(range_date) }
+
+    it { is_expected.to eq(start_date: '2019-12-17', end_date: '2019-12-19') }
+  end
 end
