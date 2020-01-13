f626c404e9d6e85b0c4067014a4173a18852b459
Alicia <Alicia.barrett89@gmail.com>
2020-01-12 23:04:11 -0500

    WIP

--- spec/support/pagination_shared_example.rb
+++ spec/support/pagination_shared_example.rb
@@ -1,8 +1,19 @@
 shared_examples_for "pagination" do
     let(:model_f) { described_class.to_s.underscore.to_sym }
 
-    it "works" do
-        create_list(:model_f, 100)
-    end
+    describe "#index" do
+        context "initialized with 100 records" do
 
+            it "says it has 100 records" do
+                multiple_instances_of_object = create_list(:model_f, 100)
+                expect(multiple_instances_of_object.size).to eq(100)
+            end
+
+            it "returns paginated results" do
+            end
+
+            it "allows user to jump to a different page" do
+            end
+        end
+    end
 end
