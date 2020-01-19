86ad35f22bfbf7d6c791f6bc702c480df3a72d5f
Alicia <Alicia.barrett89@gmail.com>
2020-01-19 15:23:37 -0500

    WIP

--- spec/support/pagination_shared_example.rb
+++ spec/support/pagination_shared_example.rb
@@ -5,15 +5,18 @@ shared_examples_for "pagination" do
         context "initialized with 100 records" do
 
             it "says it has 100 records" do
-                multiple_instances_of_object = create_list(:model_f, 100)
+                multiple_instances_of_object = create_list(model_f, 100)
                 expect(multiple_instances_of_object.size).to eq(100)
             end
 
-            it "returns paginated results" do
+            it "should default to #{Kaminari.config.default_per_page} records per page" do
+            puts "==="
+            puts current_path
+            puts "==="
             end
 
-            it "allows user to jump to a different page" do
-            end
+    #         # it "allows user to jump to a different page" do
+    #         # end
         end
     end
 end
