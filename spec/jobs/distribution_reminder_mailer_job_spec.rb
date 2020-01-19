38c43fbe86a3a1286a78d10d8ddd9714a77fa857
mdworken <mdworken@gmail.com>
2020-01-18 22:22:55 -0500

    Adds conditional logic to not send mailers for past distributions and reminder mailers for partners that don't want reminders 1409

--- spec/jobs/distribution_reminder_mailer_job_spec.rb
+++ spec/jobs/distribution_reminder_mailer_job_spec.rb
@@ -21,4 +21,48 @@ RSpec.describe DistributionReminderJob, type: :job do
       end
     end
   end
+
+  describe "conditionally sending the emails" do
+    let(:organization) { create :organization }
+
+    let(:past_distribution) { create(:distribution, issued_at: Time.zone.now - 1.week) }
+    let(:future_distribution) { create(:distribution, issued_at: Time.zone.now + 1.week) }
+
+    let(:partner_with_no_reminders) { create :partner, send_reminders: false }
+    let(:partner_with_reminders) { create :partner, send_reminders: true }
+    let(:distribution_without_reminder) { create(:distribution, partner: partner_with_no_reminders) }
+    let(:distribution_with_reminder) { create(:distribution, partner: partner_with_reminders) }
+
+    it "does not send mail for past distributions" do
+      Sidekiq::Testing.inline! do
+        expect do
+          DistributionReminderJob.perform_async(past_distribution.id)
+        end.to_not change { ActionMailer::Base.deliveries.count }
+      end
+    end
+
+    it "sends mail for future distributions" do
+      Sidekiq::Testing.inline! do
+        expect do
+          DistributionReminderJob.perform_async(future_distribution.id)
+        end.to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
+    end
+
+    it "does not send mail for future distributions if the partner wants no reminders" do
+      Sidekiq::Testing.inline! do
+        expect do
+          DistributionReminderJob.perform_async(distribution_without_reminder.id)
+        end.to_not change { ActionMailer::Base.deliveries.count }
+      end
+    end
+
+    it "sends mail for future distributions where the partner wants reminders" do
+      Sidekiq::Testing.inline! do
+        expect do
+          DistributionReminderJob.perform_async(distribution_with_reminder.id)
+        end.to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
+    end
+  end
 end
