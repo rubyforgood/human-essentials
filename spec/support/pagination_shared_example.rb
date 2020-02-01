b01b46c165d38a8d276bc6db8038b10b45f28ca0
Alicia <Alicia.barrett89@gmail.com>
2020-02-01 10:49:10 -0500

    Added donations to shared example and cleaned up code.

--- spec/support/pagination_shared_example.rb
+++ spec/support/pagination_shared_example.rb
@@ -1,34 +1,28 @@
 shared_examples_for "pagination", type: :feature do
-    let(:model_f) { described_class.to_s.underscore.to_sym }
-    let(:plural_model_name){ described_class.to_s.underscore.pluralize }
-    let!(:url_prefix) { "/#{@organization.to_param}" }
+  let(:model_f) { described_class.to_s.underscore.to_sym }
+  let(:plural_model_name){ described_class.to_s.underscore.pluralize }
+  let!(:url_prefix) { "/#{@organization.to_param}" }
 
-    scenario "User visits page displays with 100 records in it" do
-
-        Kaminari.configure do |config|
-            config.default_per_page = 25
-        end
-
-        sign_in(@user)
-
-        create_list(model_f, 100)
-
-        visit url_prefix + "/" + plural_model_name
-
-        list_body = page.find("tbody")
-        items = list_body.all("tr")
-
-        expect(items.count).to eq 25
-
-        within(".pagination") do
-            expect(page.find("li.active").text).to eq("1")
-        end
-
-        visit url_prefix + "/" + plural_model_name + "?page=" + "3"
-
-        within(".pagination") do
-            expect(page.find("li.active").text).to eq("3")
-        end
-
+  scenario "User visits page displays with 100 records in it" do
+    Kaminari.configure do |config|
+      config.default_per_page = 25
     end
+
+    sign_in(@user)
+
+    create_list(model_f, 100)
+
+    visit url_prefix + "/" + plural_model_name
+
+    within("tbody") do
+        items = page.all("tr")
+        expect(items.count).to eq 25
+    end
+
+    click_link('3')
+
+    within(".pagination") do
+      expect(page.find("li.active").text).to eq("3")
+    end
+  end
 end
