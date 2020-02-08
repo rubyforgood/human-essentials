b86e158ee1670c1559c9b40b4d122a540edd3078
Alicia <Alicia.barrett89@gmail.com>
2020-02-08 12:46:52 -0500

    Fix
    The build kept failing on this code because I set the Kaminari
    config to 25 in the spec and it wasn't resetting to it's default.
    
    In the hopes of getting the build to pass, we'll just rely on the
    default being set in the kaminari config already.

--- spec/support/pagination_shared_example.rb
+++ spec/support/pagination_shared_example.rb
@@ -3,20 +3,17 @@ shared_examples_for "pagination", type: :feature do
   let(:plural_model_name) { described_class.to_s.underscore.pluralize }
   let!(:url_prefix) { "/#{@organization.to_param}" }
 
-  scenario "the user visits page that displays 100 records" do
-    Kaminari.configure do |config|
-      config.default_per_page = 25
-    end
+  scenario "user can visit the paginated results for a given resource" do
 
     sign_in(@user)
 
-    create_list(model_f, 100)
+    create_list(model_f, 200)
 
     visit url_prefix + "/" + plural_model_name
 
     within("tbody") do
       items = page.all("tr")
-      expect(items.count).to eq 25
+      expect(items.count).to eq 50
     end
 
     click_link('3')
