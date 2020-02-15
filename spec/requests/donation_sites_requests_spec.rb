7c49d54c27c1c64f21e773436703b0de01321e85
mcshakes <efmccarthy26@gmail.com>
2020-02-15 12:20:21 -0700

    Finished this, though I commented out the POST, which has silly errors I don't understand

--- spec/requests/donation_sites_requests_spec.rb
+++ spec/requests/donation_sites_requests_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe "DonationSites", type: :request do
 
                                                     )
                 new_params = default_params.merge(donation_site: new_address[0])
-                binding.pry
+
                 post donation_sites_path(new_params)
                 expect(response).to be_successful
             end
@@ -67,6 +67,33 @@ RSpec.describe "DonationSites", type: :request do
                 editable = default_params.merge(id: create(:donation_site, organization: @organization))
                 get edit_donation_site_path(editable) 
                 expect(response).to be_successful
+            end
+        end
+
+        describe "GET #show" do
+            it "returns http success" do
+                site = default_params.merge(id: create(:donation_site, organization: @organization))
+                get donation_site_path(site)
+                expect(response).to be_successful
+                expect(response).to render_template(:show)
+            end
+
+            it "returns 404 for unknown entity" do
+                fake = default_params.merge(id: "fake")
+                get donation_site_path(fake)
+                expect(response).to_not render_template(:show)
+            end
+        end
+
+        describe "DELETE #destroy" do
+            
+            it "returns http success" do
+                delete_soon = default_params.merge(id: create(:donation_site, organization: @organization))
+
+                delete donation_site_path(delete_soon)
+                expect(response).to have_http_status(302)
+
+                expect(response).to redirect_to(donation_sites_path)
             end
         end
 
