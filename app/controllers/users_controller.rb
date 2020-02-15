2a5f18d5438c8af6b8bde96c186354d5f84de076
Lucas Hiago <lucashiago63@gmail.com>
2020-02-15 20:30:35 -0300

    Add new users request specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,4 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
 end
---
+++ spec/requests/users_requests_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Users", type: :request do
+  let(:default_params) do
+    { organization_id: @organization.to_param }
+  end
+
+  before do
+    sign_in(@user)
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get users_path(default_params)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get new_user_path(default_params)
+      expect(response).to be_successful
+    end
+  end
+end
