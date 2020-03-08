ae2bc2141a97b49a1c051dbd69b3af195935e9b8
Lucas Hiago <lucashiago63@gmail.com>
2020-03-08 18:38:24 -0300

    Remove old admin controller spec

--- spec/requests/admin_requests_spec.rb
+++ spec/requests/admin_requests_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe "Admin", type: :request do
     end
 
     it "allows a user to load the dashboard" do
-      get admin_dashboard_path()
+      get admin_dashboard_path
       expect(response).to be_successful
     end
     context "with rendered views" do
       it "shows a logout button" do
-        get admin_dashboard_path()
+        get admin_dashboard_path
         expect(response.body).to match(/log out/im)
       end
     end
@@ -20,7 +20,7 @@ RSpec.describe "Admin", type: :request do
     it "disallows dashboard access, redirecting to the normal dashboard" do
       [@organization_admin, @user].each do |u|
         sign_in(u)
-        get admin_dashboard_path()
+        get admin_dashboard_path
         expect(response).to redirect_to(dashboard_path)
         expect(response).to have_error
       end
