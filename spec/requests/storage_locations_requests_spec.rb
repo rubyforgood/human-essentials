f610d96b4ba07fca0b66ad09eb1abf6f1be45ffd
Lucas Hiago <lucashiago63@gmail.com>
2020-03-07 23:52:21 -0300

    Add new storage_locations request specs

---
+++ spec/requests/storage_locations_requests_spec.rb
@@ -0,0 +1,109 @@
+RSpec.describe "StorageLocations", type: :request do
+  let(:default_params) do
+    { organization_id: @organization.to_param }
+  end
+
+  context "While signed in" do
+    before do
+      sign_in(@user)
+    end
+
+    describe "GET #index" do
+      it "returns http success" do
+        get storage_locations_path(default_params)
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #new" do
+      it "returns http success" do
+        get new_storage_location_path(default_params)
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #edit" do
+      it "returns http success" do
+        get edit_storage_location_path(default_params.merge(id: create(:storage_location, organization: @organization)))
+        expect(response).to be_successful
+      end
+    end
+
+    describe "POST #import_csv" do
+      let(:model_class) { StorageLocation }
+
+      context "with a csv file" do
+        let(:file) { fixture_file_upload("#{model_class.name.underscore.pluralize}.csv", "text/csv") }
+        subject { post import_csv_storage_locations_path(default_params), params: { file: file } }
+
+        it "invokes .import_csv" do
+          expect(model_class).to respond_to(:import_csv).with(2).arguments
+        end
+
+        it "redirects" do
+          subject
+          expect(response).to be_redirect
+        end
+
+        it "presents a flash notice message" do
+          subject
+          expect(response).to have_notice "#{model_class.name.underscore.humanize.pluralize} were imported successfully!"
+        end
+      end
+
+      context "without a csv file" do
+        subject { post import_csv_storage_locations_path(default_params) }
+
+        it "redirects to :index" do
+          subject
+          expect(response).to be_redirect
+        end
+
+        it "presents a flash error message" do
+          subject
+          expect(response).to have_error "No file was attached!"
+        end
+      end
+
+      context "csv file with wrong headers" do
+        let(:file) { fixture_file_upload("wrong_headers.csv", "text/csv") }
+        subject { post import_csv_storage_locations_path(default_params), params: { file: file } }
+
+        it "redirects" do
+          subject
+          expect(response).to be_redirect
+        end
+
+        it "presents a flash error message" do
+          subject
+          expect(response).to have_error "Check headers in file!"
+        end
+      end
+    end
+
+    describe "GET #show" do
+      it "returns http success" do
+        get storage_location_path(default_params.merge(id: create(:storage_location, organization: @organization)))
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #destroy" do
+      it "redirects to #index" do
+        delete storage_location_path(default_params.merge(id: create(:storage_location, organization: @organization)))
+        expect(response).to redirect_to(storage_locations_path)
+      end
+    end
+
+    context "Looking at a different organization" do
+      let(:object) { create(:storage_location, organization: create(:organization)) }
+      include_examples "requiring authorization"
+    end
+  end
+
+  context "While not signed in" do
+    let(:object) { create(:storage_location) }
+
+    include_examples "requiring authorization"
+  end
+end
