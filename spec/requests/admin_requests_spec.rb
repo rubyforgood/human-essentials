2cbeca0fa15e8e68d4b12e8ef175506524c849d6
Lucas Hiago <lucashiago63@gmail.com>
2020-03-08 18:38:01 -0300

    Add new admin requests spec

---
+++ spec/requests/admin_requests_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe "Admin", type: :request do
+  context "while signed in as a super admin" do
+    before do
+      sign_in(@super_admin_no_org)
+    end
+
+    it "allows a user to load the dashboard" do
+      get admin_dashboard_path()
+      expect(response).to be_successful
+    end
+    context "with rendered views" do
+      it "shows a logout button" do
+        get admin_dashboard_path()
+        expect(response.body).to match(/log out/im)
+      end
+    end
+  end
+
+  context "while signed in as a non-super-admin" do
+    it "disallows dashboard access, redirecting to the normal dashboard" do
+      [@organization_admin, @user].each do |u|
+        sign_in(u)
+        get admin_dashboard_path()
+        expect(response).to redirect_to(dashboard_path)
+        expect(response).to have_error
+      end
+    end
+  end
+end
