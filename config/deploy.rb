40d8213b19eccc2dc81afbbb9cec2f79cceef2a9
Sean Marcia <smarcia@gmail.com>
2020-03-08 23:05:51 -0400

    Update capistrano deployment file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.11.2"
+lock "3.12.0"
 
 set :repo_url,        "git@github.com:rubyforgood/diaper.git"
 set :application,     "diaper_base"
