90d92894f0032f4c23576d21c9f6372787b24d10
Lucas Hiago <lucashiago63@gmail.com>
2020-03-15 23:33:12 -0300

    Add new admin requests spec (#1545)
    * Add new admin requests spec
    
    * Remove old admin controller spec

--- spec/requests/admin_requests_spec.rb
+++ spec/requests/admin_requests_spec.rb
@@ -1,29 +1,26 @@
-RSpec.describe AdminController, type: :controller do
+RSpec.describe "Admin", type: :request do
   context "while signed in as a super admin" do
     before do
       sign_in(@super_admin_no_org)
     end
 
     it "allows a user to load the dashboard" do
-      get :dashboard
+      get admin_dashboard_path
       expect(response).to be_successful
     end
     context "with rendered views" do
-      render_views
       it "shows a logout button" do
-        get :dashboard
+        get admin_dashboard_path
         expect(response.body).to match(/log out/im)
       end
     end
   end
 
-  # Since all Admin namespace actions inherit from AdminController, this will serve as a
-  # blanket check for access for non-super-admins
   context "while signed in as a non-super-admin" do
     it "disallows dashboard access, redirecting to the normal dashboard" do
       [@organization_admin, @user].each do |u|
         sign_in(u)
-        get :dashboard
+        get admin_dashboard_path
         expect(response).to redirect_to(dashboard_path)
         expect(response).to have_error
       end
