8083fb0a17eca853945fafb9953ffff468fe3486
Aaron Hill <armahillo@gmail.com>
2020-04-01 17:41:55 -0400

    Removes ADR files (wrong app) and fixes whitespace in JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,7 +38,7 @@ window.setTimeout(function() {
   // When the user is given an error message, we should not auto-hide it so that
   // they can fully read it and potentially copy/paste it into an issue.
   $(".alert").not(".error").fadeTo(1000, 0).slideUp(1000, function(){
-      $(this).remove();
+    $(this).remove();
   });
 }, 2500);
 
