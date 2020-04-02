81cac6b7e87a9ffe6679e373aed3b23353befe2d
Aaron Hill <armahillo@gmail.com>
2020-04-02 10:16:23 -0400

    Adds a space for styling to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,7 @@ function isShortHeightScreen() {
 window.setTimeout(function() {
   // When the user is given an error message, we should not auto-hide it so that
   // they can fully read it and potentially copy/paste it into an issue.
-  $(".alert").not(".error").fadeTo(1000, 0).slideUp(1000, function(){
+  $(".alert").not(".error").fadeTo(1000, 0).slideUp(1000, function() {
     $(this).remove();
   });
 }, 2500);
