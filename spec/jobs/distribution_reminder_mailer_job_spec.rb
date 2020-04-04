bdf1516bb678c79ca9851cae6b74b27716e23ddf
Aaron Hill <armahillo@gmail.com>
2020-04-04 19:20:57 -0400

    Fixes specs for PartnerMailerJob

--- spec/jobs/distribution_reminder_mailer_job_spec.rb
+++ spec/jobs/distribution_reminder_mailer_job_spec.rb
@@ -1,19 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe DistributionReminderJob, type: :job do
-  describe '#perform' do
-    let(:organization) { create :organization }
-    let(:distribution) { create :distribution }
-
-    it 'sends an email' do
-      Sidekiq::Testing.inline! do
-        expect do
-          DistributionReminderJob.perform_later(distribution.id)
-        end .to change { ActionMailer::Base.deliveries.count }.by(1)
-      end
-    end
-  end
-
   describe "conditionally sending the emails" do
     let(:organization) { create :organization }
 
