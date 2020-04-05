3c87677490f5c7d570e9dd10ba779c15f5f6a478
Aaron Hill <armahillo@gmail.com>
2020-04-05 10:39:00 -0400

    Fixes specs for ReminderDeadlineJob

--- app/jobs/reminder_deadline_job.rb
+++ app/jobs/reminder_deadline_job.rb
@@ -1,6 +1,7 @@
 class ReminderDeadlineJob < ApplicationJob
   def perform
     if Flipper.enabled?(:reminders_active)
+      # TODO: This query can probably be improved
       organizations = Organization.where('reminder_day = ? and deadline_day is not null', Date.current.day)
       organizations.each do |organization|
         organization.partners.where(send_reminders: true).each do |partner|
