5a291dc20c9647fe0c21eea07de7c01b61ff3e8e
androidgrl <kawaharajamie@gmail.com>
2020-04-05 13:28:11 -0700

    Commit auto changes to integer
    All these changes seem to happen when we migrate.

--- app/models/barcode_item.rb
+++ app/models/barcode_item.rb
@@ -4,7 +4,6 @@
 #
 #  id               :integer          not null, primary key
 #  barcodeable_type :string           default("Item")
-#  global           :boolean          default(FALSE)
 #  quantity         :integer
 #  value            :string
 #  created_at       :datetime         not null
