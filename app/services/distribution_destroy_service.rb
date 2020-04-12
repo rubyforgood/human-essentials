f0d3eca43df05bcd912c0d97185c38b260f43964
Edwin Mak <edwinthinks@gmail.com>
2020-04-12 18:13:38 -0500

    Refactor DistributionDestroyService with spec coverage (#1627)
    * Refactor DistributionDestroyService with spec coverage
    
    * Rubocop fixes

--- app/services/distribution_destroy_service.rb
+++ app/services/distribution_destroy_service.rb
@@ -1,16 +1,33 @@
 class DistributionDestroyService
+  attr_reader :distribution, :error
+
   def initialize(distribution_id)
-    @distribution = Distribution.find(distribution_id)
+    @distribution_id = distribution_id
   end
 
   def call
+    @distribution = Distribution.find(@distribution_id)
+    @organization = distribution.organization
+
     @distribution.transaction do
       @distribution.destroy!
-      @distribution.storage_location.increase_inventory @distribution
-      OpenStruct.new(success?: true)
+      @distribution.storage_location.increase_inventory(@distribution)
     end
+  rescue ActiveRecord::RecordNotFound => e
+    Rails.logger.error "[!] DistributionsController#destroy failed to destroy distribution #{@distribution_id} because it does not exist"
+    @error = e
+  rescue Errors::InsufficientAllotment => e
+    @distribution.line_items.assign_insufficiency_errors(e.insufficient_items)
+    Rails.logger.error "[!] DistributionsController#destroy failed because of Insufficient Allotment #{@organization.short_name}: #{@distribution.errors.full_messages} [#{e.message}]"
+    @error = e
   rescue StandardError => e
     Rails.logger.error "[!] DistributionsController#destroy failed to destroy distribution for #{@distribution.organization.short_name}: #{@distribution.errors.full_messages} [#{e.inspect}]"
-    OpenStruct.new(success: false, distribution: @distribution, error: e)
+    @error = e
+  ensure
+    return self
+  end
+
+  def success?
+    @error.nil?
   end
 end
