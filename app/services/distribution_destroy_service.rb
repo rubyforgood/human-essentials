81921921c5bb2fdfffbed7eb104a27754bf0a726
Edwin Mak <edwinthinks@gmail.com>
2020-05-03 08:13:37 -0500

    Utilize DistributionService superclass on Distribution-like services

--- app/services/distribution_destroy_service.rb
+++ app/services/distribution_destroy_service.rb
@@ -1,33 +1,14 @@
-class DistributionDestroyService
-  attr_reader :distribution, :error
+class DistributionDestroyService < DistributionService
 
   def initialize(distribution_id)
     @distribution_id = distribution_id
   end
 
   def call
-    @distribution = Distribution.find(@distribution_id)
-    @organization = distribution.organization
-
-    @distribution.transaction do
+    perform_distribution_service do
       @distribution.destroy!
       @distribution.storage_location.increase_inventory(@distribution)
     end
-  rescue ActiveRecord::RecordNotFound => e
-    Rails.logger.error "[!] DistributionsController#destroy failed to destroy distribution #{@distribution_id} because it does not exist"
-    @error = e
-  rescue Errors::InsufficientAllotment => e
-    @distribution.line_items.assign_insufficiency_errors(e.insufficient_items)
-    Rails.logger.error "[!] DistributionsController#destroy failed because of Insufficient Allotment #{@organization.short_name}: #{@distribution.errors.full_messages} [#{e.message}]"
-    @error = e
-  rescue StandardError => e
-    Rails.logger.error "[!] DistributionsController#destroy failed to destroy distribution for #{@distribution.organization.short_name}: #{@distribution.errors.full_messages} [#{e.inspect}]"
-    @error = e
-  ensure
-    return self
   end
 
-  def success?
-    @error.nil?
-  end
 end
---
+++ app/services/distribution_service.rb
@@ -0,0 +1,56 @@
+class DistributionService
+  attr_reader :error, :distribution_id
+
+  def perform_distribution_service(&block)
+    distribution.transaction do
+      yield block
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    Rails.logger.error "[!] #{self.class.name} failed to destroy distribution #{distribution_id} because it does not exist"
+    set_error(e)
+  rescue Errors::InsufficientAllotment => e
+    distribution.line_items.assign_insufficiency_errors(e.insufficient_items)
+    Rails.logger.error "[!] #{self.class.name} failed because of Insufficient Allotment #{distribution_organization.short_name}: #{distribution.errors.full_messages} [#{e.message}]"
+    set_error(e)
+  rescue StandardError => e
+    Rails.logger.error "[!] #{self.class.name} failed to destroy distribution for #{distribution_organization.short_name}: #{distribution.errors.full_messages} [#{e.inspect}]"
+    set_error(e)
+  ensure
+    return self
+  end
+
+  def success?
+    error.nil?
+  end
+
+  def distribution
+    # Return distribution if it has already been defined
+    return @distribution if @distribution
+
+    # Otherwise try to get this value with possibly
+    # provided distribution_id from initialize
+    if @distribution_id.present?
+      @distribution = Distribution.find(@distribution_id)
+    end
+  end
+
+  def distribution_id
+    return @distribution_id if @distribution_id
+
+    if distribution.present?
+      @distribution_id = distribution.id
+    end
+  end
+
+  private
+
+  def distribution_organization
+    @distribution_organization ||= distribution&.organization
+  end
+
+  def set_error(e)
+    @error = e
+  end
+
+end
+
