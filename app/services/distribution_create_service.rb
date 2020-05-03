21512a0c7e20cbd62785c5c349f171ac9977c983
Edwin Mak <edwinthinks@gmail.com>
2020-05-03 08:53:07 -0500

    I <3 rubocop

--- app/services/distribution_create_service.rb
+++ app/services/distribution_create_service.rb
@@ -1,5 +1,4 @@
 class DistributionCreateService < DistributionService
-
   def initialize(distribution_params, request_id = nil)
     @distribution = Distribution.new(distribution_params)
     @request = Request.find(request_id) if request_id
