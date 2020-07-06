08647d2a30272c19fcb3d50a228657eb1be93858
Sean Marcia <smarcia@gmail.com>
2020-07-06 17:29:23 -0400

    Fix deploy file to match dependabot updated version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.12.0"
+lock "3.14.0"
 
 set :repo_url,        "git@github.com:rubyforgood/diaper.git"
 set :application,     "diaper_base"
