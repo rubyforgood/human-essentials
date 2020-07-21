1d386ce338b9d2e0a0a97c60230c67a7fb63a33a
Lucas Hiago <lucashiago63@gmail.com>
2020-07-21 19:31:37 -0300

    Add new admin organization requests spec (#1548)
    * Add new admin organizations requests spec
    
    * Remove old admin organization controller spec
    
    * Corrects rubocop warning

--- spec/requests/admin/organizations_requests_spec.rb
+++ spec/requests/admin/organizations_requests_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe Admin::OrganizationsController, type: :controller do
+RSpec.describe "Admin::Organizations", type: :request do
   let(:default_params) do
     { organization_id: @organization.id }
   end
@@ -10,7 +10,7 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
 
     describe "GET #new" do
       it "returns http success" do
-        get :new
+        get new_admin_organization_path
         expect(response).to be_successful
       end
     end
@@ -20,7 +20,7 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
 
       context "with valid params" do
         it "redirects to #index" do
-          post :create, params: { organization: valid_organization_params }
+          post admin_organizations_path({ organization: valid_organization_params })
 
           expect(response).to redirect_to(admin_organizations_path)
         end
@@ -30,7 +30,7 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
         let(:invalid_params) { valid_organization_params.merge(name: nil) }
 
         it "renders #create with an error message" do
-          post :create, params: { organization: invalid_params }
+          post admin_organizations_path({ organization: invalid_params })
 
           expect(subject).to render_template("new")
           expect(flash[:error]).to be_present
@@ -40,7 +40,7 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
 
     describe "GET #index" do
       it "returns http success" do
-        get :index
+        get admin_organizations_path
         expect(response).to be_successful
       end
     end
@@ -48,27 +48,29 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
     describe "PATCH #update" do
       let(:organization) { create(:organization, name: "Original Name") }
       subject do
-        patch :update,
-              params: default_params.merge(
-                id: organization.id,
-                organization: { name: updated_name }
-              )
+        patch admin_organization_path(default_params.merge(id: organization.id, organization: { name: updated_name }))
       end
 
       context "with a valid update" do
         let(:updated_name) { "Updated Name" }
+        let(:redirect_status) { 302 }
 
         it "redirects to #index" do
-          expect(subject).to have_http_status(:redirect)
-          expect(subject).to redirect_to(admin_organizations_path)
+          expect(subject).to be(redirect_status)
+          expect(subject).to redirect_to(admin_organizations_path({ organization_id: @organization }))
         end
       end
 
       context "with an invalid update" do
         let(:updated_name) { nil }
+        let(:successful) { 200 }
+
+        subject do
+          patch admin_organization_path(default_params.merge(id: organization.id, organization: { name: updated_name }))
+        end
 
         it "returns http success" do
-          expect(subject).to be_successful
+          expect(subject).to be(successful)
         end
 
         it "redirects to #edit with an error message" do
@@ -83,8 +85,8 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
 
       context "with a valid organization id" do
         it "redirects to #index" do
-          delete :destroy, params: { id: organization.id }
-          expect(response).to redirect_to(admin_organizations_path)
+          delete admin_organization_path({ id: organization.id })
+          expect(response).to redirect_to(admin_organizations_path({ organization_id: @organization }))
         end
       end
     end
@@ -93,7 +95,7 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
       let!(:organization) { create(:organization) }
 
       it "returns http success" do
-        get :edit, params: { id: organization.id }
+        get edit_admin_organization_path({ id: organization.id })
         expect(response).to be_successful
       end
     end
@@ -102,38 +104,22 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
       let!(:organization) { create(:organization) }
 
       it "returns http success" do
-        get :show, params: { id: organization.id }
-        expect(response).to be_successful
-      end
-    end
-  end
-=begin
-
-    describe "GET #show" do
-      it "returns http success" do
-        get :show, params: { id: @organization.id }
-        expect(response).to be_successful
-      end
-    end
-
-    describe "GET #edit" do
-      it "returns http success" do
-        get :edit, params: { id: @organization.id }
+        get admin_organization_path({ id: organization.id })
         expect(response).to be_successful
       end
     end
 
     describe "PUT #update" do
       it "redirect" do
-        put :update, params: { id: @organization.id, organization: { name: "Foo" } }
+        put admin_organization_path({ id: @organization.id, organization: { name: "Foo" } })
         expect(response).to be_redirect
       end
     end
 
     describe "DELETE #destroy" do
       it "redirects" do
-        delete :destroy, params: { id: @organization.id }
-        expect(response).to be_redirect
+        delete admin_organization_path({ id: @organization.id })
+        expect(response).to redirect_to(admin_organizations_path({ organization_id: @organization }))
       end
     end
   end
@@ -145,38 +131,37 @@ RSpec.describe Admin::OrganizationsController, type: :controller do
 
     describe "GET #new" do
       it "redirects" do
-        get :new
+        get new_admin_organization_path
         expect(response).to be_redirect
       end
     end
 
     describe "POST #create" do
       it "redirects" do
-        post :create, params: { organization: attributes_for(:organization) }
+        post admin_organizations_path({ organization: attributes_for(:organization) })
         expect(response).to be_redirect
       end
     end
 
     describe "GET #index" do
       it "redirects" do
-        get :index
+        get admin_organizations_path
         expect(response).to be_redirect
       end
     end
 
     describe "GET #edit" do
       it "redirects" do
-        get :edit, params: { id: @organization.id }
+        get edit_admin_organization_path({ id: @organization.id })
         expect(response).to be_redirect
       end
     end
 
     describe "PUT #update" do
       it "redirects" do
-        put :update, params: { id: @organization.id, organization: { name: "Foo" } }
+        put admin_organization_path({ id: @organization.id, organization: { name: "Foo" } })
         expect(response).to be_redirect
       end
     end
   end
-=end
 end
