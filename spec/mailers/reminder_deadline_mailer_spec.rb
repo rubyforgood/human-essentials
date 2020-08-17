6a078c0b867fb65e63767702a1ffbf7d75a5502e
Guillermo S <gs8rj@virginia.edu>
2020-08-16 21:16:54 -0400

    Organization from email address for distribution email reminder (#1782)

--- spec/mailers/reminder_deadline_mailer_spec.rb
+++ spec/mailers/reminder_deadline_mailer_spec.rb
@@ -1,6 +1,7 @@
 describe ReminderDeadlineMailer do
   describe 'notify deadline' do
     let(:organization) { create :organization }
+    let!(:user) { create(:organization_admin, organization: organization) }
     let(:partner) { create :partner, organization: organization }
     let(:mail) { ReminderDeadlineMailer.notify_deadline(partner, organization) }
 
@@ -13,11 +14,21 @@ describe ReminderDeadlineMailer do
     end
 
     it 'renders the sender email' do
-      expect(mail.from).to contain_exactly('info@diaper.app')
+      expect(mail.from).to contain_exactly(organization.email)
     end
 
     it 'renders the body' do
       expect(mail.body).to include(organization.deadline_day)
+    end
+
+    context 'organization email not present' do
+      before do
+        allow(organization).to receive(:email).and_return(nil)
+      end
+
+      it 'renders the sender email as admin email' do
+        expect(mail.from).to contain_exactly(user.email)
+      end
     end
   end
 end
