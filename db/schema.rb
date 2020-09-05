e6cdd1e7ac942347556db476aaccf6a6920c2f1a
Emily Giurleo <e.m.giurleo@gmail.com>
2020-09-05 17:25:42 -0400

    index users on discarded at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_145818) do
+ActiveRecord::Schema.define(version: 2020_09_05_204920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -349,6 +349,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_145818) do
     t.boolean "super_admin", default: false
     t.datetime "last_request_at"
     t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_users_on_discarded_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
