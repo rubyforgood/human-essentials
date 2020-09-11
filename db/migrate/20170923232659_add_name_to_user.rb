d209da65bbb5d4755548edc265e437eff383bba1
Sean Marcia <smarcia@gmail.com>
2020-09-10 23:14:13 -0400

    Got feedback from a diaper bank who suggested that you should only be able to convert others to non admin, not yourself which might happen by accident. (#1827)

--- db/migrate/20170923232659_add_name_to_user.rb
+++ db/migrate/20170923232659_add_name_to_user.rb
@@ -2,13 +2,13 @@
 class AddNameToUser < ActiveRecord::Migration[5.1]
   def up
     add_column :users, :name, :string, null: false, default: "CHANGEME"
-    puts "Updating existing users:"
-    User.all.each do |u|
-      new_name = u.email.split("@").first
-      u.update_attributes(name: new_name)
-      puts "Updated #{u.email} with #{u.name}"
-    end
-    User.reset_column_information
+    # puts "Updating existing users:"
+    # User.all.each do |u|
+    #   new_name = u.email.split("@").first
+    #   u.update_attributes(name: new_name)
+    #   puts "Updated #{u.email} with #{u.name}"
+    # end
+    # User.reset_column_information
   end
 
   def down
