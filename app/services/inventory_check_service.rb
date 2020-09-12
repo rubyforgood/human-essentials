b006d21f1ef2d5706dd906f9a2a5f60a0afb7bef
Nankai Pan <nankai.pan@roosterteeth.com>
2020-09-12 15:00:38 -0500

    Refactoring: extract methods

--- app/services/inventory_check_service.rb
+++ app/services/inventory_check_service.rb
@@ -8,33 +8,34 @@ class InventoryCheckService
   end
 
   def call
-    items_below_minimum_quantity = []
-    items_below_recommended_quantity = []
-
-    storage_location = @distribution.storage_location
-
-    items_below_minimum_quantity = @distribution.line_items.select do |line_item|
-      inventory_item = line_item.item.inventory_item_at(storage_location.id)
-      inventory_item.lower_than_on_hand_minimum_quantity?
-    end.map(&:item)
-
-    items_below_recommended_quantity = @distribution.line_items.select do |line_item|
-      inventory_item = line_item.item.inventory_item_at(storage_location.id)
-      inventory_item.lower_than_on_hand_recommended_quantity?
-    end.map(&:item)
-
-    items_below_recommended_quantity = items_below_recommended_quantity - items_below_minimum_quantity
-
     unless items_below_minimum_quantity.empty?
       @error = "The following items have fallen below the minimum " \
         "on hand quantity: #{items_below_minimum_quantity.map(&:name).join(", ")}"
     end
 
-    unless items_below_recommended_quantity.empty?
+    unless deduplicate_items_below_recommended_quantity.empty?
       @alert = "The following items have fallen below the recommended " \
-        "on hand quantity: #{items_below_recommended_quantity.map(&:name).join(", ")}"
+        "on hand quantity: #{deduplicate_items_below_recommended_quantity.map(&:name).join(", ")}"
     end
 
     self
+  end
+
+  def items_below_minimum_quantity
+    @distribution.line_items.select do |line_item|
+      inventory_item = line_item.item.inventory_item_at(@distribution.storage_location.id)
+      inventory_item.lower_than_on_hand_minimum_quantity?
+    end.map(&:item)
+  end
+
+  def items_below_recommended_quantity
+    @distribution.line_items.select do |line_item|
+      inventory_item = line_item.item.inventory_item_at(@distribution.storage_location.id)
+      inventory_item.lower_than_on_hand_recommended_quantity?
+    end.map(&:item)
+  end
+
+  def deduplicate_items_below_recommended_quantity
+    items_below_recommended_quantity - items_below_minimum_quantity
   end
 end
