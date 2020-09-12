2b9c7c6de7704bf61d779438160b3a56d7f645af
Dayne <dayne@code.org>
2020-09-12 12:31:14 -0700

    Updating database seed with new 'delivery_method' field

--- db/seeds.rb
+++ db/seeds.rb
@@ -314,7 +314,8 @@ end
   distribution = Distribution.create!(storage_location: storage_location,
                                       partner:          random_record_for_org(pdx_org, Partner),
                                       organization:     pdx_org,
-                                      issued_at:        Faker::Date.between(from: 4.days.ago, to: Time.zone.today))
+                                      issued_at:        Faker::Date.between(from: 4.days.ago, to: Time.zone.today),
+                                      delivery_method: Distribution.delivery_methods.keys.sample)
 
   stored_inventory_items_sample.each do |stored_inventory_item|
     distribution_qty = rand(stored_inventory_item.quantity / 2)
