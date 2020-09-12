468ead86efccf488f6ff154090ac3f84b16fb446
Albert Chae <albertchae@users.noreply.github.com>
2020-09-11 17:44:44 -0700

    Add missing third argument for subject

--- lib/previews/distribution_mailer_preview.rb
+++ lib/previews/distribution_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Provides a preview of a distribution email
 class DistributionMailerPreview < ActionMailer::Preview
   def partner_mailer
-    DistributionMailer.partner_mailer(Organization.first, Distribution.first)
+    DistributionMailer.partner_mailer(Organization.first, Distribution.last, "preview subject")
   end
 end
