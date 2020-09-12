37d47aca775e085be54b733d0253ff8eac518c57
Nankai Pan <nankai.pan@roosterteeth.com>
2020-09-12 16:39:21 -0500

    Refactoring: extract methods for set_error and set_alert

--- app/services/inventory_check_service.rb
+++ app/services/inventory_check_service.rb
@@ -9,27 +9,35 @@ class InventoryCheckService
 
   def call
     unless items_below_minimum_quantity.empty?
-      @error = "The following items have fallen below the minimum " \
-        "on hand quantity: #{items_below_minimum_quantity.map(&:name).join(", ")}"
+      set_error
     end
 
     unless deduplicate_items_below_recommended_quantity.empty?
-      @alert = "The following items have fallen below the recommended " \
-        "on hand quantity: #{deduplicate_items_below_recommended_quantity.map(&:name).join(", ")}"
+      set_alert
     end
 
     self
   end
 
+  def set_error
+    @error = "The following items have fallen below the minimum " \
+      "on hand quantity: #{items_below_minimum_quantity.map(&:name).join(", ")}"
+  end
+
+  def set_alert
+    @alert = "The following items have fallen below the recommended " \
+      "on hand quantity: #{deduplicate_items_below_recommended_quantity.map(&:name).join(", ")}"
+  end
+
   def items_below_minimum_quantity
-    @distribution.line_items.select do |line_item|
+    @_items_below_min ||= @distribution.line_items.select do |line_item|
       inventory_item = line_item.item.inventory_item_at(@distribution.storage_location.id)
       inventory_item.lower_than_on_hand_minimum_quantity?
     end.map(&:item)
   end
 
   def items_below_recommended_quantity
-    @distribution.line_items.select do |line_item|
+    @_items_below_rec ||= @distribution.line_items.select do |line_item|
       inventory_item = line_item.item.inventory_item_at(@distribution.storage_location.id)
       inventory_item.lower_than_on_hand_recommended_quantity?
     end.map(&:item)
