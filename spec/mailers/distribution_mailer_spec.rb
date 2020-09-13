f7cfa0e2431822133559019258a2b98e64f16b3d
Dayne <dayne@code.org>
2020-09-12 17:11:00 -0700

    Updated 'partner_mailer' to specify delivery or pickup

--- spec/mailers/distribution_mailer_spec.rb
+++ spec/mailers/distribution_mailer_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe DistributionMailer, type: :mailer do
   before do
     @organization.default_email_text = "Default email text example"
-    partner = create(:partner, name: 'PARTNER')
-    @distribution = create(:distribution, organization: @user.organization, comment: "Distribution comment", partner: partner)
+    @partner = create(:partner, name: 'PARTNER')
+    @distribution = create(:distribution, organization: @user.organization, comment: "Distribution comment", partner: @partner)
   end
 
   describe "#partner_mailer" do
@@ -17,6 +17,23 @@ RSpec.describe DistributionMailer, type: :mailer do
       expect(mail.body.encoded).to match("Distribution comment")
       expect(mail.subject).to eq("test subject from DEFAULT")
     end
+
+    context "with deliver_method: :pick_up" do
+      it "renders the body with 'picked up' specified" do
+        distribution = create(:distribution, organization: @user.organization, comment: "Distribution comment", partner: @partner, delivery_method: :pick_up)
+        mail = DistributionMailer.partner_mailer(@organization, distribution, 'test subject')
+        expect(mail.body.encoded).to match("distribution has been set to be picked up on")
+      end
+    end
+
+    context "with deliver_method: :delivery" do
+      it "renders the body with 'delivered' specified" do
+        distribution = create(:distribution, organization: @user.organization, comment: "Distribution comment", partner: @partner, delivery_method: :delivery)
+        mail = DistributionMailer.partner_mailer(@organization, distribution, 'test subject')
+        expect(mail.body.encoded).to match("distribution has been set to be delivered on")
+      end
+    end
+
   end
 
   describe "#reminder_email" do
