d62e6f4ebcab0c6e42c5e0480497114eb474211e
Nankai Pan <nankai.pan@roosterteeth.com>
2020-09-13 14:20:03 -0500

    Enqueue PartnerMailerJob into sidekiq default queue (#1835)
    * Enqueue PartnerMailerJob into sidekiq default queue
    
    * Fix test

--- app/jobs/partner_mailer_job.rb
+++ app/jobs/partner_mailer_job.rb
@@ -1,5 +1,7 @@
 # This job notifies a Partner that they have a pending distribution
-class PartnerMailerJob < ApplicationJob
+class PartnerMailerJob
+  include Sidekiq::Worker
+
   def perform(org_id, dist_id, subject)
     current_organization = Organization.find(org_id)
     distribution = Distribution.find(dist_id)
