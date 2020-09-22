713755fef99906c09c56a84a95242734bf31ce11
Seth Lieberman <seth.lieberman@powerhrg.com>
2020-09-22 10:17:09 -0400

    feature/add-distributions-status-filtering (#1867)
    * Add ability to filter distributions by state(status)
    
    * Update filtering to match current filtering convention
    
    Use `class_filter` to apply filters
    
    Add automated tests for state filter

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -51,6 +51,7 @@ class DistributionsController < ApplicationController
     @total_items_paginated_distributions = total_items(@paginated_distributions)
     @items = current_organization.items.alphabetized
     @partners = @distributions.collect(&:partner).uniq.sort_by(&:name)
+    @states = Distribution.states.transform_keys(&:humanize)
   end
 
   def create
@@ -193,7 +194,7 @@ class DistributionsController < ApplicationController
   def filter_params
     return {} unless params.key?(:filters)
 
-    params.require(:filters).slice(:by_item_id, :by_partner)
+    params.require(:filters).slice(:by_item_id, :by_partner, :by_state)
   end
 
   def perform_inventory_check
