22d1467a2295c9f1ce322a8de250427f621e74d1
Rodolfo Santos <rodolfo.santos@powerhrg.com>
2020-09-22 20:50:58 -0300

    Fix specs

--- app/jobs/partner_mailer_job.rb
+++ app/jobs/partner_mailer_job.rb
@@ -1,6 +1,6 @@
 # This job notifies a Partner that they have a pending distribution
 class PartnerMailerJob < ApplicationJob
-  def perform(org_id, dist_id, subject, distribution_changes)
+  def perform(org_id, dist_id, subject, distribution_changes={})
     current_organization = Organization.find(org_id)
     distribution = Distribution.find(dist_id)
     DistributionMailer.partner_mailer(current_organization, distribution, subject, distribution_changes).deliver_now
