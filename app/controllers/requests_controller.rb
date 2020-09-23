928b0bd269463746f201b1cb9787d15e3bf8c6d2
Jayson Mandani <jaysonmandani@gmail.com>
2020-09-23 21:02:19 +0800

    1852 Feature/calculate product totals (#1889)
    * Adds calculation for the product totals
    
    * Add table on the modal

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,8 +3,12 @@ class RequestsController < ApplicationController
   def index
     setup_date_range_picker
 
-    @requests = current_organization.ordered_requests.during(helpers.selected_range)
+    @requests = current_organization
+                .ordered_requests
+                .during(helpers.selected_range)
+
     @paginated_requests = @requests.page(params[:page])
+    @calculate_product_totals = total_items(@requests)
 
     respond_to { |format| format.html }
   end
@@ -34,6 +38,20 @@ class RequestsController < ApplicationController
   end
 
   private
+
+  def total_items(requests)
+    request_items = []
+
+    requests.pluck(:request_items).each do |items|
+      items.map { |json| request_items << [Item.find(json['item_id']).name, json['quantity']] }
+    end
+
+    request_items.inject({}) do |item, (quantity, total)|
+      item[quantity] ||= 0
+      item[quantity] += total
+      item
+    end
+  end
 
   def load_items
     return unless @request.request_items
