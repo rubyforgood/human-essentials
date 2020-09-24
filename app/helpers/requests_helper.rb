98798dcf4db630cd371a120b751dd887b4f0a1ca
Gerald Abrencillo <gerald.abrencillo@powerhrg.com>
2020-09-24 23:55:44 +0800

    Add indicator that a request is over the limit

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -4,4 +4,10 @@ module RequestsHelper
 
     "(#{partner.quota})"
   end
+
+  def quota_column_class(total, partner)
+    return "" if partner.quota.blank? || total < partner.quota
+
+    "text-danger font-weight-bold"
+  end
 end
