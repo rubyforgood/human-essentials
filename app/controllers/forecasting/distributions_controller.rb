0fc132f4d5c6a4d6303eec8b49c6938dfaf98932
Jayson Mandani <jaysonmandani@gmail.com>
2020-09-25 22:50:24 +0800

    Add forecasting for purchases and refactor forecasting

--- app/controllers/forecasting/distributions_controller.rb
+++ app/controllers/forecasting/distributions_controller.rb
@@ -1,34 +1,6 @@
-class Forecasting::DistributionsController < ApplicationController
+class Forecasting::DistributionsController < Forecasting::BaseController
   def index
-    @series     = series
-    @categories = categories
-  end
-
-  private
-
-  def series
-    items = []
-
-    current_organization.items.sort.each do |item|
-      items << {name: item.name, data: total_items(item.line_items).values}
-    end
-
-    items.to_json
-  end
-
-  def categories
-    keys = total_items(LineItem.all).keys
-
-    dates(keys)
-  end
-
-  def dates(dates)
-    dates.sort.uniq.map {|date| Date::ABBR_MONTHNAMES[date.month]}
-  end
-
-  def total_items(line_items)
-    line_items.where(itemizable_type: "Distribution")
-              .group_by_month(:created_at)
-              .sum(:quantity)
+    @series = series('Distribution')
+    @title = 'Monthly Distributions'
   end
 end
--- app/controllers/forecasting/donations_controller.rb
+++ app/controllers/forecasting/donations_controller.rb
@@ -1,34 +1,6 @@
-class Forecasting::DonationsController < ApplicationController
+class Forecasting::DonationsController < Forecasting::BaseController
   def index
-    @categories = categories
-    @series = series
-  end
-
-  private
-
-  def series
-    items = []
-
-    current_organization.items.sort.each do |item|
-      items << {name: item.name, data: total_items(item.line_items).values}
-    end
-
-    items.to_json
-  end
-
-  def categories
-    keys = total_items(LineItem.all).keys
-
-    dates(keys)
-  end
-
-  def dates(dates)
-    dates.sort.uniq.map {|date| Date::ABBR_MONTHNAMES[date.month]}
-  end
-
-  def total_items(line_items)
-    line_items.where(itemizable_type: "Donation")
-              .group_by_month(:created_at)
-              .sum(:quantity)
+    @series = series('Donation')
+    @title = 'Monthly Donations'
   end
 end
