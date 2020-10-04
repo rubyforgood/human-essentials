bc1303d992e7ea1047811a42dfa8d786706bcf9c
Edwin Mak <edwinthinks@gmail.com>
2020-10-04 14:27:30 -0500

    Update KitInventory deallocation service

--- app/services/deallocate_kit_inventory_service.rb
+++ app/services/deallocate_kit_inventory_service.rb
@@ -1,9 +1,10 @@
 class DeallocateKitInventoryService
-  attr_reader :kit, :storage_location, :error
+  attr_reader :error
 
-  def initialize(kit, storage_location = nil)
+  def initialize(kit:, storage_location:, decrease_by:)
     @kit = kit
     @storage_location = storage_location
+    @decrease_by = decrease_by
   end
 
   def deallocate
@@ -18,17 +19,38 @@ class DeallocateKitInventoryService
 
   private
 
+  attr_reader :kit, :storage_location, :decrease_by
+
   def validate_storage_location
     raise Errors::StorageLocationDoesNotMatch if storage_location.organization != kit.organization
   end
 
   def deallocate_inventory_items
     ActiveRecord::Base.transaction do
-      storage_location.increase_inventory(kit)
+      storage_location.increase_inventory(kit_content)
+      storage_location.decrease_inventory(associated_kit_item)
     end
   end
 
   def set_error(error)
     @error = error.message
   end
+
+  def kit_content
+    kit.to_a.map do |item|
+      item.merge({
+        quantity: item[:quantity] * decrease_by
+      })
+    end
+  end
+
+  def associated_kit_item
+    [
+      {
+        item_id: kit.item.id,
+        quantity: decrease_by
+      }
+    ]
+  end
+
 end
