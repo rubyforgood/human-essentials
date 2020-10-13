05f85f7a31250f21c91701cbc40af7b77a0c8283
Juarez Lustosa <juarez.lustosa@gmail.com>
2020-10-13 13:07:58 -0300

    Add Unknown item when is nil

--- app/services/requests_total_items_service.rb
+++ app/services/requests_total_items_service.rb
@@ -7,11 +7,10 @@ class RequestsTotalItemsService
     return unless requests
 
     request_items_array = []
-    items_names = Item.where(id: request_items_ids).as_json(only: [:id, :name])
 
     request_items.each do |items|
       items.each do |json|
-        request_items_array << [item_name(items_names, json['item_id']), json['quantity']]
+        request_items_array << [item_name(json['item_id']), json['quantity']]
       end
     end
 
@@ -26,16 +25,20 @@ class RequestsTotalItemsService
 
   attr_accessor :requests
 
-  def request_items_ids
-    request_items.flat_map { |jitem| jitem.map { |item| item["item_id"] } }
-  end
-
   def request_items
     @request_items ||= requests.pluck(:request_items)
   end
 
-  def item_name(items_names, id)
-    item_found = items_names.select { |item| item["id"] == id }
-    item_found.first["name"].presence || ""
+  def request_items_ids
+    request_items.flat_map { |jitem| jitem.map { |item| item["item_id"] } }
+  end
+
+  def items_names
+    @items_names ||= Item.where(id: request_items_ids).as_json(only: [:id, :name])
+  end
+
+  def item_name(id)
+    item_found = items_names.find { |item| item["id"] == id }
+    item_found["name"].presence || "*Unknown Item*"
   end
 end
--- spec/services/requests_total_items_service_spec.rb
+++ spec/services/requests_total_items_service_spec.rb
@@ -3,10 +3,10 @@ RSpec.describe RequestsTotalItemsService, type: :service do
     subject { described_class.new(requests: requests).calculate }
 
     context 'when the request items is not blank' do
+      let(:sample_items) { Item.active.sample(3) }
+      let(:item_names) { sample_items.pluck(:name) }
+      let(:item_ids) { sample_items.pluck(:id) }
       let(:requests) do
-        items = Item.active.sample(3)
-        item_ids = items.pluck(:id)
-
         [
           create(:request, request_items: item_ids.map { |k| { "item_id" => k, "quantity" => 20 } }),
           create(:request, request_items: item_ids.map { |k| { "item_id" => k, "quantity" => 10 } })
@@ -16,22 +16,37 @@ RSpec.describe RequestsTotalItemsService, type: :service do
       it 'return items with correct quantities calculated' do
         expect(subject.first.last).to eq(30)
       end
+
+      it 'return the names of items correctly' do
+        expect(subject.keys).to eq(item_names)
+      end
+    end
+
+    context 'when item name is nil' do
+      let(:item) do
+        i = build(:item, name: nil)
+        i.save(validate: false)
+        i
+      end
+      let(:requests) do
+        [create(:request, request_items: [{ "item_id" => item.id, "quantity" => 20 }])]
+      end
+
+      it 'return Unknown Item' do
+        expect(subject.first.first).to eq('*Unknown Item*')
+      end
     end
 
     context 'when provided with requests that have no request items' do
       let(:requests) { [create(:request, request_items: {})] }
 
-      it 'return blank when requests itens is blank' do
-        expect(subject).to be_blank
-      end
+      it { is_expected.to be_blank }
     end
 
     context 'when provided requests is nil' do
       let(:requests) { nil }
 
-      it 'return blank when requests itens is blank' do
-        expect(subject).to be_blank
-      end
+      it { is_expected.to be_blank }
     end
   end
 end
