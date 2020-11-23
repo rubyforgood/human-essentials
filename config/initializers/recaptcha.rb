db1c1a7f18b4285af591b16c90af64d3456f36c4
Esquith Allen <18001622+esquith@users.noreply.github.com>
2020-11-23 10:15:04 -0500

    comments

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
 Recaptcha.configure do |config|
-  config.site_key   = Rails.application.secrets[:RECAPTCHA_PUBLIC_KEY]
-  config.secret_key = Rails.application.secrets[:RECAPTCHA_PRIVATE_KEY]
+  # Call keys from .env file here
+  # config.site_key   = Rails.application.secrets[:RECAPTCHA_PUBLIC_KEY]
+  # config.secret_key = Rails.application.secrets[:RECAPTCHA_PRIVATE_KEY]
 end
