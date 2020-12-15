255b4cfd0630dc5d77bfe8701109ac471f337314
Matthew Dodds <matthew.dodds@powerhrg.com>
2020-12-15 14:45:59 -0500

    Avoid failing in the NotifyPartnerJob because of deleted requests

--- app/jobs/notify_partner_job.rb
+++ app/jobs/notify_partner_job.rb
@@ -1,6 +1,8 @@
 class NotifyPartnerJob < ApplicationJob
   def perform(request_id)
-    request = Request.find(request_id)
+    request = Request.find_by(id: request_id)
+
+    return unless request
 
     RequestsConfirmationMailer.confirmation_email(request).deliver_later
   end
