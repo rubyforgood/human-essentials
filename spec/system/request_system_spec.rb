0590995c55bdf21613cfd83bc9bd56d31da98fc5
Matthew Dodds <matthew.dodds@powerhrg.com>
2020-12-15 11:31:07 -0500

    Fix rubocop violation

--- spec/system/request_system_spec.rb
+++ spec/system/request_system_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Requests", type: :system, js: true do
   let(:partner1) { create(:partner, name: "This Guy", email: "thisguy@example.com") }
   let(:partner2) { create(:partner, name: "That Guy", email: "ntg@example.com") }
 
-  before { travel_to Time.zone.local(2020, 01, 01) }
+  before { travel_to Time.zone.local(2020, 1, 1) }
   after { travel_back }
 
   context "#index" do
