c2207b8dc172a683c6b2b03627aff08c796b91b5
Natalia <hi@nataliagalan.me>
2021-01-05 15:00:38 -0600

    working on Capybara test to check for modal on staging env

--- spec/system/log_in_system_spec.rb
+++ spec/system/log_in_system_spec.rb
@@ -17,8 +17,23 @@ RSpec.describe "Authentication", type: :system, js: true do
       fill_in "user_email", with: "deactivated@example.com"
       fill_in "user_password", with: "password"
       find('input[name="commit"]').click
-
       expect(page).to have_content("Invalid Email or password")
     end
   end
+
+  example "Check for modal in staging env" do
+    allow(Rails.env).to receive_message_chain(:staging?).and_return(true)
+    # allow(Rails).to receive_message_chain(:env, :staging?).and_return(true)
+    visit "users/sign_in"
+    find('#warningModal')
+    expect(page).to have_content 'This site is for TEST purposes only!'
+  end
+
+  example "Check there is no modal for non staging env" do
+    allow(Rails.env).to receive_message_chain(:staging?).and_return(false)
+    visit "users/sign_in"
+    page.assert_no_selector('#warningModal')
+  end
+
+  # TODO: write tests for /users/password/new
 end
