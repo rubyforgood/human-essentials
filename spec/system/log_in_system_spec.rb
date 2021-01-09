f9994b2e1b47da8e3d34e2e0dd1d2388fbab5e68
Natalia Galán <hi@nataliagalan.me>
2021-01-09 08:51:33 -0600

    2056 sandbox site warning (#2085)
    * deletes unused icheck from layouts/devise and deletes icheck.min.js
    
    * Prevents users from closing the modal warning by clicking outside of it or pressing the esc key in order to direct their attention to the reminder itself; users can only select the continue button after checking the checkbox; Adds bold, red text and yellow buttons as warning cues; Rewords the warning for non-tech-savvy users; Changes background to a repeated watermark image to visually differentiate from diaper.app; Changes conditional in new.html.erb and devise.html.erb to check for Rails.env.stating
    
    * changes modal id from exampleModal to warningModal; Moves the modal and corresponding JS logic to devise.html.erb so that it works on both the login and pw reset page; Moves custom styles to app/assets/stylesheets/custom.scss; Rewords from safely sign in to if you meant; Removes Safely Sign In btn; Adds minor spacing fixes to code comments
    
    * Adds background image asset for login and pw reset pages
    
    * changes condition check to Rails.env.staging in devise.html.erb file
    
    * working on Capybara test to check for modal on staging env
    
    * adds Capybara tests to check for modal/no modal in login and pw reset pages when in staging env
    
    * Update app/views/layouts/devise.html.erb
    
    Co-authored-by: Edwin Mak <edwinthinks@gmail.com>
    
    * adds Edwins refactor to the capybara test
    
    * Change digest setting in development
    
    Co-authored-by: Edwin Mak <edwinthinks@gmail.com>

--- spec/system/log_in_system_spec.rb
+++ spec/system/log_in_system_spec.rb
@@ -17,8 +17,33 @@ RSpec.describe "Authentication", type: :system, js: true do
       fill_in "user_email", with: "deactivated@example.com"
       fill_in "user_password", with: "password"
       find('input[name="commit"]').click
-
       expect(page).to have_content("Invalid Email or password")
+    end
+  end
+
+  describe 'Showing the modal warning in staging' do
+    ["/users/sign_in", "/users/password/new"].each do |path|
+      context "when accessing #{path} in the staging environment" do
+        before do
+          allow(Rails.env).to receive(:staging?).and_return(true)
+          visit path
+        end
+
+        it 'should render the modal' do
+          expect(page).to have_content 'This site is for TEST purposes only!'
+        end
+      end
+
+      context "when accessing #{path} not in the staging environment" do
+        before do
+          allow(Rails.env).to receive(:staging?).and_return(false)
+          visit path
+        end
+
+        it 'should not render the modal' do
+          page.assert_no_selector('#warningModal')
+        end
+      end
     end
   end
 end
