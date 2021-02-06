3d8d5ea7d4531126bf1f005cf913717266427736
Edwin Mak <edwinthinks@gmail.com>
2021-02-06 12:46:07 -0500

    Fix brittle test and add flipper to hide features

--- app/controllers/partners/sessions_controller.rb
+++ app/controllers/partners/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Partners::SessionsController < Devise::SessionsController
+  before_action :redirect_to_root, unless: -> { Rails.env.test? || Flipper.enabled?(:onebase) }
   before_action :configure_sign_in_params, only: [:create]
 
   skip_before_action :authorize_user
@@ -28,5 +29,11 @@ class Partners::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+
+  private
+
+  def redirect_to_root
+    redirect_to root_path
   end
 end
