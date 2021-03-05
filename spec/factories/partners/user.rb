2c1bba83c24968c3ac5253c26124fc99bb3ba8a5
Edwin <edwinthinks@gmail.com>
2021-03-04 19:16:49 -0500

    Fixs factorybot linting

--- spec/factories/partners/user.rb
+++ spec/factories/partners/user.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :partners_user, class: Partners::User do
     email { Faker::Internet.email }
     password { 'password' }
+    associaton { :partners_partner }
   end
 end
 
