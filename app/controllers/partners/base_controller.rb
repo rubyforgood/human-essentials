198b252d1e634d901732370b59ab7cb7614f1065
Albert Chae <albertchae@users.noreply.github.com>
2021-03-07 01:09:23 -0800

    Use helper_method to move most partner specific helpers into base_controller

--- app/controllers/partners/base_controller.rb
+++ app/controllers/partners/base_controller.rb
@@ -10,5 +10,32 @@ module Partners
     def redirect_to_root
       redirect_to root_path
     end
+
+    helper_method :current_partner
+    def current_partner
+      current_partner_user.partner
+    end
+
+    helper_method :valid_items
+    def valid_items
+      # @valid_items ||= DiaperBankClient.get_available_items(current_partner.diaper_bank_id)
+      @valid_items ||= current_partner.organization.valid_items
+    end
+
+    helper_method :item_id_to_display_string_map
+    def item_id_to_display_string_map
+      @item_id_to_display_string_map ||= valid_items.each_with_object({}) do |item, hash|
+        # hash[item["id"].to_i] = item["name"]
+        hash[item[:id].to_i] = item[:name]
+      end
+    end
+
+    # Copied from partner's application_helper.rb
+    # https://github.com/rubyforgood/partner/blob/c597ca849f0074d3b8a458e1950d19c4b3e6f4d0/app/helpers/application_helper.rb#L10-L12I
+    helper_method :valid_items_for_select
+    def valid_items_for_select(items)
+      # items.map { |item| [item["name"], item["id"]] }.sort
+      items.map { |item| [item[:name], item[:id]] }.sort
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -123,4 +123,14 @@ module ApplicationHelper
       <script>#{base_script}</script>
     HTML
   end
+
+  # Copied from partner's application_helper.rb
+  # https://github.com/rubyforgood/partner/blob/f0df3bc0d3357461c358ab87eeceaa3d3e288ebf/app/helpers/application_helper.rb#L50-L55
+  # using helper_method and sticking it in base_controller doesn't work because of link_to
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, { sort: column, direction: direction }, { class: css_class }
+  end
 end
