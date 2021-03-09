6dacfab265f0cf43968090580e07909d0a545abc
Beigelman <daniel.beigelman@codeminer42.com>
2021-03-09 12:16:38 -0300

    fix: reports requests tests

--- spec/requests/reports/nbdn_requests_spec.rb
+++ spec/requests/reports/nbdn_requests_spec.rb
@@ -12,20 +12,20 @@ RSpec.describe "Reports", type: :request do
 
     describe "GET /index" do
       it "returns http success" do
-        get reports_path(default_params)
+        get reports_nbdn_annual_path(default_params)
         expect(response).to have_http_status(:success)
       end
     end
 
     describe "GET /show" do
       it "returns http success" do
-        get "#{reports_path(default_params)}/2018"
+        get "#{reports_nbdn_annual_path(default_params)}/2018"
         expect(response).to have_http_status(:success)
       end
 
       it "return not found if the year params is not number" do
         expect do
-          get "#{reports_path(default_params)}/test"
+          get "#{reports_nbdn_annual_path(default_params)}/test"
         end.to raise_error(ActionController::RoutingError)
       end
     end
