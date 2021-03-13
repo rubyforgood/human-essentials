1513912befb8c8e623d28feb5cd4d54bf47b8729
Albert Chae <albertchae@users.noreply.github.com>
2021-03-13 11:38:23 -0800

    Fix rubocop errors for hash syntax in production/staging config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -17,12 +17,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "staging.humanessentials.app" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['MAILTRAP_USERNAME'],
-    :password => ENV['MAILTRAP_PASSWORD'],
-    :address => 'smtp.mailtrap.io',
-    :domain => 'smtp.mailtrap.io',
-    :port => '2525',
-    :authentication => :cram_md5
+    user_name: ENV['MAILTRAP_USERNAME'],
+    password: ENV['MAILTRAP_PASSWORD'],
+    address: 'smtp.mailtrap.io',
+    domain: 'smtp.mailtrap.io',
+    port: '2525',
+    authentication: :cram_md5
   }
 
   # Full error reports are disabled and caching is turned on.
