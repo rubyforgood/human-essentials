68d5dac225a16f05152331007563e4f8b884158c
Albert Chae <albertchae@users.noreply.github.com>
2021-03-13 11:38:23 -0800

    Fix rubocop errors for hash syntax in production/staging config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,13 +17,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "diaper.app" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'diaper.app',
-    :enable_starttls_auto => true
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'diaper.app',
+    enable_starttls_auto: true
   }
 
   # Full error reports are disabled and caching is turned on.
