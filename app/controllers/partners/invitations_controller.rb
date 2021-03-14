afea396ee9e9b3b52e355b247a4f691fdd01f0d1
Edwin <edwinthinks@gmail.com>
2021-03-14 08:00:30 -0500

    Add comments in the invitations_controller override

--- app/controllers/partners/invitations_controller.rb
+++ app/controllers/partners/invitations_controller.rb
@@ -7,8 +7,22 @@ class Partners::InvitationsController < Devise::InvitationsController
   # This one causes a redirect require_no_authentication
   skip_before_action :require_no_authentication
 
+  #
+  # This update action was added to override and add new redirection behaviors
+  # when accepting an invitation. Namely, we want do not want to redirect
+  # newly invited users to the unfinished version of partnerbase within
+  # this repo. Instead we'll want to redirect them to the legacy partnerbase
+  # application until we are ready.
+  #
+  # **Once we've migrated all users to partnerbase within this repo,
+  # we can erase this file entirely**
+  #
+  # Here is a reference of the original endpoint code that that was modified
+  # https://github.com/scambra/devise_invitable/blob/f6a308d427de1bd263337be4a334f586962ce589/app/controllers/devise/invitations_controller.rb#L45-L68
+  #
   def update
     raw_invitation_token = update_resource_params[:invitation_token]
+    # The self.resource is a partner_user record
     self.resource = accept_resource
     invitation_accepted = resource.errors.empty?
 
@@ -16,6 +30,8 @@ class Partners::InvitationsController < Devise::InvitationsController
 
     if invitation_accepted
       if redirect_to_legacy_partnerbase_app?
+        # Redirect to the legacy partnerbase login url outside of development
+        # to avoid them from going to the unfinished partnerbase in the repo.
         redirect_to legacy_partnerbase_login_url
       elsif resource.class.allow_insecure_sign_in_after_accept
         flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
@@ -43,3 +59,4 @@ class Partners::InvitationsController < Devise::InvitationsController
     ENV.fetch("PARTNER_BASE_URL") + "/users/sign_in"
   end
 end
+
