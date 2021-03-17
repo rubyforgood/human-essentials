ec89c8f5dfa15068ae2df5441590f0e2ae95295c
dropkickfish <thackrayaf@gmail.com>
2021-03-17 17:06:06 +0000

    Updated spec with context for staging

--- spec/system/account_request_system_spec.rb
+++ spec/system/account_request_system_spec.rb
@@ -1,66 +1,71 @@
 RSpec.describe 'Account request flow', type: :system, js: true do
-  it 'should prompt prospective users to request an account on the live app if the new account form is accessed on a staging environment' do
-    visit new_account_request_path
+  context 'when in staging' do
+    before do
+      allow(Rails.env).to receive(:staging?).and_return(true)
+    end
 
-    # Check for staging
-    if Rails.env.development? || Rails.env.production? || Rails.env.test?
-      expect(page).to have_selector(id: 'new_account_request')
-    else
+    it 'should prompt prospective users to request an account on the live app' do
+      visit new_account_request_path
       expect(page).to have_link('click here', href: 'https://diaper.app/account_requests/new')
     end
   end
 
-  it 'should allow prospect users to request an account via a form. And that request form data gets used to create an organization' do
-    visit root_path
+  context 'when not in staging' do
+    before do
+      allow(Rails.env).to receive(:staging?).and_return(false)
+    end
+    it 'should allow prospective users to request an account via a form. And that request form data gets used to create an organization' do
+      visit root_path
 
-    click_button('Request A Demo', match: :first)
+      click_button('Request A Demo', match: :first)
 
-    account_request_attrs = FactoryBot.attributes_for(:account_request)
+      account_request_attrs = FactoryBot.attributes_for(:account_request)
 
-    fill_in 'Name', with: account_request_attrs[:name]
-    fill_in 'Email', with: account_request_attrs[:email]
-    fill_in 'Organization name', with: account_request_attrs[:organization_name]
-    fill_in 'Organization website', with: account_request_attrs[:organization_website]
-    fill_in 'Request Details (min 50 characters)', with: account_request_attrs[:request_details]
+      fill_in 'Name', with: account_request_attrs[:name]
+      fill_in 'Email', with: account_request_attrs[:email]
+      fill_in 'Organization name', with: account_request_attrs[:organization_name]
+      fill_in 'Organization website', with: account_request_attrs[:organization_website]
+      fill_in 'Request Details (min 50 characters)', with: account_request_attrs[:request_details]
 
-    expect(AccountRequest.count).to eq(0)
+      expect(AccountRequest.count).to eq(0)
 
-    expect { click_button 'Submit' }.to change(AccountRequest, :count).by(1)
+      expect { click_button 'Submit' }.to change(AccountRequest, :count).by(1)
 
-    created_account_request = AccountRequest.last
+      created_account_request = AccountRequest.last
 
-    # Request Received
-    expect(page).to have_content('Request Received!')
-    expect(page).to have_content("We've sent you a email with instructions on next steps at #{created_account_request.email}!")
+      # Request Received
+      expect(page).to have_content('Request Received!')
+      expect(page).to have_content("We've sent you a email with instructions on next steps at #{created_account_request.email}!")
 
-    # Access link within email they would have received
-    visit confirmation_account_requests_path(token: created_account_request.identity_token)
+      # Access link within email they would have received
+      visit confirmation_account_requests_path(token: created_account_request.identity_token)
 
-    expect(created_account_request.confirmed_at).to eq(nil)
+      expect(created_account_request.confirmed_at).to eq(nil)
 
-    click_link 'I\'m ready! Let\'s go!'
+      click_link 'I\'m ready! Let\'s go!'
 
-    expect(created_account_request.reload.confirmed_at).not_to eq(nil)
+      expect(created_account_request.reload.confirmed_at).not_to eq(nil)
 
-    expect(page).to have_content('Confirmed!')
-    expect(page).to have_content('We will be processing your request now.')
+      expect(page).to have_content('Confirmed!')
+      expect(page).to have_content('We will be processing your request now.')
 
-    # Access link within email sent to admin user to process the request.
-    sign_in(@super_admin)
-    visit new_admin_organization_path(token: created_account_request.identity_token)
+      # Access link within email sent to admin user to process the request.
+      sign_in(@super_admin)
+      visit new_admin_organization_path(token: created_account_request.identity_token)
 
-    fill_in 'Short name', with: 'fakeshortname'
+      fill_in 'Short name', with: 'fakeshortname'
 
-    click_button 'Save'
+      click_button 'Save'
 
-    # Expect to see the a new organization with the name provided
-    # originally in the AccountRequest
-    expect(page).to have_content('All Diaperbase Organizations')
-    expect(page).to have_content(created_account_request.organization_name)
-    expect(page).to have_content(created_account_request.email)
+      # Expect to see the a new organization with the name provided
+      # originally in the AccountRequest
+      expect(page).to have_content('All Diaperbase Organizations')
+      expect(page).to have_content(created_account_request.organization_name)
+      expect(page).to have_content(created_account_request.email)
 
-    # Ensure the AccountRequest is not considered processed
-    expect(created_account_request.reload.processed?).to eq(true)
+      # Ensure the AccountRequest is not considered processed
+      expect(created_account_request.reload.processed?).to eq(true)
+    end
   end
 end
 
