3ab8f0e5edb701b6a6dd7ff2afe02ffad2e1bf5e
Edwin <edwinthinks@gmail.com>
2021-03-20 15:03:46 -0500

    Override the subject for when partner users invite other partner users

---
+++ app/mailers/custom_devise_mailer.rb
@@ -0,0 +1,16 @@
+class CustomDeviseMailer < Devise::Mailer
+  protected
+
+  def subject_for(key)
+    return super unless key.to_s == 'invitation_instructions'
+
+    # Replace the invitation instruction subject for partner users
+    # that were invited by other partner users.
+
+    if resource.is_a?(PartnerUser) && resource.id != resource.partner.primary_user.id
+      "You've been invited to #{resource.partner.name}'s partnerbase account"
+    else
+      super
+    end
+  end
+end
