d7337ecd7ac06ee5aa8b4f36f1b546f7dba03ca5
Edwin <edwinthinks@gmail.com>
2021-03-20 15:34:15 -0500

    Add browser spec for inviting coworkers as a partner user

--- spec/system/partners/coworker_invitations_system_spec.rb
+++ spec/system/partners/coworker_invitations_system_spec.rb
@@ -1,59 +1,31 @@
 RSpec.describe "Coworking invitations", type: :system, js: true do
-  describe 'creating a request' do
+  describe 'inviting a new user as a partner user' do
     let(:partner_user) { partner.primary_partner_user }
     let!(:partner) { FactoryBot.create(:partner) }
 
-    context 'GIVEN a partner user is permitted to make a request' do
+    context 'GIVEN a partner user complete the process to invite a coworker' do
+      let(:new_user_name) { Faker::Name.first_name }
+      let(:new_user_email) { Faker::Internet.email }
+
       before do
         login_as(partner_user, scope: :partner_user)
-        visit new_partners_request_path
+        visit partner_user_root_path
+
+        click_on partner_user.email
+        click_on 'My Co-Workers'
+
+        click_on 'Invite new user'
+
+        fill_in 'Name', with: new_user_name
+        fill_in 'Email', with: new_user_email
+
+        click_on 'Create User'
+
+        assert page.has_content? "You have invited #{new_user_name} to join your organization!"
       end
 
-      context 'WHEN they create a request properly' do
-        let(:items_to_select) { Organization.find(partner_user.partner.diaper_bank_id).valid_items.sample(3) }
-        let(:item_details) do
-          items_to_select.map do |item|
-            {
-              name: item[:name],
-              quantity: Faker::Number.within(range: 5..25)
-            }
-          end
-        end
-
-        before do
-          fill_in 'Comments', with: Faker::Lorem.paragraph
-
-          # Select items
-          item_details.each_with_index do |item, idx|
-            if idx != 0
-              click_link 'Add Another Item'
-            end
-
-            last_row = find_all('tr').last
-            last_row.find('option', text: item[:name], exact_text: true).select_option
-            last_row.find_all('.form-control').last.fill_in(with: item[:quantity])
-          end
-        end
-
-        context 'THEN a request records will be created and the partner will be notified via flash message on the dashboard' do
-          before do
-            expect { click_button 'Submit Essentials Request' }.to change { Partners::Request.count + Request.count }.by(2)
-
-            expect(current_path).to eq(partners_dashboard_path)
-            expect(page).to have_content('Request was successfully created.')
-          end
-
-          it 'AND the partner_user can view the details of the created request in a seperate page' do
-            partner_request_id = Partners::Request.last.id
-
-            visit partners_request_path(id: Partners::Request.last.id)
-
-            expect(page).to have_content("#{partner_user.partner.name} Request ID: #{partner_request_id}")
-            item_details.each do |item|
-              expect(page).to have_content("#{item[:quantity]} of #{item[:name]}")
-            end
-          end
-        end
+      it 'should create a new partner user for the partner account' do
+        expect(Partners::User.find_by(email: new_user_email, partner: partner_user.partner)).not_to eq(nil)
       end
     end
   end
