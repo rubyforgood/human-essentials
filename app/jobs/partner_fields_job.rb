8429f44fbfb4a0de24d403d07f25640ac8fcb02a
Edwin Mak <edwinthinks@gmail.com>
2021-03-22 16:20:06 -0500

    Utilize DB connection to partner to update partner sections (#2237)

--- app/jobs/partner_fields_job.rb
+++
@@ -1,9 +0,0 @@
-class PartnerFieldsJob
-  include Sidekiq::Worker
-  include PartnerFormClient
-
-  def perform(organization_id)
-    @organization = Organization.find(organization_id)
-    PartnerFormClient.post(@organization.attributes)
-  end
-end
