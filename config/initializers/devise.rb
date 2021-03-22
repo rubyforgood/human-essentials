b5d24ee1e63901624de489d0b5f4854674723969
Edwin <edwinthinks@gmail.com>
2021-03-22 16:33:07 -0500

    Fix the incorrect subject on partner invitations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = '"Diaper App" <accounts@diaper.app>'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = "CustomDeviseMailer"
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
