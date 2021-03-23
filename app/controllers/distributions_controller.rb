639fb2d4a78afb4140823721c571c27ea459acb4
Edwin <edwinthinks@gmail.com>
2021-03-23 08:48:07 -0500

    Fix ordering of distributions list

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -39,7 +39,7 @@ class DistributionsController < ApplicationController
 
     @distributions = current_organization
                      .distributions
-                     .order('distributions.created_at ASC')
+                     .order('distributions.created_at DESC')
                      .apply_filters(filter_params, helpers.selected_range)
     @paginated_distributions = @distributions.page(params[:page])
     @total_value_all_distributions = total_value(@distributions)
