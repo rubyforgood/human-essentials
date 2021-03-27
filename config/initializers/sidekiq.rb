54498f3a929eb685e68bd8a3d826058d4c34a417
Edwin Mak <edwinthinks@gmail.com>
2021-03-27 16:49:12 -0500

    Enable recertification requests without the legacy API approach (#2236)
    * Add the ability to recertify partners without the API
    
    * Add system spec of the requesting recertification
    
    * Update spec/services/partner_request_recertification_service_spec.rb
    
    Co-authored-by: Scott Steele <scottlsteele@gmail.com>
    
    Co-authored-by: Scott Steele <scottlsteele@gmail.com>

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -13,3 +13,18 @@ Sidekiq::Scheduler.enabled = ENV["DISABLE_SIDEKIQ_SCHEDULER"] ? false : true
 Sidekiq.schedule = YAML.load_file(File.expand_path(Rails.root + 'config/scheduler.yml', __FILE__))
 SidekiqScheduler::Scheduler.load_schedule!
 Sidekiq::Extensions.enable_delay!
+
+#
+# Added this conditional to run the async jobs immediately
+# in development. This allows us to see the results of
+# enqueuing a mailer job instantly.
+#
+# This effectively turns .deliver_later to .deliver_now in
+# the development environment.
+#
+# Refer to https://makandracards.com/makandra/28125-perform-sidekiq-jobs-immediately-in-development
+# for context and details.
+if Rails.env.development?
+  require 'sidekiq/testing'
+  Sidekiq::Testing.inline!
+end
