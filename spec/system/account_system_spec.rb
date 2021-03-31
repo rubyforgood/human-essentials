b92c51f800e2d465db242eb975b99c09b85d613d
Andy Thackray <33702528+dropkickfish@users.noreply.github.com>
2021-03-31 13:02:20 +0100

    2211 prevent demo users from changing the email & password of shared account in staging (#2253)
    * Prevent users from changing staging credentials
    
    * Prevent users from changing staging credentials

--- spec/system/account_system_spec.rb
+++ spec/system/account_system_spec.rb
@@ -3,33 +3,54 @@ RSpec.describe "User account management", type: :system, js: true do
 
   before do
     sign_in(@user)
-    visit subject
   end
 
-  it "should change an user name" do
-    name = @user.name + "aaa"
-    fill_in "Name", with: name
-    fill_in "user_current_password", with: @user.password
-    click_button "Save"
+  context 'when in staging' do
+    before do
+      allow(Rails.env).to receive(:staging?).and_return(true)
+      visit subject
+    end
 
-    expect(page).to have_content(name)
+    it 'should display staging warning' do
+      expect(page).to have_selector('.staging-warning')
+    end
+
+    it 'should not allow the user to change staging credentials' do
+      expect(page).to have_button('Save', disabled: true)
+    end
   end
 
-  it "should change the email" do
-    email = "example@example.com"
-    fill_in "Email", with: email
-    fill_in "user_current_password", with: @user.password
-    click_button "Save"
+  context 'when not in staging' do
+    before do
+      allow(Rails.env).to receive(:staging?).and_return(false)
+      visit subject
+    end
 
-    expect(page).to have_content('Your account has been updated successfully.')
-  end
+    it "should change an user name" do
+      name = @user.name + "aaa"
+      fill_in "Name", with: name
+      fill_in "user_current_password", with: @user.password
+      click_button "Save"
 
-  it "should fail when the email is invalid" do
-    invalid_email = "invalid email"
-    fill_in "Email", with: invalid_email
-    fill_in "user_current_password", with: @user.password
-    click_button "Save"
+      expect(page).to have_content(name)
+    end
 
-    expect(page).to have_content('is invalid')
+    it "should change the email" do
+      email = "example@example.com"
+      fill_in "Email", with: email
+      fill_in "user_current_password", with: @user.password
+      click_button "Save"
+
+      expect(page).to have_content('Your account has been updated successfully.')
+    end
+
+    it "should fail when the email is invalid" do
+      invalid_email = "invalid email"
+      fill_in "Email", with: invalid_email
+      fill_in "user_current_password", with: @user.password
+      click_button "Save"
+
+      expect(page).to have_content('is invalid')
+    end
   end
 end
