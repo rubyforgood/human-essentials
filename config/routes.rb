fde05b4e280b8a9f58e4dc14208c96d4beadcfe7
Albert Chae <albertchae@users.noreply.github.com>
2021-04-04 16:21:02 -0700

    Copy FamilyRequests stuff verbatim from partner
    - copy route but leave out pickup sheet

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
     resource :dashboard, only: [:show]
     resources :requests, only: [:show, :new, :index, :create]
     resources :individuals_requests, only: [:new, :create]
+    resources :family_requests, only: [:new, :create]
     resources :users, only: [:index, :new, :create]
     resource :profile, only: [:show, :edit, :update]
     resource :approval_request, only: [:create]
