72e64cb6b3171d0d499b860fb6858923ee02fc15
albertchae <albertchae@users.noreply.github.com>
2021-04-10 13:49:44 -0700

    Update partner models with annotations (#2276)
    Just ran `bundle exec rake annotate_models`

--- spec/models/partners/child_spec.rb
+++ spec/models/partners/child_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: children
+#
+#  id                   :bigint           not null, primary key
+#  active               :boolean          default(TRUE)
+#  archived             :boolean
+#  child_lives_with     :jsonb
+#  comments             :text
+#  date_of_birth        :date
+#  first_name           :string
+#  gender               :string
+#  health_insurance     :jsonb
+#  item_needed_diaperid :integer
+#  last_name            :string
+#  race                 :jsonb
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  agency_child_id      :string
+#  family_id            :bigint
+#
 require "rails_helper"
 
 RSpec.describe Partners::Child, type: :model do
