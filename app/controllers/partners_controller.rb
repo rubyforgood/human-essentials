8ed09953691e6d269bccd5a625a30e1be6beed63
Scott Steele <scottlsteele@gmail.com>
2021-04-10 17:43:37 -0400

    refactor: deactivate/reactivate partner with db service object
    Fixes #2168

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -126,12 +126,14 @@ class PartnersController < ApplicationController
 
   def deactivate
     @partner = current_organization.partners.find(params[:id])
-    response = DiaperPartnerClient.put(partner_id: @partner.id, status: "deactivated")
 
-    if response.is_a?(Net::HTTPSuccess) && @partner.update(status: "deactivated")
+    svc = PartnerDeactivateService.new(partner: @partner)
+    svc.call
+
+    if svc.errors.none?
       redirect_to partners_path, notice: "#{@partner.name} successfully deactivated!"
     else
-      redirect_to partners_path, error: "#{@partner.name} failed to deactivate!"
+      redirect_to partners_path, error: "#{@partner.name} failed to deactivate due to: #{svc.errors.full_messages}"
     end
   end
 
@@ -142,11 +144,13 @@ class PartnersController < ApplicationController
       redirect_to(partners_path, error: "#{@partner.name} is not deactivated!") && return
     end
 
-    response = DiaperPartnerClient.put(partner_id: @partner.id, status: "verified")
-    if response.is_a?(Net::HTTPSuccess) && @partner.update(status: "approved")
+    svc = PartnerReactivateService.new(partner: @partner)
+    svc.call
+
+    if svc.errors.none?
       redirect_to partners_path, notice: "#{@partner.name} successfully reactivated!"
     else
-      redirect_to partners_path, error: "#{@partner.name} failed to reactivate!"
+      redirect_to partners_path, error: "#{@partner.name} failed to reactivate due to: #{svc.errors.full_messages}"
     end
   end
 
