1a1b61d0e97236c89bf272b2f3394b6f12450018
Albert Chae <albertchae@users.noreply.github.com>
2021-04-10 12:53:30 -0700

    Edit paths and nest under partner modules - page loads successfully

--- app/controllers/partners/family_requests_controller.rb
+++ app/controllers/partners/family_requests_controller.rb
@@ -1,26 +1,24 @@
-class FamilyRequestsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :verify_status_in_diaper_base
-  before_action :authorize_verified_partners
+module Partners
+  class FamilyRequestsController < BaseController
+    def new
+      @filterrific = initialize_filterrific(
+        current_partner.children
+                       .order(last_name: :asc)
+                       .order(first_name: :asc),
+        params[:filterrific]
+      ) || return
+      @children = @filterrific.find
+    end
 
-  def new
-    @filterrific = initialize_filterrific(
-      current_partner.children
-          .order(last_name: :asc)
-          .order(first_name: :asc),
-      params[:filterrific]
-    ) || return
-    @children = @filterrific.find
-  end
+    def create
+      children = current_partner.children.active.where.not(item_needed_diaperid: [nil, 0])
+      request = FamilyRequestPayloadService.execute(children: children, partner: current_partner)
 
-  def create
-    children = current_partner.children.active.where.not(item_needed_diaperid: [nil, 0])
-    request = FamilyRequestPayloadService.execute(children: children, partner: current_partner)
+      FamilyRequestService.execute(request)
 
-    FamilyRequestService.execute(request)
-
-    redirect_to partner_requests_path, notice: "Requested items successfuly!"
-  rescue ActiveModel::ValidationError
-    render :new
+      redirect_to partner_requests_path, notice: "Requested items successfully!"
+    rescue ActiveModel::ValidationError
+      render :new
+    end
   end
 end
