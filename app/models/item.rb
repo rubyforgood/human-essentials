a9751e07917ec321b365185b46fe180f94c3f2f8
Daniel Beigelman <50420424+Beigelman@users.noreply.github.com>
2021-04-17 09:29:47 -0300

    2261 prevent setting items to a negative number (#2283)
    * Create distribution quantity field validation
    
    * Create migration to convert negative distribution quantatiy values to positive
    
    * Add postgres contraint on distribution quantity field
    
    * Add validation tests do item model
    
    * fix lint errors

--- app/models/item.rb
+++ app/models/item.rb
@@ -32,6 +32,8 @@ class Item < ApplicationRecord
   validates :name, uniqueness: { scope: :organization }
   validates :name, presence: true
   validates :organization, presence: true
+  validates :distribution_quantity, :on_hand_recommended_quantity, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
+  validates :on_hand_minimum_quantity, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :line_items, dependent: :destroy
   has_many :inventory_items, dependent: :destroy
