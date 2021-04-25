f50fa1fc9c034b2fabc69a819f7426af1db79e92
Albert Chae <albertchae@users.noreply.github.com>
2021-04-24 19:45:28 -0700

    Update active storage
    Followed the recommended fix in https://github.com/rails/rails/issues/40895
    
    ```
    rails active_storage:update
    rails db:migrate
    ```
    
    This fixes the factory_bot:lint task and many specs

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_175100) do
-
+ActiveRecord::Schema.define(version: 20_210_425_024_157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+    t.index %w(record_type record_id name), name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -43,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index %w(record_type record_id name blob_id), name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -54,7 +53,16 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index %w(blob_id variation_digest), name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "adjustments", id: :serial, force: :cascade do |t|
@@ -91,7 +99,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.datetime "updated_at", null: false
     t.integer "organization_id"
     t.string "barcodeable_type", default: "Item"
-    t.index ["barcodeable_type", "barcodeable_id"], name: "index_barcode_items_on_barcodeable_type_and_barcodeable_id"
+    t.index %w(barcodeable_type barcodeable_id), name: "index_barcode_items_on_barcodeable_type_and_barcodeable_id"
     t.index ["organization_id"], name: "index_barcode_items_on_organization_id"
   end
 
@@ -118,7 +126,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.string "business_name"
     t.float "latitude"
     t.float "longitude"
-    t.index ["latitude", "longitude"], name: "index_diaper_drive_participants_on_latitude_and_longitude"
+    t.index %w(latitude longitude), name: "index_diaper_drive_participants_on_latitude_and_longitude"
   end
 
   create_table "diaper_drives", force: :cascade do |t|
@@ -157,7 +165,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.integer "organization_id"
     t.float "latitude"
     t.float "longitude"
-    t.index ["latitude", "longitude"], name: "index_donation_sites_on_latitude_and_longitude"
+    t.index %w(latitude longitude), name: "index_donation_sites_on_latitude_and_longitude"
     t.index ["organization_id"], name: "index_donation_sites_on_organization_id"
   end
 
@@ -204,7 +212,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
+    t.index %w(feature_key key value), name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "inventory_items", id: :serial, force: :cascade do |t|
@@ -234,6 +242,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.index ["kit_id"], name: "index_items_on_kit_id"
     t.index ["organization_id"], name: "index_items_on_organization_id"
     t.index ["partner_key"], name: "index_items_on_partner_key"
+    t.check_constraint "distribution_quantity >= 0", name: "distribution_quantity_nonnegative"
   end
 
   create_table "kits", force: :cascade do |t|
@@ -244,7 +253,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.boolean "active", default: true
     t.boolean "visible_to_partners", default: true, null: false
     t.integer "value_in_cents", default: 0
-    t.index ["name", "organization_id"], name: "index_kits_on_name_and_organization_id", unique: true
+    t.index %w(name organization_id), name: "index_kits_on_name_and_organization_id", unique: true
     t.index ["organization_id"], name: "index_kits_on_organization_id"
   end
 
@@ -255,7 +264,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.string "itemizable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["itemizable_id", "itemizable_type"], name: "index_line_items_on_itemizable_id_and_itemizable_type"
+    t.index %w(itemizable_id itemizable_type), name: "index_line_items_on_itemizable_id_and_itemizable_type"
   end
 
   create_table "manufacturers", force: :cascade do |t|
@@ -286,7 +295,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.integer "default_storage_location"
     t.text "partner_form_fields", default: [], array: true
     t.integer "account_request_id"
-    t.index ["latitude", "longitude"], name: "index_organizations_on_latitude_and_longitude"
+    t.index %w(latitude longitude), name: "index_organizations_on_latitude_and_longitude"
     t.index ["short_name"], name: "index_organizations_on_short_name"
   end
 
@@ -341,7 +350,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.float "longitude"
     t.integer "square_footage"
     t.string "warehouse_type"
-    t.index ["latitude", "longitude"], name: "index_storage_locations_on_latitude_and_longitude"
+    t.index %w(latitude longitude), name: "index_storage_locations_on_latitude_and_longitude"
     t.index ["organization_id"], name: "index_storage_locations_on_organization_id"
   end
 
@@ -387,7 +396,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
-    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
+    t.index %w(invited_by_type invited_by_id), name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
@@ -403,9 +412,10 @@ ActiveRecord::Schema.define(version: 2021_01_07_175100) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["latitude", "longitude"], name: "index_vendors_on_latitude_and_longitude"
+    t.index %w(latitude longitude), name: "index_vendors_on_latitude_and_longitude"
   end
 
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "adjustments", "organizations"
   add_foreign_key "adjustments", "storage_locations"
   add_foreign_key "adjustments", "users"
