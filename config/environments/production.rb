5131c98838f71f860fd17d9ef32ea0c72d2d7219
Edwin <edwinthinks@gmail.com>
2021-04-27 11:20:26 -0500

    Fix the default_url_option for mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.action_mailer.default_url_options = { host: "diaper.app" }
+  config.action_mailer.default_url_options = { host: "www.humanessentials.app" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
