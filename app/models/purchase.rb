aff0a063af9b0562f42a3d3bcbc6e3e038fb853b
Samantha Lurio <samantha.lurio@gmail.com>
2021-05-20 07:57:23 -0400

    #2354 Add purchase date in the export CSV for purchases (#2369)
    * purchase date added to export CSV
    
    * updated csv purchase date code accoring to comments

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -87,16 +87,17 @@ class Purchase < ApplicationRecord
   end
 
   def self.csv_export_headers
-    ["Purchases from", "Storage Location", "Quantity of Items", "Variety of Items", "Amount spent in Cents"]
+    ["Purchases from", "Storage Location", "Purchased Date", "Quantity of Items", "Variety of Items", "Amount spent in Cents"]
   end
 
   def csv_export_attributes
     [
       purchased_from_view,
       storage_location.name,
+      issued_at.strftime("%Y-%m-%d"),
       line_items.total,
       line_items.size,
-      amount_spent_in_cents,
+      amount_spent_in_cents
     ]
   end
 
