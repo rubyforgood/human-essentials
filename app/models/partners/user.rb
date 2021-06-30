52d9f47dcf4814c5b96fc8c32d52c6cd11b35940
Edwin <edwinthinks@gmail.com>
2021-06-29 21:32:21 -0400

    Move devise definitions in partner_usersso that trackable option works

--- app/models/partners/user.rb
+++ app/models/partners/user.rb
@@ -30,11 +30,6 @@ module Partners
   class User < Base
     self.table_name = "users"
 
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable, :invitable
-
     has_many :requests, class_name: 'Partners::Request', foreign_key: :partner_id, dependent: :destroy, inverse_of: :user
     belongs_to :partner, dependent: :destroy
   end
