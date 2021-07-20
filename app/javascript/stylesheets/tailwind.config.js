60ab98efe7704b96ef87d423617d31d4ad80b67b
Edwin <edwinthinks@gmail.com>
2021-07-20 18:54:02 -0500

    Add a bit of visual treatment on pages to indicate failure or succes

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse-once': 'pulse 1s ease-in-out'
+      }
+    },
   },
   variants: {
     extend: {},
