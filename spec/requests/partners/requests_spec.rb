ee407c2e70ed4bc005f4b79272fbf93a594f39b8
Edwin Mak <edwinthinks@gmail.com>
2021-07-20 20:04:49 -0500

    Prevent confusion caused by not being clear why requests fail or when they succeed (#2438)
    * Add a bit of visual treatment on pages to indicate failure or succes
    
    * Make it more clear when a request is made correctly versus not
    
    * Add helpful logging to capture request failures
    
    * Fix broken spec

--- spec/requests/partners/requests_spec.rb
+++ spec/requests/partners/requests_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe "/partners/requests", type: :request do
       end
       it 'should redirect to the dashboard' do
         subject.call
-        expect(response).to redirect_to(partners_dashboard_path)
+        expect(response).to redirect_to(partners_request_path(Request.last.id))
       end
     end
 
