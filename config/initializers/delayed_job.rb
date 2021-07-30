33e2a2a69836c118c63ae97ea67b0cccd9e4ef28
Mauricio Lima <mauricio.lima@codeminer42.com>
2021-07-30 12:42:28 -0300

    chore: install delayed_job gem

---
+++ config/initializers/delayed_job.rb
@@ -0,0 +1,9 @@
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 60
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.read_ahead = 10
+Delayed::Worker.default_queue_name = 'default'
+Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.raise_signal_exceptions = :term
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
