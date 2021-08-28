5a73dbd9d7e617291918f65e613440ad08f87305
Edwin <edwinthinks@gmail.com>
2021-08-28 17:54:05 -0500

    Update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -132,7 +132,6 @@ $(document).ready(function () {
   var hash = location.hash.replace(/^#/, '');  // ^ means starting, meaning only match the first hash
   if (hash) {
     $('.nav-tabs a[href="#' + hash + '"]').tab('show');
-
   }
 
   // Change hash for page-reload
