bf35d1c430a5755f1786247c6c6c56c40c1d44aa
Ikko Ashimine <eltociear@gmail.com>
2021-09-19 01:06:42 +0900

    Fix typo in seeds.rb
    seperate -> separate

--- db/seeds.rb
+++ db/seeds.rb
@@ -187,7 +187,7 @@ note = [
   # ----------------------------------------------------------------------------
   # Creating associated records within the Partnerbase database
   #
-  # **We have two seperate database. One for diaperbase and the other for partnerbase**
+  # **We have two separate database. One for diaperbase and the other for partnerbase**
   # ----------------------------------------------------------------------------
 
   partner = Partners::Partner.create!({
