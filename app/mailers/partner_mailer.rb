c839128d6f53e5bf46d3494cc6ef66e31a1b8dd7
Patrick McClernan <patmccler@gmail.com>
2021-10-02 12:38:05 -0400

    Send email when applicant approved by org (#2532)

--- app/mailers/partner_mailer.rb
+++ app/mailers/partner_mailer.rb
@@ -6,4 +6,11 @@ class PartnerMailer < ApplicationMailer
 
     mail to: partner.email, subject: "[Action Required] Please Update Your Agency Information"
   end
+
+  def application_approved(partner:)
+    @partner = partner
+    @organization = partner.organization
+
+    mail to: partner.email, subject: "Application Approved"
+  end
 end
