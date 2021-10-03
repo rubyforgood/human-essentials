630a25f88cdfb6e6404b56f5a1e8ecdf77ffe999
Edwin <edwinthinks@gmail.com>
2021-10-02 19:52:18 -0500

    Include requestor information in the request list view

--- spec/models/partners/request_spec.rb
+++ spec/models/partners/request_spec.rb
@@ -17,7 +17,6 @@ require "rails_helper"
 RSpec.describe Partners::Request, type: :model do
   describe 'associations' do
     it { should belong_to(:partner) }
-    it { should belong_to(:partner_user) }
     it { should have_many(:item_requests).dependent(:destroy) }
     it { should have_many(:child_item_requests).through(:item_requests) }
   end
