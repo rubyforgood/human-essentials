2e76ff73777b21a22d21583b56b06ea57f93a953
exbinary <exbinary@gmail.com>
2021-10-12 22:29:31 -0400

    Stop webmock from spawning too many connections
    See: https://github.com/teamcapybara/capybara#gotchas

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -4,4 +4,4 @@ allowed_sites = [
   "https://github.com/mozilla/geckodriver/releases",
   "https://selenium-release.storage.googleapis.com"
 ]
-WebMock.disable_net_connect!(allow_localhost: true, allow: allowed_sites)
+WebMock.disable_net_connect!(allow_localhost: true, allow: allowed_sites, net_http_connect_on_start: true)
