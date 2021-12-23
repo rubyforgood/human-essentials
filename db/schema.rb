95bc2373fe6e5cb5c05ddaa00d68b8272018cce7
Bob Mazanec <bobmazanec@users.noreply.github.com>
2021-12-23 13:42:04 -0600

    Restore schema version to match contents (#2646)
    036b1a7 overwrote the version with an earlier one
    (essentially "30 July 2021 15:33:47"), but did not remove the
    "24 Sep 2021" version's changes.
    
    Co-authored-by: Bob Mazanec <bob@wizeservices.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_730_153_347) do
+ActiveRecord::Schema.define(version: 2021_09_24_162800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
