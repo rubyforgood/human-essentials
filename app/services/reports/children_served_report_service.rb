443acdea154d9dfc48fec424b46a92fafa34ce27
Daniel Orner <daniel.orner@wishabi.com>
2021-12-30 15:11:09 -0500

    2097: Children Served report

--- app/services/reports/children_served_report_service.rb
+++ app/services/reports/children_served_report_service.rb
@@ -1,31 +1,50 @@
 module Reports
   class ChildrenServedReportService
+    include ActionView::Helpers::NumberHelper
     attr_reader :year, :organization
 
+    # @param year [Integer]
+    # @param organization [Organization]
     def initialize(year:, organization:)
       @year = year
       @organization = organization
     end
 
+    # @return [Hash]
     def report
-      @report ||= {
-        children_served_by_partner: children_served_by_partner.to_s,
-        monthly_children_served: monthly_children_served
-      }
+      @report ||= { name: 'Children Served',
+                    entries: {
+                      'Average children served monthly' => number_with_delimiter(average_children_monthly.round),
+                      'Total children served' => number_with_delimiter(total_children_served),
+                      'Diapers per child monthly' => number_with_delimiter(per_child_monthly.round),
+                      'Repackages diapers?' => organization.repackage_essentials? ? 'Y' : 'N',
+                      'Monthly diaper distributions?' => organization.distribute_monthly? ? 'Y' : 'N'
+                    } }
     end
 
-    def columns_for_csv
-      %i[children_served_by_partner monthly_children_served]
+    # @return [Integer]
+    def total_children_served
+      @total_children_served ||= organization
+                                 .distributions
+                                 .for_year(year)
+                                 .joins(line_items: :item)
+                                 .merge(Item.disposable)
+                                 .sum('line_items.quantity / items.distribution_quantity') || 0
     end
 
-    def children_served_by_partner
-      organization.partners.map do |partner|
-        partner.profile.children.count
-      end.sum
+    # @return [Float]
+    def average_children_monthly
+      total_children_served / 12.0
     end
 
-    def monthly_children_served
-      (children_served_by_partner / 12).to_s
+    # @return [Float]
+    def per_child_monthly
+      organization
+        .distributions
+        .for_year(year)
+        .joins(line_items: :item)
+        .merge(Item.disposable)
+        .average('items.distribution_quantity') || 0.0
     end
   end
 end
--- spec/services/reports/children_served_report_service_spec.rb
+++ spec/services/reports/children_served_report_service_spec.rb
@@ -1,36 +1,55 @@
-RSpec.xdescribe Reports::ChildrenServedReportService, type: :service do
-  describe "#children_served_by_partner" do
-    it "counts the children from partner organizations" do
-      family = create(:partners_family, partner: organization.partners.first.profile)
-      create(:partners_child, family: family)
+RSpec.describe Reports::ChildrenServedReportService, type: :service, skip_seed: true do
+  let(:year) { 2020 }
+  let(:organization) { create(:organization) }
 
-      expect(report.children_served_by_partner).to eq(1)
-    end
-
-    it "does't count the children from other organizations partners" do
-      other_organization = create(:organization)
-      create(:partner, organization_id: other_organization.id)
-      family = create(:partners_family, partner: other_organization.partners.first.profile)
-      create(:partners_child, family: family)
-
-      expect(report.children_served_by_partner).to eq(0)
-    end
+  subject(:report) do
+    described_class.new(organization: organization, year: year)
   end
 
-  describe "#monthly_children_served" do
-    it "finds the monthly average number of children" do
-      family = create(:partners_family, partner: organization.partners.first.profile)
-      12.times { create(:partners_child, family: family) }
-
-      expect(report.monthly_children_served).to eq("1")
+  describe '#report' do
+    it 'should report zero values' do
+      expect(report.report).to eq({
+                                    name: 'Children Served',
+                                    entries: {
+                                      'Average children served monthly' => "0",
+                                      'Total children served' => "0",
+                                      'Diapers per child monthly' => "0",
+                                      'Repackages diapers?' => 'N',
+                                      'Monthly diaper distributions?' => 'N'
+                                    }
+                                  })
     end
-  end
 
-  def organization
-    @organization ||= create(:organization)
-  end
+    it 'should report normal values' do
+      seed_base_items_for_tests
+      Organization.seed_items(organization)
+      organization.update!(distribute_monthly: true, repackage_essentials: true)
 
-  def report
-    described_class.new(organization: organization, year: Time.zone.now.year)
+      within_time = Time.zone.parse("2020-05-31 14:00:00")
+      outside_time = Time.zone.parse("2019-05-31 14:00:00")
+
+      disposable_item = organization.items.disposable.first
+      disposable_item.update!(distribution_quantity: 20)
+      non_disposable_item = organization.items.where.not(id: organization.items.disposable).first
+
+      # Distributions
+      distributions = create_list(:distribution, 2, issued_at: within_time, organization: organization)
+      outside_distributions = create_list(:distribution, 2, issued_at: outside_time, organization: organization)
+      (distributions + outside_distributions).each do |dist|
+        create_list(:line_item, 5, :distribution, quantity: 200, item: disposable_item, itemizable: dist)
+        create_list(:line_item, 5, :distribution, quantity: 300, item: non_disposable_item, itemizable: dist)
+      end
+
+      expect(report.report).to eq({
+                                    name: 'Children Served',
+                                    entries: {
+                                      'Average children served monthly' => "8",
+                                      'Total children served' => "100",
+                                      'Diapers per child monthly' => "20",
+                                      'Repackages diapers?' => 'Y',
+                                      'Monthly diaper distributions?' => 'Y'
+                                    }
+                                  })
+    end
   end
 end
