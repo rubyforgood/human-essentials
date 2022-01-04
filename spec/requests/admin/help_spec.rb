f0b2b6e545f8ddc9da56ec6aa3e55b779f37656e
Andy Strube <andystrube64@gmail.com>
2022-01-04 13:04:07 -0800

    Added a test for the admin help page

--- spec/requests/admin/help_spec.rb
+++ spec/requests/admin/help_spec.rb
@@ -1,21 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Helps", type: :request do
-  # let(:default_params) do
-  #   { organization_id: @organization.id }
-  # end
+  context "while signed in as a super admin" do
+    before do
+      sign_in(@super_admin_no_org)
+    end
 
-  # context "When logged in as a super admin" do
-  #   before do
-  #     sign_in(@super_admin)
-  #   end
-
-  #   describe "GET #partners_help" do
-  #     it "returns http success" do
-  #       get partners_help_path
-  #       expect(response).to be_successful
-  #     end
-  #   end
-  # end
-
+    it "allows a user to load the help page" do
+      get admin_help_path
+      expect(response).to be_successful
+    end
+  end
 end
