e0ff853f24333758fd672c339d9755054bbd4b40
Andy Strube <andystrube64@gmail.com>
2022-01-06 11:32:02 -0800

    wrote test for articles_for_partners function

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,5 +12,26 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  
+  describe "#articles_for_partners" do
+    it "should filter out articles meant for organizations" do
+      article_1 = build(:article)
+      article_1.update_attribute(:for_partners, false)
+      article_1.update_attribute(:for_organizations, true)
+
+      article_2 = build(:article)
+      article_2.update_attribute(:for_partners, true)
+      article_2.update_attribute(:for_organizations, false)
+
+      article_3 = build(:article)
+      article_3.update_attribute(:for_partners, true)
+      article_3.update_attribute(:for_organizations, true)
+      
+      articles = [article_1, article_2, article_3]
+      partner_articles = Article.articles_for_partners
+
+      expect(partner_articles.count).to eq 2
+      expect(partner_articles.first.for_partners).to eq true
+      expect(partner_articles.last.for_partners).to eq true
+    end
+  end
 end
