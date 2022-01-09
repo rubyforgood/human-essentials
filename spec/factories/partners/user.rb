155015f43d2f4194deaab41fb29830559b159b32
Edwin <edwinthinks@gmail.com>
2022-01-09 10:34:13 -0500

    Ensure every partner_user has a unique email address

--- spec/factories/partners/user.rb
+++ spec/factories/partners/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :partners_user, class: Partners::User do
     name { "Partner User" }
-    email { "partner_user@example.com" }
+    sequence(:email) { |n| "partner_user_#{n}@example.com" }
     partner { Partners::Partner.first || create(:partners_partner) }
     password { "password!" }
     password_confirmation { "password!" }
