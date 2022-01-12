75055765f12bfa6dde2d813d08cbe552a484b463
Daniel Orner <daniel.orner@wishabi.com>
2022-01-11 22:09:47 -0500

    Add N/A test

--- spec/requests/storage_locations_requests_spec.rb
+++ spec/requests/storage_locations_requests_spec.rb
@@ -108,24 +108,37 @@ RSpec.describe "StorageLocations", type: :request do
           expect(response).to be_successful
           expect(response.body).to include("Smithsonian")
           expect(response.body).to include("Test Item")
-          expect(response.body).to include("<td>200</td>")
+          expect(response.body).to include("200")
         end
 
         context "with version date set" do
-          it "should show the version specified" do
-            travel 1.day do
-              inventory_item.update!(quantity: 100)
+          context "with a version found" do
+            it "should show the version specified" do
+              travel 1.day do
+                inventory_item.update!(quantity: 100)
+              end
+              travel 1.week do
+                inventory_item.update!(quantity: 300)
+              end
+              travel 2.weeks do
+                get storage_location_path(storage_location, default_params.merge(format: response_format,
+                  version_date: 9.days.ago.to_date.to_s(:db)))
+                expect(response).to be_successful
+                expect(response.body).to include("Smithsonian")
+                expect(response.body).to include("Test Item")
+                expect(response.body).to include("100")
+              end
             end
-            travel 1.week do
-              inventory_item.update!(quantity: 300)
-            end
-            travel 2.weeks do
+          end
+
+          context "with no version found" do
+            it "should show N/A" do
               get storage_location_path(storage_location, default_params.merge(format: response_format,
-                version_date: 9.days.ago.to_date.to_s(:db)))
+                version_date: 1.week.ago.to_date.to_s(:db)))
               expect(response).to be_successful
               expect(response.body).to include("Smithsonian")
               expect(response.body).to include("Test Item")
-              expect(response.body).to include("<td>100</td>")
+              expect(response.body).to include("N/A")
             end
           end
         end
