face984ffa5721a923aa15215c0806bd8d18ba85
Edwin Mak <edwinthinks@gmail.com>
2022-01-14 06:00:40 -0500

    Enable setting reminder and deadline on partner group (#2680)
    * Add new columns to PartnerGroup
    
    * Add service to fetch partners that need to be reminded
    
    * Add conditionals to handle deactivated & send_reminder
    
    * Configure deadline email job to utilize partner group definitions
    
    * WIP
    
    * Add UI to set reminder deadlines for partner groups
    
    * Remove unused scope in Organization
    
    * Update app/javascript/packs/application.js
    
    * Update Gemfile.lock
    
    * Update Gemfile.lock
    
    * Okay Rubocop, you win
    
    * Add helpful comment inReminderDeadlineJob
    
    * Add PR suggestions
    
    * Add PR suggestion on JS
    
    * Add metadata on ReminderDeadlineJob
    
    * Address more PR suggestions

---
+++ app/services/partners/fetch_partners_to_remind_now_service.rb
@@ -0,0 +1,22 @@
+module Partners
+  class FetchPartnersToRemindNowService
+    def fetch
+      current_day = Time.current.day
+      deactivated_status = ::Partner.statuses[:deactivated]
+
+      partners_with_group_reminders = ::Partner.left_joins(:partner_group)
+        .where(partner_groups: {reminder_day_of_month: current_day})
+        .where.not(partner_groups: {deadline_day_of_month: nil})
+        .where.not(status: deactivated_status)
+
+      partners_with_only_organization_reminders = ::Partner.left_joins(:partner_group, :organization)
+        .where(partner_groups: {reminder_day_of_month: nil})
+        .where(send_reminders: true)
+        .where(organizations: {reminder_day: current_day})
+        .where.not(organizations: {deadline_day: nil})
+        .where.not(status: deactivated_status)
+
+      (partners_with_group_reminders + partners_with_only_organization_reminders).flatten.uniq
+    end
+  end
+end
