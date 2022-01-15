36fcc88e95aa65b07763dc2c7902db8f2abdc47b
Andy Strube <andystrube64@gmail.com>
2022-01-14 16:07:36 -0800

    Made search method private in the help controller

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -3,6 +3,8 @@ class HelpController < ApplicationController
     @bank_questions = search(params[:keyword])
   end
 
+  private
+
   def search(keyword)
     if keyword.present?
       Question.for_banks.where("title ILIKE ?", "%#{keyword}%")
