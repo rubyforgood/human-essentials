a9226c29698380d2f288f60b5bc3d4622b9faf8f
Daniel Orner <daniel.orner@wishabi.com>
2022-01-25 10:14:07 -0500

    last spec fix

--- app/models/account_request.rb
+++ app/models/account_request.rb
@@ -77,7 +77,7 @@ class AccountRequest < ApplicationRecord
 
   def email_not_already_used_by_user
     user = User.find_by(email: email)
-    if user && user.organization_id != organization.id
+    if user && (!organization || user.organization_id != organization.id)
       errors.add(:email, 'already used by an existing User')
     end
   end
