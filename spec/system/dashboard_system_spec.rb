dd1ed6add8a0d85984131c6205ceb770a1107fc7
Bob Mazanec <bob@wizeservices.com>
2022-01-26 12:02:10 -0600

    Run only the dashboard spec
    to compare its CI runtime with another branch

--- spec/system/dashboard_system_spec.rb
+++ spec/system/dashboard_system_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "Dashboard", type: :system, js: true, skip_seed: true do
+RSpec.describe "Dashboard", type: :system, js: true, skip_seed: true, focus: true do
   context "With a new Diaper bank" do
     before :each do
       @new_organization = create(:organization)
