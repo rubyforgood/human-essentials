659df13ab035b38399ab67697fecb9bc5c999a04
Sean Marcia <smarcia@gmail.com>
2022-01-26 22:12:26 -0500

    Hotfix for strong migrations prod issue (#2733)

--- db/migrate/20211230033135_add_fields_to_organization.rb
+++ db/migrate/20211230033135_add_fields_to_organization.rb
@@ -1,6 +1,8 @@
 class AddFieldsToOrganization < ActiveRecord::Migration[6.1]
   def change
-    add_column :organizations, :repackage_essentials, :boolean, null: false, default: false
-    add_column :organizations, :distribute_monthly, :boolean, null: false, default: false
+    add_column :organizations, :repackage_essentials, :boolean, null: false
+    add_column :organizations, :distribute_monthly, :boolean, null: false
+    change_column_default :organizations, :repackage_essentials, from: nil, to: false
+    change_column_default :organizations, :distribute_monthly, from: nil, to: false
   end
 end
