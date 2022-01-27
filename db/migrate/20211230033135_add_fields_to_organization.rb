2ba8ea239f7a742c0345d7b87a166c0f6740f50b
Edwin <edwinthinks@gmail.com>
2022-01-27 06:42:44 -0500

    Ensure organizations have the repackage_essentials defined before adding not null constraint

--- db/migrate/20211230033135_add_fields_to_organization.rb
+++ db/migrate/20211230033135_add_fields_to_organization.rb
@@ -1,8 +1,6 @@
 class AddFieldsToOrganization < ActiveRecord::Migration[6.1]
   def change
-    add_column :organizations, :repackage_essentials, :boolean, null: false
-    add_column :organizations, :distribute_monthly, :boolean, null: false
-    change_column_default :organizations, :repackage_essentials, from: nil, to: false
-    change_column_default :organizations, :distribute_monthly, from: nil, to: false
+    add_column :organizations, :repackage_essentials, :boolean, default: false, null: false
+    add_column :organizations, :distribute_monthly, :boolean, default: false, null: false
   end
 end
