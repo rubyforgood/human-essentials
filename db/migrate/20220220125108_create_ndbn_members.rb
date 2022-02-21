84ba7f618395fd2ac8651b8f6dddf49364f97f09
Edwin Mak <edwinthinks@gmail.com>
2022-02-21 15:14:39 -0500

    [2748] Add ability to capture and sync NDBN Membership ids & account names (#2771)
    * Add SyncNDBNMembers
    
    * Add rake task to run NDBN syncing
    
    * Update to use Nokogiri instead of new gem
    
    * Use Webmock

---
+++ db/migrate/20220220125108_create_ndbn_members.rb
@@ -0,0 +1,9 @@
+class CreateNdbnMembers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ndbn_members, primary_key: :ndbn_member_id do |t|
+      t.string :account_name, null: false
+
+      t.timestamps
+    end
+  end
+end
