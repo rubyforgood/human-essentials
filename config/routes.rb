e60a0af9f111eb028ffc465fb6b21c39ec127ce5
Daniel Orner <daniel.orner@wishabi.com>
2022-02-21 22:43:34 -0500

    2557: Add Login with Google functionality (#2755)
    * 2557: Log in using Google
    
    * Lint fixes
    
    * Fix spec

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,10 @@ def set_up_flipper
 end
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   devise_for :partner_users, controllers: { sessions: "partners/sessions", invitations: 'partners/invitations', passwords: 'partners/passwords' }
   resources :logins, only: [:new, :create], controller: "consolidated_logins"
 
