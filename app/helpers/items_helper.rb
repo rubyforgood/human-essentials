594a5915c4a8dde507e167ed8aef9e7ec331f730
Bob Mazanec <cable.tripper_0p@icloud.com>
2022-02-23 16:06:37 -0600

    Show "$0" on dashboard
    Move dollar_value()'s number_to_currency logic to dollar_presentation(),
    and use that function in the org. dashboard so "$0" shows when
    appropriate
    
    dollar_value() uses dollar_presentation() for the basic formatting, but keeps
    its "blank if zero?" logic so existing callers are unaffected

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,10 +1,18 @@
 # Encapsulates methods that need some business logic
 module ItemsHelper
+  def dollar_presentation(value)
+    dollars = cents_to_dollar value
+    rounds_to_self = dollars.round == value
+    precision = rounds_to_self ? 0 : 2
+
+    ActionController::Base.helpers.number_to_currency dollars, precision: precision
+  end
+
   def dollar_value(value, addition = '')
     if value.zero?
       ''
     else
-      addition + ActionController::Base.helpers.number_to_currency(cents_to_dollar(value), precision: cents_to_dollar(value).round == value ? 0 : 2)
+      addition + dollar_presentation(value)
     end
   end
 
