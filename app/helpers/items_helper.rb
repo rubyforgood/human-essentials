b6b4fbf22e5ebd99e4f12ade924b82b12236d64f
Bob Mazanec <cable.tripper_0p@icloud.com>
2022-02-24 09:43:06 -0600

    Add/restore parens
    to match style

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,18 +1,18 @@
 # Encapsulates methods that need some business logic
 module ItemsHelper
   def dollar_presentation(value)
-    dollars = cents_to_dollar value
+    dollars = cents_to_dollar(value)
     rounds_to_self = dollars.round == value
     precision = rounds_to_self ? 0 : 2
 
-    ActionController::Base.helpers.number_to_currency dollars, precision: precision
+    ActionController::Base.helpers.number_to_currency(dollars, precision: precision)
   end
 
   def dollar_value(value)
     if value.zero?
       ''
     else
-      dollar_presentation value
+      dollar_presentation(value)
     end
   end
 
