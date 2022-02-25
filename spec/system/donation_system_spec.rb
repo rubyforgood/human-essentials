7828fe694eb43ca537e3abae2bf6f503634b0314
CL Fisher <clfisher@rogers.com>
2022-02-25 11:05:55 -0500

    creating a new product drive instead of new diaper drive -- excluding the button "create diaper drive", which is pending the change to "ProductDrive" for the active record, etc.

--- spec/system/donation_system_spec.rb
+++ spec/system/donation_system_spec.rb
@@ -220,10 +220,10 @@ RSpec.describe "Donations", type: :system, js: true do
           end.to change { Donation.count }.by(1)
         end
 
-        it "Allows User to create a Diaper Drive from donation" do
+        it "Allows User to create a Product Drive from donation" do
           select Donation::SOURCES[:diaper_drive], from: "donation_source"
           select "---Create new Product Drive---", from: "donation_diaper_drive_id"
-          expect(page).to have_content("New Diaper Drive")
+          expect(page).to have_content("New Product Drive")
           fill_in "diaper_drive_name", with: "drivenametest"
           fill_in "diaper_drive_start_date", with: Time.current
           click_on "diaper_drive_submit"
