b7309b667c697f9ad7bf6a78051c9f3741696ea8
Sean Marcia <smarcia@gmail.com>
2022-03-26 22:04:35 -0400

    Revert "chore: node maint and upgrade (#2829)" (#2841)
    This reverts commit 1418e5bb7b973b9ac583cc42acb15528b0a62e3b.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import "actiontext"
 import Highcharts from 'highcharts';
 require("highcharts/modules/data")(Highcharts)
 require("highcharts/modules/exporting")(Highcharts)
