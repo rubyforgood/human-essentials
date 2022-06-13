ee35af4d1fcd5f9a3b6db1d0eb914589d917d146
Daniel Orner <daniel.orner@wishabi.com>
2022-06-12 23:20:35 -0400

    2895: Show inventory for storage location (#2955)
    * 2895: Show inventory for storage location
    
    * Fix specs
    
    * Spec fix
    
    * one more fix

--- spec/models/request_item_spec.rb
+++ spec/models/request_item_spec.rb
@@ -4,24 +4,57 @@
 RSpec.describe RequestItem, type: :model do
   context "Methods >" do
     describe "#from_json" do
-      let(:organization) { build :organization }
-      let(:request) { build :request, organization: organization }
-      let(:request_item_json) { request.request_items.first }
-      let(:item) { Item.find(request_item_json['item_id']) }
+      let(:organization) { create :organization }
+      let(:location) { create :storage_location, organization: organization }
+      let(:other_location) { create :storage_location, organization: organization }
+      let(:request) { build :request, organization: organization, request_items: request_item_json }
+      let(:item1) { create(:item, organization: organization) }
+      let(:item2) { create(:item, organization: organization) }
+      let(:request_item_json) do
+        [
+          {item_id: item1.id, quantity: 15}.stringify_keys,
+          {item_id: item2.id, quantity: 18}.stringify_keys
+        ]
+      end
 
-      subject { described_class.from_json(request_item_json, organization) }
+      subject do
+        organization.update!(default_storage_location: location.id)
+        described_class.from_json(request_item_json.first, request)
+      end
+
+      before(:each) do
+        create(:inventory_item,
+          storage_location: other_location,
+          item_id: item1.id,
+          quantity: 10)
+        create(:inventory_item,
+          storage_location: location,
+          item_id: item1.id,
+          quantity: 20)
+        create(:inventory_item,
+          storage_location: other_location,
+          item_id: item2.id,
+          quantity: 30)
+        create(:inventory_item,
+          storage_location: location,
+          item_id: item2.id,
+          quantity: 40)
+      end
 
       it 'has the correct name' do
-        expect(subject.name).to eq(item.name)
+        expect(subject.name).to eq(item1.name)
       end
 
       it 'has the correct quantity' do
-        expect(subject.quantity).to eq(request_item_json['quantity'])
+        expect(subject.quantity).to eq(15)
       end
 
       it 'has the correct amount on hand' do
-        # NOTE: this is an unpersisted organization that shouldn't have anything on hand
-        expect(subject.on_hand).to eq(0)
+        expect(subject.on_hand).to eq(30)
+      end
+
+      it 'has the correct amount on hand for location' do
+        expect(subject.on_hand_for_location).to eq(20)
       end
     end
   end
