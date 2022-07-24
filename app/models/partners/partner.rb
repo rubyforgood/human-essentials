f3a6cf9ace1cb126958dcc04c170cc634b6a508c
Maxwell Brundige <mbrundige@ksmconsulting.com>
2022-07-23 22:30:57 -0400

    [3037] hide show/edit fields that are no longer used

--- app/models/partners/partner.rb
+++ app/models/partners/partner.rb
@@ -101,6 +101,21 @@ module Partners
     has_one_attached :proof_of_form_990
     has_many_attached :documents
 
+    # TODO [7/23/2022] - check to see if the following fields should still be ignored
+    self.ignored_columns = %w[
+      evidence_based_description
+      program_client_improvement
+      incorporate_plan
+      turn_away_child_care
+      responsible_staff_position
+      trusted_pickup
+      serve_income_circumstances
+      internal_db
+      maac
+      pick_up_method
+      ages_served
+    ]
+
     VERIFIED_STATUS = 'verified'.freeze
     RECERTIFICATION_REQUESTED_STATUS = 'recertification_required'.freeze
     DEACTIVATED_STATUS = "deactivated".freeze
