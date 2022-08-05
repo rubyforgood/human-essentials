fbcd32ac19293773c061d832c5c9638387d5f6dd
Italo Matos <italomatos@Mac-mini-de-Italo.local>
2022-08-05 20:01:52 -0300

    fix invitation instructions texts and add comment to explain the reason user object needs to reload before send email

--- app/services/partner_invite_service.rb
+++ app/services/partner_invite_service.rb
@@ -9,6 +9,8 @@ class PartnerInviteService
     return self unless valid?
 
     partner.update!(status: 'invited')
+    # skip invitation is necessary because when email will be send for this situation needs partner reference updated,
+    # and, in this case, we create invite, reload object and send invitation email.
     user = User.invite!(email: partner.email, partner: partner.profile, skip_invitation: true)
 
     user.reload
