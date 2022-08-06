b8332ee985d9361b421caad1aa1cbf37ed70f539
Edwin <edwinthinks@gmail.com>
2022-08-06 13:24:42 -0500

    Fix rubocop violations

--- clock.rb
+++ clock.rb
@@ -1,8 +1,8 @@
-require 'clockwork'
-require File.expand_path('../config/environment', __FILE__)
+require "clockwork"
+require File.expand_path("../config/environment", __FILE__)
 include Clockwork
 
-every(1.days, 'Reset staging demo seed', at: '01:30') do
+every(1.day, "Reset staging demo seed", at: "01:30") do
   if Rails.env.staging?
     `rails reset_demo`
   end
