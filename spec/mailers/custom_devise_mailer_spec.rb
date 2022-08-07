d36a88beaeacfd2d2234386d3bf19aea8fad4dfd
Ítalo Matos <italomatos@gmail.com>
2022-08-07 10:04:12 -0300

    Update resource before to process email so that partner invitation emails have correct body (#3027)
    * remove rspec-retry
    
    * Revert "remove rspec-retry"
    
    This reverts commit 890b29cfd29df370ce2e47136cd2d58999db3e17.
    
    * add spec to partner invite service and hotfix to send correct email
    
    * add spec to check subject and body invite
    
    * refactory to use user model
    
    * fix partner invite service spec
    
    * fix invitation_instructions to partner users
    
    * fix invitation instructions texts and add comment to explain the reason user object needs to reload before send email
    
    * fix spec expect message
    
    Co-authored-by: Italo Matos <italomatos@Mac-mini-de-Italo.local>
    Co-authored-by: Edwin Mak <edwinthinks@gmail.com>

---
+++ spec/mailers/custom_devise_mailer_spec.rb
@@ -0,0 +1,41 @@
+RSpec.describe CustomDeviseMailer, type: :mailer, skip_seed: true do
+  describe "#invitation_instructions" do
+    let(:user) { create(:user) }
+    let(:mail) { described_class.invitation_instructions(user, SecureRandom.uuid) }
+
+    context "when partner is invited" do
+      let(:partner) do
+        partner = create(:partner, :uninvited)
+        partner.profile.primary_user.delete
+        partner.profile.reload
+        partner
+      end
+
+      let(:user) { create(:user, partner: partner.profile) }
+
+      it "invites to primary user" do
+        expect(mail.subject).to eq("You've been invited to be a partner with #{user.partner.organization.name}")
+        expect(mail.html_part.body).to include("You've been invited to become a partner with <strong>#{user.partner.organization.name}!</strong>")
+      end
+    end
+
+    context "when other partner users invited" do
+      let(:partner) { create(:partner) }
+      let(:user) { create(:user, partner: partner.profile) }
+
+      it "invites to partner user" do
+        expect(mail.subject).to eq("You've been invited to #{user.partner.name}'s partnerbase account")
+        expect(mail.html_part.body).to include("You've been invited to <strong>#{user.partner.name}'s</strong> account for requesting items from <strong>#{user.partner.organization.name}!")
+      end
+    end
+
+    context "when user is invited" do
+      let(:user) { create(:user) }
+
+      it "invites to user" do
+        expect(mail.subject).to eq("Your Human Essentials App Account Approval")
+        expect(mail.html_part.body).to include("Your request has been approved and you're invited to become an user of the Human Essentials inventory management system!")
+      end
+    end
+  end
+end
