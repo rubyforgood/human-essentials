a271a689d9b7d11fe5952580e12e9fe93017a254
Andy Strube <andystrube64@gmail.com>
2022-09-12 14:08:29 -0700

    Partners can edit their names (#3141)
    * Partner users can edit their names
    
    * Added some tests
    
    Co-authored-by: Edwin Mak <edwinthinks@gmail.com>

--- spec/requests/partners/user_requests_spec.rb
+++ spec/requests/partners/user_requests_spec.rb
@@ -8,6 +8,28 @@ RSpec.describe "/partners/users", type: :request do
     sign_in(partner_user)
   end
 
+  describe "GET #edit" do
+    it "successfully loads the page" do
+      get edit_partners_user_path(partner_user)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "PATCH #update" do
+    it "lets the name be updated", :aggregate_failures do
+      patch partners_user_path(
+        id: partner_user.id,
+        user: {
+          name: "New name"
+        }
+      )
+      expect(response).to be_redirect
+      expect(response.request.flash[:success]).to eq "User information was successfully updated!"
+      partner_user.reload
+      expect(partner_user.name).to eq "New name"
+    end
+  end
+
   describe "GET #switch_to_bank_role" do
     context "with a bank role" do
       it "should redirect to the bank path" do
