badc1f37c56857d5f6c495b54b0f466178b24431
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2022-09-15 19:04:43 -0500

    Change delete on storage locations to deactivate (#3122)
    * Allow users to deactivate Storage Locations
    
    * Move StorageLocation discard responsibility
    
    * Remove specific error class
    
    Co-authored-by: Edwin Mak <edwinthinks@gmail.com>
    
    Co-authored-by: Edwin Mak <edwinthinks@gmail.com>

--- app/models/partners/partner.rb
+++ app/models/partners/partner.rb
@@ -17,6 +17,8 @@
 #  describe_storage_space         :text
 #  distribution_times             :string
 #  distributor_type               :string
+#  enable_child_based_requests    :boolean          default(TRUE), not null
+#  enable_individual_requests     :boolean          default(TRUE), not null
 #  essentials_budget              :string
 #  essentials_funding_source      :string
 #  essentials_use                 :string
