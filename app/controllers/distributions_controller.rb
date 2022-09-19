cff84460e99b18cab45cbb3cf6957403787ab1a1
Edwin <edwinthinks@gmail.com>
2022-09-19 17:32:27 -0500

    Update distributions flow to use turbo_stream flash

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -7,7 +7,7 @@ class DistributionsController < ApplicationController
   include DateRangeHelper
   include DistributionHelper
 
-  before_action :enable_turbo!, only: %i[index]
+  before_action :enable_turbo!, only: %i[index new show]
   skip_before_action :authenticate_user!, only: %i(calendar)
   skip_before_action :authorize_user, only: %i(calendar)
 
@@ -76,7 +76,11 @@ class DistributionsController < ApplicationController
       flash[:notice] = "Distribution created!"
 
       perform_inventory_check
-      redirect_to(distribution_path(result.distribution)) && return
+
+      respond_to do |format|
+        format.html { redirect_to distribution_path(result.distribution) }
+        format.turbo_stream { redirect_to distribution_path(result.distribution) }
+      end
     else
       @distribution = result.distribution
       if params[:request_id].present?
@@ -84,11 +88,23 @@ class DistributionsController < ApplicationController
         # does not match any known Request
         @distribution.request = Request.find(params[:request_id])
       end
-      flash[:error] = insufficient_error_message(result.error.message)
       @distribution.line_items.build if @distribution.line_items.size.zero?
       @items = current_organization.items.alphabetized
       @storage_locations = current_organization.storage_locations.alphabetized
-      render :new
+
+      flash_error = insufficient_error_message(result.error.message)
+
+      respond_to do |format|
+        format.html do
+          flash[:error] = flash_error
+          render :new 
+        end
+
+        format.turbo_stream do 
+          flash.now[:error] = flash_error 
+          render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash")
+        end
+      end
     end
   end
 
