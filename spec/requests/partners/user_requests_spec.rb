4982fba3280cefc66f8fc37fe73c44e2534b94e8
Daniel Orner <daniel.orner@wishabi.com>
2022-09-20 11:06:02 -0400

    Remove old tests

--- spec/requests/partners/user_requests_spec.rb
+++ spec/requests/partners/user_requests_spec.rb
@@ -28,23 +28,4 @@ RSpec.describe "/partners/users", type: :request do
       expect(partner_user.name).to eq "New name"
     end
   end
-
-  describe "GET #switch_to_bank_role" do
-    context "with a bank role" do
-      it "should redirect to the bank path" do
-        partner_user.update!(organization_id: @organization.id)
-        get switch_to_bank_role_partners_users_path
-        expect(response).to redirect_to(dashboard_path(@organization))
-      end
-    end
-
-    context "without a bank role" do
-      it "should redirect to the root path with an error" do
-        get switch_to_bank_role_partners_users_path
-        message = "Attempted to switch to a bank role but you have no bank associated with your account!"
-        expect(flash[:alert]).to eq(message)
-        expect(response).to redirect_to("/")
-      end
-    end
-  end
 end
