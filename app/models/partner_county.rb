cc7d55b7e8bf8a26a907352b4f5087cb059b2e3f
CL Fisher <clfisher@rogers.com>
2022-10-21 13:04:44 -0400

    partner county validation

--- app/models/partner_county.rb
+++ app/models/partner_county.rb
@@ -12,4 +12,5 @@
 class PartnerCounty < ApplicationRecord
   belongs_to :partner
   belongs_to :county
+  validates_inclusion_of :client_share, :in=>0..100
 end
--- spec/models/partner_county_spec.rb
+++ spec/models/partner_county_spec.rb
@@ -20,5 +20,12 @@ RSpec.describe PartnerCounty, type: :model do
     it "must belong to a county" do
       expect(build(:partner_county, partner: create(:partner), county_id: nil, client_share: 0)).not_to be_valid
     end
+    it "must only allow  client shares between 0 and 100 inclusive" do
+      expect(build(:partner_county, partner: create(:partner), county: create(:county), client_share: -1)).not_to be_valid
+      expect(build(:partner_county, partner: create(:partner), county: create(:county), client_share: 101)).not_to be_valid
+      expect(build(:partner_county, partner: create(:partner), county: create(:county), client_share: 1)).to be_valid
+      expect(build(:partner_county, partner: create(:partner), county: create(:county), client_share: 100)).to be_valid
+    end
+
   end
 end
