0d8e824d21b19cbb92f02b4689853c3819c7853d
Edwin <edwinthinks@gmail.com>
2022-10-22 13:15:22 -0500

    Update AUthenticationLayoutComponent to render content

--- spec/components/authentication_layout_component_spec.rb
+++ spec/components/authentication_layout_component_spec.rb
@@ -3,8 +3,13 @@
 require "rails_helper"
 
 RSpec.describe AuthenticationLayoutComponent, type: :component do
-  subject { render_inline(described_class.new(side_image_path:  side_image_path)) }
+  subject do
+    render_inline(described_class.new(side_image_path:  side_image_path)) do
+      content
+    end
+  end
   let(:side_image_path) { 'https://via.placeholder.com/300x300' }
+  let(:content) { 'Content' }
 
   it 'should have the logo component' do
     expect(subject.text).to include('Human Essentials')
@@ -12,6 +17,10 @@ RSpec.describe AuthenticationLayoutComponent, type: :component do
 
   it 'should have the image with the src set to the side_image_path' do
     expect(subject.css('img').first.attributes['src'].value).to eq(side_image_path)
+  end
+
+  it 'should render the content' do
+    expect(subject.text).to include(content)
   end
 
 end
