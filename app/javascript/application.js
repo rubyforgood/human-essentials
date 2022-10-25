e2a6d09d5fddcdc4ad02a1e60f2d418f10186caf
Alvaro Sanchez <hello@alvarosanchez.dev>
2022-10-25 10:26:06 -0500

    WIP: Enable and use import maps

---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import ConfettiGenerator from "confetti-js";
+let conf = new ConfettiGenerator(0);
+console.log("Hello from importmap!")
---
+++ config/importmap.rb
@@ -0,0 +1,6 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "admin-lte", to: "https://ga.jspm.io/npm:admin-lte@3.2.0/dist/js/adminlte.min.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
+pin "confetti-js", to: "https://ga.jspm.io/npm:confetti-js@0.0.18/dist/index.js"
