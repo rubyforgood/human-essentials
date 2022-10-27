e0e2e7884e418afa5b5ee3b301db8519ac8b4769
Edwin <edwinthinks@gmail.com>
2022-10-27 09:48:42 -0500

    Fix issue sidenav with nested options not opening

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+/**
+ * Load all javascript needed to run the AdminLTE theme and
+ * all the interactions.
+ */
+import 'admin-lte'
+
 console.log("Hello from importmap-rails!")
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "admin-lte", to: "https://ga.jspm.io/npm:admin-lte@3.2.0/dist/js/adminlte.min.js"
