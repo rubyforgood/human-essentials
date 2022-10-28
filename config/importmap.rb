f09e67861fef77123fea7fd0060bfa0cb26e1e45
Edwin <edwinthinks@gmail.com>
2022-10-27 20:52:23 -0500

    Fix issue in which line items UI supported by cocoon was not working

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,5 @@
 
 pin "application", preload: true
 pin "admin-lte", to: "https://ga.jspm.io/npm:admin-lte@3.2.0/dist/js/adminlte.min.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js", preload: true
+pin "@oddcamp/cocoon-vanilla-js", to: "https://ga.jspm.io/npm:@oddcamp/cocoon-vanilla-js@1.1.3/index.js"
