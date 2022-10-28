bacf4e64269501ae8a5dc1bf636f0f250d589b8b
Edwin <edwinthinks@gmail.com>
2022-10-27 20:52:23 -0500

    Fix issue in which line items UI supported by cocoon was not working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,11 @@
  * Load all javascript needed to run the AdminLTE theme and
  * all the interactions.
  */
+import jQuery from 'jquery'
+window.jQuery = jQuery
+window.$ = jQuery
+
 import 'admin-lte'
+import "@oddcamp/cocoon-vanilla-js";
 
 console.log("Hello from importmap-rails!")
