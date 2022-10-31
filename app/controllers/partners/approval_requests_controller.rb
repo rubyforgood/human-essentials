40e92f4950a159108299485944da47fd6cd5e30e
Andy Strube <andystrube64@gmail.com>
2022-10-31 14:53:16 -0700

    Small refactor

--- app/controllers/partners/approval_requests_controller.rb
+++ app/controllers/partners/approval_requests_controller.rb
@@ -7,7 +7,7 @@ module Partners
       if svc.errors.none?
         flash[:success] = "You have submitted your details for approval."
       else
-        flash[:error] = "#{svc.errors.to_a.join(" ")}"
+        flash[:error] = svc.errors.to_a.first
       end
 
       redirect_to partners_profile_path
