8ccdba2142847198f1d669fff270b4efe3992e6f
CL Fisher <clfisher@rogers.com>
2022-11-16 15:48:19 -0500

    sorting the report by region, then county name
    sorting the report by region, then county name

--- app/controllers/distributions_by_county_controller.rb
+++ app/controllers/distributions_by_county_controller.rb
@@ -2,34 +2,41 @@ class DistributionsByCountyController < ApplicationController
   include DateRangeHelper
   include DistributionHelper
 
+
+
   def show
     setup_date_range_picker
     distributions = current_organization.distributions.includes(:partner).during(helpers.selected_range)
-    @breakdown = {}
-    @breakdown["Unspecified"] = {}
-    @breakdown["Unspecified"][:num_items] = 0
-    @breakdown["Unspecified"][:amount] = 0.00
+    breakdown_hash = {}
+    breakdown_hash["Unspecified"] = {}
+    breakdown_hash["Unspecified"][:region] = "ZZZ" #after all natural region names
+    breakdown_hash["Unspecified"][:num_items] = 0
+    breakdown_hash["Unspecified"][:amount] = 0.00
     distributions.each do |distribution|
       partner_counties = distribution.partner.partner_counties
       num_items_for_distribution = distribution.line_items.total
       value_of_distribution = distribution.line_items.total_value
       if partner_counties.size == 0
-        @breakdown["Unspecified"][:num_items] += num_items_for_distribution
-        @breakdown["Unspecified"][:amount] += value_of_distribution
+        breakdown_hash["Unspecified"][:num_items] += num_items_for_distribution
+        breakdown_hash["Unspecified"][:amount] += value_of_distribution
       else
         partner_counties.each do |pc|
           name = pc.county.name
-          if !@breakdown[name]
-            @breakdown[name] = {}
-            @breakdown[name][:region] = pc.county.region
-            @breakdown[name][:num_items] = num_items_for_distribution * pc.client_share
-            @breakdown[name][:amount] = value_of_distribution * pc.client_share
+          if !breakdown_hash[name]
+            breakdown_hash[name] = {}
+            breakdown_hash[name][:name] = name
+            breakdown_hash[name][:region] = pc.county.region
+            breakdown_hash[name][:num_items] = num_items_for_distribution * pc.client_share
+            breakdown_hash[name][:amount] = value_of_distribution * pc.client_share
           else
-            @breakdown[name][:num_items] = @breakdown[name][:num_items] + num_items_for_distribution * pc.client_share
-            @breakdown[name][:amount] = @breakdown[name][:amount] + value_of_distribution * pc.client_share
+            breakdown_hash[name][:num_items] = breakdown_hash[name][:num_items] + num_items_for_distribution * pc.client_share
+            breakdown_hash[name][:amount] = breakdown_hash[name][:amount] + value_of_distribution * pc.client_share
           end
         end
       end
     end
+
+    @breakdown = breakdown_hash.sort_by{|k,v| [v[:region],k]}
+
   end
 end
