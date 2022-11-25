0aef44797c3b7e1c820ad2368849e511e8b913fc
Edwin <edwinthinks@gmail.com>
2022-11-25 06:51:40 -0500

    Fix rubocop complaints

--- app/controllers/partner_users_controller.rb
+++ app/controllers/partner_users_controller.rb
@@ -5,29 +5,29 @@ class PartnerUsersController < ApplicationController
 
   def index
     @users = @partner.profile.users
-    @user = User.new(name: '')
+    @user = User.new(name: "")
   end
 
   def create
     user = UserInviteService.invite(email: user_params[:email], name: user_params[:name], roles: [Role::PARTNER], resource: @partner.profile)
     user.valid?
-    
+
     respond_to do |format|
       format.turbo_stream do
         if user.errors.none?
           flash.now[:notice] = "#{user.name} has been invited. Invitation email sent to #{user.email}"
 
           render turbo_stream: [
-            turbo_stream.replace("partners/#{@partner.id}/form", partial: 'partner_users/form', locals: { partner: @partner, user: User.new(name: '') }),
+            turbo_stream.replace("partners/#{@partner.id}/form", partial: "partner_users/form", locals: {partner: @partner, user: User.new(name: "")}),
             turbo_stream.replace("flash", partial: "shared/flash"),
-            turbo_stream.replace("partners/#{@partner.id}/users", partial: 'partner_users/users', locals: { users: @partner.reload.profile.users, partner: @partner })
+            turbo_stream.replace("partners/#{@partner.id}/users", partial: "partner_users/users", locals: {users: @partner.reload.profile.users, partner: @partner})
           ]
         else
           flash.now[:error] = "Invitation failed. Check the form for errors."
           render turbo_stream: [
-            turbo_stream.replace("partners/#{@partner.id}/form", partial: 'partner_users/form', locals: { partner: @partner, user: user }),
+            turbo_stream.replace("partners/#{@partner.id}/form", partial: "partner_users/form", locals: {partner: @partner, user: user}),
             turbo_stream.replace("flash", partial: "shared/flash")
-          ], status: 400
+          ], status: :bad_request
         end
       end
     end
@@ -42,13 +42,13 @@ class PartnerUsersController < ApplicationController
           flash.now[:notice] = "Access to #{user.name} has been revoked."
           render turbo_stream: [
             turbo_stream.replace("flash", partial: "shared/flash"),
-            turbo_stream.replace("partners/#{@partner.id}/users", partial: 'partner_users/users', locals: { users: @partner.reload.profile.users, partner: @partner })
+            turbo_stream.replace("partners/#{@partner.id}/users", partial: "partner_users/users", locals: {users: @partner.reload.profile.users, partner: @partner})
           ]
         else
           flash.now[:error] = "Invitation failed. Check the form for errors."
           render turbo_stream: [
             turbo_stream.replace("flash", partial: "shared/flash")
-          ], status: 400
+          ], status: :bad_request
         end
       end
     end
@@ -59,7 +59,7 @@ class PartnerUsersController < ApplicationController
 
     if user.invitation_accepted_at.nil?
       user.invite!
-    else 
+    else
       user.errors.add(:base, "User has already accepted invitation.")
     end
 
@@ -69,13 +69,13 @@ class PartnerUsersController < ApplicationController
           flash.now[:notice] = "Invitation email sent to #{user.email}"
           render turbo_stream: [
             turbo_stream.replace("flash", partial: "shared/flash"),
-            turbo_stream.replace("partners/#{@partner.id}/users", partial: 'partner_users/users', locals: { users: @partner.reload.profile.users, partner: @partner })
+            turbo_stream.replace("partners/#{@partner.id}/users", partial: "partner_users/users", locals: {users: @partner.reload.profile.users, partner: @partner})
           ]
         else
           flash.now[:error] = user.errors.full_messages.to_sentence
           render turbo_stream: [
             turbo_stream.replace("flash", partial: "shared/flash")
-          ], status: 400
+          ], status: :bad_request
         end
       end
     end
@@ -90,6 +90,4 @@ class PartnerUsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :name)
   end
-
-
 end
