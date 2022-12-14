b5ffc8776a8e1595b5e451adae07e90e448f1862
Kayla McEowen <KaylaGallatin@users.noreply.github.com>
2022-12-14 06:27:17 -0600

    Explicitly add organization for super admins (#3282)
    Co-authored-by: KaylaGallatin <kaylagal@verizon.net>

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -40,7 +40,7 @@ class OrganizationsController < ApplicationController
     user = User.find(params[:user_id])
     raise ActiveRecord::RecordNotFound unless user.has_role?(Role::ORG_USER, current_organization)
     user.add_role(Role::ORG_ADMIN, current_organization)
-    redirect_to organization_path, notice: "User has been promoted!"
+    redirect_to user_update_redirect_path, notice: "User has been promoted!"
   end
 
   def demote_to_user
@@ -53,21 +53,21 @@ class OrganizationsController < ApplicationController
       notice = "Admin has been changed to User!"
     end
 
-    redirect_to organization_path, notice: notice
+    redirect_to user_update_redirect_path, notice: notice
   end
 
   def deactivate_user
     user = User.with_discarded.find_by!(id: params[:user_id])
     raise ActiveRecord::RecordNotFound unless user.has_role?(Role::ORG_USER, current_organization)
     user.discard!
-    redirect_to organization_path, notice: "User has been deactivated."
+    redirect_to user_update_redirect_path, notice: "User has been deactivated."
   end
 
   def reactivate_user
     user = User.with_discarded.find_by!(id: params[:user_id])
     raise ActiveRecord::RecordNotFound unless user.has_role?(Role::ORG_USER, current_organization)
     user.undiscard!
-    redirect_to organization_path, notice: "User has been reactivated."
+    redirect_to user_update_redirect_path, notice: "User has been reactivated."
   end
 
   private
@@ -88,5 +88,13 @@ class OrganizationsController < ApplicationController
       :enable_individual_requests, :enable_quantity_based_requests,
       partner_form_fields: []
     )
+  end
+
+  def user_update_redirect_path
+    if current_user.has_role?(Role::SUPER_ADMIN)
+      admin_organization_path(current_organization.id)
+    else
+      organization_path
+    end
   end
 end
