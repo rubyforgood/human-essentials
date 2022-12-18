09a6f5c50f246addfcdf335998902720a596f9b8
Edwin <edwinthinks@gmail.com>
2022-12-18 12:44:03 -0500

    Update to load stimulus controllers

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-//import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
-window.Stimulus = application;
-
-//const context = require.context("/", true, /\.js$/)
-//Stimulus.load(definitionsFromContext(context))
 
 // Configure Stimulus development experience
 application.debug = false
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,8 +4,11 @@ pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.1.0/dist/stimulus.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "admin-lte", to: "https://ga.jspm.io/npm:admin-lte@3.2.0/dist/js/adminlte.min.js"
 pin "@oddcamp/cocoon-vanilla-js", to: "https://ga.jspm.io/npm:@oddcamp/cocoon-vanilla-js@1.1.3/index.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@4.6.2/dist/js/bootstrap.js"
 pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
+pin "highcharts", to: "https://ga.jspm.io/npm:highcharts@10.3.2/highcharts.js"
+pin "select2", to: "https://ga.jspm.io/npm:select2@4.1.0-rc.0/dist/js/select2.js"
