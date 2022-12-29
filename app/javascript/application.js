bed8bde533a1d3d125875c037e83ead4fb3376aa
Daniel Orner <daniel.orner@wishabi.com>
2022-12-29 12:46:47 -0500

    Fixes for fontawesome, select2, litepicker

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,19 +5,100 @@
  * all the interactions.
  */
 import jQuery from 'jquery'
+window.jQuery = jQuery
+window.$ = jQuery
+
 import 'admin-lte'
 import "@oddcamp/cocoon-vanilla-js";
 import { Turbo } from "@hotwired/turbo-rails"
+import "trix"
+import "@rails/actiontext"
+
+import {DateTime} from "luxon";
+import 'litepicker';
+import { Calendar } from '@fullcalendar/core';
+import luxonPlugin from '@fullcalendar/luxon'
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import toastr from 'toastr';
+import 'litepicker/ranges';
 
 import 'bootstrap'
 import 'controllers'
 
-window.jQuery = jQuery
-window.$ = jQuery
-
 // Disable turbo by default to avoid issues with turbolinks
 Turbo.session.drive = false
 
-console.log("Hello from importmap-rails!")
+
+// Global toastr options
+window.toastr = toastr;
+toastr.options = {
+  "timeOut": "1400"
+}
+
+function isMobileResolution() {
+  return $(window).width() < 992;
+}
+
+function isShortHeightScreen() {
+  return $(window).height() < 768 && !isMobileResolution();
+}
+
+window.contentLoaded = false;
+
+// es-module-shims calls DOMContentLoaded twice for some reason
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.contentLoaded) {
+    return;
+  }
+  window.contentLoaded = true;
+  const isMobile = isMobileResolution();
+  const isShortHeight = isShortHeightScreen();
+
+  const calendarElement = document.getElementById('calendar');
+  if (calendarElement) {
+    new Calendar(calendarElement, {
+      timeZone: 'UTC',
+      firstDay: 1,
+      plugins: [luxonPlugin, dayGridPlugin, listPlugin],
+      displayEventTime: true,
+      eventLimit: true,
+      events: 'schedule.json',
+      height: isMobile || isShortHeight ? 'auto' : 'parent',
+      defaultView: isMobile ? 'listWeek' : 'month'
+    }).render();
+  }
+
+  const rangeElement = document.getElementById("filters_date_range");
+  if (!rangeElement) {
+    return;
+  }
+
+  const today = DateTime.now();
+  const startDate = new Date(rangeElement.dataset["initialStartDate"]);
+  const endDate = new Date(rangeElement.dataset["initialEndDate"]);
+
+  const picker = new Litepicker({
+    element: rangeElement,
+    plugins: ['ranges'],
+    startDate: startDate,
+    endDate: endDate,
+    format: "MMMM D, YYYY",
+    ranges: {
+      customRanges: {
+        'All Time': [today.minus({ 'years': 100}).toJSDate(), today.toJSDate()],
+        'Today': [today.toJSDate(), today.toJSDate()],
+        'Yesterday': [today.minus({'days': 1}).toJSDate(), today.minus({'days': 1}).toJSDate()],
+        'Last 7 Days': [today.minus({'days': 6}).toJSDate(), today.toJSDate()],
+        'Last 30 Days': [today.minus({'days': 29}).toJSDate(), today.toJSDate()],
+        'This Month': [today.startOf('month').toJSDate(), today.endOf('month').toJSDate()],
+        'Last Month': [today.minus({'months': 1}).startOf('month').toJSDate(),
+          today.minus({'month': 1}).endOf('month').toJSDate()],
+        'This Year': [today.startOf('year').toJSDate(), today.endOf('year').toJSDate()]
+      }
+    }
+  });
+  picker.setDateRange(startDate, endDate);
+}, false);
 
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js", preload: true
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.1.0/dist/stimulus.js"
@@ -11,4 +11,18 @@ pin "@oddcamp/cocoon-vanilla-js", to: "https://ga.jspm.io/npm:@oddcamp/cocoon-va
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@4.6.2/dist/js/bootstrap.js"
 pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
 pin "highcharts", to: "https://ga.jspm.io/npm:highcharts@10.3.2/highcharts.js"
-pin "select2", to: "https://ga.jspm.io/npm:select2@4.1.0-rc.0/dist/js/select2.js"
+pin "select2", to: "https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"
+pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.4/dist/trix.esm.min.js"
+pin "@rails/actiontext", to: "https://ga.jspm.io/npm:@rails/actiontext@7.0.4/app/assets/javascripts/actiontext.js"
+pin "luxon", to: "https://ga.jspm.io/npm:luxon@1.28.0/build/cjs-browser/luxon.js"
+pin "litepicker", to: "https://cdn.jsdelivr.net/npm/litepicker/dist/litepicker.js"
+pin "litepicker/ranges", to: "https://cdn.jsdelivr.net/npm/litepicker/dist/plugins/ranges.js"
+pin "toastr", to: "https://ga.jspm.io/npm:toastr@2.1.4/toastr.js"
+pin "@fullcalendar/core", to: "https://ga.jspm.io/npm:@fullcalendar/core@6.0.1/index.js"
+pin "preact", to: "https://ga.jspm.io/npm:preact@10.11.3/dist/preact.module.js"
+pin "preact/compat", to: "https://ga.jspm.io/npm:preact@10.11.3/compat/dist/compat.module.js"
+pin "preact/hooks", to: "https://ga.jspm.io/npm:preact@10.11.3/hooks/dist/hooks.module.js"
+pin "@fullcalendar/luxon", to: "https://ga.jspm.io/npm:@fullcalendar/luxon@6.0.1/index.js"
+pin "@fullcalendar/core/", to: "https://ga.jspm.io/npm:@fullcalendar/core@6.0.1/"
+pin "@fullcalendar/daygrid", to: "https://ga.jspm.io/npm:@fullcalendar/daygrid@6.0.1/index.js"
+pin "@fullcalendar/list", to: "https://ga.jspm.io/npm:@fullcalendar/list@6.0.1/index.js"
