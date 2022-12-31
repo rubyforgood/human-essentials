ac7913394babb7769f01213691a28505893948b6
CL Fisher <clfisher@rogers.com>
2022-12-30 20:31:07 -0500

    has simple tests for changing county client share levels

--- spec/system/partners/partner_counties_system_spec.rb
+++ spec/system/partners/partner_counties_system_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe "Partner counties behaviour", type: :system, js: true do
   describe 'changing the client share' do
+
     let!(:partner1) { create(:partner, :with_4_counties) }
     let(:partner1_user) { partner1.primary_user }
 
@@ -8,25 +9,21 @@ RSpec.describe "Partner counties behaviour", type: :system, js: true do
         login_as(partner1_user)
         visit edit_partners_profile_path
       end
+
       it "handles an invalid total client share properly" do
-        expect(page).to have_field("profile_partner_counties_attributes_0_client_share", with: "25")
-        fill_in "profile_partner_counties_attributes_0_client_share", with: "26"
 
-        expect(page).to have_field("profile_partner_counties_attributes_0_client_share", with: "26")
-        puts "County 1 is: " + partner1.partner_counties[0].county.name
-        expect(page).to have_content("101 %")
-        expect(page).to have_selector("partner-county-client-share-total-warning", visible: true)
+        fill_in 'profile_partner_counties_attributes_0_client_share', with: '26'
+        expect(page).to have_content('101 %')
+        expect(page).to have_content('The total client share must be either 0 or 100 %')
       end
 
-      it "handles changing back to a valid total client share properly" do
-        fill_in "profile_partner_counties_attributes_0_client_share", with: "26"
-        expect(page).to have_selector("partner-county-client-share-total-warning", visible: true)
-        fill_in "profile_partner_counties_attributes_1_client_share", with: "24"
-        expect(page).to have_selector("partner-county-client-share-total-warning", visible: false)
+      it "handles a changed but correct total client share properly" do
+
+        fill_in 'profile_partner_counties_attributes_0_client_share', with: '26'
+        fill_in 'profile_partner_counties_attributes_1_client_share', with: '24'
+        expect(page).to have_content('100 %')
+        expect(page).not_to have_content('The total client share must be either 0 or 100 %')
       end
-
-
-
     end
   end
 end
