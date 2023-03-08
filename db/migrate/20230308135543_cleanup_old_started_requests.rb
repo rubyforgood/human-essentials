480ca5440d79882b76ced3a4cad694dee067f6b1
CL Fisher <clfisher@rogers.com>
2023-03-08 09:29:23 -0500

    migration to be run now and in 1 month should act based on updated, not created date

--- db/migrate/20230308135543_cleanup_old_started_requests.rb
+++ db/migrate/20230308135543_cleanup_old_started_requests.rb
@@ -1,6 +1,6 @@
 class CleanupOldStartedRequests < ActiveRecord::Migration[7.0]
   def up
-    Request.where(status: 'started').where('created_at < ?', 1.month.ago).each do |request|
+    Request.where(status: 'started').where('updated_at < ?', 1.month.ago).each do |request|
       request.update!(status: 'fulfilled')
     end
   end
