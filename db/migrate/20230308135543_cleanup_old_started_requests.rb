5a9bc425b5fddeddb4f9597ac825047b44a18546
CL Fisher <clfisher@rogers.com>
2023-03-08 09:14:54 -0500

    migration to be run now and in 1 month

---
+++ db/migrate/20230308135543_cleanup_old_started_requests.rb
@@ -0,0 +1,10 @@
+class CleanupOldStartedRequests < ActiveRecord::Migration[7.0]
+  def up
+    Request.where(status: 'started').where('created_at < ?', 1.month.ago).each do |request|
+      request.update!(status: 'fulfilled')
+    end
+  end
+  def down
+
+  end
+end
