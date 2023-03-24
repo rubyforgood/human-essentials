8ffe80dfdcea39a2264d7ed81ca84cc389abb3fd
Daniel Orner <daniel.orner@wishabi.com>
2023-03-24 15:43:02 -0400

    Vendor cocoon into the repo

--- config/importmap.rb
+++ config/importmap.rb
@@ -29,7 +29,6 @@ pin "@fullcalendar/daygrid", to: "https://ga.jspm.io/npm:@fullcalendar/daygrid@6
 pin "@fullcalendar/list", to: "https://ga.jspm.io/npm:@fullcalendar/list@6.0.1/index.js"
 pin "quagga", to: "https://ga.jspm.io/npm:quagga@0.12.1/dist/quagga.min.js"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js", preload: true
-pin "cocoon-js-vanilla", to: "https://ga.jspm.io/npm:cocoon-js-vanilla@1.3.0/index.js"
 # NOTE: This has been vendored into vendor/javascript because there isn't a JS module exported by this package
 pin "filterrific", to: "filterrific.js"
 pin "bootstrap-select", to: "https://ga.jspm.io/npm:bootstrap-select@1.13.18/dist/js/bootstrap-select.js"
