e197edc98eca5e93bb2edd301359db6440fbb471
CL Fisher <clfisher@rogers.com>
2023-04-02 20:55:53 -0400

    better migrations

--- db/migrate/20230314192630_update_bad_distribution_issued_ats.rb
+++ db/migrate/20230314192630_update_bad_distribution_issued_ats.rb
@@ -1,9 +1,6 @@
 class UpdateBadDistributionIssuedAts < ActiveRecord::Migration[7.0]
   def up
-    Distribution.unscoped.where("issued_at <  ?","2000-01-01").find_each do |dist|
-      # Using update_attribute, rather than update! because some of the distributions
-      # will not validate for other reasons (storage locations missing items)
-      dist.update_attribute(:issued_at, dist.created_at)
-    end
+    Distribution.unscoped.where("issued_at <  ?","2000-01-01").
+      update_all('issued_at=created_at, updated_at=NOW()')
   end
 end
