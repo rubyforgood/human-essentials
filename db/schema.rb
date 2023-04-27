271fb666ec6505f381f98d96a4a88a20943fdf12
Hein Tay Zar <heintayzarhm@gmail.com>
2023-04-27 15:22:33 +0630

    Change back the version

--- db/schema.rb
+++ db/schema.rb
@@ -217,8 +217,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_135543) do
     t.integer "organization_id"
     t.datetime "issued_at", precision: nil
     t.string "agency_rep"
-    t.boolean "reminder_email_enabled", default: false, null: false
     t.integer "state", default: 5, null: false
+    t.boolean "reminder_email_enabled", default: false, null: false
     t.integer "delivery_method", default: 0, null: false
     t.index ["organization_id"], name: "index_distributions_on_organization_id"
     t.index ["partner_id"], name: "index_distributions_on_partner_id"
