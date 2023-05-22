bcb04cfed821c60f912ed254173df35d4bac0328
Blazej Piaskowski <blazej.piaskowski@gmail.com>
2023-05-22 06:26:09 +0200

    add missing test for ItemCategories requests

---
+++ spec/requests/item_categories_requests_spec.rb
@@ -0,0 +1,99 @@
+require "rails_helper"
+
+RSpec.describe "ItemCategories", type: :request do
+  let(:default_params) { {organization_id: @organization.to_param} }
+
+  before do
+    sign_in(@user)
+  end
+
+  let(:valid_attributes) {
+    {
+      name: "Item Category",
+      description: "Test description"
+    }
+  }
+
+  let(:invalid_attributes) {
+    {
+      name: "",
+      description: nil
+    }
+  }
+
+  describe "GET #show" do
+    let!(:item_category) { create(:item_category, organization: @organization) }
+
+    it "renders a successful response" do
+      get item_category_url(default_params.merge(id: item_category.id))
+      expect(response).to be_successful
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "GET #new" do
+    it "renders a successful response" do
+      get new_item_category_url(default_params)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #edit" do
+    let!(:item_category) { create(:item_category, organization: @organization) }
+
+    it "renders a successful response" do
+      get edit_item_category_url(default_params.merge(id: item_category.id))
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid parameters" do
+      it "creates a new ItemCategory then redirects" do
+        expect {
+          post item_categories_url(default_params.merge(item_category: valid_attributes))
+        }.to change(ItemCategory, :count).by(1)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(items_path(organization: @organization))
+        expect(ItemCategory.last.organization).to eq(@organization)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new ItemCategory" do
+        expect {
+          post item_categories_url(default_params.merge(item_category: invalid_attributes))
+        }.to change(ItemCategory, :count).by(0)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    let!(:item_category) { create(:item_category, organization: @organization) }
+
+    context "with valid parameters" do
+      let(:new_attributes) {
+        {
+          name: "New Category",
+          description: "New description"
+        }
+      }
+
+      it "updates the ItemCategory and redirects" do
+        put item_category_url(default_params.merge(id: item_category.id, item_category: new_attributes))
+        item_category.reload
+        expect(item_category.name).to eq("New Category")
+        expect(item_category.description).to eq("New description")
+        expect(response).to redirect_to(item_category_path(item_category))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not render a successful response" do
+        put item_category_url(default_params.merge(id: item_category.id, item_category: invalid_attributes))
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+end
