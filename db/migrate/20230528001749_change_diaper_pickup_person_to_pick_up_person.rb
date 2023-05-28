4c798860bc176d3244f133f792a8b2b17ad14afa
lokisk1155 <shawn12345d@gmail.com>
2023-05-27 20:24:48 -0400

    migration added

--- db/migrate/20230528001749_change_diaper_pickup_person_to_pick_up_person.rb
+++ db/migrate/20230528001749_change_diaper_pickup_person_to_pick_up_person.rb
@@ -1,24 +1,20 @@
 class ChangeDiaperPickupPersonToPickUpPerson < ActiveRecord::Migration[7.0]
   def up
-    [Organization, Partner].each do |model|
-      model.find_each do |record|
-        if record.partner_form_fields.include? 'diaper_pickup_person'
-          record.partner_form_fields.delete('diaper_pickup_person')
-          record.partner_form_fields << 'pick_up_person'
-          record.save
-        end
+    Organization.find_each do |organization|
+      if organization.partner_form_fields.include? 'diaper_pickup_person'
+        organization.partner_form_fields.delete('diaper_pickup_person')
+        organization.partner_form_fields << 'pick_up_person'
+        organization.save
       end
     end
   end
 
   def down
-    [Organization, Partner].each do |model|
-      model.find_each do |record|
-        if record.partner_form_fields.include? 'pick_up_person'
-          record.partner_form_fields.delete('pick_up_person')
-          record.partner_form_fields << 'diaper_pickup_person'
-          record.save
-        end
+    Organization.find_each do |organization|
+      if organization.partner_form_fields.include? 'pick_up_person'
+        organization.partner_form_fields.delete('pick_up_person')
+        organization.partner_form_fields << 'diaper_pickup_person'
+        organization.save
       end
     end
   end
