b58a254e5b777ba578a45b567e1dc020ee9f1570
lokisk1155 <shawn12345d@gmail.com>
2023-05-28 09:03:04 -0400

    delete by index

--- db/migrate/20230528001749_change_diaper_pickup_person_to_pick_up_person.rb
+++ db/migrate/20230528001749_change_diaper_pickup_person_to_pick_up_person.rb
@@ -2,8 +2,8 @@ class ChangeDiaperPickupPersonToPickUpPerson < ActiveRecord::Migration[7.0]
   def up
     Organization.find_each do |organization|
       if organization.partner_form_fields.include? 'diaper_pickup_person'
-        organization.partner_form_fields.delete('diaper_pickup_person')
-        organization.partner_form_fields << 'pick_up_person'
+        index = organization.partner_form_fields.index('diaper_pickup_person')
+        organization.partner_form_fields[index] = 'pick_up_person'
         organization.save
       end
     end
@@ -12,8 +12,8 @@ class ChangeDiaperPickupPersonToPickUpPerson < ActiveRecord::Migration[7.0]
   def down
     Organization.find_each do |organization|
       if organization.partner_form_fields.include? 'pick_up_person'
-        organization.partner_form_fields.delete('pick_up_person')
-        organization.partner_form_fields << 'diaper_pickup_person'
+        index = organization.partner_form_fields.index('pick_up_person')
+        organization.partner_form_fields[index] = 'diaper_pickup_person'
         organization.save
       end
     end
