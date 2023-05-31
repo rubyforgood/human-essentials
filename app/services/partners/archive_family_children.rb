b5ed4e5b82f9f05bf3d694bc402fa38ee7bd607b
Grant McAllister <grantmca2221@gmail.com>
2023-05-30 19:11:42 -0500

    Added ignore for update_all for rubocop since the Child class has no validations

--- app/services/partners/archive_family_children.rb
+++ app/services/partners/archive_family_children.rb
@@ -14,7 +14,9 @@ module Partners
       if family.children.exists?
         ActiveRecord::Base.transaction do
           family.update(archived: true, updated_at: Time.zone.now)
+          # rubocop:disable Rails::SkipsModelValidations
           family.children.update_all(archived: true, updated_at: Time.zone.now)
+          # rubocop:enable Rails::SkipsModelValidations
         rescue => e
           errors.add(:base, e.message)
           raise ActiveRecord::Rollback
