73774983b3fb7f28710dcebb0c622a24ca8fff6b
Daniel Orner <daniel.orner@flipp.com>
2023-06-16 15:09:21 -0400

    Fix broken tests part 1

--- spec/factories/partners/profile.rb
+++ spec/factories/partners/profile.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :partner_profile, class: Partners::Profile do
     partner { Partner.first || create(:partner) }
     essentials_bank_id { Organization.try(:first).id || create(:organization).id }
+    website { 'http://some-site.org' }
   end
 end
