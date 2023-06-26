838e7e683d301fcc2c20ebe6f3fccf3324ffc3ff
Florent Chatterji <florent.chatterji@gmail.com>
2023-06-26 12:10:24 -0400

    fix tests and lint

--- spec/factories/partners/profile.rb
+++ spec/factories/partners/profile.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
     essentials_bank_id { Organization.try(:first).id || create(:organization).id }
     website { "http://some-site.org" }
     name { Faker::Company.name }
-    agency_type { Partner::AGENCY_TYPES['CAREER'] }
+    agency_type { Partner::AGENCY_TYPES["CAREER"] }
     city { Faker::Address.city }
     state { Faker::Address.state }
-    zip_code { Faker::Address.zip}
+    zip_code { Faker::Address.zip }
     address1 { Faker::Address.street_address }
-    program_name { Faker::Lorem.characters(number:5) }
-    program_description { Faker::Lorem.characters(number:15) }
+    program_name { Faker::Lorem.characters(number: 5) }
+    program_description { Faker::Lorem.characters(number: 15) }
   end
 end
