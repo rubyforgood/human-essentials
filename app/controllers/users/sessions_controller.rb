ad1e841020a1ba9b5cbe2eb7d26314eb63d894fa
Alexander Kern <alexander.t.kern@gmail.com>
2023-06-29 10:41:58 -0700

    Fixed unintended changes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -27,7 +27,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # protected
 
-  # If you have extra gitparams to permit, append them to the sanitizer.
+  # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
