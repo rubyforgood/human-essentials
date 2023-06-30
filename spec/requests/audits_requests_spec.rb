34fc78b31d445c8044127cba1ceff92711723240
Jade Stewart <jadekstewart3@gmail.com>
2023-06-30 15:19:45 -0600

    test: fix test to test for controller changes

--- spec/requests/audits_requests_spec.rb
+++ spec/requests/audits_requests_spec.rb
@@ -121,7 +121,7 @@ RSpec.describe "Audits", type: :request do
         it "re-renders the 'new' template with an error message when an invalid storage location is given" do
           post audits_path(default_params.merge(audit: invalid_storage_location_attributes))
           expect(response).to render_template(:new)
-          expect(flash[:error]).to eq("Storage location must be selected.")
+          expect(flash[:error]).to eq("Storage location can't be blank")
         end
       end
     end
