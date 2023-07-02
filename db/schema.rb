5076322901a9e3311684ba8d9cd0ab12c2fc2f30
Zeeshan Haider <zeshan.webdeveloper@gmail.com>
2023-07-03 01:54:13 +0500

    Add "Shipped" to delivery method. Also add a shipping cost field (#3678)
    * column shipping_cost added to distributions
    
    * added shipping_cost field on distribution form
    
    * save shipping_cost to distribution
    
    * shipping cost label added
    
    * shipping cost added to export and view
    
    * backfill distribution's shipping cost
    
    * test case added for distribution shipping_cost
    
    * fixed failing specs
    
    * for testing
    
    * fixed faling spec
    
    * added test case for shipping cost field
    
    * added distribution shipping cost on view page
    
    * rubocop linting fixed
    
    * distribution stimulus controller added for shipping cost
    
    * renamed distribution controller to destribution-delivery
    
    * PR requested changes applied
    
    * PR requested changes applied
    
    * extra empty line removed
    
    * removed typo error
    
    * PR requested change applied
    
    * PR requested change applied
    
    * wrapped shipping cost with it's own div
    
    * code refactored with PR requested changes
    
    * rubocop linting fixed
    
    * code refactored

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_140424) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_055847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -235,6 +235,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_140424) do
     t.integer "state", default: 5, null: false
     t.boolean "reminder_email_enabled", default: false, null: false
     t.integer "delivery_method", default: 0, null: false
+    t.decimal "shipping_cost", precision: 8, scale: 2
     t.index ["organization_id"], name: "index_distributions_on_organization_id"
     t.index ["partner_id"], name: "index_distributions_on_partner_id"
     t.index ["storage_location_id"], name: "index_distributions_on_storage_location_id"
