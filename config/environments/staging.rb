37a2f7eeaf951c5e9c83bcb014a55bf272865e62
Daniel Orner <daniel.orner@flipp.com>
2023-07-02 23:11:13 -0400

    request_id messing up stuff

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -56,9 +56,6 @@ Rails.application.configure do
 
   config.log_level = :info
 
-  # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
-
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
