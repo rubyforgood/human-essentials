6a14d6c21cb659ca40f1e9b2db9fbaea4beea098
zeeshan-haidar <zeshan.webdeveloper@gmail.com>
2023-07-04 10:45:32 +0500

    code refactored

--- db/migrate/20230612073836_remove_kit_allocation_type_from_kit_allocations.rb
+++
@@ -1,7 +0,0 @@
-class RemoveKitAllocationTypeFromKitAllocations < ActiveRecord::Migration[7.0]
-  def change
-    safety_assured {
-      remove_column :kit_allocations, :kit_allocation_type
-    }
-  end
-end
