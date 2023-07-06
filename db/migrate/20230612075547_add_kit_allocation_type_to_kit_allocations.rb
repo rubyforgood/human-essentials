a99cae29329cea2829ba959fd62f9b241c393f5e
Zeeshan Haider <zeshan.webdeveloper@gmail.com>
2023-07-07 00:34:01 +0500

    3609 Inventory out should include items allocated to kits. Inventory in should include kits allocated  (#3641)

---
+++ db/migrate/20230612075547_add_kit_allocation_type_to_kit_allocations.rb
@@ -0,0 +1,10 @@
+class AddKitAllocationTypeToKitAllocations < ActiveRecord::Migration[7.0]
+  def change
+    safety_assured do
+      create_enum :kit_allocation_type, ["inventory_in", "inventory_out"]
+      change_table :kit_allocations do |t|
+        t.enum :kit_allocation_type, enum_type: "kit_allocation_type", default: "inventory_in", null: false
+      end
+    end
+  end
+end
