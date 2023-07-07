e2bf290d7dca175f7b5e21c5cccfe437d8b1a1fc
zeeshan-haidar <zeshan.webdeveloper@gmail.com>
2023-07-07 18:55:02 +0500

    removed integer kit_allocation_type

--- db/migrate/20230606061826_create_kit_allocations.rb
+++ db/migrate/20230606061826_create_kit_allocations.rb
@@ -4,7 +4,6 @@ class CreateKitAllocations < ActiveRecord::Migration[7.0]
       t.references :organization, null: false, foreign_key: true
       t.references :storage_location, null: false, foreign_key: true
       t.references :kit, null: false, foreign_key: true
-      t.integer :kit_allocation_type, null: false, default: 0
       t.timestamps
     end
   end
