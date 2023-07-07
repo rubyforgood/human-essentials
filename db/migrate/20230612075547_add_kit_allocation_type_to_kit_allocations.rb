59fc6c86ca39cb544b48c137f09156a6f25d547e
Daniel Orner <daniel.orner@flipp.com>
2023-07-07 15:12:44 -0400

    Fix crash kit migration  (#3718)

--- db/migrate/20230612075547_add_kit_allocation_type_to_kit_allocations.rb
+++ db/migrate/20230612075547_add_kit_allocation_type_to_kit_allocations.rb
@@ -3,7 +3,7 @@ class AddKitAllocationTypeToKitAllocations < ActiveRecord::Migration[7.0]
     safety_assured do
       create_enum :kit_allocation_type, ["inventory_in", "inventory_out"]
       if column_exists?(:kit_allocations, :kit_allocation_type)
-        remove_column :kit_allocations, :kit_allocation_type)
+        remove_column :kit_allocations, :kit_allocation_type
       end
       change_table :kit_allocations do |t|
         t.enum :kit_allocation_type, enum_type: "kit_allocation_type", default: "inventory_in", null: false
