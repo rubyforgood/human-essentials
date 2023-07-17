874aa8d50903a1d82a7a3943758534e5e7a92984
Alexander Kern <alexander.t.kern@gmail.com>
2023-07-17 16:25:13 -0700

    creating test partner and org for sessions requests spec

--- spec/requests/sessions_requests_spec.rb
+++ spec/requests/sessions_requests_spec.rb
@@ -6,24 +6,50 @@ RSpec.describe "Sessions", type: :request, order: :defined do
       # sign in as an org_admin
       sign_in(@organization_admin)
 
-      # sign in as a partner
+      # create a basic test org and partner
+      
+      org = Organization.create!(
+        name: "Test Organization",
+        short_name: "testorg",
+        email: "testorg@example.com",
+        enable_child_based_requests: true,
+        enable_individual_requests: true,
+        enable_quantity_based_requests: true
+      )
+        
+      partner = Partner.create!(
+        name: "Test Partner",
+        email: "test_partner@example.com",
+        organization_id: org.id,
+        status: Partner.statuses[:invited] # Set the status as invited
+      )
+
+      # # sign in as a partner
       user = User.create!(
         email: "partner123@example.com",
         name: "Test Partner",
-        password: "password!"
+        password: "password!",
+        organization_id: org.id,
+        partner_id: partner.id
       )
-      user.add_role(:partner, create(:organization))
-      post "/users/sign_in/",
-        params: {user: {email: "partner123@example.com", password: "password!"}}
+
+      # print(p)
+      user.add_role(:partner, partner)
+      sign_in(user)
+
+      # post "/users/sign_in/",
+      #   params: {user: {email: "partner123@example.com", password: "password!"}}
     end
 
     it "successfully reach the partners dashboard" do
-      get dashboard_path
+      get partner_user_root_path
+      # puts(response.body)
       expect(response).to be_successful
     end
 
     it "cannot access the admin dashboard" do
       get admin_dashboard_path
+      print(admin_dashboard_path)
       expect(response).not_to be_successful
     end
   end
