d6fefa34fa3ad5e14f0bce717f85fb463662b23d
Alexander Kern <alexander.t.kern@gmail.com>
2023-07-18 14:22:46 -0700

    rubocop fixes to sessions requests spec file

--- spec/requests/sessions_requests_spec.rb
+++ spec/requests/sessions_requests_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe "Sessions", type: :request, order: :defined do
   context "Sign in as user without logging off as an admin" do
     before do
       # sign in as an org_admin
-      sign_in(@organization_admin);
+      sign_in(@organization_admin)
       # sign in as a user without explicitly logging as org_admin
-      sign_in(@user);
+      sign_in(@user)
     end
-  
+
     it "cannot access admin dashboard" do
       get root_path
       expect(response).not_to redirect_to(admin_dashboard_url(@organization_admin))
