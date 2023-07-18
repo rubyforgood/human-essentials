f13536361f57d934af87dec18ac739a1c4fc1b0a
Alexander Kern <alexander.t.kern@gmail.com>
2023-07-18 14:17:01 -0700

    simplified sessions_requests_spec tests

--- spec/requests/sessions_requests_spec.rb
+++ spec/requests/sessions_requests_spec.rb
@@ -1,56 +1,22 @@
 require "rails_helper"
 
 RSpec.describe "Sessions", type: :request, order: :defined do
-  context "Sign in as an org_admin before signing in as a partner" do
+  context "Sign in as user without logging off as an admin" do
     before do
       # sign in as an org_admin
-      sign_in(@organization_admin)
-
-      # create a basic test org and partner
-      
-      org = Organization.create!(
-        name: "Test Organization",
-        short_name: "testorg",
-        email: "testorg@example.com",
-        enable_child_based_requests: true,
-        enable_individual_requests: true,
-        enable_quantity_based_requests: true
-      )
-        
-      partner = Partner.create!(
-        name: "Test Partner",
-        email: "test_partner@example.com",
-        organization_id: org.id,
-        status: Partner.statuses[:invited] # Set the status as invited
-      )
-
-      # # sign in as a partner
-      user = User.create!(
-        email: "partner123@example.com",
-        name: "Test Partner",
-        password: "password!",
-        organization_id: org.id,
-        partner_id: partner.id
-      )
-
-      # print(p)
-      user.add_role(:partner, partner)
-      sign_in(user)
-
-      # post "/users/sign_in/",
-      #   params: {user: {email: "partner123@example.com", password: "password!"}}
+      sign_in(@organization_admin);
+      # sign in as a user without explicitly logging as org_admin
+      sign_in(@user);
+    end
+  
+    it "cannot access admin dashboard" do
+      get root_path
+      expect(response).not_to redirect_to(admin_dashboard_url(@organization_admin))
     end
 
-    it "successfully reach the partners dashboard" do
-      get partner_user_root_path
-      # puts(response.body)
-      expect(response).to be_successful
-    end
-
-    it "cannot access the admin dashboard" do
-      get admin_dashboard_path
-      print(admin_dashboard_path)
-      expect(response).not_to be_successful
+    it "properly accesses the organization dashboard" do
+      get root_path
+      expect(response).to redirect_to(dashboard_url(@organization))
     end
   end
 end
