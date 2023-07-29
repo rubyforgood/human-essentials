251f09c6785a567206742ef49f47249eb17b71f4
Brock Wilcox <awwaiid@thelackthereof.org>
2023-07-29 16:21:11 -0400

    Bring in date filter from other branch [#3733]

--- app/controllers/reports/itemized_donations_controller.rb
+++ app/controllers/reports/itemized_donations_controller.rb
@@ -1,5 +1,6 @@
 class Reports::ItemizedDonationsController < ApplicationController
   def index
+    setup_date_range_picker
     @donations = current_organization.donations.during(helpers.selected_range)
     @itemized_donation_data = DonationItemizedBreakdownService.new(organization: current_organization, donation_ids: @donations.pluck(:id)).fetch
   end
