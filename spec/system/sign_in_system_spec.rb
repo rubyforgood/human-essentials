42e241493a864fca7696444bdfa49125550e23c5
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-07-31 09:09:25 -0400

    forgot 1 spec

--- spec/system/sign_in_system_spec.rb
+++ spec/system/sign_in_system_spec.rb
@@ -40,13 +40,13 @@ RSpec.describe "User sign-in handling", type: :system, js: true do
   end
 
   context "when users are valid and don't belong to an organization" do
-    it "redirects to home " do
+    it "redirects to 403 " do
       user_no_org = create(:user, organization: nil)
       fill_in "Email", with: user_no_org.email
       fill_in "Password", with: user_no_org.password
       click_button "Log in"
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path("/403")
     end
   end
 end
