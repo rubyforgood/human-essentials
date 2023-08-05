87d9c5f5bf6d790fb7b8bb0b1dd373227c71729f
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-08-05 13:07:24 -0400

    big refactor on sessions + alpha order

--- app/controllers/partners/family_requests_controller.rb
+++ app/controllers/partners/family_requests_controller.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module Partners
   class FamilyRequestsController < BaseController
     before_action :verify_partner_is_active
@@ -11,29 +13,40 @@ module Partners
         params[:filterrific]
       ) || return
       @children = @filterrific.find
-      session[:children_ids] = @children.map(&:id)
     end
 
     def confirmation
-      @children = current_partner.children.active.where(id: session[:children_ids]).where.not(item_needed_diaperid: [nil, 0])
+      selected_children_and_items = params[:children].values.select { |child| child.has_key?("active") }
+      session[:children_and_item_ids] = selected_children_and_items
+      items = {}
 
-      @items = @children.each_with_object({}).with_index do |(child, items), index|
-        item = Item.find(child.item_needed_diaperid)
-        items[index] = { item_name: item.name, child_name: "#{child.first_name} #{child.last_name}" } unless item.nil?
+      set = Set.new
+      selected_children_and_items.each.with_index do |child_and_item, index|
+        item = Item.find(child_and_item["item_id"])
+        child = current_partner.children.find_by(id: child_and_item["child_id"])
+        if child && item
+          items[index] = { item_name: item.name, child_name: "#{child.last_name}, #{child.first_name} " }
+          set << ("#{child.first_name} #{child.last_name}")
+        end
       end
 
+      @items = items.sort_by { |index, data| [data[:child_name], data[:item_name]] }.to_h
+
+      @total_children = set.length
       @total_items_requested = @items.keys.length
 
       render :confirmation
     end
 
     def create
-      children = current_partner.children.active.where(id: session[:children_ids]).where.not(item_needed_diaperid: [nil, 0])
+      child_and_item_ids = session[:children_and_item_ids].map { |child_and_item| [child_and_item["child_id"], child_and_item["item_id"]] }.uniq
 
-      children_grouped_by_item_id = children.group_by(&:item_needed_diaperid)
-      family_requests_attributes = children_grouped_by_item_id.map do |item_id, item_requested_children|
-        { item_id: item_id, person_count: item_requested_children.size, children: item_requested_children }
-      end
+      family_requests_attributes = child_and_item_ids.map do |child_id, item_id|
+        child = current_partner.children.active.find_by(id: child_id)
+        if child
+          { item_id: item_id, person_count: 1, children: [child] }
+        end
+      end.compact
 
       create_service = Partners::FamilyRequestCreateService.new(
         partner_user_id: current_user.id,
@@ -51,5 +64,9 @@ module Partners
         redirect_to new_partners_family_request_path, error: "Request failed! #{create_service.errors.map { |error| error.message.to_s }}}"
       end
     end
+  end
+
+  def family_request_params
+    params.permit(children: {})
   end
 end
