f19d7218ca7cd5b44e58f43728f47965f3c3257e
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-08-06 15:09:45 -0400

    and capy?

--- spec/system/sign_in_system_spec.rb
+++ spec/system/sign_in_system_spec.rb
@@ -52,8 +52,10 @@ RSpec.describe "User sign-in handling", type: :system, js: true do
     end
 
     it "redirects to 403" do
-      expect(page).to have_content("The page you were looking for is forbidden.")
-      expect(page).to have_current_path("/403")
+      Capybara.using_wait_time 10 do
+        expect(page).to have_content("The page you were looking for is forbidden.")
+        expect(page).to have_current_path("/403")
+      end 
     end
   end
 end
