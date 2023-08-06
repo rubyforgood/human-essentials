2927d38c11f396a80058a52e42cf382d7575df18
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-08-06 16:49:13 -0400

    linter

--- spec/requests/static_requests_spec.rb
+++ spec/requests/static_requests_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "Static", type: :request do
   end
 
   describe "Non super user without org signed in" do
-    let(:user_no_org) { User.create(email: 'no-org-user@example.org2', password: 'password!') }
+    let(:user_no_org) { User.create(email: "no-org-user@example.org2", password: "password!") }
     before do
       user_no_org.add_role(:org_user)
       sign_in(user_no_org)
