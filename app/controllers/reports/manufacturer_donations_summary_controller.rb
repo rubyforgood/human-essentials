d7d4f85f8c6a6984320b0374bd4ba09b16ffd841
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-08-07 13:33:06 -0400

    try thisg

--- app/controllers/reports/manufacturer_donations_summary_controller.rb
+++ app/controllers/reports/manufacturer_donations_summary_controller.rb
@@ -1,5 +1,6 @@
 class Reports::ManufacturerDonationsSummaryController < ApplicationController
   def index
     @recent_donations_from_manufacturers = current_organization.donations.during(helpers.selected_range).by_source(:manufacturer)
+    puts 'try'
   end
 end
