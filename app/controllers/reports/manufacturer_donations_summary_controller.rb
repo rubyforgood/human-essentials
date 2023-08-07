cbaf88f9bbd812248bd06a39ab8b445fc026c79f
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-08-07 13:25:23 -0400

    add top manufac

--- app/controllers/reports/manufacturer_donations_summary_controller.rb
+++ app/controllers/reports/manufacturer_donations_summary_controller.rb
@@ -1,5 +1,6 @@
 class Reports::ManufacturerDonationsSummaryController < ApplicationController
   def index
     @recent_donations_from_manufacturers = current_organization.donations.during(helpers.selected_range).by_source(:manufacturer)
+    @top_manufacturers = current_organization.manufacturers.by_donation_count
   end
 end
