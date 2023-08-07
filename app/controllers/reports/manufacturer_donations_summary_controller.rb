41aac223001ccd58acee6ab17b81796893dc312e
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-08-07 13:47:29 -0400

    dates + new donation button

--- app/controllers/reports/manufacturer_donations_summary_controller.rb
+++ app/controllers/reports/manufacturer_donations_summary_controller.rb
@@ -1,6 +1,10 @@
 class Reports::ManufacturerDonationsSummaryController < ApplicationController
   def index
+    setup_date_range_picker
+
     @recent_donations_from_manufacturers = current_organization.donations.during(helpers.selected_range).by_source(:manufacturer)
     @top_manufacturers = current_organization.manufacturers.by_donation_count
+    @donations = current_organization.donations.during(helpers.selected_range)
+    @recent_donations = @donations.recent
   end
 end
