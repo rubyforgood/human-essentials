4281e9e330dec797dd56d5d474008e11d980e99d
Daniel Orner <daniel.orner@flipp.com>
2023-08-20 10:47:17 -0400

    more better?

--- app/events/distribution_created.rb
+++ app/events/distribution_created.rb
@@ -1,24 +1,11 @@
-module Types
-  include Dry.Types()
-end
-
 class DistributionCreated < ApplicationEvent
 
-  class Payload < Dry::Struct
-    attribute :organization_id, Types::Integer
-    attribute :storage_location_id, Types::Integer
-    attribute :partner_id, Types::Integer
-    attribute :items, Types::Array.of(Types.Instance(EventTypes::EventLineItem))
-  end
-
   # @param distribution [Distribution]
-  # @return [DistributionCreatedEvent]
   def self.publish(distribution)
-    event = self.from_payload(Payload.new(
+    event = self.from_payload(EventTypes::InventoryPayload.new(
       organization_id: distribution.organization_id,
-      storage_location_id: distribution.storage_location_id,
       partner_id: distribution.partner_id,
-      items: distribution.line_items.map { |i| EventTypes::EventLineItem.from_line_item(i) }
+      items: EventTypes::EventLineItem.from_line_items(distribution.line_items, to: distribution.storage_location_id)
     ))
     Rails.configuration.event_store.publish(event, stream_name: "org-#{distribution.organization_id}")
   end
--- app/events/event_types/event_line_item.rb
+++ app/events/event_types/event_line_item.rb
@@ -4,18 +4,32 @@ end
 
 module EventTypes
   class EventLineItem < Dry::Struct
-    attribute :quantity, Types::Integer
+    attribute :quantity, Types::Integer # can be positive or negative
     attribute :item_id, Types::Integer
     attribute :item_value_in_cents, Types::Integer
+    attribute :from_storage_location, Types::Integer.optional
+    attribute :to_storage_location, Types::Integer.optional
 
     # @param line_item [LineItem]
+    # @param from [Integer]
+    # @param to [Integer]
     # @return [EventLineItem]
-    def self.from_line_item(line_item)
+    def self.from_line_item(line_item, from: nil, to: nil)
       self.new(
         quantity: line_item.quantity,
         item_id: line_item.item_id,
-        item_value_in_cents: line_item.item.value_in_cents
+        item_value_in_cents: line_item.item.value_in_cents,
+        from_storage_location: from,
+        to_storage_location: to
       )
+    end
+
+    # @param line_item [Array<LineItem>]
+    # @param from [Integer]
+    # @param to [Integer]
+    # @return [Array<EventLineItem>]
+    def self.from_line_items(line_items, from: nil, to: nil)
+      line_items.map { |i| self.from_line_item(i, from: from, to: to)}
     end
 
   end
