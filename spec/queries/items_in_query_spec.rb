8bbdfa68e5dd452e19caa1eb9b11805f84133395
Brock Wilcox <awwaiid@thelackthereof.org>
2023-08-20 14:50:12 -0400

    Revert "3609 updated inventory in out queries to fetch data from KitAllocation (#3737)"
    This reverts commit 1a389043797f0ff15e4b15d4ab4d9fff9688cafc.

--- spec/queries/items_in_query_spec.rb
+++ spec/queries/items_in_query_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe ItemsInQuery do
       create_list(:purchase, 2, :with_items, item: create(:item), item_quantity: 5, storage_location: storage_location)
       create_list(:adjustment, 2, :with_items, item: create(:item), item_quantity: 5, storage_location: storage_location)
       create_list(:transfer, 2, :with_items, item_quantity: 5, item: other_storage_location.inventory_items.first.item, from: other_storage_location, to: storage_location)
-      kit = create(:kit, :with_item, organization: @organization)
-      create(:kit_allocation, :with_items, kit_allocation_type: "inventory_in", kit_id: kit.id, storage_location: storage_location, organization_id: @organization.id)
     end
 
     it "returns a collection with the fields name, item_id, quantity" do
@@ -21,22 +19,22 @@ RSpec.describe ItemsInQuery do
       expect(subject.first).to be_respond_to(:item_id)
     end
 
-    it "includes donations, purchases, adjustments, transfers, kit_allocations among sources" do
-      expect(subject.to_a.size).to eq(5)
+    it "includes donations, purchases, adjustments, transfers among sources" do
+      expect(subject.to_a.size).to eq(4)
     end
 
     it "does not count negative adjustments towards in-flow" do
       shared_item = create(:item)
       create(:donation, :with_items, item: shared_item, item_quantity: 10, storage_location: storage_location)
       create(:adjustment, :with_items, item: shared_item, item_quantity: -10, storage_location: storage_location)
-      expect(subject.to_a.size).to eq(6)
+      expect(subject.to_a.size).to eq(5)
     end
 
     it "does not count transfers going in the negative direction" do
       shared_item = create(:item)
       create(:donation, :with_items, item: shared_item, item_quantity: 10, storage_location: storage_location)
       create(:transfer, :with_items, item_quantity: 10, item: shared_item, from: storage_location, to: other_storage_location)
-      expect(subject.to_a.size).to eq(6)
+      expect(subject.to_a.size).to eq(5)
     end
   end
 end
