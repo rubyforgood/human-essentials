73b51101b7628bac978d269ddfeeaa506ed4016b
Grant <grantmca2221@gmail.com>
2023-09-08 22:14:17 -0500

    Fixed a small typo on the migration

--- db/migrate/20230430025441_add_archive_to_families.rb
+++ db/migrate/20230430025441_add_archive_to_families.rb
@@ -1,6 +1,6 @@
 class AddArchiveToFamilies < ActiveRecord::Migration[7.0]
   def up
-    add_column :families, :archived, :boolean, nill: false, default: false
+    add_column :families, :archived, :boolean, nil: false, default: false
   end
 
   def down
