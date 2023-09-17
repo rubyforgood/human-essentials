e0bd0b2935cf30c61f4651fd879704cdf5f36b09
Scott Steele <scottlsteele@gmail.com>
2023-09-17 12:08:22 -0400

    fix: bad schema version introduced in #3623

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_025441) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_185722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
