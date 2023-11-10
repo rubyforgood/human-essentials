f7c7d8b53b84e6f1311b4458c682d0267734e8fc
Daniel Orner <daniel.orner@flipp.com>
2023-11-10 14:43:55 -0500

    Fix adjustment logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_22_140444) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_194231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
