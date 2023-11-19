69bc2df42b333f9e2984f94747f329919796854c
Brock Wilcox <awwaiid@thelackthereof.org>
2023-11-19 09:57:27 -0500

    Extend max delayed job from 5 mins to 20 mins

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,7 +1,7 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.max_run_time = 20.minutes
 Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
