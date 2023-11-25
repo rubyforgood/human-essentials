cb98537a2accaeb3720f959652a6a2d2f1e058c3
Kiran <kiranbpatil@gmail.com>
2023-11-24 18:29:51 -0800

    3571 - Update invitation reset emails

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe User, type: :mailer do
       expect(mail.subject).to eq("Role Added")
     end
   end
+
+  describe "#send_reset_password_instructions" do
+    context "user gets an email with instructions" do
+      let(:user) { create(:user) }
+
+      before(:each) do
+        user.send_reset_password_instructions
+      end
+
+      let(:mail) { ActionMailer::Base.deliveries.last }
+
+      it "sends an email with instructions" do
+        expect(mail.body.encoded).to include("Someone has requested a link to change your password. You can do this through the link below.")
+        expect(mail.body.encoded).to include("For security reasons these invitations expire. This invitation will expire in 8 hours or if a new password reset is triggered.")
+        expect(mail.body.encoded).to include("<If your invitation has an expired message, go <a href="http://localhost/users/password/new">here</a> a'nd enter your email address to reset your password. This will also accept the invitation.")
+        expect(mail.body.encoded).to include("If you didn't request this, please ignore this email.")
+        expect(mail.body.encoded).to include("Your password won't change until you access the link above and create a new one.")
+      end
+    end
+  end  
 end
