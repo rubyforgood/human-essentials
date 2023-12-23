a6b4849a07326a134dcc5abb173cdef5ffc007f8
Brock Wilcox <awwaiid@thelackthereof.org>
2023-12-23 13:16:04 -0500

    Clean out some lint [#3734]

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,6 @@ class DashboardController < ApplicationController
   def index
     setup_date_range_picker
 
-
     distributions = current_organization.distributions.includes(:partner).during(helpers.selected_range)
     @itemized_distribution_data = DistributionItemizedBreakdownService.new(organization: current_organization, distribution_ids: distributions.pluck(:id)).fetch
 
