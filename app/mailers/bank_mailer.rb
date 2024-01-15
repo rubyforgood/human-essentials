9d703a19f3191f8c5d7d32611462821e43f735fd
MasumaJaffery <masumajaffery1@gmail.com>
2024-01-15 23:59:28 +0500

    Update

--- app/mailers/bank_mailer.rb
+++
@@ -1,8 +0,0 @@
-# app/mailers/bank_mailer.rb
-
-class BankMailer < ApplicationMailer
-  def notify_request_submission(partner_request)
-    @partner_request = partner_request
-    mail(to: @partner_request.bank.contact_email, subject: 'Partner Request Submitted')
-  end
-end
