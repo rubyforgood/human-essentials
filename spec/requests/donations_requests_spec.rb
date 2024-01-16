d5f56a9cd48c17f7dacb4920f6219cddd4d9fcc8
YuriBocharov <quesadillaman@gmail.com>
2024-01-16 17:10:38 -0500

    feat: adds specs for donation details
    This commit adds RSpec model, request and service specs
    for expected behavior of donation sources:
    
    Source column shows type of donation
    Details columns shows name for all but misc donations.
    Those show a truncated comment.
    
    Service specs test csv creation.
    Model specs test the newly added donation.details method.

--- spec/requests/donations_requests_spec.rb
+++ spec/requests/donations_requests_spec.rb
@@ -19,11 +19,58 @@ RSpec.describe "Donations", type: :request do
       before do
         create(:donation)
       end
-
       context "html" do
         let(:response_format) { 'html' }
 
         it { is_expected.to be_successful }
+
+        it "should have the columns source and details" do
+          expect(subject.body).to include("<th>Source</th>")
+          expect(subject.body).to include("<th>Details</th>")
+        end
+
+        context "when given a product drive" do
+          let(:product_drive) { create(:product_drive) }
+          let(:donation) { create(:donation, source: "Product Drive", product_drive: product_drive) }
+
+          it "should display Product Drive and the name of the drive" do
+            donation
+            expect(subject.body).to include("<td>#{donation.source}</td>")
+            expect(subject.body).to include("<td>#{product_drive.name}</td>")
+          end
+        end
+
+        context "when given a donation site" do
+          let(:donation_site) { create(:donation_site) }
+          let(:donation) { create(:donation, source: "Donation Site", donation_site: donation_site) }
+
+          it "should display Donation Site and the name of the site" do
+            donation
+            expect(subject.body).to include("<td>#{donation.source}</td>")
+            expect(subject.body).to include("<td>#{donation_site.name}</td>")
+          end
+        end
+
+        context "when given a manufacturer" do
+          let(:manufacturer) { create(:manufacturer) }
+          let(:donation) { create(:donation, source: "Manufacturer", manufacturer: manufacturer) }
+
+          it "should display Manufacturer and the manufacturer name" do
+            donation
+            expect(subject.body).to include("<td>#{donation.source}</td>")
+            expect(subject.body).to include("<td>#{manufacturer.name}</td>")
+          end
+        end
+
+        context "when given a misc donation" do
+          let(:donation) { create(:donation, source: "Misc. Donation", comment: Faker::Lorem.paragraph) }
+
+          it "should display Misc Donation and a truncated comment" do
+            donation
+            expect(subject.body).to include("<td>#{donation.source}</td>")
+            expect(subject.body).to include("<td>#{donation.comment[..25]}</td>")
+          end
+        end
       end
 
       context "csv" do
