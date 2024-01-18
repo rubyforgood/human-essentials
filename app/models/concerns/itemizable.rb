3cb7e0d2baf75de1264c45fa417b543673a2ef4c
YuriBocharov <quesadillaman@gmail.com>
2024-01-18 15:47:29 -0500

    feat: add to_a flipper deprecation flag
    Adds the flag :deprecate_to_a.
    
    With flag on it will raise an error and log a warning.
    With flag off it will silently call the correct method.

--- app/models/concerns/itemizable.rb
+++ app/models/concerns/itemizable.rb
@@ -109,13 +109,12 @@ module Itemizable
     end
   end
 
-  # TODO: I'm not sure if this should just be removed or leave as a warning for now?
   def to_a
-    line_item_values unless Flipper.enabled?(:deprecate_to_a)
+    line_item_hashes unless Flipper.enabled?(:deprecate_to_a)
 
     Rails.logger.warn "Called #to_a on an Itemizable #{inspect}."
     Rails.logger.warn caller.join("\n")
-    raise StandardError, "Calling to_a on an Itemizable is deprecated. Use #line_item_values instead."
+    raise StandardError, "Calling to_a on an Itemizable is deprecated. Use #line_item_hashes instead."
   end
 
   private
