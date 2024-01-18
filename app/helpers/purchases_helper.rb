e07d71e73d69b90844f8bd5713adf11b443f805e
Sean <44501054+PhantomSean@users.noreply.github.com>
2024-01-18 12:30:18 +1100

    3992 preserve storage location when editing purchase (#4003)
    * Bump rubocop-rails from 2.23.0 to 2.23.1
    
    Bumps [rubocop-rails](https://github.com/rubocop/rubocop-rails) from 2.23.0 to 2.23.1.
    - [Release notes](https://github.com/rubocop/rubocop-rails/releases)
    - [Changelog](https://github.com/rubocop/rubocop-rails/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rubocop/rubocop-rails/compare/v2.23.0...v2.23.1)
    
    ---
    updated-dependencies:
    - dependency-name: rubocop-rails
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Preserved storage location on purchase edit page
    
    Improved test
    
    Fixing test affected by my change
    
    Fixed to reintroduce default storage location for new purchases
    
    * Bump rubocop-performance from 1.19.1 to 1.20.1
    
    Bumps [rubocop-performance](https://github.com/rubocop/rubocop-performance) from 1.19.1 to 1.20.1.
    - [Release notes](https://github.com/rubocop/rubocop-performance/releases)
    - [Changelog](https://github.com/rubocop/rubocop-performance/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rubocop/rubocop-performance/compare/v1.19.1...v1.20.1)
    
    ---
    updated-dependencies:
    - dependency-name: rubocop-performance
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Simplified changes
    
    Fix issue causing factory_bot_lint failure
    
    Moved logic to a helper function
    
    * Improving test
    
    * Removing trailing whitespace
    
    * Moving tests into requests
    
    * Changing selector to match other tests
    
    * spacing fix
    
    * Fixing lint error
    
    * Fixing lint error
    
    ---------
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -3,4 +3,8 @@ module PurchasesHelper
   def purchased_from(purchase)
     purchase.purchased_from.nil? ? "" : "(#{purchase.purchased_from})"
   end
+
+  def new_purchase_default_location(purchase)
+    purchase.new_record? ? current_organization.intake_location : purchase.storage_location_id
+  end
 end
