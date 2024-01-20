5a2605babc0316987200ecf0c6a0c5ed9642e216
YuriBocharov <quesadillaman@gmail.com>
2024-01-19 19:38:10 -0500

    fix: a11y for filter labels
    Before labels were using the wrong id parameter and
    were not correctly labeling select elements.

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -1,12 +1,16 @@
 module FilterHelper
   def filter_select(label: nil, scope:, collection:, key: :id, value: :name, selected: nil)
-    label ||= "Filter #{scope.to_s.gsub("_", ' ')}"
-    label_tag(label) + collection_select(:filters, scope, collection || {}, key, value, { include_blank: true, selected: selected }, class: "form-control")
+    sanitize_scope = scope.to_s.gsub("_", " ")
+    label ||= "Filter #{sanitize_scope}"
+    label_id = "filters_#{sanitize_scope}"
+    label_tag(label_id, label) + collection_select(:filters, scope, collection || {}, key, value, { include_blank: true, selected: selected }, class: "form-control")
   end
 
   def filter_text(label: nil, scope:, selected: nil)
-    label ||= "Filter #{scope.to_s.gsub("_", ' ')}"
-    label_tag(label) + text_field(:filters, scope, class: "form-control", value: selected)
+    sanitize_scope = scope.to_s.gsub("_", " ")
+    label ||= "Filter #{sanitize_scope}"
+    label_id = "filters_#{sanitize_scope}"
+    label_tag(label_id, label) + text_field(:filters, scope, class: "form-control", value: selected)
   end
 
   def filter_checkbox(label: nil, scope:, selected: nil)
