b3395d3d50d670b0e4da3fdaec3694470c7bc8a6
Jade Stewart <jadekstewart3@gmail.com>
2024-01-23 16:43:19 -0700

    Modify test to reflect new calculation

--- spec/services/reports/acquisition_report_service_spec.rb
+++ spec/services/reports/acquisition_report_service_spec.rb
@@ -143,7 +143,7 @@ RSpec.describe Reports::AcquisitionReportService, type: :service, persisted_data
       expect(subject.report).to eq({
         entries: { "Disposable diapers distributed" => "220",
                    "Cloth diapers distributed" => "300",
-                   "Average monthly disposable diapers distributed" => "17",
+                   "Average monthly disposable diapers distributed" => "18",
                    "Total product drives" => 2,
                    "Disposable diapers collected from drives" => "600",
                    "Cloth diapers collected from drives" => "900",
