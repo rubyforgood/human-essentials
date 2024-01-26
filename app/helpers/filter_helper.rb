dde1e0c2b7f133066de434dce7a668eb66a467ff
YuriBocharov <quesadillaman@gmail.com>
2024-01-26 14:10:02 -0500

    fix: changes label helper to use uuid
    In the off chance that there are two filters with the same
    scope on a page they would both have the same id.
    
    This adds a unique uuid value to the filters.

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -2,15 +2,15 @@ module FilterHelper
   def filter_select(label: nil, scope:, collection:, key: :id, value: :name, selected: nil)
     sanitize_scope = scope.to_s.gsub("_", " ")
     label ||= "Filter #{sanitize_scope}"
-    label_id = "filters_#{sanitize_scope}"
-    label_tag(label_id, label) + collection_select(:filters, scope, collection || {}, key, value, { include_blank: true, selected: selected }, class: "form-control")
+    id = "filters_#{scope}_#{SecureRandom.uuid}"
+    label_tag(id, label) + collection_select(:filters, scope, collection || {}, key, value, { include_blank: true, selected: selected }, {class: "form-control", id: id})
   end
 
   def filter_text(label: nil, scope:, selected: nil)
     sanitize_scope = scope.to_s.gsub("_", " ")
     label ||= "Filter #{sanitize_scope}"
-    label_id = "filters_#{sanitize_scope}"
-    label_tag(label_id, label) + text_field(:filters, scope, class: "form-control", value: selected)
+    id = "filters_#{scope}_#{SecureRandom.uuid}"
+    label_tag(id, label) + text_field(:filters, scope, class: "form-control", id: id, value: selected)
   end
 
   def filter_checkbox(label: nil, scope:, selected: nil)
