a58b8adc15bbebb2a1c108afb2efece948b0fa33
Daniel Orner <daniel.orner@flipp.com>
2024-02-01 08:36:30 -0500

    move to env variable

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ scheduler = Rufus::Scheduler.singleton
 
 # TODO: Re-enable this on production once we figure out why it's running nonstop.
 
-unless Rails.env.production?
+if ENV['ENABLE_HISTORICAL_CACHE_JOB'] == 'true'
   scheduler.cron '0 3 * * *' do
     system('bundle exec rake cache_historical_data')
   end
