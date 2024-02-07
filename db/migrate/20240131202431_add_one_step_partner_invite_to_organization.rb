e959bb19e21a39853a840c338bd44775dc7d8489
Vincent Truong <vtruong74@outlook.com>
2024-02-07 12:39:40 -0600

    Set column to not null AFTER setting default value in migration

--- db/migrate/20240131202431_add_one_step_partner_invite_to_organization.rb
+++
@@ -1,6 +0,0 @@
-class AddOneStepPartnerInviteToOrganization < ActiveRecord::Migration[7.0]
-  def change
-    add_column :organizations, :one_step_partner_invite, :boolean, null: false
-    change_column_default :organizations, :one_step_partner_invite, false
-  end
-end
