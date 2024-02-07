f6ea0d2614e87ce01918f1cb52c6bb368b9b8a4e
PhantomSean <sean.a.stewart97@gmail.com>
2024-02-07 16:50:20 +1100

    tests

--- spec/models/product_drive_spec.rb
+++ spec/models/product_drive_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe ProductDrive, type: :model do
 
   it "calculates donation quantity by date" do
     create(:donation, :with_items, item_quantity: 2, product_drive: product_drive, issued_at: '26-01-2023')
-    expect(product_drive.donation_quantity_by_date(Time.zone.parse('23/01/2023')..Time.zone.parse('26/01/2023')))
+    expect(product_drive.donation_quantity_by_date(Time.zone.parse('23/01/2023')..Time.zone.parse('26/01/2023'), nil))
       .to eq 2
   end
 
@@ -40,6 +40,12 @@ RSpec.describe ProductDrive, type: :model do
     expect(result.length).to eq product_drive.organization.items.count
     expect(result.count(2)).to eq(1)
     expect(result.count(0)).to eq(product_drive.organization.items.count - 1)
+  end
+
+  it "calculates and returns all donated organization item quantities by category and date" do
+    create(:donation, :with_items, item_quantity: 2, product_drive: product_drive, issued_at: '26-01-2023')
+    result = product_drive.donation_quantity_by_date(Time.zone.parse('23/01/2023')..Time.zone.parse('26/01/2023'), 1)
+    expect(result).to eq product_drive.organization.items.where(item_category_id: 1).count
   end
 
   describe "validations" do
@@ -69,8 +75,9 @@ RSpec.describe ProductDrive, type: :model do
   end
 
   describe "distinct_items" do
-    let!(:item_1) { create(:item, name: "item_1") }
-    let!(:item_2) { create(:item, name: "item_2") }
+    let!(:item_category) { create(:item_category, id: 1) }
+    let!(:item_1) { create(:item, name: "item_1", item_category_id: 1) }
+    let!(:item_2) { create(:item, name: "item_2", item_category_id: 1) }
     let!(:item_3) { create(:item, name: "item_3") }
     let!(:item_4) { create(:item, name: "item_4") }
 
@@ -122,7 +129,7 @@ RSpec.describe ProductDrive, type: :model do
       expect(product_drive.distinct_items_count).to eq 3
     end
 
-    it("counts the distince items correctly by given date") do
+    it("counts the distinct items correctly by given date") do
       product_drive = create(:product_drive)
       donation_within_date = create(:donation, product_drive: product_drive, issued_at: '26-01-2023')
       donation_out_of_date = create(:donation, product_drive: product_drive, issued_at: '20-01-2023')
@@ -134,6 +141,22 @@ RSpec.describe ProductDrive, type: :model do
 
       expect(product_drive.distinct_items_count_by_date(Time.zone.parse('23/01/2023')..Time.zone.parse('26/01/2023')))
         .to eq(1)
+    end
+
+    it("counts the distinct items correctly by given category") do
+      product_drive = create(:product_drive)
+      donation_within_date = create(:donation, product_drive: product_drive, issued_at: '26-01-2023')
+      donation_out_of_date = create(:donation, product_drive: product_drive, issued_at: '20-01-2023')
+      line_item_1 = create(:line_item, itemizable: donation_within_date, item: item_1, quantity: 4)
+      line_item_2 = create(:line_item, itemizable: donation_within_date, item: item_2, quantity: 5)
+      line_item_3 = create(:line_item, itemizable: donation_within_date, item: item_3, quantity: 6)
+
+      donation_within_date.line_items << line_item_1
+      donation_within_date.line_items << line_item_2
+      donation_within_date.line_items << line_item_3
+
+      expect(product_drive.distinct_items_count_by_date(Time.zone.parse('23/01/2023')..Time.zone.parse('26/01/2023'), 1))
+        .to eq(2)
     end
   end
 
@@ -148,6 +171,31 @@ RSpec.describe ProductDrive, type: :model do
 
       it "does not return other_name" do
         expect(described_class.by_name('some_name')).not_to include(product_drive2)
+      end
+    end
+
+    describe ".by_item_category_id" do
+      let!(:item_category) { create(:item_category, id: 1) }
+      let!(:item) { create(:item, name: "item_1", item_category_id: 1) }
+      let!(:product_drive1) { create(:product_drive, name: 'some_name') }
+      let!(:product_drive2) { create(:product_drive, name: 'other_name') }
+
+      it "returns the product_drive with name some_name" do
+        donation = create(:donation, product_drive: product_drive1)
+        line_item = create(:line_item, itemizable: donation, item: item, quantity: 4)
+
+        donation.line_items << line_item
+
+        expect(described_class.by_item_category_id(1)).to include(product_drive1)
+      end
+
+      it "does not return other_name" do
+        donation = create(:donation, product_drive: product_drive1)
+        line_item = create(:line_item, itemizable: donation, item: item, quantity: 4)
+
+        donation.line_items << line_item
+
+        expect(described_class.by_item_category_id(1)).not_to include(product_drive2)
       end
     end
 
