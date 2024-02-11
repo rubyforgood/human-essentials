a6428bf16c782123d5f0dca0613d444851135df9
Brock Wilcox <awwaiid@thelackthereof.org>
2024-02-10 22:14:19 -0500

    Consolidate product drives summary into main reports controller

--- app/controllers/reports/product_drives_summary_controller.rb
+++
@@ -1,7 +0,0 @@
-class Reports::ProductDrivesSummaryController < ApplicationController
-  def index
-    setup_date_range_picker
-    @donations = current_organization.donations.during(helpers.selected_range)
-    @recent_donations = @donations.recent
-  end
-end
