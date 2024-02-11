0d2146c5812a4cc66c29ff59047ab90c80e3b8be
Brock Wilcox <awwaiid@thelackthereof.org>
2024-02-10 22:09:54 -0500

    Consolidate manufacturer donations into main reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,4 +5,13 @@ class ReportsController < ApplicationController
     @donations = current_organization.donations.during(helpers.selected_range)
     @recent_donations = @donations.recent
   end
+
+  def manufacturer_donations_summary
+    setup_date_range_picker
+
+    @recent_donations_from_manufacturers = current_organization.donations.during(helpers.selected_range).by_source(:manufacturer)
+    @top_manufacturers = current_organization.manufacturers.by_donation_count
+    @donations = current_organization.donations.during(helpers.selected_range)
+    @recent_donations = @donations.recent
+  end
 end
