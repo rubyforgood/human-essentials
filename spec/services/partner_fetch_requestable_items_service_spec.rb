7ca7ebfadac06779f3f92f7a92929a4abad9007e
YuriBocharov <quesadillaman@gmail.com>
2024-02-14 17:13:42 -0500

    fix: rework tests
    Previously tests were simply re-stating the original code.
    
    Now specific items are set on the org and partner
    The tests then check in the service object return
    a correct subset of those items.

--- spec/services/partner_fetch_requestable_items_service_spec.rb
+++ spec/services/partner_fetch_requestable_items_service_spec.rb
@@ -4,8 +4,24 @@ describe PartnerFetchRequestableItemsService do
   describe '#call' do
     subject { described_class.new(partner_id: partner_id).call }
     let(:partner_id) { partner.id }
-    let(:partner) { create(:partner) }
-    let(:organization) { partner.organization }
+    let(:partner) { create(:partner, organization:) }
+    let(:organization) { create(:organization, skip_items: true, items:) }
+    let(:items) {
+      [
+        build(:item, active: true, visible_to_partners: true),
+        build(:item, active: false, visible_to_partners: true),
+        build(:item, active: true, visible_to_partners: false),
+        build(:item, active: false, visible_to_partners: false)
+      ]
+    }
+    let(:partner_items) {
+      [
+        build(:item, active: true, visible_to_partners: true),
+        build(:item, active: false, visible_to_partners: true),
+        build(:item, active: true, visible_to_partners: false),
+        build(:item, active: false, visible_to_partners: false)
+      ]
+    }
 
     context 'when the partner id does not match any Partner' do
       let(:partner_id) { 0 }
@@ -21,7 +37,7 @@ describe PartnerFetchRequestableItemsService do
       end
 
       it 'should return all active and visible items' do
-        expected_items = organization.items.active.visible.map { |item| [item.name, item.id] }.sort
+        expected_items = [[items[0].name, items[0].id]]
         expect(subject).to eq(expected_items)
       end
     end
@@ -29,12 +45,14 @@ describe PartnerFetchRequestableItemsService do
     context 'when the partner is in a partner group' do
       before do
         pg = create(:partner_group)
-        pg.item_categories << create(:item_category, organization: organization)
+        item_category = create(:item_category, organization: organization)
+        item_category.items << partner_items
+        pg.item_categories << item_category
         partner.update(partner_group: pg)
       end
 
       it 'should return all active and visible items specified by the item associated with' do
-        expected_items = partner.requestable_items.active.visible.map { |item| [item.name, item.id] }.sort
+        expected_items = [[partner_items[0].name, partner_items[0].id]]
         expect(subject).to eq(expected_items)
       end
     end
