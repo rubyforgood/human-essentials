019e679e9dc93af3f25bd2165117a51a98f96f57
isidzukuri <axesigon@gmail.com>
2024-02-16 09:26:46 +0200

    simplify DispersedPastDatesGenerator

--- lib/dispersed_past_dates_generator.rb
+++ lib/dispersed_past_dates_generator.rb
@@ -1,25 +1,18 @@
 class DispersedPastDatesGenerator
   DAYS_RANGES = [0..6, 7..30, 31..300, 350..700].freeze
 
-  def initialize(dates_quantity)
-    @dates_quantity = dates_quantity
-    @current_iteration = 0.0
-    @dates_per_range = (dates_quantity.to_f / DAYS_RANGES.size).ceil
+  def initialize
+    @current_index = 0
   end
 
   def next
-    range_index = (current_iteration / dates_per_range).to_i
-
-    if dates_quantity > current_iteration
-      @current_iteration += 1
+    day = Time.zone.today - rand(DAYS_RANGES[@current_index]).days
+    if DAYS_RANGES.size - 1 > @current_index
+      @current_index = @current_index.next
     else
-      0.0
+      @current_index = 0
     end
 
-    Time.zone.today - rand(DAYS_RANGES[range_index]).days
+    day
   end
-
-  private
-
-  attr_reader :dates_quantity, :dates_per_range, :current_iteration
 end
