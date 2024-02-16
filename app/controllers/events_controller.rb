9f74bfcb9adf9e0e2aac8fbf15039a9fc27dfe57
Daniel Orner <daniel.orner@flipp.com>
2024-02-16 16:31:01 -0500

    Add type and storage location filters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,12 @@ class EventsController < ApplicationController
   def index
     setup_date_range_picker
 
-    @events = Event.for_organization(current_organization).during(helpers.selected_range)
+    @events = Event.for_organization(current_organization).
+      class_filter(filter_params).
+      includes(:eventable)
+    if params.dig(:filters, :date_range).present?
+      @events = @events.during(helpers.selected_range)
+    end
     @items = current_organization.items
     @locations = current_organization.storage_locations
 
@@ -14,7 +19,7 @@ class EventsController < ApplicationController
   end
 
   def filter_params
-    {}
+    params.require(:filters).permit(:by_type, :by_storage_location)
   end
   helper_method :filter_params
 end
