7e9749dca5c3c36dac88b31ecd701d821cf2a76a
isidzukuri <axesigon@gmail.com>
2024-02-21 11:03:51 +0200

    remove db/migrate/20240219121949_create_needed_item.rb

--- db/migrate/20240219121949_create_needed_item.rb
+++
@@ -1,9 +0,0 @@
-class CreateNeededItem < ActiveRecord::Migration[7.0]
-  def change
-    create_table :needed_items do |t|
-      t.integer :child_id, null: false, index: true
-      t.integer :item_id, null: false, index: true
-      t.timestamps
-    end
-  end
-end
