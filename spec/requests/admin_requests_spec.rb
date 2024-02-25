4a81658aabef2bf03d04bfa6d2ad78d1db1ce0a5
cancelei <glauber.bannwart@gmail.com>
2024-02-25 11:42:08 -0300

    Add user list display and edit links in admin dashboard

--- spec/requests/admin_requests_spec.rb
+++ spec/requests/admin_requests_spec.rb
@@ -15,7 +15,23 @@ RSpec.describe "Admin", type: :request do
     end
 
     context "when the user has a name" do
+      let!(:users_list) { create_list(:user, 25) }
       let!(:user_with_name) { create(:user, name: "John Doe", email: "john@example.com") }
+
+      it "displays the users list" do
+        get admin_dashboard_path
+        users_list.each do |user|
+          expect(response.body).to include(user.name)
+        end
+      end
+
+      it "provides edit links for user profiles" do
+        get admin_dashboard_path
+        expect(response).to be_successful
+
+        edit_user_path_pattern = %r{admin/users/#{user_with_name.id}/edit\?organization_id=\w+}
+        expect(response.body).to match(edit_user_path_pattern)
+      end
 
       it "displays the user's name" do
         get admin_dashboard_path
@@ -31,8 +47,17 @@ RSpec.describe "Admin", type: :request do
         get admin_dashboard_path
         expect(response.body).to include("noname@example.com")
       end
+
+      it "provides edit links for user profiles" do
+        get admin_dashboard_path
+        expect(response).to be_successful
+
+        edit_user_path_pattern = %r{admin/users/#{user_without_name.id}/edit\?organization_id=\w+}
+        expect(response.body).to match(edit_user_path_pattern)
+      end
     end
   end
+
   context "while signed in as a non-super-admin" do
     it "disallows dashboard access, redirecting to the normal dashboard" do
       [@organization_admin, @user].each do |u|
