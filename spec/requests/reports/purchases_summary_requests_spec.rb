c2638fe11400f6c111903b791ad76933ab011925
Brock Wilcox <awwaiid@thelackthereof.org>
2024-03-02 19:40:27 -0500

    Move purchase summary report spec from system to requests

--- spec/requests/reports/purchases_summary_requests_spec.rb
+++ spec/requests/reports/purchases_summary_requests_spec.rb
@@ -12,6 +12,63 @@ RSpec.describe "Purchases", type: :request do
         expect(response.body).to include("Recent purchases")
       end
     end
+
+    context "with filters" do
+      before do
+        # Create a bunch of historical purchases
+        create :purchase, :with_items, item_quantity: 2, issued_at: 0.days.ago
+        create :purchase, :with_items, item_quantity: 3, issued_at: 1.day.ago
+        create :purchase, :with_items, item_quantity: 7, issued_at: 3.days.ago
+        create :purchase, :with_items, item_quantity: 11, issued_at: 10.days.ago
+        create :purchase, :with_items, item_quantity: 13, issued_at: 20.days.ago
+        create :purchase, :with_items, item_quantity: 17, issued_at: 30.days.ago
+      end
+
+      let(:formatted_date_range) { date_range.map { _1.to_formatted_s(:date_picker) }.join(" - ") }
+
+      before do
+        get reports_purchases_summary_path(@user.organization), params: {filters: {date_range: formatted_date_range}}
+      end
+
+      context "today" do
+        let(:date_range) { [0.days.ago, 0.days.ago] }
+        it "shows the correct total and links" do
+          expect(response.body).to include("2 items from")
+        end
+      end
+
+      context "yesterday" do
+        let(:date_range) { [1.day.ago, 1.day.ago] }
+        it "shows the correct total and links" do
+          expect(response.body).to include("3 items from")
+        end
+      end
+
+      context "a weekish ago" do
+        let(:date_range) { [14.days.ago, 7.days.ago] }
+        it "shows the correct total and links" do
+          expect(response.body).to include("11 items from")
+        end
+      end
+
+      context "two weekish ago" do
+        let(:date_range) { [25.days.ago, 7.days.ago] }
+        it "shows the correct total and links" do
+          expect(response.body).to include("11 items from")
+          expect(response.body).to include("13 items from")
+        end
+      end
+
+      context "a long time" do
+        let(:date_range) { [900.days.ago, 1.day.ago] }
+        it "shows the correct total and links" do
+          expect(response.body).to include("$50.00")
+          expect(response.body).to include("3 items from")
+          expect(response.body).to include("7 items from")
+          expect(response.body).to include("11 items from")
+        end
+      end
+    end
   end
 
   context "while not signed in" do
