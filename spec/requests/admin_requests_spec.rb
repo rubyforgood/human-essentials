eed4fae20e98f6794c9c11322c540df7b7af617b
cancelei <glauber.bannwart@gmail.com>
2024-03-03 11:01:37 -0300

    Fix index name and remove "users_list" code in specs

--- spec/requests/admin_requests_spec.rb
+++ spec/requests/admin_requests_spec.rb
@@ -15,14 +15,12 @@ RSpec.describe "Admin", type: :request do
     end
 
     context "when the user has a name" do
-      let!(:users_list) { create_list(:user, 25) }
       let!(:user_with_name) { create(:user, name: "John Doe", email: "john@example.com") }
 
-      it "displays the users list" do
+      it "displays the user's name" do
         get admin_dashboard_path
-        users_list.each do |user|
-          expect(response.body).to include(user.name)
-        end
+        expect(response.body).to include("John Doe")
+        expect(response.body).not_to include("john@example.com")
       end
 
       it "provides edit links for user profiles" do
@@ -31,12 +29,6 @@ RSpec.describe "Admin", type: :request do
 
         edit_user_path_pattern = %r{admin/users/#{user_with_name.id}/edit\?organization_id=\w+}
         expect(response.body).to match(edit_user_path_pattern)
-      end
-
-      it "displays the user's name" do
-        get admin_dashboard_path
-        expect(response.body).to include("John Doe")
-        expect(response.body).not_to include("john@example.com")
       end
     end
 
