98adf8320db4218ee8a6cec4630a3025d9a2f901
elasticspoon <quesadillaman@gmail.com>
2024-03-08 15:19:26 -0500

    Refactor request spec test (#4147)
    * fix: individuals request preserve values
    
    Fixes #4135
    
    When individuals requests were submitted with incorrect values
    the error page did not preserve correct or incorrect inputs. This
    was happening because the controller created a new request on
    error.
    
    Now the new request will be populated with the items from the
    failed request.
    
    I attempted to reuse the failed request, however, because the
    individuals request is a wrapper around a family request that
    behavior was not possible.
    
    * refactor: system specs moved to request specs
    
    * fix: minor code review changes

--- spec/system/partners/managing_requests_system_spec.rb
+++ spec/system/partners/managing_requests_system_spec.rb
@@ -1,123 +1,15 @@
 RSpec.describe "Managing requests", type: :system, js: true do
   describe 'creating a # individuals request' do
     let(:partner_user) { partner.primary_user }
-    let!(:partner) { FactoryBot.create(:partner) }
+    let!(:partner) { FactoryBot.create(:partner, status: :approved) }
 
     context 'GIVEN a partner user is permitted to make a request' do
-      describe 'Select Input Tests' do
-        let(:requestable_items) { [["Item 1", 1], ["Item 2", 2], ["Item 3", 3]] }
-        context 'WHEN they reach the page' do
-          before do
-            allow_any_instance_of(PartnerFetchRequestableItemsService).to receive(:call).and_return(requestable_items)
-            login_as(partner_user)
-            visit new_partners_individuals_request_path
-          end
-          it 'should show the proper items in the select box' do
-            expected_items = requestable_items.map(&:first).unshift('Select an item')
-            expect(page.all('select[name="partners_family_request[items_attributes][0][item_id]"] option').map(&:text)).to eq(expected_items)
-          end
-
-          context 'WHEN they create a request improperly' do
-            before {
-              click_button 'Submit Essentials Request'
-              click_link 'Add Another Item'
-            }
-            it 'should show the proper items in the select box' do
-              expected_items = requestable_items.map(&:first).unshift('Select an item')
-              expect(page.all('select[name="partners_family_request[items_attributes][0][item_id]"] option').map(&:text)).to eq(expected_items)
-            end
-          end
-        end
-      end
-
       before do
         login_as(partner_user)
         visit new_partners_individuals_request_path
       end
 
-      context 'WHEN they create a request improperly by not inputting anything' do
-        before do
-          click_button 'Submit Essentials Request'
-        end
-
-        it 'should show an error message with the instructions ' do
-          expect(page).to have_content('Oops! Something went wrong with your Request')
-          expect(page).to have_content('Ensure each line item has a item selected AND a quantity greater than 0.')
-          expect(page).to have_content('Still need help? Submit a support ticket here and we will do our best to follow up with you via email.')
-        end
-      end
-
-      context 'WHEN they create a request with only a comment' do
-        before do
-          fill_in 'Comments', with: Faker::Lorem.paragraph
-        end
-
-        it 'should be created without any issue' do
-          expect { click_button 'Submit Essentials Request' }.to change { Request.count }.by(1)
-
-          expect(current_path).to eq(partners_request_path(Request.last.id))
-          expect(page).to have_content('Request has been successfully created!')
-          expect(page).to have_content("#{partner.organization.name} should have received the request.")
-        end
-      end
-
-      context 'WHEN they create a request with blank lines' do
-        before do
-          fill_in 'Comments', with: Faker::Lorem.paragraph
-
-          # fill in an item
-          click_link 'Add Another Item'
-          item = partner_user.partner.organization.valid_items.sample
-          last_row = find_all('tr').last
-          last_row.find('option', text: item[:name], exact_text: true).select_option
-          last_row.find_all('.form-control').last.fill_in(with: 1)
-
-          # Trigger another row but keep it empty. It should still be valid!
-          click_link 'Add Another Item'
-        end
-
-        it 'should be created without any issue' do
-          expect { click_button 'Submit Essentials Request' }.to change { Request.count }.by(1)
-
-          expect(current_path).to eq(partners_request_path(Request.last.id))
-          expect(page).to have_content('Request has been successfully created!')
-          expect(page).to have_content("#{partner.organization.name} should have received the request.")
-        end
-      end
-
-      context 'WHEN they create a request completely empty request' do
-        before do
-          fill_in 'Comments', with: Faker::Lorem.paragraph
-
-          # Select items
-          3.times do |idx|
-            if idx != 0
-              click_link 'Add Another Item'
-            end
-
-            last_row = find_all('tr').last
-            last_row.find_all('.form-control').last.fill_in(with: 1)
-          end
-
-          click_button 'Submit Essentials Request'
-        end
-
-        it 'should show an error message with the instructions ' do
-          expect(page).to have_content('Oops! Something went wrong with your Request')
-          expect(page).to have_content('Ensure each line item has a item selected AND a quantity greater than 0.')
-          expect(page).to have_content('Still need help? Submit a support ticket here and we will do our best to follow up with you via email.')
-        end
-
-        it "should show invalid values in the form" do
-          expected_items = page.all('select').map(&:value)
-          expected_quantities = page.all('input[type="number"]').map(&:value)
-
-          expect(expected_items).to eq(["", "", ""])
-          expect(expected_quantities).to eq(["1", "1", "1"])
-        end
-      end
-
-      context 'WHEN they create a request properly' do
+      context 'WHEN a request is built using add and remove buttons' do
         let(:items_to_select) { partner_user.partner.organization.valid_items.sample(3) }
         let(:item_details) do
           items_to_select.map do |item|
@@ -125,6 +17,7 @@ RSpec.describe "Managing requests", type: :system, js: true do
 
             {
               name: item[:name],
+              id: item[:id],
               quantity_per_person: default_quantity,
               person_count: Faker::Number.within(range: 5..25)
             }
@@ -149,25 +42,16 @@ RSpec.describe "Managing requests", type: :system, js: true do
           find_all('td').last.click
         end
 
-        context 'THEN a request records will be created and the partner will be notified via flash message on the dashboard' do
-          before do
+        context 'THEN a request records will be created' do
+          it "creates the correct request" do
             expect { click_button 'Submit Essentials Request' }.to change { Request.count }.by(1)
 
-            expect(current_path).to eq(partners_request_path(Request.last.id))
-            expect(page).to have_content('Request has been successfully created!')
-          end
+            created_request = Request.last
+            expected_items = item_details.map { |item| {"item_id" => item[:id], "quantity" => item[:quantity_per_person] * item[:person_count]} }
+            deleted_item = expected_items.pop
 
-          it 'AND the partner_user can view the details of the created individuals request in a seperate page' do
-            visit partners_request_path(id: Request.last.id)
-
-            # Should have the proper quantity per each item.
-            deleted_item = item_details.pop
-            item_details.each do |item|
-              expect(page).to have_content("#{item[:quantity].to_i * item[:quantity_per_person]} of #{item[:name]}")
-            end
-
-            # Should not have the last item: it was deleted.
-            expect(page).to_not have_content("#{deleted_item[:quantity].to_i * deleted_item[:quantity_per_person]} of #{deleted_item[:name]}")
+            expect(created_request.request_items).to match_array(expected_items)
+            expect(created_request.request_items).to_not include(deleted_item)
           end
         end
       end
@@ -179,126 +63,17 @@ RSpec.describe "Managing requests", type: :system, js: true do
     let!(:partner) { FactoryBot.create(:partner) }
 
     context 'GIVEN a partner user is permitted to make a request' do
-      describe 'Select Input Tests' do
-        let(:requestable_items) { [["Item 1", 1], ["Item 2", 2], ["Item 3", 3]] }
-        context 'WHEN they reach the page' do
-          before do
-            allow_any_instance_of(PartnerFetchRequestableItemsService).to receive(:call).and_return(requestable_items)
-            login_as(partner_user)
-            visit new_partners_request_path
-          end
-
-          it 'should show the proper items in the select box' do
-            expected_items = requestable_items.map(&:first).unshift('Select an item')
-            expect(page.all('select[name="request[item_requests_attributes][0][item_id]"] option').map(&:text)).to eq(expected_items)
-          end
-
-          context 'WHEN they create a request improperly' do
-            before {
-              click_button 'Submit Essentials Request'
-              click_link 'Add Another Item'
-            }
-
-            it 'should show the proper items in the select box' do
-              expected_items = requestable_items.map(&:first).unshift('Select an item')
-              expect(page.all('select option').map(&:text)).to eq(expected_items)
-            end
-          end
-        end
-      end
-
       before do
         login_as(partner_user)
         visit new_partners_request_path
       end
 
-      context 'WHEN they create a request completely empty request' do
-        before do
-          click_button 'Submit Essentials Request'
-        end
-
-        it 'should show an error message with the instructions ' do
-          expect(page).to have_content('Oops! Something went wrong with your Request')
-          expect(page).to have_content('Ensure each line item has a item selected AND a quantity greater than 0.')
-          expect(page).to have_content('Still need help? Submit a support ticket here and we will do our best to follow up with you via email.')
-        end
-      end
-
-      context 'WHEN they create a request with only a comment' do
-        before do
-          fill_in 'Comments', with: Faker::Lorem.paragraph
-        end
-
-        it 'should be created without any issue' do
-          expect { click_button 'Submit Essentials Request' }.to change { Request.count }.by(1)
-
-          expect(current_path).to eq(partners_request_path(Request.last.id))
-          expect(page).to have_content('Request has been successfully created!')
-          expect(page).to have_content("#{partner.organization.name} should have received the request.")
-        end
-      end
-
-      context 'WHEN a request has invalid values' do
-        before do
-          fill_in 'Comments', with: Faker::Lorem.paragraph
-
-          # Select items
-          3.times do |idx|
-            if idx != 0
-              click_link 'Add Another Item'
-            end
-
-            last_row = find_all('tr').last
-            last_row.find_all('.form-control').last.fill_in(with: 1)
-          end
-
-          click_button 'Submit Essentials Request'
-        end
-
-        it 'should show an error message with the instructions ' do
-          expect(page).to have_content('Oops! Something went wrong with your Request')
-          expect(page).to have_content('Ensure each line item has a item selected AND a quantity greater than 0.')
-          expect(page).to have_content('Still need help? Submit a support ticket here and we will do our best to follow up with you via email.')
-        end
-
-        it "should show invalid values in the form" do
-          expected_items = page.all('select').map(&:value)
-          expected_quantities = page.all('input[type="number"]').map(&:value)
-
-          expect(expected_items).to eq(["", "", ""])
-          expect(expected_quantities).to eq(["1", "1", "1"])
-        end
-      end
-
-      context 'WHEN they create a request with blank lines' do
-        before do
-          fill_in 'Comments', with: Faker::Lorem.paragraph
-
-          # fill in an item
-          click_link 'Add Another Item'
-          item = partner_user.partner.organization.valid_items.sample
-          last_row = find_all('tr').last
-          last_row.find('option', text: item[:name], exact_text: true).select_option
-          last_row.find_all('.form-control').last.fill_in(with: 1)
-
-          # Trigger another row but keep it empty. It should still be valid!
-          click_link 'Add Another Item'
-        end
-
-        it 'should be created without any issue' do
-          expect { click_button 'Submit Essentials Request' }.to change { Request.count }.by(1)
-
-          expect(current_path).to eq(partners_request_path(Request.last.id))
-          expect(page).to have_content('Request has been successfully created!')
-          expect(page).to have_content("#{partner.organization.name} should have received the request.")
-        end
-      end
-
-      context 'WHEN they create a request properly' do
+      context 'WHEN a request is built using add and remove buttons' do
         let(:items_to_select) { partner_user.partner.organization.valid_items.sample(3) }
         let(:item_details) do
           items_to_select.map do |item|
             {
+              id: item[:id],
               name: item[:name],
               quantity: Faker::Number.within(range: 5..25)
             }
@@ -326,25 +101,16 @@ RSpec.describe "Managing requests", type: :system, js: true do
           click_link 'Add Another Item'
         end
 
-        context 'THEN a request records will be created and the partner will be notified via flash message on the dashboard' do
-          before do
+        context 'THEN a request records will be created ' do
+          it "creates the correct request" do
             expect { click_button 'Submit Essentials Request' }.to change { Request.count }.by(1)
 
-            expect(current_path).to eq(partners_request_path(Request.last.id))
-            expect(page).to have_content('Request has been successfully created!')
-            expect(page).to have_content("#{partner.organization.name} should have received the request.")
-          end
+            created_request = Request.last
+            expected_items = item_details.map { |item| {"item_id" => item[:id], "quantity" => item[:quantity]} }
+            deleted_item = expected_items.pop
 
-          it 'AND the partner_user can view the details of the created request in a seperate page' do
-            visit partners_request_path(id: Request.last.id)
-
-            deleted_item = item_details.pop
-            item_details.each do |item|
-              expect(page).to have_content("#{item[:quantity]} of #{item[:name]}")
-            end
-
-            # Should not have the last item: it was deleted.
-            expect(page).not_to have_content("#{deleted_item[:quantity]} of #{deleted_item[:name]}")
+            expect(created_request.request_items).to match_array(expected_items)
+            expect(created_request.request_items).to_not include(deleted_item)
           end
         end
       end
