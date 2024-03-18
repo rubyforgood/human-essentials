3d657dc7536310a28d606aded294c350128c9eaa
cancelei <glauber.bannwart@gmail.com>
2024-03-18 10:20:19 -0300

    revert Improve migration to lock

--- db/migrate/20240303144633_add_contact_info_to_donation_sites.rb
+++ db/migrate/20240303144633_add_contact_info_to_donation_sites.rb
@@ -1,9 +1,7 @@
 class AddContactInfoToDonationSites < ActiveRecord::Migration[7.0]
   def change
-    change_table :donation_sites do |t|
-      t.string :contact_name
-      t.string :email
-      t.string :phone
-    end
+    add_column :donation_sites, :contact_name, :string
+    add_column :donation_sites, :email, :string
+    add_column :donation_sites, :phone, :string
   end
 end
