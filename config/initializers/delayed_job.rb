0d3aa31860f879a019562b6f1c9089f199f2da15
elasticspoon <mail@yuribocharov.dev>
2024-03-18 15:55:29 -0400

    fix: remove ?!?!?!?!?
    Not !?!?!?!? worthy

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -10,6 +10,6 @@ Delayed::Worker.logger = ActiveSupport::Logger.new(STDOUT)
 # https://github.com/rubyforgood/human-essentials/issues/4065
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.queue_attributes = {
-  # bigger number is lower priority ?!?!?!?!?!
+  # bigger number is lower priority
   "low_priority" => { priority: 10 } 
 }
