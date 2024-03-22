b7eceb642b2e6c2121fcb292a408bffe210ab465
MasumaJaffery <masumajaffery1@gmail.com>
2024-03-22 22:06:31 +0500

    Update Mailer

--- app/mailers/organization_mailer.rb
+++ app/mailers/organization_mailer.rb
@@ -1,20 +1,18 @@
+# organization.rb
+class Organization < ApplicationRecord
+  def send_submission_notification(partner)
+    OrganizationMailer.request_submission_notification(organization: self, partner: partner).deliver_now if email_notification_opt_in?
+  end
+end
+
+# organization_mailer.rb
 class OrganizationMailer < ApplicationMailer
   default from: "Please do not reply to this email as this mail box is not monitored — Human Essentials <no-reply@humanessentials.app>"
-
-  def partner_approval_request(organization:, partner:)
-    @partner = partner
-    @organization = organization
-
-    mail(to: @organization.email, subject: "[Action Required] Approval requested for #{@partner.name}")
-  end
 
   def request_submission_notification(organization:, partner:)
     @partner = partner
     @organization = organization
 
-    if @organization.email_notification_opt_in?  # Check if email notification is opted in
-      # Send email to the bank's contact email
-      mail(to: @organization.contact_email, subject: "New Request Submitted by #{@partner.name}")
-    end
+    mail(to: @organization.contact_email, subject: "New Request Submitted by #{@partner.name}")
   end
 end
