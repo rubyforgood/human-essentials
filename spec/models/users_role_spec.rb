a24d5727c0749b7fe93e5f780b90250754f5df13
elasticspoon <mail@yuribocharov.dev>
2024-03-24 21:19:55 -0400

    refactor: lvl 2 difficulty replacements

--- spec/models/users_role_spec.rb
+++ spec/models/users_role_spec.rb
@@ -6,32 +6,32 @@
 #  role_id :bigint
 #  user_id :bigint
 #
-RSpec.describe UsersRole, type: :model do
+RSpec.describe UsersRole, type: :model, seed_items: false do
   describe "#current_role_for" do
     context "for org user" do
       it "should return org user" do
-        user = FactoryBot.create(:user)
+        user = create(:user)
         expect(described_class.current_role_for(user).name).to eq("org_user")
       end
     end
 
     context "for org admin" do
       it "should return org admin" do
-        user = FactoryBot.create(:organization_admin)
+        user = create(:organization_admin)
         expect(described_class.current_role_for(user).name).to eq("org_admin")
       end
     end
 
     context "for super admin" do
       it "should return super admin" do
-        user = FactoryBot.create(:super_admin)
+        user = create(:super_admin)
         expect(described_class.current_role_for(user).name).to eq("super_admin")
       end
     end
 
     context "for partner user" do
       it "should return partner user" do
-        user = FactoryBot.create(:partner_user)
+        user = create(:partner_user)
         expect(described_class.current_role_for(user).name).to eq("partner")
       end
     end
