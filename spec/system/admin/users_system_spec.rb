076b968f18b1dead067725b82d0365a631e4529d
Brock Wilcox <awwaiid@thelackthereof.org>
2024-03-29 11:11:19 -0400

    Remove unnecessary and unused organization_id params from some path helpers

--- spec/system/admin/users_system_spec.rb
+++ spec/system/admin/users_system_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Admin Users Management", type: :system, js: true do
     end
 
     it "creates an user" do
-      visit admin_users_path(organization_id: @organization.id)
+      visit admin_users_path
       click_link "Invite a new user"
       find('#user_organization_id option:last-of-type').select_option
       fill_in "user_name", with: "TestUser"
