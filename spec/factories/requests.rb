f6126da1bec087011a2899cfc229e7e2bce68da0
elasticspoon <mail@yuribocharov.dev>
2024-03-30 20:03:44 -0400

    refactor: remove item seeding from mailers

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -24,7 +24,7 @@ end
 FactoryBot.define do
   factory :request do
     partner { Partner.try(:first) || create(:partner) }
-    organization { Organization.try(:first) || create(:organization, skip_items: true) }
+    organization { Organization.try(:first) || create(:organization, :with_items) }
     request_items { random_request_items }
     comments { "Urgent" }
     partner_user { ::User.partner_users.first || create(:partners_user) }
