52b9d1b0c1c54fb7fad42d90fbb239730b2d9dee
elasticspoon <mail@yuribocharov.dev>
2024-04-02 11:59:46 -0400

    feat: allow SuperAdmin to upload NDBN members as CSV
    Add NDBN sync via CSV upload to SuperAdmin role
    Adds NDBN member index view with file upload
    Adds Admin::NDBNMember controller with index and create actions
    Adds links to NDBN index to SuperAdmin sidebar
    Removes old sync service
    Adds unit, service, request tests
    
    Co-authored-by: Veez Remsik <almighty@veez.us>

---
+++ app/controllers/admin/ndbn_members_controller.rb
@@ -0,0 +1,26 @@
+class Admin::NDBNMembersController < AdminController
+  def index
+  end
+
+  def create
+    service = SyncNDBNMembers.new(file_params)
+    service.call
+
+    if service.errors.none?
+      redirect_to admin_ndbn_members_path, notice: "NDBN Members have been updated!"
+    else
+      redirect_to admin_ndbn_members_path, error: service.errors.full_messages
+    end
+  end
+
+  private
+
+  def file_params
+    params[:member_file]&.tempfile
+  end
+
+  def ndbn_members
+    @ndbn_members ||= NDBNMember.all
+  end
+  helper_method :ndbn_members
+end
