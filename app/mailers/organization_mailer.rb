1e305189ebb08f0424985eb025707bb2d96df317
MasumaJaffery <masumajaffery1@gmail.com>
2024-04-03 20:51:46 +0500

    Rectify

--- app/mailers/organization_mailer.rb
+++ app/mailers/organization_mailer.rb
@@ -1,10 +1,3 @@
-# organization.rb
-class Organization < ApplicationRecord
-  def send_submission_notification(partner)
-    OrganizationMailer.request_submission_notification(organization: self, partner: partner).deliver_now if email_notification_opt_in?
-  end
-end
-
 # organization_mailer.rb
 class OrganizationMailer < ApplicationMailer
   default from: "Please do not reply to this email as this mail box is not monitored — Human Essentials <no-reply@humanessentials.app>"
