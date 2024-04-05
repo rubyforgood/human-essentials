ea15885743c36b1ea84035b5dfaec26b17a37509
Daniel Orner <daniel.orner@flipp.com>
2024-04-05 16:57:54 -0400

    Fix spec

--- spec/system/admin/organizations_system_spec.rb
+++ spec/system/admin/organizations_system_spec.rb
@@ -1,7 +1,8 @@
-RSpec.describe "Admin Organization Management", type: :system, js: true do
+RSpec.describe "Admin Organization Management", type: :system, js: true, seed_items: false, skip_transaction: true do
+  let(:super_admin) { create(:super_admin) }
   context "while logged in as a super admin and there are no organizations" do
     before do
-      sign_in(@super_admin)
+      sign_in(super_admin)
     end
 
     it "pagination does not appear" do
@@ -13,12 +14,12 @@ RSpec.describe "Admin Organization Management", type: :system, js: true do
   end
 
   context "while logged in as a super admin and the per page limit is reached but not passed" do
-    # The first org being the default org that gets made during test setup.
+    let!(:first_org) { create(:organization, name: 'first_org')}
     let!(:second_org) { create(:organization, name: 'second_org') }
     let!(:third_org) { create(:organization, name: 'third_org') }
 
     before do
-      sign_in(@super_admin)
+      sign_in(super_admin)
     end
 
     # The per page limit is set to 3 for the tests
@@ -37,7 +38,7 @@ RSpec.describe "Admin Organization Management", type: :system, js: true do
     let!(:fourth_org) { create(:organization, name: 'fourth_org') }
 
     before do
-      sign_in(@super_admin)
+      sign_in(super_admin)
     end
 
     it "pagination does appear" do
@@ -54,11 +55,11 @@ RSpec.describe "Admin Organization Management", type: :system, js: true do
   end
 
   context "While signed in as an Administrative User (super admin)" do
-    let!(:foo_org) { Organization.first }
+    let!(:foo_org) { create(:organization, name: 'foo') }
     let!(:bar_org) { create(:organization, name: 'bar') }
     let!(:baz_org) { create(:organization, name: 'baz') }
     before :each do
-      sign_in(@super_admin)
+      sign_in(super_admin)
     end
 
     it "filters by organizations by name in organizations index page" do
@@ -140,45 +141,6 @@ RSpec.describe "Admin Organization Management", type: :system, js: true do
       expect(page).to have_content("Contact Info")
       expect(page).to have_content("Default email text")
       expect(page).to have_content("Users")
-    end
-  end
-  context "While signed in as an Administrative User with no organization (super admin no org)" do
-    before :each do
-      sign_in(@super_admin_no_org)
-    end
-
-    it "creates a new organization" do
-      visit new_admin_organization_path
-      org_params = attributes_for(:organization)
-      fill_in "organization_name", with: org_params[:name]
-      fill_in "organization_short_name", with: org_params[:short_name]
-      fill_in "organization_url", with: org_params[:url]
-      fill_in "organization_email", with: org_params[:email]
-      fill_in "organization_street", with: "1500 Remount Road"
-      fill_in "organization_city", with: "Front Royal"
-      select("VA", from: "organization_state")
-      fill_in "organization_zipcode", with: "22630"
-
-      admin_user_params = attributes_for(:organization_admin)
-      fill_in "organization_user_name", with: admin_user_params[:name]
-      fill_in "organization_user_email", with: admin_user_params[:email]
-
-      click_on "Save"
-
-      expect(page).to have_content("All Human Essentials Organizations")
-
-      within("tr.#{org_params[:short_name]}") do
-        first(:link, "View").click
-      end
-
-      expect(page).to have_content(org_params[:name])
-      expect(page).to have_content("Remount")
-      expect(page).to have_content("Front Royal")
-      expect(page).to have_content("VA")
-      expect(page).to have_content("22630")
-      expect(page).to have_content(admin_user_params[:name])
-      expect(page).to have_content(admin_user_params[:email])
-      expect(page).to have_content("invited")
     end
   end
 end
