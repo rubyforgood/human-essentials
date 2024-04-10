930b68fba8b7ef2d6e27448650ed0bbef04cfa66
Brock Wilcox <awwaiid@thelackthereof.org>
2024-04-10 12:26:43 -0400

    Group low-inventory report by item, not storage location

--- app/queries/low_inventory_query.rb
+++ app/queries/low_inventory_query.rb
@@ -1,8 +1,23 @@
 class LowInventoryQuery
   def self.call(organization)
-    scope = organization.inventory_items
-    scope.where("inventory_items.quantity < items.on_hand_minimum_quantity")
-      .or(scope.where("inventory_items.quantity < items.on_hand_recommended_quantity"))
-      .includes(:item, :storage_location)
+    sql_query = <<-SQL
+      SELECT
+        items.id,
+        items.name,
+        items.on_hand_minimum_quantity,
+        items.on_hand_recommended_quantity,
+        sum(inventory_items.quantity) as total_quantity
+      FROM inventory_items
+      JOIN items ON items.id = inventory_items.item_id
+      JOIN storage_locations ON storage_locations.id = inventory_items.storage_location_id
+      WHERE storage_locations.organization_id = ?
+      GROUP BY items.id, items.name, items.on_hand_minimum_quantity, items.on_hand_recommended_quantity
+      HAVING sum(inventory_items.quantity) < items.on_hand_minimum_quantity
+        OR sum(inventory_items.quantity) < items.on_hand_recommended_quantity
+      ORDER BY items.name
+    SQL
+
+    sanitized_sql = ActiveRecord::Base.send(:sanitize_sql_array, [sql_query, organization.id])
+    ActiveRecord::Base.connection.execute(sanitized_sql).to_a
   end
 end
