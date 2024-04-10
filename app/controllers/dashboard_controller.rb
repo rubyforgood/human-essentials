f4202a95051085658e6b10430f46952faa84e3dd
Brock Wilcox <awwaiid@thelackthereof.org>
2024-04-10 12:26:19 -0400

    Only show outstanding requests for the current org

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,8 @@ class DashboardController < ApplicationController
     @org_stats = OrganizationStats.new(current_organization)
     @total_inventory = current_organization.total_inventory
     @partners_awaiting_review = current_organization.partners.awaiting_review
-    @outstanding_requests = Request.includes([:partner, :partner_user]).where(status: %i[pending started]).order(:created_at)
+    @outstanding_requests = current_organization.ordered_requests.where(status: %i[pending started]).order(:created_at)
+
     @low_inventory_report = LowInventoryQuery.call(current_organization)
 
     # passing nil here filters the announcements that didn't come from an organization
