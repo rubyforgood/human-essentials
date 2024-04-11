e59f1c6cb8aba67fc2dad220b2f3186dccfef5b5
cancelei <glauber.bannwart@gmail.com>
2024-04-11 18:21:26 -0300

    Refactor dynamic_items_controller.js and _form.html.erb to improve code readability and maintainability

--- app/javascript/controllers/dynamic_items_controller.js
+++ app/javascript/controllers/dynamic_items_controller.js
@@ -1,17 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["itemsContainer", "template"]
+  static targets = ["itemsContainer",  "addTemplate", "addButton"]
 
-  addNewItem(event) {
+  addItem(event) {
     event.preventDefault();
-    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
+    const content = this.addTemplateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
     this.itemsContainerTarget.insertAdjacentHTML("beforeend", content);
   }
 
   removeItem(event) {
     event.preventDefault();
-    let itemToRemove = event.target.closest(".nested-fields");
-    itemToRemove.remove();
+    const itemToRemove = event.target.closest(this.getRemoveSelector(event));
+    if (itemToRemove) {
+      if (event.target.dataset.removeSoft === "true") {
+        // Optionally handle soft delete logic here
+        console.log("Soft delete, handle accordingly.");
+      } else {
+        itemToRemove.remove();
+      }
+    }
+  }
+
+  getRemoveSelector(event) {
+    return event.target.dataset.removeParentSelector;
   }
 }
