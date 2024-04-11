32ffe4174145990f0e26fc868eaf7f39eed8bacb
elasticspoon <mail@yuribocharov.dev>
2024-04-11 18:01:39 -0400

    refactor: remove error mixin

--- app/services/sync_ndbn_members.rb
+++ app/services/sync_ndbn_members.rb
@@ -1,33 +1,24 @@
 class SyncNDBNMembers
-  include ServiceObjectErrorsMixin
+  def self.upload(member_file)
+    begin
+      member_entries = parse_csv(member_file)
+    rescue ParseError => e
+      return [e.message]
+    end
 
-  def initialize(member_file)
-    @member_file = member_file
-  end
-  attr_reader :member_file
-
-  def call
-    member_entries = parse_csv
-
-    return if errors.any?
-
-    member_entries.each do |member_id, account_name|
+    member_entries.flat_map do |member_id, account_name|
       ndbn_member = NDBNMember.find_or_initialize_by(ndbn_member_id: member_id.to_i)
       ndbn_member.account_name = account_name
+      ndbn_member.save
 
-      unless ndbn_member.save
-        ndbn_member.errors.full_messages.each do |msg|
-          error = "Issue with #{member_id}: #{account_name} -> #{msg}"
-          errors.add(:base, error)
-        end
+      ndbn_member.errors.full_messages.flat_map do |msg|
+        "Issue with #{member_id}: #{account_name} -> #{msg}"
       end
     end
   end
 
-  private
-
-  def parse_csv
-    return errors.add(:base, "CSV upload is required.") if member_file.nil?
+  def self.parse_csv(member_file)
+    raise ParseError, "CSV upload is required." if member_file.nil?
 
     raw = CSV.parse(member_file)
 
@@ -35,6 +26,7 @@ class SyncNDBNMembers
       member_id.match(/^\d+$/) && member_name.present?
     end
   rescue CSV::MalformedCSVError
-    errors.add(:base, "The CSV File provided was invalid.")
+    raise ParseError, "The CSV File provided was invalid."
   end
+  private_class_method :parse_csv
 end
