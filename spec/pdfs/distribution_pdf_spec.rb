bfb4c2ab5dcc1cf1be77ec56f1462773ac328322
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2024-04-17 13:52:11 +0200

    add more specs depending on organization print options on distributions

--- spec/pdfs/distribution_pdf_spec.rb
+++ spec/pdfs/distribution_pdf_spec.rb
@@ -6,12 +6,13 @@ describe DistributionPdf do
   let(:item2) { FactoryBot.create(:item, name: "Item 2", value_in_cents: 200) }
   let(:item3) { FactoryBot.create(:item, name: "Item 3", value_in_cents: 300) }
   let(:item4) { FactoryBot.create(:item, name: "Item 4", package_size: 25, value_in_cents: 400) }
-  let(:org_hide_packages_and_values) do
+
+  let(:org_hiding_packages_and_values) do
     FactoryBot.create(:organization, name: DEFAULT_TEST_ORGANIZATION_NAME,
       hide_value_columns_on_receipt: true, hide_package_column_on_receipt: true)
   end
-  let(:org_hide_packages) { FactoryBot.create(:organization, name: DEFAULT_TEST_ORGANIZATION_NAME, hide_package_column_on_receipt: true) }
-  let(:org_hide_values) { FactoryBot.create(:organization, name: DEFAULT_TEST_ORGANIZATION_NAME, hide_value_columns_on_receipt: true) }
+  let(:org_hiding_packages) { FactoryBot.create(:organization, name: DEFAULT_TEST_ORGANIZATION_NAME, hide_package_column_on_receipt: true) }
+  let(:org_hiding_values) { FactoryBot.create(:organization, name: DEFAULT_TEST_ORGANIZATION_NAME, hide_value_columns_on_receipt: true) }
 
   before(:each) do
     FactoryBot.create(:line_item, itemizable: distribution, item: item1, quantity: 50)
@@ -48,7 +49,7 @@ describe DistributionPdf do
   context "with request data" do
     describe "#hide_columns" do
       it "hides value and package columns when true on organization" do
-        pdf = described_class.new(org_hide_packages_and_values, distribution)
+        pdf = described_class.new(org_hiding_packages_and_values, distribution)
         data = pdf.request_data
         results = pdf.hide_columns(data)
         expect(results).to eq([
@@ -61,24 +62,72 @@ describe DistributionPdf do
           ["Total Items Received", 200, 150]
         ])
       end
+
+      it "hides value columns when true on organization" do
+        pdf = described_class.new(org_hiding_values, distribution)
+        data = pdf.request_data
+        results = pdf.hide_columns(data)
+        expect(results).to eq([
+          ["Items Received", "Requested", "Received", "Packages"],
+          ["Item 1", "", 50, "1"],
+          ["Item 2", 30, 100, nil],
+          ["Item 3", 50, "", nil],
+          ["Item 4", 120, "", nil],
+          ["", "", ""],
+          ["Total Items Received", 200, 150, ""]
+        ])
+      end
     end
   end
 
   context "with non request data" do
-    specify "#hide_columns" do
-      Request.find_by(distribution:).destroy
-      pdf = described_class.new(org_hide_packages_and_values, distribution)
-      data = pdf.non_request_data
+    it "hides value and package columns when true on organization" do
+      pdf = described_class.new(org_hiding_packages_and_values, distribution)
+      data = pdf.request_data
       results = pdf.hide_columns(data)
       expect(results).to eq([
-        ["Items Received", "Quantity"],
-        ["Item 1", 50],
-        ["Item 2", 100],
-        ["", ""],
-        ["Total Items Received", 150]
+        ["Items Received", "Requested", "Received"],
+        ["Item 1", "", 50],
+        ["Item 2", 30, 100],
+        ["Item 3", 50, ""],
+        ["Item 4", 120, ""],
+        ["", "", ""],
+        ["Total Items Received", 200, 150]
       ])
     end
-    # add specs with different set of params
+
+    it "hides value columns when true on organization" do
+      pdf = described_class.new(org_hiding_values, distribution)
+      data = pdf.request_data
+      results = pdf.hide_columns(data)
+      expect(results).to eq([
+        ["Items Received", "Requested", "Received", "Packages"],
+        ["Item 1", "", 50, "1"],
+        ["Item 2", 30, 100, nil],
+        ["Item 3", 50, "", nil],
+        ["Item 4", 120, "", nil],
+        ["", "", ""],
+        ["Total Items Received", 200, 150, ""]
+      ])
+    end
+  end
+  context "regardles of request data" do
+    describe "#hide_columns" do
+      it "hides package column when true on organization" do
+        pdf = described_class.new(org_hiding_packages, distribution)
+        data = pdf.request_data
+        results = pdf.hide_columns(data)
+        expect(results).to eq([
+          ["Items Received", "Requested", "Received", "Value/item", "In-Kind Value Received"],
+          ["Item 1", "", 50, "$1.00", "$50.00"],
+          ["Item 2", 30, 100, "$2.00", "$200.00"],
+          ["Item 3", 50, "", "$3.00", nil],
+          ["Item 4", 120, "", "$4.00", nil],
+          ["", "", "", "", ""],
+          ["Total Items Received", 200, 150, "", "$250.00"]
+        ])
+      end
+    end
   end
 end
 # rubocop:enable Layout/ArrayAlignment
