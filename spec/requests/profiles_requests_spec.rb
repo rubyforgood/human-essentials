410c9df4836b47142ea048491d50bd7f76f453bd
Jade Stewart <jadekstewart3@gmail.com>
2024-04-18 14:43:48 -0600

    Added test to ensure program address is being updated with the partner profile

--- spec/requests/profiles_requests_spec.rb
+++ spec/requests/profiles_requests_spec.rb
@@ -31,6 +31,28 @@ RSpec.describe "Profiles", type: :request do
         expect(partner.profile.facebook).to eq("facebooksucks")
       end
 
+      it "updates partner program address" do 
+        new_partner_program_params = { 
+            name: partner.name, profile: {
+            program_address1: "123 Happy Pl", 
+            program_city: "Golden", 
+            program_state: "CO", 
+            program_zip_code: 80401 
+          }
+        }
+
+        put profile_path(default_params.merge(id: partner, partner: new_partner_program_params))
+
+        partner.profile.reload
+
+        expect(response).to have_http_status(:redirect)
+        expect(partner.profile.program_address1).to eq("123 Happy Pl")
+        expect(partner.profile.program_address2).to be_blank
+        expect(partner.profile.program_city).to eq("Golden")
+        expect(partner.profile.program_state).to eq("CO")
+        expect(partner.profile.program_zip_code).to eq(80401)
+      end
+
       it "redirects to #show" do
         put profile_path(default_params.merge(id: partner, partner: partner_params))
         expect(response).to redirect_to(partner_path(partner) + "#partner-information")
