eec777ff58d7aca65b2903f968cf57b06acfbdbb
Andrew Eisenberg <aeisenberg@github.com>
2024-04-20 22:36:27 -0700

    Remove commented code

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,8 +2,6 @@ RUBY_VERSION="$(cat .ruby-version | tr -d '\n')"
 
 cp .devcontainer/.env.codespaces .env
 
-# source /usr/local/rvm/scripts/rvm
-
 # If the project's required ruby version changes from 3.2.2, this command
 # will download and compile the correct version, but it will take a long time.
 if [ "$RUBY_VERSION" != "3.2.2" ]; then
