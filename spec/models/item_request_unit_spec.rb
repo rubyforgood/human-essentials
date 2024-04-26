f31245a852c35130d7638dc89b436c42fb34bccf
Daniel Orner <daniel.orner@flipp.com>
2024-04-26 10:43:18 -0400

    fix lints

--- spec/models/item_request_unit_spec.rb
+++ spec/models/item_request_unit_spec.rb
@@ -2,12 +2,12 @@ RSpec.describe ItemRequestUnit, type: :model do
   context "Validations >" do
     let(:organization) { create(:organization) }
     let(:item) { create(:item, organization: organization) }
-    it 'should only be valid if the organization has a corresponding unit' do
-      unit = build(:item_request_unit, item: item, name: 'pack')
+    it "should only be valid if the organization has a corresponding unit" do
+      unit = build(:item_request_unit, item: item, name: "pack")
       expect(unit.valid?).to eq(false)
       expect(unit.errors.full_messages).to eq(["Name is not supported by the organization"])
 
-      create(:request_unit, organization: organization, name: 'pack')
+      create(:request_unit, organization: organization, name: "pack")
       organization.reload
       expect(unit.valid?).to eq(true)
     end
