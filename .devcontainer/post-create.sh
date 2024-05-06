55f459971a6c7b8dae92dd4309bb2a4dd999c1fc
Andrew Eisenberg <aeisenberg@github.com>
2024-05-06 23:14:36 +0000

    Copy `.env` only if it does not already exist

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,7 @@
 RUBY_VERSION="$(cat .ruby-version | tr -d '\n')"
 
-cp -f .devcontainer/.env.codespaces .env
+# copy the file only if it doesn't already exist
+cp -n .devcontainer/.env.codespaces .env
 
 # If the project's required ruby version changes from 3.2.2, this command
 # will download and compile the correct version, but it will take a long time.
