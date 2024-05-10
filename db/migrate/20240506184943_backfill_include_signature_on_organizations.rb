ee10266c3ff97743497030e5b34680ce0fa36cc0
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2024-05-10 13:05:34 -0400

    fix: rename column

--- db/migrate/20240506184943_backfill_include_signature_on_organizations.rb
+++ db/migrate/20240506184943_backfill_include_signature_on_organizations.rb
@@ -2,6 +2,6 @@ class BackfillIncludeSignatureOnOrganizations < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def up
-    Organization.update_all(include_signature_fields_on_distribution_printout: false)
+    Organization.update_all(signature_for_distribution_pdf: false)
   end
 end
