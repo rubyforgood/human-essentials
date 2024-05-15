95110f98fb9301fe6f2d3288945ea4240fb09b12
elasticspoon <mail@yuribocharov.dev>
2024-05-14 22:46:26 -0400

    fix: specs that got added

--- spec/requests/reports/distributions_summary_requests_spec.rb
+++ spec/requests/reports/distributions_summary_requests_spec.rb
@@ -1,19 +1,22 @@
 require "rails_helper"
 
-RSpec.describe "Distributions", type: :request do
+RSpec.describe "Distributions", type: :request, skip_seed: true do
+  let(:organization) { create(:organization, skip_items: true) }
+  let(:user) { create(:user, organization: organization) }
+
   context "while signed in" do
     before do
       travel_to("2024-01-17")
     end
 
     before do
-      sign_in(@user)
+      sign_in(user)
     end
 
     context "the index page" do
       context "without filters" do
         before do
-          get reports_distributions_summary_path(@user.organization)
+          get reports_distributions_summary_path(user.organization)
         end
 
         it "shows a list of recent distributions" do
@@ -28,18 +31,18 @@ RSpec.describe "Distributions", type: :request do
       context "with filters" do
         before do
           # Create a bunch of historical distributions
-          create :distribution, :with_items, item_quantity: 2, issued_at: 0.days.ago
-          create :distribution, :with_items, item_quantity: 3, issued_at: 1.day.ago
-          create :distribution, :with_items, item_quantity: 7, issued_at: 3.days.ago
-          create :distribution, :with_items, item_quantity: 11, issued_at: 10.days.ago
-          create :distribution, :with_items, item_quantity: 13, issued_at: 20.days.ago
-          create :distribution, :with_items, item_quantity: 17, issued_at: 30.days.ago
+          create :distribution, :with_items, item_quantity: 2, issued_at: 0.days.ago, organization: organization
+          create :distribution, :with_items, item_quantity: 3, issued_at: 1.day.ago, organization: organization
+          create :distribution, :with_items, item_quantity: 7, issued_at: 3.days.ago, organization: organization
+          create :distribution, :with_items, item_quantity: 11, issued_at: 10.days.ago, organization: organization
+          create :distribution, :with_items, item_quantity: 13, issued_at: 20.days.ago, organization: organization
+          create :distribution, :with_items, item_quantity: 17, issued_at: 30.days.ago, organization: organization
         end
 
         let(:formatted_date_range) { date_range.map { _1.to_formatted_s(:date_picker) }.join(" - ") }
 
         before do
-          get reports_distributions_summary_path(@user.organization), params: {filters: {date_range: formatted_date_range}}
+          get reports_distributions_summary_path(user.organization), params: {filters: {date_range: formatted_date_range}}
         end
 
         context "today" do
@@ -83,7 +86,7 @@ RSpec.describe "Distributions", type: :request do
   context "while not signed in" do
     describe "GET #index" do
       it "redirects user to sign in page" do
-        get reports_distributions_summary_path(@user.organization)
+        get reports_distributions_summary_path(user.organization)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
