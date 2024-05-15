41a0429a527f2727f2bb506fd4e2bf2050323607
nathan <nathan.gordon.thomas@gmail.com>
2024-05-15 12:58:33 -0600

    Remove request_type from tests

--- spec/services/partners/family_request_create_service_spec.rb
+++ spec/services/partners/family_request_create_service_spec.rb
@@ -5,7 +5,6 @@ describe Partners::FamilyRequestCreateService do
     subject { described_class.new(**args).call }
     let(:args) do
       {
-        request_type: "quantity",
         partner_user_id: partner_user.id,
         comments: comments,
         for_families: for_families,
@@ -109,7 +108,7 @@ describe Partners::FamilyRequestCreateService do
         let(:fake_request_create_service) { instance_double(Partners::RequestCreateService, call: -> {}, errors: [], partner_request: -> {}) }
 
         before do
-          allow(Partners::RequestCreateService).to receive(:new).with(request_type: "quantity", partner_user_id: partner_user.id, comments: comments, for_families: false, item_requests_attributes: contain_exactly(*expected_item_request_attributes)).and_return(fake_request_create_service)
+          allow(Partners::RequestCreateService).to receive(:new).with(partner_user_id: partner_user.id, comments: comments, for_families: false, item_requests_attributes: contain_exactly(*expected_item_request_attributes)).and_return(fake_request_create_service)
         end
 
         it 'should send the correct request payload to the Partners::RequestCreateService and call it' do
--- spec/services/partners/request_create_service_spec.rb
+++ spec/services/partners/request_create_service_spec.rb
@@ -5,7 +5,6 @@ describe Partners::RequestCreateService do
     subject { described_class.new(**args).call }
     let(:args) do
       {
-        request_type: "quantity",
         partner_user_id: partner_user.id,
         comments: comments,
         item_requests_attributes: item_requests_attributes
