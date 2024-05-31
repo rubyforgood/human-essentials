1d6e00b3e882c3f711ba8dfdb7096860587c9052
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-05-30 23:18:08 -0500

    Since a custom error message was added for name validation, this seed item method within organization needed to be updated to reflect the new error message. This is because it was checking for the presence of ActiveRecords typical "been taken" message. within its rescue.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -214,7 +214,7 @@ class Organization < ApplicationRecord
     rescue ActiveRecord::RecordInvalid => e
       Rails.logger.info "[SEED] Duplicate item! #{e.record.name}"
       existing_item = items.find_by(name: e.record.name)
-      if e.to_s.match(/been taken/).present? && existing_item.other?
+      if e.to_s.match(/already exists/).present? && existing_item.other?
         Rails.logger.info "Changing Item##{existing_item.id} from Other to #{e.record.partner_key}"
         existing_item.update(partner_key: e.record.partner_key)
         existing_item.reload
