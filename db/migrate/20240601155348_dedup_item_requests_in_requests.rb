3447b50ce6c6711c8cb6f0de0f056c2c4b4a2729
Scott Steele <scottlsteele@gmail.com>
2024-06-01 15:12:24 -0400

    squashme! update jsonb request_items in migration

--- db/migrate/20240601155348_dedup_item_requests_in_requests.rb
+++ db/migrate/20240601155348_dedup_item_requests_in_requests.rb
@@ -5,20 +5,31 @@ class DedupItemRequestsInRequests < ActiveRecord::Migration[7.1]
     Request.find_each do |request|
       grouped_item_requests = request.item_requests.to_a.group_by(&:item_id)
 
-      grouped_item_requests.each do |item_id, item_requests|
-        next if item_requests.size == 1
-
-        Partners::ItemRequest.transaction do
+      Request.transaction do
+        request_items = grouped_item_requests.map do |item_id, item_requests|
           quantity = item_requests.map { |item_request| item_request.quantity.to_i }.sum.to_s
           children = item_requests.flat_map(&:children).uniq
 
-          item_requests.first.update!(
-            quantity: quantity,
-            children: children
-          )
+          primary_item_request = item_requests.shift
 
-          item_requests.drop(1).each(&:destroy!)
+          # If item_requests isn't empty, then there were more
+          # item_requests with that same item_id
+          if !item_requests.empty?
+            primary_item_request.update!(
+              quantity: quantity,
+              children: children
+            )
+
+            item_requests.each(&:destroy!)
+          end
+
+          {
+            item_id: item_id,
+            quantity: quantity
+          }
         end
+
+        request.update!(request_items: request_items)
       end
     end
   end
