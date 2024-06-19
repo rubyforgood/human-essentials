db4862ac0ecc19336dea789ce3155e84c42dc7ee
Brock Wilcox <awwaiid@thelackthereof.org>
2024-06-19 12:41:04 -0400

    Hide the select2 drop-down for units [#4396]

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -5,10 +5,17 @@ import "select2"
 export default class extends Controller {
   static values = {
     config: { type: Object, default: {} },
-  }
+    hideDropdown: { type: Boolean, default: false }
+  };
 
   connect() {
-    $(this.element).select2(this.configValue);
+    const select2 = $(this.element).select2(this.configValue);
+
+    if (this.hideDropdownValue) {
+      select2.on('select2:open', function (e) {
+        $('.select2-container--open .select2-dropdown--below').css('display','none');
+      });
+    }
 
     /**
      * This is a workaround to auto focus on the select2 input when it is opened.
