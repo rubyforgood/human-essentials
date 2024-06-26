21d9dd9545d187d84b20728d7789765cf6ad205c
elasticspoon <mail@yuribocharov.dev>
2024-06-26 16:58:40 -0400

    fix: simplify migration
    All discarded users are org_user meaning they have been deactivated. To
    simplify everything we will just remove all thier roles.

--- db/migrate/20240624185108_reactivate_users_and_remove_org_user_roles.rb
+++ db/migrate/20240624185108_reactivate_users_and_remove_org_user_roles.rb
@@ -1,10 +1,10 @@
 class ReactivateUsersAndRemoveOrgUserRoles < ActiveRecord::Migration[7.1]
   def up
-    users = User.unscoped.joins(:roles).where.not(discarded_at: nil).where("roles.name": "org_user")
+    users = User.unscoped.where.not(discarded_at: nil)
     users.each do |user|
       user.transaction do
         user.update!(discarded_at: nil)
-        user.roles.where(name: "org_user").delete_all
+        user.roles.delete_all
       end
     end
   end
