3281007975e2624fa73ea269783b210cf372e4eb
Jimmy Li <jimmy.jiajian.li@gmail.com>
2024-07-08 00:07:48 -0400

    Add launch.json default on codespace postCreate

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,6 +2,7 @@ RUBY_VERSION="$(cat .ruby-version | tr -d '\n')"
 
 # copy the file only if it doesn't already exist
 cp -n .devcontainer/.env.codespaces .env
+mkdir -p .vscode && cp -n .devcontainer/launch.json.codespaces .vscode/launch.json
 
 # If the project's required ruby version changes from 3.2.2, this command
 # will download and compile the correct version, but it will take a long time.
