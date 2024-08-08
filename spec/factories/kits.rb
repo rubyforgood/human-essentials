a0cac69c1808b6499bfc02b6f7a42cf76d20c437
Jimmy Li <jimmy.jiajian.li@gmail.com>
2024-08-08 23:48:16 +0000

    Remove line items from kits factory

--- spec/factories/kits.rb
+++ spec/factories/kits.rb
@@ -16,12 +16,6 @@ FactoryBot.define do
     sequence(:name) { |n| "Default Kit Name #{n} - Don't Match" }
     organization
 
-    after(:build) do |instance, _|
-      if instance.line_items.blank?
-        instance.line_items << build(:line_item, item: create(:item, organization: instance.organization), itemizable: nil)
-      end
-    end
-
     # See #3652, changes to this factory are in progress
     # For now, to create corresponding item and line item and persist to database call KitCreateService with:
     # kit_params = attributes_for(:kit)
