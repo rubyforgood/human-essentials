6a5ac86f3f5df3fa3f1d7abc7373e226b3aa0bd1
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-08-23 13:01:45 -0700

    Updating rspec test description to be more accurate

--- spec/requests/partner_users_requests_spec.rb
+++ spec/requests/partner_users_requests_spec.rb
@@ -122,7 +122,7 @@ RSpec.describe PartnerUsersController, type: :request do
       )
     end
 
-    context "when the partner needs to reset a user's password" do
+    context "when a bank needs to reset a partner user's password" do
       it "resends the reset password email and redirects back to root_path" do
         expect { post reset_password_partner_user_path(default_params.merge(partner_id: partner, id: partner_user)) }.to change { ActionMailer::Base.deliveries.count }.by(1)
         expect(response).to redirect_to(root_path)
