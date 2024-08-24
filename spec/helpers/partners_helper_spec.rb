cf5ed2daa9ff83ff94b7fa9ca4210063847c388a
Daniela Baron <baron.daniela@gmail.com>
2024-08-24 10:14:37 -0400

    [#4504] Add step-wise partner profile edit form: - Introduce feature flag to control rendering of new partner step forms. - Update controller edit and update actions to use feature flag. - Add accordion UI for expand/collapse of sections in step forms. - Build out partner step forms for each section. - Update partner profile validation to set errors at field level. - Add service to map profile field errors to sections they belong in. - Update accordion styles to render as open if they contain an error. - Replace bootstrap caret icons with saving indicator during form submission. - Add system tests for partner profile step-wise editing.

---
+++ spec/helpers/partners_helper_spec.rb
@@ -0,0 +1,42 @@
+describe PartnersHelper, type: :helper do
+  describe "show_submit_for_approval?" do
+    it "returns true if invited" do
+      partner = build_stubbed(:partner, status: :invited)
+      expect(helper.show_submit_for_approval?(partner)).to be_truthy
+    end
+
+    it "returns true if recertification required" do
+      partner = build_stubbed(:partner, status: :recertification_required)
+      expect(helper.show_submit_for_approval?(partner)).to be_truthy
+    end
+
+    it "returns false if awaiting review" do
+      partner = build_stubbed(:partner, status: :awaiting_review)
+      expect(helper.show_submit_for_approval?(partner)).to be_falsey
+    end
+  end
+
+  describe "submit_for_approval_disabled?" do
+    it "returns true if partner profile has errors" do
+      profile = build_stubbed(:partner_profile)
+      profile.errors.add(:base, "Some error message")
+
+      expect(helper.submit_for_approval_disabled?(profile)).to be_truthy
+    end
+
+    it "returns false if partner profile is valid" do
+      profile = build_stubbed(:partner_profile)
+      expect(helper.submit_for_approval_disabled?(profile)).to be_falsey
+    end
+  end
+
+  describe "partial_display_name" do
+    it "returns the humanized name by default" do
+      expect(helper.partial_display_name("agency_stability")).to eq("Agency stability")
+    end
+
+    it "returns the custom display name when overridden" do
+      expect(helper.partial_display_name("attached_documents")).to eq("Additional Documents")
+    end
+  end
+end
---
+++ spec/system/partners/profile_edit_system_spec.rb
@@ -0,0 +1,93 @@
+RSpec.describe "Partners profile edit", type: :system, js: true do
+  let!(:partner1) { create(:partner, status: "invited") }
+  let(:partner1_user) { partner1.primary_user }
+
+  context "step-wise editing is enabled" do
+    before do
+      Flipper.enable(:partner_step_form)
+      login_as(partner1_user)
+      visit edit_partners_profile_path
+    end
+
+    it "displays all sections in a closed state by default" do
+      within ".accordion" do
+        expect(page).to have_css("#agency_information.accordion-collapse.collapse", visible: false)
+        expect(page).to have_css("#program_delivery_address.accordion-collapse.collapse", visible: false)
+
+        partner1.partials_to_show.each do |partial|
+          expect(page).to have_css("##{partial}.accordion-collapse.collapse", visible: false)
+        end
+
+        expect(page).to have_css("#partner_settings.accordion-collapse.collapse", visible: false)
+      end
+    end
+
+    it "allows sections to be opened, closed, filled in any order, and submit for approval" do
+      # Media
+      find("button[data-bs-target='#media_information']").click
+      expect(page).to have_css("#media_information.accordion-collapse.collapse.show", visible: true)
+      within "#media_information" do
+        fill_in "Website", with: "https://www.example.com"
+      end
+      find("button[data-bs-target='#media_information']").click
+      expect(page).to have_css("#media_information.accordion-collapse.collapse", visible: false)
+
+      # Executive director
+      find("button[data-bs-target='#executive_director']").click
+      expect(page).to have_css("#executive_director.accordion-collapse.collapse.show", visible: true)
+      within "#executive_director" do
+        fill_in "Executive Director Name", with: "Lisa Smith"
+      end
+
+      # Save Progress
+      all("input[type='submit'][value='Save Progress']").last.click
+      expect(page).to have_css(".alert-success", text: "Details were successfully updated.")
+
+      # Submit for Approval
+      expect(page).to have_link("Submit Profile for Approval", href: partners_approval_request_path)
+      first(:link, "Submit Profile for Approval").click
+      expect(current_path).to eq(partners_profile_path)
+      expect(page).to have_css(".alert-success", text: "You have submitted your details for approval.")
+      expect(page).to have_css(".badge", text: "Awaiting Review")
+    end
+
+    it "displays the edit view with sections containing validation errors expanded" do
+      # Open up Media section and clear out website value
+      find("button[data-bs-target='#media_information']").click
+      within "#media_information" do
+        fill_in "Website", with: ""
+      end
+
+      # Open Pick up person section and fill in 4 email addresses
+      find("button[data-bs-target='#pick_up_person']").click
+      within "#pick_up_person" do
+        fill_in "Pick Up Person's Email", with: "email1@example.com, email2@example.com, email3@example.com, email4@example.com"
+      end
+
+      # Open Partner Settings section and uncheck all options
+      find("button[data-bs-target='#partner_settings']").click
+      within "#partner_settings" do
+        uncheck "Enable Quantity-based Requests" if has_checked_field?("Enable Quantity-based Requests")
+        uncheck "Enable Child-based Requests (unclick if you only do bulk requests)" if has_checked_field?("Enable Child-based Requests (unclick if you only do bulk requests)")
+        uncheck "Enable Requests for Individuals" if has_checked_field?("Enable Requests for Individuals")
+      end
+
+      # Save Progress
+      all("input[type='submit'][value='Save Progress']").last.click
+
+      # Expect an alert-danger message containing validation errors
+      expect(page).to have_css(".alert-danger", text: /There is a problem/)
+      expect(page).to have_content("No social media presence must be checked if you have not provided any of Website, Twitter, Facebook, or Instagram.")
+      expect(page).to have_content("Enable child based requests At least one request type must be set")
+      expect(page).to have_content("Pick up email can't have more than three email addresses")
+
+      # Expect media section, executive director section, and partner settings section to be opened
+      expect(page).to have_css("#media_information.accordion-collapse.collapse.show", visible: true)
+      expect(page).to have_css("#pick_up_person.accordion-collapse.collapse.show", visible: true)
+      expect(page).to have_css("#partner_settings.accordion-collapse.collapse.show", visible: true)
+
+      # Submit for Approval is disabled
+      expect(page).to have_css("span.btn.btn-success.disabled", text: "Submit Profile for Approval")
+    end
+  end
+end
