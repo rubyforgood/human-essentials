1b228ae2a7e38709ea48052e193d1774b2a46fae
Aaryan <53212802+Aaryanpal@users.noreply.github.com>
2024-09-21 01:22:12 +0530

    4634 - Add the Delete button on the Partner Group Page (#4649)
    * Add the Delete button on the Partner Group Page
    
    * Replace Destroyed with Deleted
    
    * Resolve Comments
    
    * Added Request Spec for PartnerGroup and remove corresponding system-spec
    
    * Fix lint
    
    * fix the request-spec

--- app/controllers/partner_groups_controller.rb
+++ app/controllers/partner_groups_controller.rb
@@ -1,4 +1,6 @@
 class PartnerGroupsController < ApplicationController
+  before_action :set_partner_group, only: %i[edit destroy]
+
   def new
     @partner_group = current_organization.partner_groups.new
     @item_categories = current_organization.item_categories
@@ -16,7 +18,6 @@ class PartnerGroupsController < ApplicationController
   end
 
   def edit
-    @partner_group = current_organization.partner_groups.find(params[:id])
     @item_categories = current_organization.item_categories
   end
 
@@ -30,7 +31,22 @@ class PartnerGroupsController < ApplicationController
     end
   end
 
+  def destroy
+    if @partner_group.partners.any?
+      redirect_to partners_path + "#nav-partner-groups", alert: "Partner Group cannot be deleted."
+    else
+      @partner_group.destroy
+      respond_to do |format|
+        format.html { redirect_to partners_path + "#nav-partner-groups", notice: "Partner Group was successfully deleted." }
+      end
+    end
+  end
+
   private
+
+  def set_partner_group
+    @partner_group = current_organization.partner_groups.find(params[:id])
+  end
 
   def partner_group_params
     params.require(:partner_group).permit(:name, :send_reminders, :deadline_day, :reminder_day, item_category_ids: [])
