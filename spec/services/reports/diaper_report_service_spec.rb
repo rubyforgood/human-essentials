ebc609819ae71eff4b2d9e9d3a8eb5f3d57ee4dc
sbpipb <julianmacmang@gmail.com>
2024-10-03 17:40:15 +0800

    lint fixes

--- spec/services/reports/diaper_report_service_spec.rb
+++ spec/services/reports/diaper_report_service_spec.rb
@@ -45,16 +45,16 @@ RSpec.describe Reports::DiaperReportService, type: :service, persisted_data: tru
 
       # Donations outside drives
       non_drive_donations = create_list(:donation, 2,
-                                        product_drive: nil,
-                                        issued_at: within_time,
-                                        money_raised: 1000,
-                                        organization: organization)
+        product_drive: nil,
+        issued_at: within_time,
+        money_raised: 1000,
+        organization: organization)
 
       non_drive_donations += create_list(:donation, 2,
-                                         product_drive: nil,
-                                         issued_at: outside_time,
-                                         money_raised: 1000,
-                                         organization: organization)
+        product_drive: nil,
+        issued_at: outside_time,
+        money_raised: 1000,
+        organization: organization)
 
       non_drive_donations.each do |donation|
         create_list(:line_item, 3, :donation, quantity: 20, item: disposable_item, itemizable: donation)
@@ -63,22 +63,22 @@ RSpec.describe Reports::DiaperReportService, type: :service, persisted_data: tru
 
       # Product drives
       drives = create_list(:product_drive, 2,
-                           start_date: within_time,
-                           end_date: nil,
-                           virtual: false,
-                           organization: organization)
+        start_date: within_time,
+        end_date: nil,
+        virtual: false,
+        organization: organization)
       outside_drives = create_list(:product_drive, 2,
-                                   start_date: outside_time - 1.month,
-                                   end_date: outside_time,
-                                   organization: organization,
-                                   virtual: false)
+        start_date: outside_time - 1.month,
+        end_date: outside_time,
+        organization: organization,
+        virtual: false)
 
       donations = (drives + outside_drives).map do |drive|
         create_list(:product_drive_donation, 3,
-                    product_drive: drive,
-                    issued_at: drive.start_date + 1.day,
-                    money_raised: 1000,
-                    organization: organization)
+          product_drive: drive,
+          issued_at: drive.start_date + 1.day,
+          money_raised: 1000,
+          organization: organization)
       end
       donations.flatten!
       donations.each do |donation|
@@ -88,22 +88,22 @@ RSpec.describe Reports::DiaperReportService, type: :service, persisted_data: tru
 
       # Virtual product drives
       vdrives = create_list(:product_drive, 2,
-                            start_date: within_time,
-                            end_date: nil,
-                            virtual: true,
-                            organization: organization)
+        start_date: within_time,
+        end_date: nil,
+        virtual: true,
+        organization: organization)
       outside_vdrives = create_list(:product_drive, 2,
-                                    start_date: outside_time - 1.month,
-                                    end_date: outside_time,
-                                    organization: organization,
-                                    virtual: true)
+        start_date: outside_time - 1.month,
+        end_date: outside_time,
+        organization: organization,
+        virtual: true)
 
       vdonations = (vdrives + outside_vdrives).map do |drive|
         create(:product_drive_donation,
-               product_drive: drive,
-               money_raised: 1000,
-               issued_at: drive.start_date + 1.day,
-               organization: organization)
+          product_drive: drive,
+          money_raised: 1000,
+          issued_at: drive.start_date + 1.day,
+          organization: organization)
       end
       vdonations.flatten!
       vdonations.each do |donation|
@@ -114,33 +114,33 @@ RSpec.describe Reports::DiaperReportService, type: :service, persisted_data: tru
       # Vendors
       vendors = [
         create(:vendor, business_name: "Vendor 1", organization: organization),
-        create(:vendor, business_name: "Vendor 2", organization: organization),
+        create(:vendor, business_name: "Vendor 2", organization: organization)
       ]
 
       # Purchases
       vendors.each do |vendor|
         purchases = [
           create(:purchase,
-                 issued_at: within_time,
-                 vendor: vendor,
-                 organization: organization,
-                 purchased_from: 'Google',
-                 amount_spent_in_cents: 1000,
-                 amount_spent_on_diapers_cents: 1000),
+            issued_at: within_time,
+            vendor: vendor,
+            organization: organization,
+            purchased_from: "Google",
+            amount_spent_in_cents: 1000,
+            amount_spent_on_diapers_cents: 1000),
           create(:purchase,
-                 issued_at: within_time,
-                 vendor: vendor,
-                 organization: organization,
-                 purchased_from: 'Walmart',
-                 amount_spent_in_cents: 2000,
-                 amount_spent_on_diapers_cents: 2000),
+            issued_at: within_time,
+            vendor: vendor,
+            organization: organization,
+            purchased_from: "Walmart",
+            amount_spent_in_cents: 2000,
+            amount_spent_on_diapers_cents: 2000)
         ]
         purchases += create_list(:purchase, 2,
-                                 issued_at: outside_time,
-                                 amount_spent_in_cents: 20_000,
-                                 amount_spent_on_diapers_cents: 20_000,
-                                 vendor: vendor,
-                                 organization: organization)
+          issued_at: outside_time,
+          amount_spent_in_cents: 20_000,
+          amount_spent_on_diapers_cents: 20_000,
+          vendor: vendor,
+          organization: organization)
         purchases.each do |purchase|
           create_list(:line_item, 3, :purchase, quantity: 20, item: disposable_item, itemizable: purchase)
           create_list(:line_item, 3, :purchase, quantity: 10, item: non_disposable_item, itemizable: purchase)
@@ -153,28 +153,28 @@ RSpec.describe Reports::DiaperReportService, type: :service, persisted_data: tru
       expect(service.distributed_disposable_diapers_from_kits).to eq(100)
     end
 
-    it 'should return the proper results on #report' do
+    it "should return the proper results on #report" do
       expect(subject.report).to eq({
-        entries: { "Disposable diapers distributed" => "320",
-                   "Cloth diapers distributed" => "300",
-                   "Average monthly disposable diapers distributed" => "27",
-                   "Total product drives" => 2,
-                   "Disposable diapers collected from drives" => "600",
-                   "Cloth diapers collected from drives" => "900",
-                   "Money raised from product drives" => "$60.00",
-                   "Total product drives (virtual)" => 2,
-                   "Money raised from product drives (virtual)" => "$20.00",
-                   "Disposable diapers collected from drives (virtual)" => "120",
-                   "Cloth diapers collected from drives (virtual)" => "60",
-                   "Disposable diapers donated" => "840",
-                   "% disposable diapers donated" => "78%",
-                   "% cloth diapers donated" => "89%",
-                   "Disposable diapers purchased" => "240",
-                   "% disposable diapers purchased" => "22%",
-                   "% cloth diapers purchased" => "11%",
-                   "Money spent purchasing diapers" => "$60.00",
-                   "Purchased from" => "Google, Walmart",
-                   "Vendors diapers purchased through" => "Vendor 1, Vendor 2"},
+        entries: {"Disposable diapers distributed" => "320",
+                  "Cloth diapers distributed" => "300",
+                  "Average monthly disposable diapers distributed" => "27",
+                  "Total product drives" => 2,
+                  "Disposable diapers collected from drives" => "600",
+                  "Cloth diapers collected from drives" => "900",
+                  "Money raised from product drives" => "$60.00",
+                  "Total product drives (virtual)" => 2,
+                  "Money raised from product drives (virtual)" => "$20.00",
+                  "Disposable diapers collected from drives (virtual)" => "120",
+                  "Cloth diapers collected from drives (virtual)" => "60",
+                  "Disposable diapers donated" => "840",
+                  "% disposable diapers donated" => "78%",
+                  "% cloth diapers donated" => "89%",
+                  "Disposable diapers purchased" => "240",
+                  "% disposable diapers purchased" => "22%",
+                  "% cloth diapers purchased" => "11%",
+                  "Money spent purchasing diapers" => "$60.00",
+                  "Purchased from" => "Google, Walmart",
+                  "Vendors diapers purchased through" => "Vendor 1, Vendor 2"},
         name: "Diapers"
       })
     end
