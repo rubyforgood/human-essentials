fbab592abe2ca370879214a9fc9681332f8b8e5d
Leni Kadali <52788034+lenikadali@users.noreply.github.com>
2024-10-15 20:34:24 +0300

    Fix test by using extended format and `subject`
    Fixed the failing test by using the extended format
    of the test expectation and used `subject` to make
    an assertion regarding the response body.

--- spec/requests/purchases_requests_spec.rb
+++ spec/requests/purchases_requests_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "Purchases", type: :request do
   let(:organization) { create(:organization) }
-  let(:storage_location) { create(:storage_location, organization: organization) }
+  let(:storage_location) { create(:storage_location, name: "Pawane Location", organization: organization) }
   let(:user) { create(:user, organization: organization) }
   let(:organization_admin) { create(:organization_admin, organization: organization) }
 
@@ -55,7 +55,9 @@ RSpec.describe "Purchases", type: :request do
       end
 
       it { is_expected.to be_successful }
-      it { is_expected.to include(storage_location.name) }
+      it "should include the storage location name" do
+        expect(subject.body).to include("Pawane Location")
+      end
     end
 
     describe "POST#create" do
