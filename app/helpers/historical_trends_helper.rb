7a274a50b5abca9ee3c68c56c570916b20723ab3
Benjamin-Couey <benjamincouey36@gmail.com>
2024-11-18 09:59:50 -0700

    Added comment clarifying logic of loop

--- app/helpers/historical_trends_helper.rb
+++ app/helpers/historical_trends_helper.rb
@@ -17,12 +17,15 @@ module HistoricalTrendsHelper
   def last_12_months
     current_month = Time.zone.now.month
     current_year = Time.zone.now.year
+    last_year = current_year - 1
     return_array = MONTHS.rotate(current_month)
     return_array.each_with_index do |month, index|
+      # Last current_month entries are in the current year, earlier entries are
+      # in the previous year.
       return_array[index] = if index >= (MONTHS.length - current_month)
         "#{month} #{current_year}"
       else
-        "#{month} #{current_year - 1}"
+        "#{month} #{last_year}"
       end
     end
     return_array
