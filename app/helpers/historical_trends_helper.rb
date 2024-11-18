f947e87ef1bbc750ed9626b496c9e7a217f2e918
Benjamin-Couey <benjamincouey36@gmail.com>
2024-11-18 09:07:26 -0700

    Updated last_12_months() to add appropriate year labels to month categories

--- app/helpers/historical_trends_helper.rb
+++ app/helpers/historical_trends_helper.rb
@@ -16,6 +16,15 @@ module HistoricalTrendsHelper
 
   def last_12_months
     current_month = Time.zone.now.month
-    MONTHS.rotate(current_month)
+    current_year = Time.zone.now.year
+    return_array = MONTHS.rotate(current_month)
+    return_array.each_with_index do |month, index|
+      return_array[index] = if index >= (MONTHS.length - current_month)
+        "#{month} #{current_year}"
+      else
+        "#{month} #{current_year - 1}"
+      end
+    end
+    return_array
   end
 end
