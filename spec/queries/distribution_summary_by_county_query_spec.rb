b59c6e13797cccdcec8b6a9cfb9586d393441313
Cory Streiff <c.streiff@pm.me>
2024-12-11 12:02:38 +0100

    Fix flaky query spec

--- spec/queries/distribution_summary_by_county_query_spec.rb
+++ spec/queries/distribution_summary_by_county_query_spec.rb
@@ -1,26 +1,6 @@
 RSpec.describe DistributionSummaryByCountyQuery do
   let(:organization) { create(:organization, name: "Some Unique Name") }
   let(:item_1) { create(:item, value_in_cents: 1050, organization: organization) }
-  let(:partner_1) do
-    create(:partner, organization:, without_profile: true) do |p|
-      p.profile = create(:partner_profile, partner: p, organization:) do |pp|
-        pp.served_areas = create_list(:partners_served_area, 4, partner_profile: pp, client_share: 25) do |sa|
-          sa.county = create(:county)
-        end
-      end
-    end
-  end
-  let(:partner_2) do
-    create(:partner, organization:, without_profile: true) do |p|
-      p.profile = create(:partner_profile, partner: p, organization:) do |pp|
-        pp.served_areas = create_list(:partners_served_area, 5, partner_profile: pp, client_share: 20) do |sa, i|
-          # create one overlapping service area
-          sa.county = i.zero? ? partner_1.profile.served_areas[0].county : create(:county)
-        end
-      end
-    end
-  end
-
   let(:now) { Time.current.to_datetime }
 
   let(:params) { {organization_id: organization.id, start_date: nil, end_date: nil} }
@@ -34,46 +14,71 @@ RSpec.describe DistributionSummaryByCountyQuery do
       expect(breakdown[0].value).to be_within(0.01).of(105000.0)
     end
 
-    it "divides the item numbers and values according to the partner profile" do
-      create(:distribution, :with_items, item: item_1, organization: organization, partner: partner_1)
-      breakdown = DistributionSummaryByCountyQuery.call(**params)
-      expect(breakdown.size).to eq(5)
-      expect(breakdown[4].quantity).to eq(0)
-      expect(breakdown[4].value).to be_within(0.01).of(0)
-      3.times do |i|
-        expect(breakdown[i].quantity).to eq(25)
-        expect(breakdown[i].value).to be_within(0.01).of(26250.0)
-      end
-    end
-
-    it "handles multiple partners with overlapping service areas properly" do
-      create(:distribution, :with_items, item: item_1, organization: organization, partner: partner_1, issued_at: now)
-      create(:distribution, :with_items, item: item_1, organization: organization, partner: partner_2, issued_at: now)
-      breakdown = DistributionSummaryByCountyQuery.call(**params)
-      num_with_45 = 0
-      num_with_20 = 0
-      num_with_0 = 0
-      # The result will have at least 1 45 and at least 1 20, and 1 0.  Anything else will be either 45 or 25 or 20
-      breakdown.each do |sa|
-        if sa.quantity == 45
-          expect(sa.value).to be_within(0.01).of(47250.0)
-          num_with_45 += 1
-        end
-
-        if sa.quantity == 25
-          expect(sa.value).to be_within(0.01).of(26250.0)
-        end
-        if sa.quantity == 20
-          expect(sa.value).to be_within(0.01).of(21000.0)
-          num_with_20 += 1
-        end
-        if sa.quantity == 0
-          expect(sa.value).to be_within(0.01).of(0)
+    context "with matching partner served areas" do
+      let!(:partner_1) do
+        create(:partner, organization:, without_profile: true) do |p|
+          p.profile = create(:partner_profile, partner: p, organization:) do |pp|
+            pp.served_areas = create_list(:partners_served_area, 4, partner_profile: pp, client_share: 25) do |sa|
+              sa.county = create(:county)
+            end
+          end
         end
       end
-      expect(num_with_45).to be > 0
-      expect(num_with_20).to be > 0
-      expect(num_with_0).to eq 0
+
+      it "divides the item numbers and values according to the partner profile" do
+        create(:distribution, :with_items, item: item_1, organization: organization, partner: partner_1)
+        breakdown = DistributionSummaryByCountyQuery.call(**params)
+        expect(breakdown.size).to eq(5)
+        expect(breakdown[4].quantity).to eq(0)
+        expect(breakdown[4].value).to be_within(0.01).of(0)
+        3.times do |i|
+          expect(breakdown[i].quantity).to eq(25)
+          expect(breakdown[i].value).to be_within(0.01).of(26250.0)
+        end
+      end
+
+      context "with overlapping served areas" do
+        let!(:partner_2) do
+          create(:partner, organization:, without_profile: true) do |p|
+            p.profile = create(:partner_profile, partner: p, organization:) do |pp|
+              pp.served_areas = create_list(:partners_served_area, 5, partner_profile: pp, client_share: 20) do |sa, i|
+                # create one overlapping service area
+                sa.county = i.zero? ? partner_1.profile.served_areas[0].county : create(:county)
+              end
+            end
+          end
+        end
+
+        it "handles multiple partners with overlapping service areas properly" do
+          create(:distribution, :with_items, item: item_1, organization: organization, partner: partner_1, issued_at: now)
+          create(:distribution, :with_items, item: item_1, organization: organization, partner: partner_2, issued_at: now)
+          breakdown = DistributionSummaryByCountyQuery.call(**params)
+          num_with_45 = 0
+          num_with_20 = 0
+          num_with_0 = 0
+          # The result will have at least 1 45 and at least 1 20, and 1 0.  Anything else will be either 45 or 25 or 20
+          breakdown.each do |sa|
+            if sa.quantity == 45
+              expect(sa.value).to be_within(0.01).of(47250.0)
+              num_with_45 += 1
+            end
+
+            if sa.quantity == 25
+              expect(sa.value).to be_within(0.01).of(26250.0)
+            end
+            if sa.quantity == 20
+              expect(sa.value).to be_within(0.01).of(21000.0)
+              num_with_20 += 1
+            end
+            if sa.quantity == 0
+              expect(sa.value).to be_within(0.01).of(0)
+            end
+          end
+          expect(num_with_45).to be > 0
+          expect(num_with_20).to be > 0
+          expect(num_with_0).to eq 0
+        end
+      end
     end
   end
 end
