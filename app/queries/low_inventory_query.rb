9a38350ba8359d6750de1daef2a25bcc21a48fa3
Cory Streiff <c.streiff@pm.me>
2024-12-11 13:11:58 +0100

    Refactor hashes to Data objects

--- app/queries/low_inventory_query.rb
+++ app/queries/low_inventory_query.rb
@@ -1,4 +1,6 @@
 class LowInventoryQuery
+  LowInventoryItem = Data.define(:id, :name, :on_hand_minimum_quantity, :on_hand_recommended_quantity, :total_quantity)
+
   def self.call(organization)
     inventory = View::Inventory.new(organization.id)
     items = inventory.all_items.uniq(&:item_id)
@@ -7,16 +9,16 @@ class LowInventoryQuery
     items.each do |item|
       quantity = inventory.quantity_for(item_id: item.id)
       if quantity < item.on_hand_minimum_quantity.to_i || quantity < item.on_hand_recommended_quantity.to_i
-        low_inventory_items.push({
+        low_inventory_items.push(LowInventoryItem.new(
           id: item.id,
           name: item.name,
           on_hand_minimum_quantity: item.on_hand_minimum_quantity,
           on_hand_recommended_quantity: item.on_hand_recommended_quantity,
           total_quantity: quantity
-        })
+        ))
       end
     end
 
-    low_inventory_items.sort_by { |item| item[:name] }
+    low_inventory_items.sort_by { |item| item.name }
   end
 end
