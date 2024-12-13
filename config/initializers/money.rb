17ad08b1137da223dd326ee3f6e9f58a0b9b7d71
Cory Streiff <c.streiff@pm.me>
2024-12-13 15:02:12 +0100

    Fix unitialized money_rails constant error

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,3 +1,12 @@
+# This MoneyRails::ActionViewExtension module should get loaded
+# automatically when ActionView gets loaded, but very rarely when
+# setting up the app locally ItemHelper is loaded beforehand which
+# try to include this module and raises an uninitialized constant error.
+#
+# See: https://github.com/RubyMoney/money-rails/issues/614
+money_gem_dir = Gem::Specification.find_by_name("money-rails").gem_dir
+require "#{money_gem_dir}/lib/money-rails/helpers/action_view_extension"
+
 MoneyRails.configure do |config|
   # set the default currency
   config.default_currency = :usd
