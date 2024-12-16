624261e34ff5435e34f67aefeca4c993b5b407df
Cory Streiff <c.streiff@pm.me>
2024-12-16 14:21:28 +0100

    Fix require statement file_path and edit comment

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,11 +1,11 @@
 # This MoneyRails::ActionViewExtension module should get loaded
-# automatically when ActionView gets loaded, but very rarely when
-# setting up the app locally ItemHelper is loaded beforehand which
-# try to include this module and raises an uninitialized constant error.
+# automatically when ActionView is loaded, but *very* rarely when
+# setting up the app locally ItemHelper is loaded beforehand,
+# which depends on MoneyRails::ActionViewExtension, so without
+# this line an uninitialized constant error is raised.
 #
 # See: https://github.com/RubyMoney/money-rails/issues/614
-money_gem_dir = Gem::Specification.find_by_name("money-rails").gem_dir
-require "#{money_gem_dir}/lib/money-rails/helpers/action_view_extension"
+require "money-rails/helpers/action_view_extension"
 
 MoneyRails.configure do |config|
   # set the default currency
