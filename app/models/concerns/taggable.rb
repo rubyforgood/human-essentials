d659a358e3d386644c05c4ad2922947d674a8466
Cory Streiff <c.streiff@pm.me>
2024-12-22 13:08:35 +0100

    Clean up by_tags scope

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -5,9 +5,6 @@ module Taggable
     has_many :taggings, as: :taggable, dependent: :destroy
     has_many :tags, through: :taggings, source: :tag
 
-    scope :by_tags, ->(tags) {
-      joins(taggings: :tag)
-        .where(tag: {name: tags})
-    }
+    scope :by_tags, ->(tags) { joins(:tags).where(tags: {name: tags}) }
   end
 end
