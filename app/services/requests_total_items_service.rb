d605dcaafbbf15579470489b05ff8a90d746ea08
Cory Streiff <90390502+coalest@users.noreply.github.com>
2025-01-10 02:04:32 +0100

    Perf: Reduce number of queries and object allocations in request index page (#4920)

--- app/services/requests_total_items_service.rb
+++ app/services/requests_total_items_service.rb
@@ -1,11 +1,9 @@
 class RequestsTotalItemsService
   def initialize(requests:)
-    @requests = requests.includes(item_requests: {item: :request_units})
+    @requests = requests
   end
 
   def calculate
-    return unless requests
-
     totals = Hash.new(0)
     item_requests.each do |item_request|
       totals[item_request.name_with_unit] += item_request.quantity.to_i
@@ -16,9 +14,7 @@ class RequestsTotalItemsService
 
   private
 
-  attr_accessor :requests
-
   def item_requests
-    @item_requests ||= requests.flat_map(&:item_requests)
+    @item_requests ||= Partners::ItemRequest.where(request: @requests)
   end
 end
