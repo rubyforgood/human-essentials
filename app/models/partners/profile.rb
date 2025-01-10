2566e169f53d8078b9b140c487aa518a7c92e57c
Benjamin-Couey <benjamincouey36@gmail.com>
2025-01-10 06:59:45 -0700

    Update agency_type to be an enum

--- app/models/partners/profile.rb
+++ app/models/partners/profile.rb
@@ -95,6 +95,17 @@ module Partners
     accepts_nested_attributes_for :served_areas, allow_destroy: true
 
     has_many_attached :documents
+    enum :agency_type, other: "OTHER", career: "CAREER", abuse: "ABUSE", bnb: "BNB",
+      church: "CHURCH", college: "COLLEGE", cdc: "CDC", health: "HEALTH",
+      outreach: "OUTREACH", legal: "LEGAL", crisis: "CRISIS", disab: "DISAB",
+      district: "DISTRICT", domv: "DOMV", ece: "ECE", child: "CHILD",
+      edu: "EDU", family: "FAMILY", food: "FOOD", foster: "FOSTER", govt: "GOVT",
+      headstart: "HEADSTART", homevisit: "HOMEVISIT", homeless: "HOMELESS",
+      hosp: "HOSP", infpan: "INFPAN", lib: "LIB", mhealth: "MHEALTH", military: "MILITARY",
+      police: "POLICE", preg: "PREG", presch: "PRESCH", ref: "REF", es: "ES",
+      hs: "HS", ms: "MS", senior: "SENIOR", tribal: "TRIBAL", treat: "TREAT",
+      twoycollege: "2YCOLLEGE", wic: "WIC"
+
     validate :check_social_media, on: :edit
 
     validate :client_share_is_0_or_100
