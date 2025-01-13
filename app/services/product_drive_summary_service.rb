b927a12707ee6be9b5309b6388d093603d2146f0
Cory Streiff <c.streiff@pm.me>
2025-01-13 16:16:14 +0100

    Refactor hash into Data object

--- app/services/product_drive_summary_service.rb
+++ app/services/product_drive_summary_service.rb
@@ -1,36 +1,30 @@
 class ProductDriveSummaryService
+  ProductDriveSummary = Data.define(:quantity, :unique_item_count, :value)
+  NULL_PRODUCT_DRIVE_TOTALS = ProductDriveSummary.new(quantity: 0, unique_item_count: 0, value: 0)
+
   class << self
+    # Calculates the total quantity, unique item count and value of product
+    # drives from donations (filtered by items donated in a given date range
+    # and within a given item category if provided).
     def call(product_drives:, within_date_range:, item_category_id: nil)
-      calculate_summary(product_drives, within_date_range, item_category_id)
+      calculate_totals(product_drives, within_date_range, item_category_id)
       self
     end
 
-    def fetch_quantity(product_drive_id)
-      @summary.dig(product_drive_id, :quantity)
-    end
-
-    def fetch_unique_item_count(product_drive_id)
-      @summary.dig(product_drive_id, :unique_item_count)
-    end
-
-    def fetch_value(product_drive_id)
-      @summary.dig(product_drive_id, :value)
+    def [](product_drive_id)
+      @product_drive_totals[product_drive_id] || NULL_PRODUCT_DRIVE_TOTALS
     end
 
     private
 
-    # Returns hash of total quantity, unique item count and value per product drive
-    # Example return: { 1 => { quantity: 15, unique_item_count: 2, value: 100 }, ...}
-    #
-    # @return [Hash<Hash<Symbol, Integer>>]
-    def calculate_summary(product_drives, within_date_range, item_category_id)
-      @summary ||= begin
+    # @return [Hash<Integer, ProductDriveSummary>]
+    def calculate_totals(product_drives, within_date_range, item_category_id)
+      @product_drive_totals ||= begin
         query = ProductDrive
           .left_joins(donations: {line_items: [:item]})
           .where(id: product_drives.ids)
           .where(donations: {issued_at: within_date_range[0]..within_date_range[1]})
           .group("product_drives.id")
-          .distinct
 
         query = query.where(items: {item_category_id: item_category_id}) if item_category_id.present?
 
@@ -40,7 +34,7 @@ class ProductDriveSummaryService
           COUNT(DISTINCT line_items.item_id) AS unique_item_count,
           COALESCE(SUM(COALESCE(items.value_in_cents, 0) * line_items.quantity), 0) AS value"
         )).to_h do |(id, quantity, unique_item_count, value)|
-          [id, {quantity:, unique_item_count:, value:}]
+          [id, ProductDriveSummary.new(quantity:, unique_item_count:, value:)]
         end
       end
     end
