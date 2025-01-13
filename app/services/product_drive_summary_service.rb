c69ba5573c9f8ee89cdb1fd10e66cd41b405d3a1
Cory Streiff <c.streiff@pm.me>
2025-01-13 18:29:50 +0100

    Refactor service class

--- app/services/product_drive_summary_service.rb
+++ app/services/product_drive_summary_service.rb
@@ -1,18 +1,13 @@
+# Calculates the quantity, unique item count, and value for product
+# drives from donations (filtered by items donated in a given date range
+# and within a given item category if provided).
 class ProductDriveSummaryService
   ProductDriveSummary = Data.define(:quantity, :unique_item_count, :value)
   NULL_PRODUCT_DRIVE_TOTALS = ProductDriveSummary.new(quantity: 0, unique_item_count: 0, value: 0)
 
   class << self
-    # Calculates the total quantity, unique item count and value of product
-    # drives from donations (filtered by items donated in a given date range
-    # and within a given item category if provided).
     def call(product_drives:, within_date_range:, item_category_id: nil)
       calculate_totals(product_drives, within_date_range, item_category_id)
-      self
-    end
-
-    def [](product_drive_id)
-      @product_drive_totals[product_drive_id] || NULL_PRODUCT_DRIVE_TOTALS
     end
 
     private
@@ -25,10 +20,11 @@ class ProductDriveSummaryService
           .where(id: product_drives.ids)
           .where(donations: {issued_at: within_date_range[0]..within_date_range[1]})
           .group("product_drives.id")
+          .distinct
 
         query = query.where(items: {item_category_id: item_category_id}) if item_category_id.present?
 
-        query.pluck(Arel.sql(
+        totals = query.pluck(Arel.sql(
           "product_drives.id AS id,
           COALESCE(SUM(line_items.quantity), 0) AS quantity,
           COUNT(DISTINCT line_items.item_id) AS unique_item_count,
@@ -36,6 +32,10 @@ class ProductDriveSummaryService
         )).to_h do |(id, quantity, unique_item_count, value)|
           [id, ProductDriveSummary.new(quantity:, unique_item_count:, value:)]
         end
+
+        # If a product drive has no matching donations/items, set totals to
+        # default values (zeroes).
+        product_drives.ids.to_h { |id| [id, NULL_PRODUCT_DRIVE_TOTALS] }.merge(totals)
       end
     end
   end
