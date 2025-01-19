c1ba4c06a12ff9ac3b83c27f09ac56396770e097
Cory Streiff <c.streiff@pm.me>
2025-01-19 15:16:11 +0100

    Fix linting

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -70,7 +70,7 @@ class Organization < ApplicationRecord
     has_many :storage_locations
     has_many :tags
     has_many :product_drive_tags, -> { joins(:taggings).where(taggings: {taggable_type: "ProductDrive"}).distinct },
-      class_name: "Tag"
+      class_name: "Tag", inverse_of: false
     has_many :inventory_items, through: :storage_locations
     has_many :kits
     has_many :transfers
