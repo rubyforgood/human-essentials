bd415795428d282c9fda6e952edfbf367ebec323
CL Fisher <clfisher@rogers.com>
2025-01-29 11:33:27 -0500

    adding bypass if not production

--- db/migrate/20250129015253_item_request_fix_for_mhm.rb
+++ db/migrate/20250129015253_item_request_fix_for_mhm.rb
@@ -1,5 +1,6 @@
 class ItemRequestFixForMhm < ActiveRecord::Migration[7.2]
   def change
+    return unless Rails.env.production?
     item_ids = [302, 14372]
     item_ids.each do |item_id|
       item = Item.find(item_id)
