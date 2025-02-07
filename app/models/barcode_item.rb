53def81b8fc3ad312fe564cfe580c2f7f3768120
Cory Streiff <90390502+coalest@users.noreply.github.com>
2025-02-07 21:11:23 +0100

    Remove old/unused #for_csv_export methods (#4995)

--- app/models/barcode_item.rb
+++ app/models/barcode_item.rb
@@ -42,11 +42,6 @@ class BarcodeItem < ApplicationRecord
 
   scope :by_value, ->(value) { where(value: value) }
 
-  scope :for_csv_export, ->(organization, *) {
-    where(organization: organization)
-      .includes(:barcodeable)
-  }
-
   scope :global, -> { where(barcodeable_type: "BaseItem") }
 
   # aliases of barcodeable
