04b88b228fc4e31997f1f86547ca2ca4ba3d6192
tbenny <tbenny001@gmail.com>
2025-02-07 17:47:53 -0800

    [4976] Showing the contact name in the product drive participant dropdown when no business name is available

--- app/models/product_drive_participant.rb
+++ app/models/product_drive_participant.rb
@@ -29,7 +29,7 @@ class ProductDriveParticipant < ApplicationRecord
   validates :business_name, presence: { message: "Must provide a name or a business name" }, if: proc { |pdp| pdp.contact_name.blank? }
   validates :comment, length: { maximum: 500 }
 
-  scope :alphabetized, -> { order(:contact_name) }
+  scope :alphabetized, -> { order(Arel.sql("COALESCE(NULLIF(business_name, ''), contact_name)")) }
   scope :with_volumes, -> {
     left_joins(donations: :line_items)
       .select("product_drive_participants.*, SUM(COALESCE(line_items.quantity, 0)) AS volume")
@@ -48,5 +48,9 @@ class ProductDriveParticipant < ApplicationRecord
     return if contact_name.blank?
 
     "#{contact_name} (participant)"
+  end
+
+  def display_name
+    "#{business_name.presence || contact_name.presence}"
   end
 end
