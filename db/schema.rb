8a77b34530f2215e1654989b48726e499c16fbf1
Jorge Junior <jorgedjr21@gmail.com>
2025-02-12 19:00:11 -0300

    revert schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -728,6 +728,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_01_151720) do
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
   end
 
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
+  end
+
   create_table "storage_locations", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "address"
