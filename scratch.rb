41dd6f30606815dbe086b973e68a9403e80cd52c
Cory Streiff <c.streiff@pm.me>
2025-03-05 14:13:08 +0100

    Remove scratch file

--- scratch.rb
+++
@@ -1,7 +0,0 @@
-
-      'show_foreign_keys'           => 'false',
-      'show_indexes'                => 'false',
-      'hide_limit_column_types'     => 'integer,bigint,boolean',
-      'hide_default_column_types'   => 'json,jsonb,hstore',
-      'format_bare'                 => 'true',
-    )
