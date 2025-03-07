7f136036f442be8574b6aadae5230a2546d3aa49
nathan <nathan.gordon.thomas@gmail.com>
2025-03-07 09:16:48 -0700

    Add migration to set default distribution quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_02_154355) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_07_161544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
