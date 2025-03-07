dcf6bc17d019599a59957223897dbf474c884fd5
Daniel Orner <daniel.orner@flipp.com>
2025-03-07 16:51:58 -0500

    lol or this

--- config/initializers/git_sha.rb
+++ config/initializers/git_sha.rb
@@ -1,9 +1,1 @@
-require 'open3'
-
-sha, _, status = Open3.capture3('git rev-parse HEAD')
-if status.exitstatus == 128
-  `git config --global --add safe.directory #{Rails.root}`
-  sha = `git rev-parse HEAD`
-end
-ENV['GIT_SHA'] = sha
-
+ENV['GIT_SHA'] = `cat #{Rails.root}/REVISION`
