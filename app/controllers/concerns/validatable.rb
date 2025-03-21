49af734698a7b27fa9ff2337c0d226dc7f3231b4
Daniel Orner <daniel.orner@flipp.com>
2025-03-21 16:48:50 -0400

    Add tests

--- app/controllers/concerns/validatable.rb
+++ app/controllers/concerns/validatable.rb
@@ -3,8 +3,8 @@ module Validatable
 
   included do
     rescue_from ActionController::InvalidAuthenticityToken do
-      if action_name == 'validate'
-        render json: { valid: false }
+      if action_name == "validate"
+        render json: {valid: false}
       else
         session_expired
       end
