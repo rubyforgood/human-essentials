61ba5cc7d3a476e91d0902ccac798ed84cc8d8e2
Daniel Orner <daniel.orner@flipp.com>
2025-03-21 16:19:43 -0400

    #5100: Fix unknown format error on expired session

---
+++ app/controllers/concerns/validatable.rb
@@ -0,0 +1,13 @@
+module Validatable
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActionController::InvalidAuthenticityToken do
+      if action_name == 'validate'
+        render json: { valid: false }
+      else
+        session_expired
+      end
+    end
+  end
+end
