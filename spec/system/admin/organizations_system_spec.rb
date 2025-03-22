ea62babc2a32be063e8e0511957a3edb1c2be7d9
JP <85654561+jp524@users.noreply.github.com>
2025-03-22 13:03:19 -0500

    Organization#edit page: ensure headings are consistent with #show page

--- spec/system/admin/organizations_system_spec.rb
+++ spec/system/admin/organizations_system_spec.rb
@@ -149,7 +149,7 @@ RSpec.describe "Admin Organization Management", type: :system, js: true, seed_it
 
       expect(page).to have_content("Organization Info")
       expect(page).to have_content("Address")
-      expect(page).to have_content("Default email content")
+      expect(page).to have_content("Distribution email content")
       expect(page).to have_content("Users")
       expect(page).to have_content("Receive email when Partner makes a Request?")
     end
