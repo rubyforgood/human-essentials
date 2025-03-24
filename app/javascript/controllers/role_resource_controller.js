10e09487b638c67e8d20438d867937b3d00e7e3e
Benjamin-Couey <benjamincouey36@gmail.com>
2025-03-24 11:08:07 -0600

    Updated new controller to accept array of values to check source's val against and passed new Role::ROLES_WITHOUT_RESOURCE

--- app/javascript/controllers/role_resource_controller.js
+++ app/javascript/controllers/role_resource_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 		static targets = ["source", "destination"]
-
+		static values = {
+			valuestohide: Array
+		}
 		sourceChanged() {
 				const val = $(this.sourceTarget).val()
 				this.destinationTargets.forEach(
-					destination_target => { $(destination_target).toggleClass("d-none", val == "super_admin"); }
+					destination_target => { $(destination_target).toggleClass("d-none", this.valuestohideValue.includes(val)); }
 				)
 		}
 }
