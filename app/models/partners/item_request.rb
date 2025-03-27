1c68b318319d81e79f1a7ee5ddb8944de8173e5c
Brock Wilcox <awwaiid@thelackthereof.org>
2025-03-27 08:36:15 -0400

    Update app/models/partners/item_request.rb
    Change style of conditional to be more cleaner
    
    Co-authored-by: Daniel Orner <daniel.orner@flipp.com>

--- app/models/partners/item_request.rb
+++ app/models/partners/item_request.rb
@@ -38,7 +38,7 @@ module Partners
 
     def name_with_unit(quantity_override = nil)
       if Flipper.enabled?(:enable_packs) && request_unit.present?
-        "#{item ? item.name : name} - #{request_unit.pluralize(quantity_override || quantity.to_i)}"
+        "#{item&.name || name} - #{request_unit.pluralize(quantity_override || quantity.to_i)}"
       else
         item ? item.name : name
       end
