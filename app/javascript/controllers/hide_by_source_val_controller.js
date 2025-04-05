0904f99a95b270811497efe692a273812f16a51c
Benjamin-Couey <benjamincouey36@gmail.com>
2025-04-05 15:24:15 -0600

    Fixed file using tabs instead of spaces

--- app/javascript/controllers/hide_by_source_val_controller.js
+++ app/javascript/controllers/hide_by_source_val_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-	static targets = ["source", "destination"]
-	static values = {
-		valuesToHide: Array
-	}
-	sourceChanged() {
-		const val = $(this.sourceTarget).val()
-		this.destinationTargets.forEach(
-			destination_target => { $(destination_target).toggleClass("d-none", this.valuesToHideValue.includes(val)); }
-		)
-	}
+  static targets = ["source", "destination"]
+  static values = {
+    valuesToHide: Array
+  }
+  sourceChanged() {
+    const val = $(this.sourceTarget).val()
+    this.destinationTargets.forEach(
+      destination_target => { $(destination_target).toggleClass("d-none", this.valuesToHideValue.includes(val)); }
+    )
+  }
 }
