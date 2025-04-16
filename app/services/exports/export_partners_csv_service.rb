bc5735d38d52fe398c952c6f8e04de4851e7b79a
Benjamin-Couey <benjamincouey36@gmail.com>
2025-04-16 13:42:57 -0600

    Split up massive base_table function

--- app/services/exports/export_partners_csv_service.rb
+++ app/services/exports/export_partners_csv_service.rb
@@ -23,10 +23,6 @@ module Exports
 
     attr_reader :partners
 
-    def headers
-      base_table.keys
-    end
-
     def base_table
       table = {
         "Agency Name" => ->(partner) { partner.name },
@@ -51,6 +47,27 @@ module Exports
         "Program Zip Code" => ->(partner) { partner.profile.program_zip_code }
       }
 
+      media_information_col(table)
+      agency_stability_col(table)
+      organizational_capacity_col(table)
+      sources_of_funding_col(table)
+      area_served_col(table)
+      population_served_col(table)
+      executive_director_col(table)
+      pick_up_person_col(table)
+      agency_distribution_information_col(table)
+
+      table["Quantity-based Requests"] = ->(partner) { partner.profile.enable_quantity_based_requests } # Columns from the partner_settings partial
+      table["Child-based Requests"] = ->(partner) { partner.profile.enable_child_based_requests }
+      table["Individual Requests"] = ->(partner) { partner.profile.enable_individual_requests }
+
+      table["Providing Diapers"] = ->(partner) { diaper_statuses[partner.id] }
+      table["Providing Period Supplies"] = ->(partner) { period_supplies_statuses[partner.id] }
+
+      table
+    end
+
+    def media_information_col(table)
       if @partials_to_show.include? "media_information"
         table["Agency Website"] = ->(partner) { partner.profile.website }
         table["Facebook"] = ->(partner) { partner.profile.facebook }
@@ -58,7 +75,9 @@ module Exports
         table["Instagram"] = ->(partner) { partner.profile.instagram }
         table["No Social Media Presence"] = ->(partner) { partner.profile.no_social_media_presence }
       end
+    end
 
+    def agency_stability_col(table)
       if @partials_to_show.include? "agency_stability"
         table["Year Founded"] = ->(partner) { partner.profile.founded }
         table["Form 990 Filed"] = ->(partner) { partner.profile.form_990 }
@@ -71,24 +90,32 @@ module Exports
         table["Receive Essentials From Other Sources"] = ->(partner) { partner.profile.receives_essentials_from_other }
         table["Currently Providing Diapers"] = ->(partner) { partner.profile.currently_provide_diapers }
       end
+    end
 
+    def organizational_capacity_col(table)
       if @partials_to_show.include? "organizational_capacity"
         table["Client Capacity"] = ->(partner) { partner.profile.client_capacity }
         table["Storage Space"] = ->(partner) { partner.profile.storage_space }
         table["Storage Space Description"] = ->(partner) { partner.profile.describe_storage_space }
       end
+    end
 
+    def sources_of_funding_col(table)
       if @partials_to_show.include? "sources_of_funding"
         table["Sources Of Funding"] = ->(partner) { partner.profile.sources_of_funding }
         table["How do you currently obtain diapers?"] = ->(partner) { partner.profile.sources_of_diapers }
         table["Essentials Budget"] = ->(partner) { partner.profile.essentials_budget }
         table["Essentials Funding Source"] = ->(partner) { partner.profile.essentials_funding_source }
       end
+    end
 
+    def area_served_col(table)
       if @partials_to_show.include? "area_served"
         table["Area Served"] = ->(partner) { county_list_by_regions[partner.id] || "" }
       end
+    end
 
+    def population_served_col(table)
       if @partials_to_show.include? "population_served"
         table["Income Requirement"] = ->(partner) { partner.profile.income_requirement_desc }
         table["Verify Income"] = ->(partner) { partner.profile.income_verification }
@@ -106,7 +133,9 @@ module Exports
         table["% Greater than 2 times FPL"] = ->(partner) { partner.profile.greater_2_times_fpl }
         table["% Poverty Unknown"] = ->(partner) { partner.profile.poverty_unknown }
       end
+    end
 
+    def executive_director_col(table)
       if @partials_to_show.include? "executive_director"
         table["Executive Director Name"] = ->(partner) { partner.profile.executive_director_name }
         table["Executive Director Phone"] = ->(partner) { partner.profile.executive_director_phone }
@@ -116,27 +145,26 @@ module Exports
         table["Primary Contact Cell"] = ->(partner) { partner.profile.primary_contact_mobile }
         table["Primary Contact Email"] = ->(partner) { partner.profile.primary_contact_email }
       end
+    end
 
+    def pick_up_person_col(table)
       if @partials_to_show.include? "pick_up_person"
         table["Pick Up Person Name"] = ->(partner) { partner.profile.pick_up_name }
         table["Pick Up Person Phone"] = ->(partner) { partner.profile.pick_up_phone }
         table["Pick Up Person Email"] = ->(partner) { partner.profile.pick_up_email }
       end
+    end
 
+    def agency_distribution_information_col(table)
       if @partials_to_show.include? "agency_distribution_information"
         table["Distribution Times"] = ->(partner) { partner.profile.distribution_times }
         table["New Client Times"] = ->(partner) { partner.profile.new_client_times }
         table["More Docs Required"] = ->(partner) { partner.profile.more_docs_required }
       end
+    end
 
-      table["Quantity-based Requests"] = ->(partner) { partner.profile.enable_quantity_based_requests } # Columns from the partner_settings partial
-      table["Child-based Requests"] = ->(partner) { partner.profile.enable_child_based_requests }
-      table["Individual Requests"] = ->(partner) { partner.profile.enable_individual_requests }
-
-      table["Providing Diapers"] = ->(partner) { diaper_statuses[partner.id] }
-      table["Providing Period Supplies"] = ->(partner) { period_supplies_statuses[partner.id] }
-
-      table
+    def headers
+      base_table.keys
     end
 
     def build_row_data(partner)
