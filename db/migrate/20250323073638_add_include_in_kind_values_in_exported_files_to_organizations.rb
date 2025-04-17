d653f8e15f7f028b1c85594f996a8cd62efe554d
Nozomi R <nozomilinxy@gmail.com>
2025-04-17 12:22:54 +0800

    Fix: simplify the include_in_kind_values_in_exported_files column migration.

--- db/migrate/20250323073638_add_include_in_kind_values_in_exported_files_to_organizations.rb
+++ db/migrate/20250323073638_add_include_in_kind_values_in_exported_files_to_organizations.rb
@@ -1,6 +1,5 @@
 class AddIncludeInKindValuesInExportedFilesToOrganizations < ActiveRecord::Migration[8.0]
   def change
-    add_column :organizations, :include_in_kind_values_in_exported_files, :boolean
-    add_check_constraint :organizations, "include_in_kind_values_in_exported_files IS NOT NULL", name: "include_in_kind_values_in_exported_files_not_null", validate: false
+    add_column :organizations, :include_in_kind_values_in_exported_files, :boolean, default: false, null: false
   end
 end
