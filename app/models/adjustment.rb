b5523916e9ec617a60af6698948b599befdbf604
Cheng-Hsun Yang <showjackyang@gmail.com>
2025-04-29 13:39:37 -0400

    Add service for csv exporting adjustments

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -34,18 +34,13 @@ class Adjustment < ApplicationRecord
     [increasing_adjustment, decreasing_adjustment]
   end
 
-  def self.csv_export_headers
-    ["Created", "Organization", "Storage Location", "Comment", "Changes"]
-  end
+  def self.generate_csv(adjustments)
+    return nil if adjustments.empty?
 
-  def csv_export_attributes
-    [
-      created_at.strftime("%F"),
-      organization.name,
-      storage_location.name,
-      comment,
-      line_items.count
-    ]
+    Exports::ExportAdjustmentsCSVService.new(
+      adjustments: adjustments,
+      organization: adjustments.first.organization
+    ).generate_csv
   end
 
   private
---
+++ app/services/exports/export_adjustments_csv_service.rb
@@ -0,0 +1,53 @@
+module Exports
+  class ExportAdjustmentsCSVService
+    def initialize(adjustments:, organization:)
+      @adjustments = adjustments
+      @organization = organization
+    end
+
+    def generate_csv
+      CSV.generate(headers: true) do |csv|
+        generate_csv_data.each { |row| csv << row }
+      end
+    end
+
+    def generate_csv_data
+      [headers] + @adjustments.map { |adjustment| build_row(adjustment) }
+    end
+
+    private
+
+    def headers
+      [
+        "Created", "Storage Location",
+        "Comment", "User", "Changes"
+      ] + item_names
+    end
+
+    def item_names
+      @organization.items.order(:name).pluck(:name).uniq
+    end
+
+    def build_row(adjustment)
+      row = [
+        adjustment.created_at.strftime("%F"),
+        adjustment.storage_location.name,
+        adjustment.comment,
+        adjustment.user&.name || "Unknown",
+        adjustment.line_items.count { |item| !item.quantity.eql?(0) }
+      ]
+
+      item_quantities = Hash.new(0)
+
+      adjustment.line_items.each do |line_item|
+        item_quantities[line_item.item.name] += line_item.quantity
+      end
+
+      item_names.each do |item_name|
+        row << item_quantities[item_name]
+      end
+
+      row
+    end
+  end
+end
