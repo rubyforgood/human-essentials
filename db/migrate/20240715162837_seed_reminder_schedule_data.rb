819897fc1c6732e2238ad45e1fd8fc11519dd9f0
Benjamin-Couey <benjamincouey36@gmail.com>
2025-05-13 08:48:56 -0600

    Fixed migration not correctly converting reminder_day to reminder_schedule

--- db/migrate/20240715162837_seed_reminder_schedule_data.rb
+++ db/migrate/20240715162837_seed_reminder_schedule_data.rb
@@ -3,14 +3,14 @@ class SeedReminderScheduleData < ActiveRecord::Migration[7.1]
     for o in Organization.all
       if o.reminder_day.present?
         reminder_schedule = o.convert_to_reminder_schedule(o.reminder_day)
-        o.update(reminder_schedule: reminder_schedule)
+        o.update_column(:reminder_schedule, reminder_schedule)
       end
     end
 
     for pg in PartnerGroup.all
       if pg.reminder_day.present?
         reminder_schedule = pg.convert_to_reminder_schedule(pg.reminder_day)
-        pg.update(reminder_schedule: reminder_schedule)
+        pg.update_column(:reminder_schedule, reminder_schedule)
       end
     end
   end
