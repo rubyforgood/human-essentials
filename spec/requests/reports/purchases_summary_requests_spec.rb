1da2ce6d1fb298fb9cdd599aa1130567d333f84d
Benjamin-Couey <benjamincouey36@gmail.com>
2025-06-06 08:23:10 -0700

    Removed non-necessary fields from purchase and updated tests that relied on those fields or factory values

--- spec/requests/reports/purchases_summary_requests_spec.rb
+++ spec/requests/reports/purchases_summary_requests_spec.rb
@@ -21,12 +21,12 @@ RSpec.describe "Purchases", type: :request do
     context "with filters" do
       before do
         # Create a bunch of historical purchases
-        create :purchase, :with_items, item_quantity: 2, issued_at: 0.days.ago, organization: organization
-        create :purchase, :with_items, item_quantity: 3, issued_at: 1.day.ago, organization: organization
-        create :purchase, :with_items, item_quantity: 7, issued_at: 3.days.ago, organization: organization
-        create :purchase, :with_items, item_quantity: 11, issued_at: 10.days.ago, organization: organization
-        create :purchase, :with_items, item_quantity: 13, issued_at: 20.days.ago, organization: organization
-        create :purchase, :with_items, item_quantity: 17, issued_at: 30.days.ago, organization: organization
+        create :purchase, :with_items, item_quantity: 2, issued_at: 0.days.ago, organization: organization, amount_spent_in_cents: 2_00
+        create :purchase, :with_items, item_quantity: 3, issued_at: 1.day.ago, organization: organization, amount_spent_in_cents: 3_00
+        create :purchase, :with_items, item_quantity: 7, issued_at: 3.days.ago, organization: organization, amount_spent_in_cents: 7_00
+        create :purchase, :with_items, item_quantity: 11, issued_at: 10.days.ago, organization: organization, amount_spent_in_cents: 11_00
+        create :purchase, :with_items, item_quantity: 13, issued_at: 20.days.ago, organization: organization, amount_spent_in_cents: 13_00
+        create :purchase, :with_items, item_quantity: 17, issued_at: 30.days.ago, organization: organization, amount_spent_in_cents: 17_00
       end
 
       let(:formatted_date_range) { date_range.map { it.to_fs(:date_picker) }.join(" - ") }
@@ -67,7 +67,7 @@ RSpec.describe "Purchases", type: :request do
       context "a long time" do
         let(:date_range) { [900.days.ago, 1.day.ago] }
         it "shows the correct total and links" do
-          expect(response.body).to include("$50.00")
+          expect(response.body).to include("$51.00")
           expect(response.body).to include("3 items from")
           expect(response.body).to include("7 items from")
           expect(response.body).to include("11 items from")
