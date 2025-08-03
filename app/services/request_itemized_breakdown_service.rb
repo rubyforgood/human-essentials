759cd3070dedc1d040bf4c623556db98c1584caa
seabeeberry <seri@Mac.fritz.box>
2025-08-03 16:03:20 +0200

    Applied requested changes for displaying red items in the overview with the same criteria for distributions. Modelled files more closely after itemized distributions

--- app/services/request_itemized_breakdown_service.rb
+++ app/services/request_itemized_breakdown_service.rb
@@ -17,15 +17,23 @@ class RequestItemizedBreakdownService
   #
   # @return [Array]
   def fetch
+    inventory = View::Inventory.new(@organization.id)
+    current_onhand = current_onhand_quantities(inventory)
+    current_min_onhand = current_onhand_minimums(inventory)
     items_requested = fetch_items_requested
 
     items_requested.map! do |item|
-      on_hand = inventory.quantity_for(item_id: item[:item_id])
-      below_requested = on_hand && item[:quantity] && on_hand < item[:quantity]
+      item_id = item[:item_id]
+
+      on_hand = current_onhand[item_id]
+      minimum = current_min_onhand[item_id]
+
+      below_onhand_minimum = on_hand && minimum && on_hand < minimum
 
       item.merge(
         on_hand: on_hand,
-        below_requested: below_requested
+        onhand_minimum: minimum,
+        below_onhand_minimum: below_onhand_minimum
       )
     end
 
@@ -34,7 +42,7 @@ class RequestItemizedBreakdownService
 
   #
   # Returns a CSV string representation of the itemized breakdown of
-  # what was distributed
+  # what was requested
   #
   # @return [String]
   def fetch_csv
@@ -45,15 +53,27 @@ class RequestItemizedBreakdownService
 
   attr_reader :organization, :request_ids
 
+  def current_onhand_quantities(inventory)
+    inventory.all_items.group_by(&:id).to_h do |id, items|
+      [id, items.sum(&:quantity)]
+    end
+  end
+
+  def current_onhand_minimums(inventory)
+    inventory.all_items.group_by(&:id).to_h do |id, items|
+      [id, items.map(&:on_hand_minimum_quantity).compact.max]
+    end
+  end
+
   def fetch_items_requested
     Request
       .includes(:partner, :organization, :item_requests)
       .where(id: @request_ids)
       .flat_map do |request|
-      request.request_items.map do |json_item|
-        RequestItem.from_json(json_item, request)
+        request.request_items.map do |json_item|
+          RequestItem.from_json(json_item, request)
+        end
       end
-    end
       .group_by { |ri| [ri.item.id, ri.unit] }
       .map do |(item_id, unit), grouped|
         item = grouped.first.item
--- spec/services/request_itemized_breakdown_service_spec.rb
+++ spec/services/request_itemized_breakdown_service_spec.rb
@@ -1,8 +1,12 @@
 RSpec.describe RequestItemizedBreakdownService, type: :service do
   let(:organization) { create(:organization) }
 
-  let(:item_a) { create(:item, organization: organization, name: "A Diapers") }
-  let(:item_b) { create(:item, organization: organization, name: "B Diapers") }
+  let(:item_a) do
+    create(:item, organization: organization, on_hand_minimum_quantity: 4, name: "A Diapers")
+  end
+  let(:item_b) do
+    create(:item, organization: organization, on_hand_minimum_quantity: 8, name: "B Diapers")
+  end
 
   let(:request_1) do
     create(:request, organization: organization, request_items: [
@@ -18,14 +22,18 @@ RSpec.describe RequestItemizedBreakdownService, type: :service do
 
   let(:expected_output) do
     [
-      {name: item_a.name, item_id: item_a.id, unit: nil, quantity: 5, on_hand: 3, below_requested: true},
-      {name: item_b.name, item_id: item_b.id, unit: nil, quantity: 10, on_hand: 20, below_requested: false}
+      {name: item_a.name, item_id: item_a.id, unit: nil, quantity: 5, on_hand: 3, onhand_minimum: 4, below_onhand_minimum: true},
+      {name: item_b.name, item_id: item_b.id, unit: nil, quantity: 10, on_hand: 20, onhand_minimum: 8, below_onhand_minimum: false}
     ]
   end
 
   before do
     allow_any_instance_of(View::Inventory).to receive(:quantity_for).with(item_id: item_a.id).and_return(3)
     allow_any_instance_of(View::Inventory).to receive(:quantity_for).with(item_id: item_b.id).and_return(20)
+    allow_any_instance_of(View::Inventory).to receive(:all_items).and_return([
+      OpenStruct.new(id: item_a.id, quantity: 3, on_hand_minimum_quantity: 4),
+      OpenStruct.new(id: item_b.id, quantity: 20, on_hand_minimum_quantity: 8)
+    ])
   end
 
   describe "#fetch" do
@@ -33,7 +41,7 @@ RSpec.describe RequestItemizedBreakdownService, type: :service do
     let(:service) { described_class.new(organization: organization, request_ids: [request_1.id, request_2.id]) }
 
     it "should include the break down of requested items" do
-      expect(subject).to match_array(expected_output)
+      expect(subject).to eq(expected_output)
     end
   end
 
