963c1588907326cb6a347b4df6a59641887dd6b5
Viikaax <vikash@telgea.com>
2025-08-23 14:42:38 +0530

    Add backward compatibility for valuesToHide parameter

--- app/javascript/controllers/hide_by_source_val_controller.js
+++ app/javascript/controllers/hide_by_source_val_controller.js
@@ -2,7 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["source", "destination"]
-  static values = { valuesToShow: Array }
+  static values = { 
+    valuesToHide: Array,
+    valuesToShow: Array 
+  }
 
   connect() {
     this.toggleVisibility()
@@ -14,8 +17,13 @@ export default class extends Controller {
 
   toggleVisibility() {
     const sourceValue = this.sourceTarget.value
-    const shouldShow = this.valuesToShowValue.includes(sourceValue)
-    
+    let shouldShow = false
+
+    if (this.hasValuesToShowValue && this.valuesToShowValue.length > 0) {
+      shouldShow = this.valuesToShowValue.includes(sourceValue)
+    } else if (this.hasValuesToHideValue && this.valuesToHideValue.length > 0) {
+      shouldShow = !this.valuesToHideValue.includes(sourceValue)
+    }
     if (shouldShow) {
       this.destinationTarget.classList.remove("d-none")
     } else {
