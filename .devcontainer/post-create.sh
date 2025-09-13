df02d4076a5036af903eb23a44e901c4cedd61dd
Scott Steele <scottlsteele@gmail.com>
2025-09-13 15:55:37 -0400

    squashme! more codespaces cleanup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,33 +1,26 @@
-RUBY_VERSION="$(cat .ruby-version | tr -d '\n')"
-
 # copy the file only if it doesn't already exist
 echo "*** Creating initial .env and vscode settings, if needed"
 cp -n .devcontainer/.env.codespaces .env
 mkdir -p .vscode && cp -n .devcontainer/launch.json.codespaces .vscode/launch.json
 
-# If the project's required ruby version changes from 3.2.2, this command
-# will download and compile the correct version, but it will take a long time.
-# TODO: Why do we install this RVM shim when we use rbenv later on???
-# if [ "$RUBY_VERSION" != "3.2.2" ]; then
-#   echo "*** Installing Ruby $RUBY_VERSION (this may take a while)"
-#   rvm install $RUBY_VERSION
-#   rvm use $RUBY_VERSION
-#   echo "Ruby $RUBY_VERSION installed"
-# fi
-
 echo "*** Setting up node"
-nvm install node
+nvm install node &
 
 echo "*** Setting up ruby environment"
-rbenv init bash
-rbenv init fish
-rbenv init zsh
+rbenv init bash &
+rbenv init fish &
+rbenv init zsh &
 
 # echo "*** Forcing platform version of nokogiri"
 # gem install nokogiri -v 1.18.1 --platform=ruby -- --use-system-libraries
 
+# If the project's required ruby version (specified in .ruby-version)
+# changes from 3.4.3, this command will download and compile the correct
+# version, but it will take a long time.
 echo "*** Installing rbenv-able Ruby ***"
-rbenv install --skip-existing
+rbenv install --skip-existing &
+
+wait
 
 echo "*** Running project bin/setup"
 bin/setup
