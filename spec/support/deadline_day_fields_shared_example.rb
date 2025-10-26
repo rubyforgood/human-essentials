bda56a6c39ec5c371144983852410c0158800f47
Benjamin Couey <47403497+Benjamin-Couey@users.noreply.github.com>
2025-10-26 07:49:12 -0700

    Spoof browser time (#5431)
    * Added sinon library to spoof browser time during tests
    
    * Updated deadline_day_controller to force RRule to calculate the next occurrence after today
    
    * Updated reminder schedule form tests to run on specific dates and times
    
    * Fixed deadline and reminder form shared example assuming the form always used the same path
    
    * Swapped explicit path names for refresh, fixed the partner page not being handled correctly, applied linter
    
    * Forgot sinon

--- spec/support/deadline_day_fields_shared_example.rb
+++ spec/support/deadline_day_fields_shared_example.rb
@@ -22,7 +22,7 @@ def safe_subtract_days(date, num)
   result
 end
 
-RSpec.shared_examples_for "deadline and reminder form" do |form_prefix, save_button, post_form_submit|
+RSpec.shared_examples_for "deadline and reminder form" do |form_prefix, save_button, post_form_submit, post_refresh|
   it "can set a reminder on a day of the month" do
     choose "Day of Month"
     fill_in "#{form_prefix}_reminder_schedule_service_day_of_month", with: 1
@@ -74,12 +74,17 @@ RSpec.shared_examples_for "deadline and reminder form" do |form_prefix, save_but
     expect(page).to have_content("Deadline day must be between 1 and 28")
   end
 
-  # These tests make assertions about the dates calculated by the javascript in deadline_day_controller.js
-  # Because we currently don't have a great way of spoofing the date the headless browser sees during tests, they
-  # cannot effectively set the date for the tests (like with travel_to) and have to make due with the actual datetime
-  # of whenever the test is run. To the best of my knowledge, the tests are sound but be aware that they could
-  # pass or fail based on when they are run!
-  describe "reported reminder and deadline dates" do
+  RSpec.shared_examples_for "reported reminder and deadline dates" do |datetime|
+    before do
+      if datetime
+        travel_to datetime
+      end
+      refresh
+      if post_refresh
+        send(post_refresh)
+      end
+    end
+
     context "when the reminder is a day of the month" do
       before do
         choose "Day of Month"
@@ -168,5 +173,17 @@ RSpec.shared_examples_for "deadline and reminder form" do |form_prefix, save_but
         expect(page).to have_content(schedule.next_occurrence.strftime("%b %d %Y"))
       end
     end
+  end
+
+  context "at the end of the month" do
+    it_behaves_like "reported reminder and deadline dates", Time.zone.local(2025, 9, 30)
+  end
+
+  context "in the middle of the month" do
+    it_behaves_like "reported reminder and deadline dates", Time.zone.local(2025, 9, 14)
+  end
+
+  context "at the start of the month" do
+    it_behaves_like "reported reminder and deadline dates", Time.zone.local(2025, 9, 1)
   end
 end
