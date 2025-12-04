39fca5f7f371cb3384e2a2eeca5eff46b8ba4cb1
Jane Wheatley <jelwheatley@gmail.com>
2025-12-04 11:26:25 -0800

    5446 merge duplicates logic

--- app/javascript/utils/audit_duplicates.js
+++ app/javascript/utils/audit_duplicates.js
@@ -41,7 +41,7 @@ $(() => {
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title">Duplicate Items Detected</h5>
-              <button type="button" class="close" data-dismiss="modal">
+              <button type="button" class="close" data-bs-dismiss="modal">
                 <span>&times;</span>
               </button>
             </div>
@@ -51,7 +51,7 @@ $(() => {
               <p>Quantities will be added together. Do you want to continue?</p>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
               <button type="button" class="btn btn-primary" id="confirmMerge">Yes, Merge Items</button>
             </div>
           </div>
@@ -66,42 +66,67 @@ $(() => {
     $('body').append(modalHtml);
     $('#duplicateItemsModal').modal('show');
     
+    // Handle close button
+    $('#duplicateItemsModal .close').on('click', function() {
+      $('#duplicateItemsModal').modal('hide');
+    });
+    
+    // Handle cancel button
+    $('#duplicateItemsModal .btn-secondary').on('click', function() {
+      $('#duplicateItemsModal').modal('hide');
+    });
+    
     // Handle confirm button
     $('#confirmMerge').on('click', function() {
       $('#duplicateItemsModal').modal('hide');
       
-      // Change form action to force_update
-      const currentAction = form.attr('action');
-      const currentUrl = window.location.pathname;
-      console.log('Current action:', currentAction);
-      console.log('Current URL:', currentUrl);
+      // Merge duplicate items before submitting
+      mergeDuplicateItems(form);
       
-      // Try to get audit ID from URL first, then from action
-      let auditId;
-      const urlMatch = currentUrl.match(/\/audits\/(\d+)/);
-      const actionMatch = currentAction.match(/\/audits\/(\d+)/);
-      
-      if (urlMatch) {
-        auditId = urlMatch[1];
-      } else if (actionMatch) {
-        auditId = actionMatch[1];
+      // Find and click the actual Save Progress button to preserve its behavior
+      const saveProgressBtn = form.find('button[name="save_progress"]');
+      if (saveProgressBtn.length > 0) {
+        // Temporarily remove our event handler to avoid infinite loop
+        saveProgressBtn.off('click');
+        saveProgressBtn.click();
       } else {
-        console.log('New audit - adding merge_duplicates field');
-        // For new audits, add a hidden field to allow duplicates
-        form.append('<input type="hidden" name="merge_duplicates" value="true">');
+        // Fallback: add hidden input and submit
+        form.append('<input type="hidden" name="save_progress" value="Save Progress">');
         form.trigger('submit');
-        return;
       }
+    });
+  }
+  
+  function mergeDuplicateItems(form) {
+    const itemQuantities = {};
+    const itemSections = [];
+    
+    // Collect all line items and their quantities
+    form.find('select[name$="[item_id]"]').each(function() {
+      const itemId = $(this).val();
+      const section = $(this).closest('.line_item_section');
+      const quantityInput = section.find('input[name*="[quantity]"]');
+      const quantity = parseInt(quantityInput.val()) || 0;
       
-      console.log('Extracted audit ID:', auditId);
-      const newAction = `/audits/${auditId}/force_update`;
-      console.log('New action:', newAction);
-      form.attr('action', newAction);
-      
-      // Submit form
-      form.off('submit');
-      console.log('Submitting form to:', form.attr('action'));
-      form.trigger('submit');
+      if (itemId && itemId !== '') {
+        itemSections.push({ itemId, section, quantity });
+        itemQuantities[itemId] = (itemQuantities[itemId] || 0) + quantity;
+      }
+    });
+    
+    // Find duplicates and merge them
+    const processedItems = new Set();
+    
+    itemSections.forEach(({ itemId, section, quantity }) => {
+      if (processedItems.has(itemId)) {
+        // This is a duplicate - remove it
+        section.remove();
+      } else {
+        // This is the first occurrence - update quantity to merged total
+        const quantityInput = section.find('input[name*="[quantity]"]');
+        quantityInput.val(itemQuantities[itemId]);
+        processedItems.add(itemId);
+      }
     });
   }
 });
