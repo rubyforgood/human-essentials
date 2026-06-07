7a92c6c3670be9d62fec772631c94857a89742ea
Brock Wilcox <awwaiid@thelackthereof.org>
2026-06-07 10:14:45 -0400

    Fix drift with models and gemfile for linux

--- spec/factories/kit_items.rb
+++ spec/factories/kit_items.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id                           :integer          not null, primary key
+#  active                       :boolean          default(TRUE)
+#  additional_info              :text
+#  barcode_count                :integer
+#  distribution_quantity        :integer
+#  name                         :string
+#  on_hand_minimum_quantity     :integer          default(0), not null
+#  on_hand_recommended_quantity :integer
+#  package_size                 :integer
+#  partner_key                  :string
+#  reporting_category           :string
+#  type                         :string           default("ConcreteItem"), not null
+#  value_in_cents               :integer          default(0)
+#  visible_to_partners          :boolean          default(TRUE), not null
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#  item_category_id             :integer
+#  kit_id                       :integer
+#  organization_id              :integer
+#
 FactoryBot.define do
   factory :kit_item do
     sequence(:name) { |n| "#{n} - Dont test this" }
