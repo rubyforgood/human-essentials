cb85aa5f421ff8257f80287224775e91debc8146
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2026-08-29 15:35:54 -0400

    Adding guard clause to reminder email when org-level disabled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_28_150820) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_29_112930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
